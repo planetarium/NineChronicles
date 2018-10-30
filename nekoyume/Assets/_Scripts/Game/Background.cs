b7beef386e8e484e16351ec15dadee11f7ea99c8
incl <lee.incl@gmail.com>
2018-10-30 14:27:24 +0900

    Feature Parallax background

--- nekoyume/Assets/_Scripts/Game/Background.cs
+++ nekoyume/Assets/_Scripts/Game/Background.cs
@@ -6,23 +6,80 @@ namespace Nekoyume.Game
 {
     public class Background : MonoBehaviour
     {
-        public Transform[] backgrounds;
+        public bool autoParallaxSize = true;
+        public float parallaxSize = 0.0f;
+        public float parallaxSpeed = 0.0f;
         private Transform cameraTransform;
+        
+        private Transform[] images;
+
+        private float lastCameraX;
+        private int leftIndex = 0;
+        private int rightIndex = 0;
 
         private void Start ()
         {
             cameraTransform = Camera.main.transform;
+
+            images = new Transform[transform.childCount];
+            for (int i = 0; i < transform.childCount; ++i)
+            {
+                images[i] = transform.GetChild(i);
+                if (autoParallaxSize && i == 0)
+                {
+                    SpriteRenderer spriteRenderer = images[i].GetComponent<SpriteRenderer>();
+                    if (spriteRenderer.sprite != null)
+                    {
+                        parallaxSize = spriteRenderer.sprite.texture.width / spriteRenderer.sprite.pixelsPerUnit;
+                    }
+                }
+                images[i].position = new Vector3(parallaxSize * i, images[i].position.y, images[i].position.z);
+            }
+
+            lastCameraX = cameraTransform.position.x;
+            leftIndex = 0;
+            rightIndex = images.Length - 1;
         }
 
-        public void Update()
+        private void Update()
         {
-            for (int i = 0; i < backgrounds.Length; i++)
+            if (parallaxSpeed != 0.0f)
             {
-                // TODO
-                var backgroundPos = backgrounds[i].position;
-                backgroundPos.x = cameraTransform.position.x;
-                backgrounds[i].position = backgroundPos;
+                float deltaX = cameraTransform.position.x - lastCameraX;
+                transform.position += Vector3.right * (deltaX * parallaxSpeed);
+                lastCameraX = cameraTransform.position.x;
             }
+
+            if (images.Length > 1)
+            {
+                if (cameraTransform.position.x < (images[leftIndex].transform.position.x))
+                    MoveLeft();
+
+                if (cameraTransform.position.x > (images[rightIndex].transform.position.x))
+                    MoveRight();
+            }
+        }
+
+        private void MoveLeft()
+        {
+            Vector3 position = Vector3.right * (images[leftIndex].position.x - parallaxSize);
+            position.y = images[rightIndex].position.y;
+            images[rightIndex].position = position;
+            leftIndex = rightIndex;
+            rightIndex--;
+            if (rightIndex < 0)
+                rightIndex = images.Length - 1;
+        }
+
+        private void MoveRight()
+        {
+            Vector3 position = Vector3.right * (images[rightIndex].position.x + parallaxSize);
+            position.y = images[leftIndex].position.y;
+            images[leftIndex].position = position;
+            rightIndex = leftIndex;
+            leftIndex++;
+            if (leftIndex == images.Length)
+                leftIndex = 0;
         }
     }
 }
