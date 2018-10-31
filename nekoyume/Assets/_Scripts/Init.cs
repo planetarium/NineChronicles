bec33b5f211f0062ccbe2c18bdb6a5d1dc062952
Yang Chun Ung <qooraven@gmail.com>
2018-10-31 13:50:09 +0900

    Check node version

---
+++ nekoyume/Assets/_Scripts/Init.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Init : MonoBehaviour {
+
+    private string version;
+	// Use this for initialization
+	void Start () {
+        version = PlayerPrefs.GetString("version", "");
+        StartCoroutine(CheckVersion(version));
+    }
+
+    IEnumerator CheckVersion(string clientVersion){
+        UnityWebRequest www = UnityWebRequest.Get("http://dev.nekoyu.me/version/");
+        yield return www.SendWebRequest();
+        if (www.error == null) {
+            string serverVersion = www.downloadHandler.text;
+            Debug.Log(serverVersion);
+            if (serverVersion != clientVersion) {
+                PlayerPrefs.SetString("version", serverVersion);
+            }
+            this.gameObject.SetActive(false);
+        }
+    }
+}
