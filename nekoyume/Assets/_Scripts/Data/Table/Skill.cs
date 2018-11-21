8ebb09e95626c56ef521785dc3c34c310c93e8b3
incl <lee.incl@gmail.com>
2018-11-21 18:51:25 +0900

    Update Data

--- nekoyume/Assets/_Scripts/Data/Table/Skill.cs
+++ nekoyume/Assets/_Scripts/Data/Table/Skill.cs
@@ -5,6 +5,8 @@ namespace Nekoyume.Data.Table
         public string Id = "";
         public string Cls = "";
         public int Power = 0;
+        public int Range = 0;
+        public int Size = 0;
         public int AttackType = 0;
         public int ElementalType = 0;
         public int Cooltime = 0;
