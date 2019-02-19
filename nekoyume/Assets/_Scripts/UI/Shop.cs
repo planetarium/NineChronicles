34614a34a7aecc7b7ead8968732ff865ed96a960
Yang Chun Ung <qooraven@gmail.com>
2019-02-19 15:25:33 +0900

    Divide panels

--- nekoyume/Assets/_Scripts/UI/Shop.cs
+++ nekoyume/Assets/_Scripts/UI/Shop.cs
@@ -1,49 +1,24 @@
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Nekoyume.UI
 {
     public class Shop : Widget
     {
-        public GameObject btnBuy;
-        public GameObject btnSell;
-        public GameObject btnConfirm;
-        public ScrollRect cart;
-        public Button sword;
-        public List<CartItem> items;
-        public GameObject itemBase;
-        public Text totalPrice;
+        public GameObject panelBuy;
+        public GameObject panelSell;
 
-        private void Awake()
+
+        public void BuyClick()
         {
-            items = new List<CartItem>();
+            panelSell.SetActive(false);
+            GetComponent<Buy>().Init();
+            panelBuy.SetActive(true);
         }
 
-        public void SwordClick()
+        public void SellClick()
         {
-            GameObject newItem = Instantiate(itemBase, cart.content);
-            CartItem item = newItem.GetComponent<CartItem>();
-            var itemInfo = sword.GetComponent<Item>();
-            item.itemName.text = itemInfo.itemName.text;
-            item.price.text = itemInfo.price.text;
-            item.info.text = itemInfo.info.text;
-            item.icon.sprite = sword.GetComponent<Image>().sprite;
-            item.gameObject.SetActive(true);
-            items.Add(item);
-            CalcTotalPrice();
-        }
-
-        public void CalcTotalPrice()
-        {
-            var total = 0;
-            foreach (var item in items)
-            {
-                int price;
-                int.TryParse(item.price.text, out price);
-                total += price;
-            }
-            totalPrice.text = total.ToString();
+            panelBuy.SetActive(false);
+            panelSell.SetActive(true);
         }
     }
 }
