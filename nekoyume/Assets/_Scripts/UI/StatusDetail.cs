f4288da0ef7557581529e9bda5c10217d7e57fce
Yang Chun Ung <qooraven@gmail.com>
2019-03-07 14:45:09 +0900

    Set player equip info when status detail toggle

--- nekoyume/Assets/_Scripts/UI/StatusDetail.cs
+++ nekoyume/Assets/_Scripts/UI/StatusDetail.cs
@@ -1,4 +1,8 @@
+using System;
 using Nekoyume.Data.Table;
+using Nekoyume.Game.Character;
+using Nekoyume.Game.Item;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Nekoyume.UI
@@ -7,6 +11,9 @@ namespace Nekoyume.UI
     {
         public Text textAtk;
         public Text textDef;
+        public GameObject[] equipSlots;
+        private Player _player;
+
 
         public void Init(Stats stats)
         {
@@ -16,11 +23,29 @@ namespace Nekoyume.UI
 
         public void CloseClick()
         {
-            var status = Widget.Find<Status>();
+            var status = Find<Status>();
             if (status)
             {
                 status.BtnStatus.group.SetAllTogglesOff();
             }
+        }
+
+        public override void Show()
+        {
+            _player = FindObjectOfType<Player>();
+            foreach (var equipment in _player.equipments)
+            {
+                var type = (ItemBase.ItemType) Enum.Parse(typeof(ItemBase.ItemType), equipment.Data.Cls);
+                foreach (var slot in equipSlots)
+                {
+                    var es = slot.GetComponent<EquipSlot>();
+                    if (es.type == type)
+                    {
+                        es.Set(equipment);
+                    }
+                }
+            }
+            base.Show();
         }
     }
 }
