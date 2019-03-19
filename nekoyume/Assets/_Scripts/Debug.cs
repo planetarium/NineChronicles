e90a6627cc9e9e08d9d2e5324b54a5eef02d2d65
boscohyun <boscohyun@gmail.com>
2019-03-19 12:17:49 +0900

    Add define symbol 'DEBUG_USE'.

--- nekoyume/Assets/_Scripts/Debug.cs
+++ nekoyume/Assets/_Scripts/Debug.cs
@@ -1,3 +1,5 @@
+#define DEBUG_USE
+
 using UnityEngine;
 using System.Collections;
 using System;
@@ -28,57 +30,57 @@ namespace Nekoyume
     {
         public static bool isDebugBuild => UnityEngine.Debug.isDebugBuild;
 
-        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        [System.Diagnostics.Conditional("DEBUG_USE")]
         public static void Log(object message)
         {
             UnityEngine.Debug.Log(message);
         }
 
-        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        [System.Diagnostics.Conditional("DEBUG_USE")]
         public static void Log(object message, UnityEngine.Object context)
         {
             UnityEngine.Debug.Log(message, context);
         }
 
-        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        [System.Diagnostics.Conditional("DEBUG_USE")]
         public static void LogError(object message)
         {
             UnityEngine.Debug.LogError(message);
         }
 
-        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        [System.Diagnostics.Conditional("DEBUG_USE")]
         public static void LogError(object message, UnityEngine.Object context)
         {
             UnityEngine.Debug.LogError(message, context);
         }
 
-        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        [System.Diagnostics.Conditional("DEBUG_USE")]
         public static void LogWarning(object message)
         {
             UnityEngine.Debug.LogWarning(message.ToString());
         }
 
-        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        [System.Diagnostics.Conditional("DEBUG_USE")]
         public static void LogWarning(object message, UnityEngine.Object context)
         {
             UnityEngine.Debug.LogWarning(message.ToString(), context);
         }
 
-        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        [System.Diagnostics.Conditional("DEBUG_USE")]
         public static void DrawLine(Vector3 start, Vector3 end, Color color = default(Color), float duration = 0.0f,
             bool depthTest = true)
         {
             UnityEngine.Debug.DrawLine(start, end, color, duration, depthTest);
         }
 
-        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        [System.Diagnostics.Conditional("DEBUG_USE")]
         public static void DrawRay(Vector3 start, Vector3 dir, Color color = default(Color), float duration = 0.0f,
             bool depthTest = true)
         {
             UnityEngine.Debug.DrawRay(start, dir, color, duration, depthTest);
         }
 
-        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        [System.Diagnostics.Conditional("DEBUG_USE")]
         public static void Assert(bool condition)
         {
             if (!condition) throw new Exception();
