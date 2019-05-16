bcef25dd7ff1228b1a25d94778474c34cca5b51b
Yang Chun Ung <qooraven@gmail.com>
2019-05-16 14:30:55 +0900

    Fix missing serializable attribute

--- nekoyume/Assets/_Scripts/Model/BattleStatus/Attack.cs
+++ nekoyume/Assets/_Scripts/Model/BattleStatus/Attack.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 
 namespace Nekoyume.Model
 {
+    [Serializable]
     public class Attack : Skill
     {
         public override IEnumerator CoExecute(IStage stage)
