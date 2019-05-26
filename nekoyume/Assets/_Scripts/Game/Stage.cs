9206075cac4a1b7551839236520312aad087f4d1
incl <lee.incl@gmail.com>
2019-05-26 23:49:29 +0900

    Add Fade

--- nekoyume/Assets/_Scripts/Game/Stage.cs
+++ nekoyume/Assets/_Scripts/Game/Stage.cs
@@ -302,8 +302,10 @@ namespace Nekoyume.Game
             {
                 AudioController.instance.PlayMusic(AudioController.MusicCode.Boss1);
                 VFXController.instance.Create<BattleBossTitleVFX>(Vector3.zero);
-
-                yield return new WaitForSeconds(3.0f);
+                StartCoroutine(Widget.Find<Blind>().FadeIn(0.4f, "", 0.2f));
+                yield return new WaitForSeconds(2.0f);
+                StartCoroutine(Widget.Find<Blind>().FadeOut(0.2f));
+                yield return new WaitForSeconds(1.0f);
             }
 
             yield return StartCoroutine(spawner.CoSetData(id, monsters));
