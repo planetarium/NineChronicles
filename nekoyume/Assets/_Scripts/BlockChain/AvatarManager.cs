ccd9d5782e949a50b6d071b82d1dc13cf13ba16f
Hyun Seungmin <boscohyun@gmail.com>
2019-05-29 21:52:19 +0900

    - Removed unused comments.

--- nekoyume/Assets/_Scripts/BlockChain/AvatarManager.cs
+++ nekoyume/Assets/_Scripts/BlockChain/AvatarManager.cs
@@ -71,43 +71,5 @@ namespace Nekoyume.BlockChain
                 timestamp: DateTime.UtcNow
             );
         }
-        
-//        /// <summary>
-//        /// FixMe. 모든 액션에 대한 랜더 단계에서 아바타 주소의 상태를 얻어 오고 있음.
-//        /// 모든 액션 생성 단계에서 각각의 변경점을 업데이트 하는 방향으로 수정해볼 필요성 있음.
-//        /// CreateNovice와 HackAndSlash 액션의 처리를 개선해서 테스트해 볼 예정.
-//        /// 시작 전에 양님에게 문의!
-//        /// </summary>
-//        public static void SubscribeAvatarUpdates()
-//        {
-//            if (States.CurrentAvatarState.Value != null)
-//            {
-//                DidAvatarStateLoaded(States.CurrentAvatarState.Value);
-//            }
-//
-//            if (!ReferenceEquals(_disposableForEveryRender, null))
-//            {
-//                return;
-//            }
-//            _disposableForEveryRender = ActionBase.EveryRender(States.CurrentAvatarState.Value.address).ObserveOnMainThread().Subscribe(eval =>
-//            {
-//                var avatarState = (AvatarState) eval.OutputStates.GetState(States.CurrentAvatarState.Value.address);
-//                if (avatarState is null)
-//                {
-//                    return;
-//                }
-//                PostActionRender(avatarState);
-//            });
-//        }
-//        
-//        private static void PostActionRender(AvatarState avatarState)
-//        {
-//            var avatarLoaded = States.CurrentAvatarState.Value == null;
-//            States.CurrentAvatarState.Value = avatarState;
-//            if (avatarLoaded)
-//            {
-//                DidAvatarStateLoaded(States.CurrentAvatarState.Value);
-//            }
-//        }
     }
 }
