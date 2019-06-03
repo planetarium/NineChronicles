eef06592febfc0376df44eda6de7fed1e1452ad3
Seunghun Lee <waydi1@gmail.com>
2019-06-03 14:15:07 +0900

    마이너 테스트를 위한 Docker-compose 파일 추가

---
+++ nekoyume/compose/container/run_seed.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PORT=$1
+
+/nekoyume/nekoyume --host host.docker.internal --port "$PORT" --private-key fbc20042b3a707a7d5a1fa577171f49cd3a9e67ab9295757c714e3f2f8c2d573 | tee /root/.config/seed.log
