cc30d82bcf8843a0d6cd819cd214206794482739
Hyun Seungmin <boscohyun@gmail.com>
2019-06-17 19:20:39 +0900

    - Fixed a bug in the `SkeletonCharacterAnimator` class when not found hud bone in spine.

--- nekoyume/Assets/_Scripts/Game/Character/SkeletonCharacterAnimator.cs
+++ nekoyume/Assets/_Scripts/Game/Character/SkeletonCharacterAnimator.cs
@@ -21,11 +21,11 @@ namespace Nekoyume.Game.Character
         public SkeletonCharacterAnimator(CharacterBase root) : base(root)
         {
         }
-        
+
         public override void ResetTarget(GameObject value)
         {
             base.ResetTarget(value);
-            
+
             if (!ReferenceEquals(Skeleton, null))
             {
                 Skeleton.AnimationState.Event -= RaiseEvent;
@@ -38,7 +38,7 @@ namespace Nekoyume.Game.Character
                 throw new NotFoundComponentException<MeshRenderer>();
             }
 
-            Skeleton = value.GetComponent<SkeletonAnimation>(); 
+            Skeleton = value.GetComponent<SkeletonAnimation>();
 
             if (ReferenceEquals(Skeleton, null))
             {
@@ -46,15 +46,19 @@ namespace Nekoyume.Game.Character
             }
 
             Skeleton.timeScale = TimeScale;
-            
+
             var hud = Skeleton.skeleton.FindBone(StringHUD);
             if (ReferenceEquals(hud, null))
             {
-                throw new SpineBoneNotFoundException(StringHUD);
+                // FixMe. HUD 본이 없는 임시 스파인에 대한 위치 처리.
+//                throw new SpineBoneNotFoundException(StringHUD);
+                HUDPosition = Vector3.zero;
+            }
+            else
+            {
+                HUDPosition = hud.GetWorldPosition(Target.transform) - Root.transform.position;
             }
 
-            HUDPosition = hud.GetWorldPosition(Target.transform) - Root.transform.position;
-            
             Skeleton.AnimationState.Event += RaiseEvent;
         }
 
@@ -62,7 +66,7 @@ namespace Nekoyume.Game.Character
         {
             return base.AnimatorValidation() && !ReferenceEquals(Skeleton, null);
         }
-        
+
         public override Vector3 GetHUDPosition()
         {
             return HUDPosition;
@@ -88,12 +92,14 @@ namespace Nekoyume.Game.Character
         private void ColorTween()
         {
             var mat = MeshRenderer.material;
-            
+
             _colorTweenSequence?.Kill();
 
             _colorTweenSequence = DOTween.Sequence();
-            _colorTweenSequence.Append(DOTween.To(() => ColorTweenFrom, value => mat.SetFloat("_FillPhase", value), ColorTweenTo, ColorTweenDuration));
-            _colorTweenSequence.Append(DOTween.To(() => ColorTweenTo, value => mat.SetFloat("_FillPhase", value), ColorTweenFrom, ColorTweenDuration));
+            _colorTweenSequence.Append(DOTween.To(() => ColorTweenFrom, value => mat.SetFloat("_FillPhase", value),
+                ColorTweenTo, ColorTweenDuration));
+            _colorTweenSequence.Append(DOTween.To(() => ColorTweenTo, value => mat.SetFloat("_FillPhase", value),
+                ColorTweenFrom, ColorTweenDuration));
             _colorTweenSequence.Play().OnComplete(() => _colorTweenSequence = null);
         }
     }
