88bc6d6cb649dd793e8edb56cfb5b713e47dd024
Yang Chun Ung <qooraven@gmail.com>
2019-06-25 20:11:27 +0900

    Replace weapon sprite

--- nekoyume/Assets/_Scripts/Game/Item/Weapon.cs
+++ nekoyume/Assets/_Scripts/Game/Item/Weapon.cs
@@ -9,10 +9,17 @@ namespace Nekoyume.Game.Item
     public class Weapon : Equipment
     {
         private const string SpritePath = "images/equipment/{0}";
+        private const string SuffixId = "0000";
 
         public static Sprite GetSprite(Equipment equipment = null)
         {
             var id = equipment?.Data.resourceId;
+            // FIXME 속성무기 스프라이트가 준비되면 아이디를 그대로 사용
+            if (id != null)
+            {
+                var sub = id.ToString().Substring(0, 4);
+                id = int.Parse(sub + SuffixId);
+            }
             return Resources.Load<Sprite>(string.Format(SpritePath, id));
         }
 
