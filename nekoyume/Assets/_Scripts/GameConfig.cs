8b68f3e7649c10bcfc6814864c6d3cbec4f32d02
Hyun Seungmin <boscohyun@gmail.com>
2019-06-27 17:23:29 +0900

    - Added some const fields in the `GameConfig` class for combination.

--- nekoyume/Assets/_Scripts/GameConfig.cs
+++ nekoyume/Assets/_Scripts/GameConfig.cs
@@ -1,10 +1,21 @@
+using System.Collections.Generic;
 using Unity.Mathematics;
 
 namespace Nekoyume
 {
     public class GameConfig
     {
+        public const float CombinationValueP1 = 30f;
+        public const float CombinationValueP2 = 1.2f;
+        public const float CombinationValueL1 = 10f;
+        public const float CombinationValueL2 = 1f;
+        public const float CombinationValueR1 = 2f;
         public const int SlotCount = 3;
+        
+        public static readonly List<int> EquipmentMaterials = new List<int>
+        {
+            303000, 303001, 303002, 303100, 303101, 303102, 303200, 303201, 303202
+        };
         public static readonly int2 ScreenSize = new int2(1136, 640);
     }
 }
