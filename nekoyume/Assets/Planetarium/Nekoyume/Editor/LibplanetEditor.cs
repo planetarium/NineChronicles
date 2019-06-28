2d2454d99ee74690efe7c8d13ad85b404c74dfcd
Hyun Seungmin <boscohyun@gmail.com>
2019-06-28 19:35:38 +0900

    - Fixed the `PlayerSettings`. - Improved the `LibplanetEditor` class.

--- nekoyume/Assets/Planetarium/Nekoyume/Editor/LibplanetEditor.cs
+++ nekoyume/Assets/Planetarium/Nekoyume/Editor/LibplanetEditor.cs
@@ -6,10 +6,23 @@ namespace Planetarium.Nekoyume.Editor
 {
     public static class LibplanetEditor
     {
-        [MenuItem("Tools/Libplanet/Delete All")]
-        public static void CreateSpinePrefab()
+        [MenuItem("Tools/Libplanet/Delete All(Editor)")]
+        public static void DeleteAllEditor()
         {
-            var info = new DirectoryInfo(Application.persistentDataPath);
+            var path = Path.Combine(Application.persistentDataPath, "../");
+            DeleteAll(path);
+        }
+        
+        [MenuItem("Tools/Libplanet/Delete All(Player)")]
+        public static void DeleteAllPlayer()
+        {
+            var path = Path.Combine(Application.persistentDataPath, $"../../{PlayerSettings.applicationIdentifier}");
+            DeleteAll(path);
+        }
+
+        private static void DeleteAll(string path)
+        {
+            var info = new DirectoryInfo(path);
             info.Delete(true);
             PlayerPrefs.DeleteAll();
         }
