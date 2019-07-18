9202e0cf8d7e1ab8b51d20d40a71678db0a34cb9
Yang Chun Ung <qooraven@gmail.com>
2019-07-18 11:57:54 +0900

    Test Fixture

--- nekoyume/Assets/Tests/TestFixture.cs
+++ nekoyume/Assets/Tests/TestFixture.cs
@@ -1,4 +1,7 @@
+using System.IO;
 using NUnit.Framework;
+using Planetarium.Nekoyume.Editor;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Tests
@@ -6,10 +9,25 @@ namespace Tests
     [SetUpFixture]
     public class TestFixture
     {
+        private string _path;
         [OneTimeSetUp]
         public void Init()
         {
+            var path = Path.Combine(Application.streamingAssetsPath, "clo_test_fixture.json");
+            _path = Path.Combine(Application.streamingAssetsPath, "clo.json");
+            if (File.Exists(_path))
+                File.Delete(_path);
+            File.Copy(path, _path);
+            LibplanetEditor.DeleteAllEditor();
             SceneManager.LoadScene("Game");
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            File.Delete(_path);
+            File.Delete(_path + ".meta");
+            LibplanetEditor.DeleteAllEditor();
         }
     }
 }
