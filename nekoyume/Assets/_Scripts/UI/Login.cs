3bd4ae0fe4705cbdfa419dcf46e57de6b8698ebf
SeungGeol Song <unengine@gmail.com>
2019-07-24 11:15:52 +0900

    added layout update on deleting avatars

--- nekoyume/Assets/_Scripts/UI/Login.cs
+++ nekoyume/Assets/_Scripts/UI/Login.cs
@@ -4,6 +4,7 @@ using Nekoyume.BlockChain;
 using Nekoyume.Game.Controller;
 using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Nekoyume.UI
 {
@@ -58,6 +59,11 @@ namespace Nekoyume.UI
                         Game.Event.OnNestEnter.Invoke();
                         Show();
                         Find<GrayLoadingScreen>()?.Close();
+
+                        //강제로 레이아웃 정렬 (업데이트)
+                        slots[index].GetComponentInChildren<HorizontalLayoutGroup>().CalculateLayoutInputHorizontal();
+                        slots[index].GetComponentInChildren<HorizontalLayoutGroup>().SetLayoutHorizontal();
+
                     });
             };
 
