0e30d3b962b8808b7cb85606196c983dc1e53925
Yang Chun Ung <qooraven@gmail.com>
2019-08-06 16:29:12 +0900

    Delete save avatar private key in PlayerPrefs

--- nekoyume/Assets/_Scripts/BlockChain/AvatarManager.cs
+++ nekoyume/Assets/_Scripts/BlockChain/AvatarManager.cs
@@ -16,58 +16,23 @@ namespace Nekoyume.BlockChain
     /// </summary>
     public static class AvatarManager
     {
-        // {Agent.PrivateKey}_private_key_{index} 구조로 로컬에 계정 정보 저장
-        public const string PrivateKeyFormat = "{0}_private_key_avatar_{1}";
-        
-        private static PrivateKey _privateKey;
-        
-        public static PrivateKey GetOrCreateAvatarPrivateKey(int index)
+        public static Address CreateAvatarAddress()
         {
-            var agentKey = ByteUtil.Hex(AgentController.Agent.PrivateKey.ByteArray);
-            var key = string.Format(PrivateKeyFormat, agentKey, index);
-            
-            if (PlayerPrefs.HasKey(key))
-            {
-                var privateKeyHex = PlayerPrefs.GetString(key);
-                _privateKey = new PrivateKey(ByteUtil.ParseHex(privateKeyHex));
-                
-                Debug.Log($"Avatar PrivateKey Loaded '{key}': {privateKeyHex}");
-            }
-            else
-            {
-                _privateKey = new PrivateKey();
-                var privateKeyHex = ByteUtil.Hex(_privateKey.ByteArray);
-                PlayerPrefs.SetString(key, privateKeyHex);
-                
-                Debug.Log($"Avatar PrivateKey Created '{key}': {privateKeyHex}");
-            }
+            var key = new PrivateKey();
+            var privateKeyHex = ByteUtil.Hex(key.ByteArray);
+            Debug.Log($"Avatar PrivateKey Created. {privateKeyHex}");
 
-            return _privateKey;
-        }
-        
-        public static Address GetOrCreateAvatarAddress(int index)
-        {
-            return GetOrCreateAvatarPrivateKey(index).PublicKey.ToAddress();
+            return key.PublicKey.ToAddress();
         }
 
         public static bool DeleteAvatarPrivateKey(int index)
         {
-            var agentKey = ByteUtil.Hex(AgentController.Agent.PrivateKey.ByteArray);
-            var key = string.Format(PrivateKeyFormat, agentKey, index);
-            if (!PlayerPrefs.HasKey(key))
-            {
-                return false;
-            }
-            
             if (States.Instance.currentAvatarKey.Value == index)
             {
                 ResetIndex();
             }
 
-            var privateKeyHex = PlayerPrefs.GetString(key);
-            PlayerPrefs.DeleteKey(key);
-            
-            Debug.Log($"Avatar PrivateKey Deleted '{key}': {privateKeyHex}");
+            Debug.Log($"Avatar PrivateKey Deleted. {index}");
             
             return true;
         }
