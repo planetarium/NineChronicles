5ef34aea173bd74611b9e524680b91e43794c737
SeungGeol Song <unengine@gmail.com>
2019-08-19 18:32:21 +0900

    Created bottom tab prefab

---
+++ nekoyume/Assets/_Scripts/UI/BottomTab.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Nekoyume.UI
+{
+    public class BottomTab : MonoBehaviour
+    {
+        public Button goToMainButton;
+        public Button inventoryButton;
+        public Button QuestButton;
+        public Button infoAndEquipButton;
+        public Button mailButton;
+        public Button dictionaryButton;
+
+        private enum ScreenState
+        {
+            Main,
+            Dungeon
+        }
+
+        void Start()
+        {
+
+        }
+
+
+    }
+}
