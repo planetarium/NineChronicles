85288399e263ea100ca8ab908a74b00b60d1add0
Hyun Seungmin <boscohyun@gmail.com>
2019-08-21 18:47:18 +0900

    - Implemented a suggestions of review.

--- nekoyume/Assets/_Scripts/Game/Controller/VFXController.cs
+++ nekoyume/Assets/_Scripts/Game/Controller/VFXController.cs
@@ -7,7 +7,7 @@ namespace Nekoyume.Game.Controller
     public class VFXController : MonoSingleton<VFXController>
     {
         protected override bool ShouldRename => true;
-        
+
         private ObjectPool _pool = null;
 
         #region Mono
@@ -24,13 +24,13 @@ namespace Nekoyume.Game.Controller
         }
 
         #endregion
-        
+
         public T Create<T>(Vector3 position) where T : VFX.VFX
         {
             var vfx = _pool.Get<T>(position);
             return vfx;
         }
-        
+
         public T Create<T>(Transform target, Vector3 offset) where T : VFX.VFX
         {
             var vfx = _pool.Get<T>();
@@ -38,37 +38,15 @@ namespace Nekoyume.Game.Controller
             return vfx;
         }
 
-        public T Create<T>(RectTransform target, Vector3 offset) where T : VFX.VFX
-        {
-            var vfx = _pool.Get<T>();
-            StartCoroutine(CoChaseTarget(vfx, target, offset));
-            return vfx;
-        }
-        
         private static IEnumerator CoChaseTarget(Component vfx, Transform target, Vector3 offset)
         {
             var g = vfx.gameObject;
             var t = vfx.transform;
-            while (!ReferenceEquals(g, null) &&
-                   !ReferenceEquals(target, null) &&
-                   g.activeSelf)
+            while (g.activeSelf &&
+                   target)
             {
                 t.position = target.position + offset;
-                
-                yield return null;
-            }
-        }
-        
-        private static IEnumerator CoChaseTarget(Component vfx, RectTransform target, Vector3 offset)
-        {
-            var g = vfx.gameObject;
-            var t = vfx.transform;
-            while (!ReferenceEquals(g, null) &&
-                   !ReferenceEquals(target, null) &&
-                   g.activeSelf)
-            {
-                t.position = target.position + offset;
-                
+
                 yield return null;
             }
         }
