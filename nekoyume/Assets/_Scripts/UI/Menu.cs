68201d0cdb6bbc4556670a5ee2004161e89e4320
Hyun Seungmin <boscohyun@gmail.com>
2019-08-30 11:41:59 +0900

    - Move the `BottomMenu` into module.

--- nekoyume/Assets/_Scripts/UI/Menu.cs
+++ nekoyume/Assets/_Scripts/UI/Menu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using Assets.SimpleLocalization;
 using Nekoyume.Game;
 using Nekoyume.Game.Controller;
+using Nekoyume.UI.Module;
 using Nekoyume.Manager;
 using UnityEngine;
 using UnityEngine.UI;
