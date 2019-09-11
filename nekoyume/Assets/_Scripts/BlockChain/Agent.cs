e4e06f11f43b3d819f753cf71f9196ff0f1d77e8
longfin <longfinfunnel@gmail.com>
2019-09-11 16:33:28 +0900

    Fix build error

--- nekoyume/Assets/_Scripts/BlockChain/Agent.cs
+++ nekoyume/Assets/_Scripts/BlockChain/Agent.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using AsyncIO;
@@ -25,7 +24,6 @@ using Nekoyume.Game.Item;
 #if BLOCK_LOG_USE
 using Nekoyume.Helper;
 #endif
-using Nekoyume.Serilog;
 using Serilog;
 using UniRx;
 using UnityEngine;
