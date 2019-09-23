23e5fe272c871a93b885323cfeceb1cf646a8f39
moreal <dev.moreal@gmail.com>
2019-09-23 20:52:37 +0900

    Avoid conflict of directory between container and host

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,28 +4,27 @@ set -e
 source "$(dirname $0)/_common.sh"
 
 title "Unit tests (Editor)"
-mkdir -p /tmp/test-result/
 /opt/Unity/Editor/Unity \
   -batchmode \
   -nographics \
   -logFile \
   -projectPath /src/ \
   -runTests \
-  -testResults /tmp/test-result/nunit.xml \
+  -testResults /tmp/test/nunit.xml \
   -testPlatform playmode \
   || true
 
-cat /tmp/test-result/nunit.xml
+cat /tmp/test/nunit.xml
 xmllint \
   --xpath 'string(/test-run/@result)' \
-  /tmp/test-result/nunit.xml \
-  > /tmp/test-result/result.txt
-[[ -f /tmp/test-result/result.txt ]]
+  /tmp/test/nunit.xml \
+  > /tmp/test/result.txt
+[[ -f /tmp/test/result.txt ]]
 
 wget -O /tmp/nunit3-junit.xslt \
   https://github.com/nunit/nunit-transforms/raw/master/nunit3-junit/nunit3-junit.xslt
 xsltproc \
-  -o /tmp/test-result/junit.xml \
+  -o /tmp/test/junit.xml \
   /tmp/nunit3-junit.xslt \
-  /tmp/test-result/nunit.xml
-cat /tmp/test-result/nunit.xml
+  /tmp/test/nunit.xml
+cat /tmp/test/nunit.xml
