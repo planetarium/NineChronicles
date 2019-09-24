584223d7f3629dd0948235eab4d4ca81419e9927
moreal <dev.moreal@gmail.com>
2019-09-24 13:21:32 +0900

    Update scripts by console-sink option

--- nekoyume/compose/container/run_seed.sh
+++ nekoyume/compose/container/run_seed.sh
@@ -2,4 +2,4 @@
 
 PORT=$1
 
-/nekoyume/nekoyume --host host.docker.internal --port "$PORT" --private-key fbc20042b3a707a7d5a1fa577171f49cd3a9e67ab9295757c714e3f2f8c2d573 | tee /root/.config/seed.log
+/nekoyume/nekoyume --host host.docker.internal --port "$PORT" --private-key fbc20042b3a707a7d5a1fa577171f49cd3a9e67ab9295757c714e3f2f8c2d573 --console-sink true | tee /root/.config/seed.log
