85d4d340a32c6eae14da8f3fe3b0d6d8059f1d02
SeungGeol Song <ssg@planetariumhq.com>
2019-10-10 18:03:18 +0900

    Add resources, boss status layout work in progress

---
+++ nekoyume/Assets/_Scripts/UI/Module/BossStatus.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Nekoyume.UI.Module
+{
+    public class BossStatus : MonoBehaviour
+    {
+        public Slider hpBar;
+        public Text hpText;
+        public BuffLayout buffLayout;
+
+        public void SetHp()
+        {
+
+        }
+
+        public void SetBuff()
+        {
+
+        }
+    }
+}
