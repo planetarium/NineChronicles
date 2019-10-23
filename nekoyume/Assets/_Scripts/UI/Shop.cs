0df0804be32e18d5f1e104d4cfb640f777f07e80
Yang Chun Ung <qooraven@gmail.com>
2019-10-23 15:22:25 +0900

    Localize shop notice

--- nekoyume/Assets/_Scripts/UI/Shop.cs
+++ nekoyume/Assets/_Scripts/UI/Shop.cs
@@ -12,6 +12,7 @@ using Nekoyume.State;
 using Nekoyume.TableData;
 using Nekoyume.UI.Model;
 using Nekoyume.UI.Module;
+using TMPro;
 using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
@@ -44,6 +45,7 @@ namespace Nekoyume.UI
         public Module.Inventory inventory;
         public ShopItems shopItems;
         public GameObject shopNotice;
+        public TextMeshProUGUI noticeText;
 
         public Model.Shop SharedModel { get; private set; }
 
@@ -58,6 +60,7 @@ namespace Nekoyume.UI
             base.Awake();
             
             SharedModel = new Model.Shop();
+            noticeText.text = LocalizationManager.Localize("UI_SHOP_NOTICE");
         }
 
         #endregion
