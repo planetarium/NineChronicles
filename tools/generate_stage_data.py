345de9e3c2c99e31dde7fb4ad62044bef0dadc86
Kijun Seo <kijunseo@gmail.com>
2019-11-26 12:13:20 +0900

    Stage reward map

--- tools/generate_stage_data.py
+++ tools/generate_stage_data.py
@@ -6,7 +6,7 @@ from collections import Counter, OrderedDict
 from operator import itemgetter
 
 ### EDIT: WORLD
-MONSTER_PARTS_DROP_RATE = 0.03
+MONSTER_PARTS_DROP_RATE = 0.5
 STAGES_PER_WORLD = 150
 STAGES_PER_MONSTER_LV = 5
 BASE_EXP = 50
@@ -16,43 +16,74 @@ MAX_WAVES = 8
 CSV_OUTPUT_FOLDER_PATH = "nekoyume/Assets/AddressableAssets/TableCSV"
 WAVE_CSV_NAME = "waves.csv"
 
+WAVE_BASE = {
+    "low": [],
+    "mid": [],
+    "high": []
+}
+
+WAVE_WORLD_1 = {
+    "low": [],
+    "mid": [],
+    "high": [],
+}
+
+WAVE_WORLD_2 = {
+    "low": [],
+    "mid": [],
+    "high": [],
+}
+
+WAVE_WORLD_3 = {
+    "low": [],
+    "mid": [],
+    "high": [],
+}
+
+WAVE_WORLD_4 = {
+    "low": [],
+    "mid": [],
+    "high": [],
+}
+
 
 # monster_id -> parts_id
 MONSTER_PARTS = {
-    201000: 306000,
-    201001: 306001,
-    201002: 306002,
-    201003: 306003,
-    201004: 306005,
-    201005: 306006,
-    202000: 306009,
-    202001: 306010,
-    202002: 306011,
-    202003: 306012,
-    202004: 306015,
-    202005: 306019,
-    202006: 306018,
-    202007: 306020,
-    203000: 306023,
-    203001: 306025,
-    203002: 306027,
-    203003: 306029,
-    203004: 306031,
-    203005: 306033,
-    203006: 306032,
-    203007: 306034,
-    204000: 306035,
-    204001: 306036,
-    204002: 306037,
-    204003: 306038,
-    204004: 306039,
-    204010: 306035,
-    204011: 306036,
-    204012: 306037,
-    204013: 306038,
-    204014: 306039,
-    205000: 306035,
-    205003: 306038
+    '201000': 306000,
+    '201001': 306001,
+    '201002': 306002,
+    '201003': 306003,
+    '201004': 306005,
+    '201005': 306006,
+    '202000': 306009,
+    '202001': 306010,
+    '202002': 306011,
+    '202003': 306012,
+    '202004': 306015,
+    '202005': 306019,
+    '202006': 306018,
+    '202007': 306020,
+    '203000': 306023,
+    '203001': 306025,
+    '203002': 306027,
+    '203003': 306029,
+    '203004': 306031,
+    '203005': 306033,
+    '203006': 306032,
+    '203007': 306034,
+    '204000': 306035,
+    '204001': 306036,
+    '204002': 306037,
+    '204003': 306038,
+    '204004': 306039,
+    '204010': 306040,
+    '204011': 306041,
+    '204012': 306042,
+    '204013': 306043,
+    '204014': 306044,
+    '205000': 306050,
+    '205003': 306051,
+    '205007': 306054
 }
 
 # extra material drops
@@ -162,9 +193,26 @@ def expand_wave_id(wid):
     return "20" + wid[0] + "0" + wid[1:]
 
 
+FOOD_MATERIAL = [302000, 302001, 302002, 302003, 302004, 302005, 302006, 302007, 302008, 302009]
+FOOD_DROP_RATE = 0.25;
+
+EQ_MAT_G1 = [303000, 303100, 303200, 303300, 303400]
+EQ_G1_DROP_RATE = 0.25;
+EQ_MAT_G2 = [303001, 303101, 303201, 303301, 303401]
+EQ_G2_DROP_RATE = 0.10;
+EQ_MAT_G3 = [303002, 303102, 303202, 303302, 303402]
+EQ_G3_DROP_RATE = 0.03;
+EQ_MAT_G4 = [303003, 303103, 303203, 303303, 303403]
+EQ_G4_DROP_RATE = 0.05;
+EQ_MAT_G5 = [303004, 303104, 303204, 303304, 303404]
+EQ_G5_DROP_RATE = 0.01;
+
+
+
+
 ## CSV SETUP ##
 stage_file = open("StageSheet.csv", "w")
-reward_file = open("stage_reward.csv", "w")
+reward_file = open("StageRewardSheet.csv", "w")
 stage_csv = csv.writer(stage_file)
 reward_csv = csv.writer(reward_file)
 stage_csv.writerow(STAGE_FIELD_NAMES)
@@ -197,7 +245,9 @@ for stage in range(1, STAGES_PER_WORLD+1):
     m_level = int((stage - 1) / STAGES_PER_MONSTER_LV) + 1
     #dstage = (stage-1) % int(STAGES_PER_MONSTER_LV) # between 0 and 2
 
-    monster_parts = []
+    #monster_parts = []
+    random_monster = None
+    last_monster = None
     # Append each wave
     # print waves
     is_boss_level = (stage % 5 == 0)
@@ -218,6 +268,9 @@ for stage in range(1, STAGES_PER_WORLD+1):
                 mid = monster_ids[i]
                 #row += [mid, level, '', monster_count[mid]]
                 row += [mid, m_level, monster_count[mid]]
+                last_monster = mid
+                if (random_monster is None) or ((random.random() > 0.5) and (wave_idx is 0)):
+                    random_monster = mid
             else:
                 row += ['', '', '']
         if is_last_wave: # if last wave, check if boss, add exp and reward
@@ -226,21 +279,29 @@ for stage in range(1, STAGES_PER_WORLD+1):
             row += [0, 0, 0]
         stage_csv.writerow(row)
 
-    ## setup stage reward
-    #reward_row = [((world - 1) * STAGES_PER_WORLD) + stage]
-    #parts = Counter(monster_parts)
-    ## five rows, prioritize stage specific reward and boss parts reward
-    #additional_mat = MATERIAL_LOCATION.get(world, {}).get(stage)
-    #if additional_mat:
-    #    reward_row += [additional_mat, 1, 1, 2]
-    #if boss_id:
-    #    reward_row += [MONSTER_PARTS[boss_id], 1, 1, 2]
-    ## add rest of them
-    #parts_sorted_by_count = OrderedDict(sorted(parts.items(), key=itemgetter(1)))
-    #for parts_id in parts_sorted_by_count:
-    #    if len(reward_row) < len(REWARD_FIELD_NAMES):
-    #        parts_cnt = int(max(math.ceil(MONSTER_PARTS_DROP_RATE * parts_sorted_by_count[parts_id]), 1))
-    #        reward_row += [parts_id, 1, parts_cnt, parts_cnt+1]
+    # setup stage reward
+    reward_row = [stage]
+    # five rows, prioritize stage specific reward and boss parts reward
 
-    #reward_row += [None] * (len(REWARD_FIELD_NAMES) - len(reward_row))
-    #reward_csv.writerow(reward_row)
+    food_mat = random.choice(FOOD_MATERIAL)
+    g1_mat = random.choice(EQ_MAT_G1)
+    g2_mat = random.choice(EQ_MAT_G2)
+
+    reward_row += [MONSTER_PARTS[random_monster], MONSTER_PARTS_DROP_RATE, 1, 3]
+    reward_row += [MONSTER_PARTS[last_monster], MONSTER_PARTS_DROP_RATE/2.0, 1, 2]
+    reward_row += [food_mat, FOOD_DROP_RATE, 1, 2]
+    reward_row += [g1_mat, EQ_G1_DROP_RATE, 1, 1]
+    reward_row += [g2_mat, EQ_G2_DROP_RATE, 1, 1]
+
+#    if additional_mat:
+#        reward_row += [additional_mat, 1, 1, 2]
+#    if boss_id:
+#        reward_row += [MONSTER_PARTS[boss_id], 1, 1, 2]
+#    # add rest of them
+#    parts_sorted_by_count = OrderedDict(sorted(parts.items(), key=itemgetter(1)))
+#    for parts_id in parts_sorted_by_count:
+#        if len(reward_row) < len(REWARD_FIELD_NAMES):
+#            parts_cnt = int(max(math.ceil(MONSTER_PARTS_DROP_RATE * parts_sorted_by_count[parts_id]), 1))
+#            reward_row += [parts_id, 1, parts_cnt, parts_cnt+1]
+
+    reward_csv.writerow(reward_row)
