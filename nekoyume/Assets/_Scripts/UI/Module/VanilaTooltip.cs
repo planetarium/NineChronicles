29f5db75e95c1a9defb9391242901b3a8aaabffc
SeungGeol Song <unengine@gmail.com>
2019-12-06 21:14:28 +0900

    Implement VanilaTooltip, add hovering tooltip to ActionPoint, DailyReward

---
+++ nekoyume/Assets/_Scripts/UI/Module/VanilaTooltip.cs
@@ -0,0 +1,28 @@
+﻿using Assets.SimpleLocalization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Nekoyume.UI.Module
+{
+    public class VanilaTooltip : Widget
+    {
+        public Image panelImage;
+        public TextMeshProUGUI titleText;
+        public TextMeshProUGUI contentText;
+
+        public void Show(string title, string content, Vector2 position, bool localize = true)
+        {
+            titleText.text = LocalizationManager.Localize(title);
+            contentText.text = LocalizationManager.Localize(content);
+            panelImage.rectTransform.position = position;
+
+            base.Show();
+        }
+
+        public void SetSize(float width, float height)
+        {
+            panelImage.rectTransform.sizeDelta = new Vector2(width, height);
+        }
+    }
+}
