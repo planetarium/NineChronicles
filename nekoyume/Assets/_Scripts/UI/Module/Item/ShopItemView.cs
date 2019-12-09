132504a2ef76d723fe297662243acba5ad3c57e3
Yang Chun Ung <qooraven@gmail.com>
2019-12-09 14:33:40 +0900

    Update shop item

--- nekoyume/Assets/_Scripts/UI/Module/Item/ShopItemView.cs
+++ nekoyume/Assets/_Scripts/UI/Module/Item/ShopItemView.cs
@@ -1,9 +1,13 @@
 using Nekoyume.UI.Model;
+using TMPro;
+using UnityEngine;
 
 namespace Nekoyume.UI.Module
 {
     public class ShopItemView : CountableItemView<ShopItem>
     {
+        public GameObject priceGroup;
+        public TextMeshProUGUI priceText;
         public override void SetData(ShopItem model)
         {
             if (model is null)
@@ -14,7 +18,26 @@ namespace Nekoyume.UI.Module
             
             base.SetData(model);
 
+            SetBg(1f);
+            priceGroup.SetActive(true);
+            priceText.text = model.Price.Value.ToString("N0");
+
             Model.View = this;
+        }
+
+        public override void Clear()
+        {
+            base.Clear();
+            SetBg(0f);
+            priceGroup.SetActive(false);
+        }
+
+        private void SetBg(float alpha)
+        {
+            var a = alpha;
+            var color = backgroundImage.color;
+            color.a = a;
+            backgroundImage.color = color;
         }
     }
 }
