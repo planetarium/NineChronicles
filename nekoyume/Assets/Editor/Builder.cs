dcff19e2e87f9f1ae78d07179b8fb1c07c07ce8e
moreal <dev.moreal@gmail.com>
2019-12-10 14:22:12 +0900

    Split build methods by platform (e.g. standalone and development)

--- nekoyume/Assets/Editor/Builder.cs
+++ nekoyume/Assets/Editor/Builder.cs
@@ -10,7 +10,7 @@ namespace Editor
 
         public static string PlayerName = PlayerSettings.productName;
 
-        [MenuItem("Build/Windows + Mac OSX + Linux")]
+        [MenuItem("Build/Standalone/Windows + Mac OSX + Linux")]
         public static void BuildAll()
         {
             BuildMacOS();
@@ -18,57 +18,101 @@ namespace Editor
             BuildLinux();
         }
 
-        [MenuItem("Build/MacOS")]
+        [MenuItem("Build/Standalone/MacOS")]
         public static void BuildMacOS()
         {
             Debug.Log("Build MacOS");
             Build(BuildTarget.StandaloneOSX, targetDirName: "MacOS");
         }
 
-        [MenuItem("Build/Windows")]
+        [MenuItem("Build/Standalone/Windows")]
         public static void BuildWindows()
         {
             Debug.Log("Build Windows");
             Build(BuildTarget.StandaloneWindows64, targetDirName: "Windows");
         }
 
-        [MenuItem("Build/Linux")]
+        [MenuItem("Build/Standalone/Linux")]
         public static void BuildLinux()
         {
             Debug.Log("Build Linux");
             Build(BuildTarget.StandaloneLinux64, targetDirName: "Linux");
         }
 
-        [MenuItem("Build/MacOS Headless")]
+        [MenuItem("Build/Standalone/MacOS Headless")]
         public static void BuildMacOSHeadless()
         {
             Debug.Log("Build MacOS Headless");
             Build(BuildTarget.StandaloneOSX, BuildOptions.EnableHeadlessMode, "MacOSHeadless");
         }
 
-        [MenuItem("Build/Linux Headless")]
+        [MenuItem("Build/Standalone/Linux Headless")]
         public static void BuildLinuxHeadless()
         {
             Debug.Log("Build Linux Headless");
             Build(BuildTarget.StandaloneLinux64, BuildOptions.EnableHeadlessMode, "LinuxHeadless");
         }
 
-        [MenuItem("Build/Windows Headless")]
+        [MenuItem("Build/Standalone/Windows Headless")]
         public static void BuildWindowsHeadless()
         {
             Debug.Log("Build Windows Headless");
             Build(BuildTarget.StandaloneWindows64, BuildOptions.EnableHeadlessMode, "WindowsHeadless");
         }
 
-        [MenuItem("Build/Linux Headless Development")]
+        [MenuItem("Build/Development/Windows + Mac OSX + Linux")]
+        public static void BuildAllDevelopment()
+        {
+            BuildMacOSDevelopment();
+            BuildWindowsDevelopment();
+            BuildLinuxDevelopment();
+        }
+
+        [MenuItem("Build/Development/MacOS")]
+        public static void BuildMacOSDevelopment()
+        {
+            Debug.Log("Build MacOS Development");
+            Build(BuildTarget.StandaloneOSX, BuildOptions.Development, targetDirName: "MacOS");
+        }
+
+        [MenuItem("Build/Development/Windows")]
+        public static void BuildWindowsDevelopment()
+        {
+            Debug.Log("Build Windows Development");
+            Build(BuildTarget.StandaloneWindows64, BuildOptions.Development, targetDirName: "Windows");
+        }
+
+        [MenuItem("Build/Development/Linux")]
+        public static void BuildLinuxDevelopment()
+        {
+            Debug.Log("Build Linux Development");
+            Build(BuildTarget.StandaloneLinux64, BuildOptions.Development, targetDirName: "Linux");
+        }
+
+        [MenuItem("Build/Development/MacOS Headless")]
+        public static void BuildMacOSHeadlessDevelopment()
+        {
+            Debug.Log("Build MacOS Headless Development");
+            Build(BuildTarget.StandaloneOSX, BuildOptions.EnableHeadlessMode, "MacOSHeadless");
+        }
+
+        [MenuItem("Build/Development/Linux Headless")]
         public static void BuildLinuxHeadlessDevelopment()
         {
-            Debug.Log("Build Linux Headless");
+            Debug.Log("Build Linux Headless Development");
             Build(
                 BuildTarget.StandaloneLinux64,
                 BuildOptions.EnableHeadlessMode | BuildOptions.Development,
                 "LinuxHeadless");
         }
+        
+        [MenuItem("Build/Standalone/Windows Headless Development")]
+        public static void BuildWindowsHeadlessDevelopment()
+        {
+            Debug.Log("Build Windows Headless Development");
+            Build(BuildTarget.StandaloneWindows64, BuildOptions.EnableHeadlessMode, "WindowsHeadless");
+        }
+
 
         public static void Build(
             BuildTarget buildTarget,
