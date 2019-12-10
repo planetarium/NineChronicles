90d372b37c6190634c84711daa43ccf0cf5b8272
Yang Chun Ung <qooraven@gmail.com>
2019-12-10 17:24:25 +0900

    Apply QA
    - localization button label
    - add blur

--- nekoyume/Assets/_Scripts/UI/Settings.cs
+++ nekoyume/Assets/_Scripts/UI/Settings.cs
@@ -21,6 +21,11 @@ namespace Nekoyume.UI
         public TextMeshProUGUI volumeMasterText;
         public Slider volumeMasterSlider;
         public Toggle volumeMasterToggle;
+        public TextMeshProUGUI confirmText;
+        public TextMeshProUGUI cancelText;
+        public TextMeshProUGUI resetKeyStoreText;
+        public TextMeshProUGUI resetStoreText;
+        public TextMeshProUGUI openLogText;
         //public TextMeshProUGUI volumeMusicText;
         //public Slider volumeMusicSlider;
         //public Toggle volumeMusicToggle;
@@ -58,6 +63,11 @@ namespace Nekoyume.UI
             //{
             //    SetVolumeSfxMute(value);
             //});
+            confirmText.text = LocalizationManager.Localize("UI_SETTINGS_CONFIRM");
+            cancelText.text = LocalizationManager.Localize("UI_CANCEL");
+            resetStoreText.text = LocalizationManager.Localize("UI_CONFIRM_RESET_STORE_TITLE");
+            resetKeyStoreText.text = LocalizationManager.Localize("UI_CONFIRM_RESET_KEYSTORE_TITLE");
+            openLogText.text = LocalizationManager.Localize("UI_OPEN_LOG");
         }
 
         #endregion
