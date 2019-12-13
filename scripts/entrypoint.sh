84e20b435862cf4b3618ccdf4ccaa1519e6d350b
Seunghun Lee <waydi1@gmail.com>
2019-12-13 17:35:12 +0900

    Add double quote to entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,8 +4,8 @@ PRIOR_DLLS_PATH=/data/$PRIOR_DLLS
 UNITY_MANAGED_PATH="/app/Nine Chronicles_Data/Managed"
 
 # replace dlls with uploaded dlls if exists.
-if [ -d $PRIOR_DLLS_PATH ]; then
-    /bin/cp $PRIOR_DLLS_PATH/*.dll $UNITY_MANAGED_PATH
+if [ -d "$PRIOR_DLLS_PATH" ]; then
+    /bin/cp "$PRIOR_DLLS_PATH"/*.dll "$UNITY_MANAGED_PATH"
 fi
 
 # entrypoint.
