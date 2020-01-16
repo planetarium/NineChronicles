b92f78e7f471baf5df689eabc3349cdbfaa8dc97
Seunghun Lee <waydi1@gmail.com>
2020-01-16 17:07:38 +0900

    Add lock for Secp256k1 instance

--- nekoyume/Assets/_Scripts/BlockChain/Secp256K1CryptoBackend.cs
+++ nekoyume/Assets/_Scripts/BlockChain/Secp256K1CryptoBackend.cs
@@ -8,20 +8,24 @@ namespace Nekoyume.BlockChain
     public class Secp256K1CryptoBackend : ICryptoBackend
     {
         private readonly Secp256k1 _instance = new Secp256k1();
+        private readonly object _instanceLock = new object();
 
         public bool Verify(
             HashDigest<SHA256> messageHash,
             byte[] signature,
             PublicKey publicKey)
         {
-            var secp256K1Signature = new byte[64];
-            _instance.SignatureParseDer(secp256K1Signature, signature);
+            lock (_instanceLock)
+            {
+                var secp256K1Signature = new byte[64];
+                _instance.SignatureParseDer(secp256K1Signature, signature);
 
-            byte[] secp256K1PublicKey = new byte[64];
-            byte[] serializedPublicKey = publicKey.Format(false);
-            _instance.PublicKeyParse(secp256K1PublicKey, serializedPublicKey);
+                byte[] secp256K1PublicKey = new byte[64];
+                byte[] serializedPublicKey = publicKey.Format(false);
+                _instance.PublicKeyParse(secp256K1PublicKey, serializedPublicKey);
 
-            return _instance.Verify(secp256K1Signature, messageHash.ToByteArray(), secp256K1PublicKey);
+                return _instance.Verify(secp256K1Signature, messageHash.ToByteArray(), secp256K1PublicKey);
+            }
         }
     }
 }
