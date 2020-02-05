43dbbb184105bd9f380d78c4e02fccb1f82df661
SeungGeol Song <unengine@gmail.com>
2020-02-05 20:35:01 +0900

    Change progress bar resource, add vfx to progressbar.

--- nekoyume/Assets/_Scripts/UI/StageProgressBar.cs
+++ nekoyume/Assets/_Scripts/UI/StageProgressBar.cs
@@ -1,6 +1,7 @@
 ﻿using Nekoyume.Game.Controller;
 using Nekoyume.Game.VFX;
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,18 +9,34 @@ namespace Nekoyume.UI
 {
     public class StageProgressBar : MonoBehaviour
     {
+        public float smoothenSpeed = 2.0f;
+
         [SerializeField]
         private Slider slider = null;
         [SerializeField]
         private RectTransform[] starImages = null;
+        [SerializeField]
+        private Image[] activatedStarImages = null;
+        [SerializeField]
+        private RectTransform vfxOffset;
         private const int MaxWave = 3;
         private int _currentStar = 0;
         private int _currentWaveHpSum = 0;
         private int _progress = 0;
 
+        private StageProgressBarVFX _stageProgressBarVFX = null;
         private Star01VFX _star01VFX = null;
         private Star02VFX _star02VFX = null;
         private Star03VFX _star03VFX = null;
+
+        private Coroutine _smoothenCoroutine = null;
+
+        private float _xLength = 0;
+
+        private void Awake()
+        {
+            _xLength = slider.GetComponent<RectTransform>().rect.width;
+        }
 
         public void Show()
         {
@@ -28,6 +45,16 @@ namespace Nekoyume.UI
 
         public void Close()
         {
+            if (!(_smoothenCoroutine is null))
+            {
+                StopCoroutine(_smoothenCoroutine);
+                _smoothenCoroutine = null;
+            }
+            activatedStarImages[0].enabled = false;
+            activatedStarImages[1].enabled = false;
+            activatedStarImages[2].enabled = false;
+            _stageProgressBarVFX?.Stop();
+            _stageProgressBarVFX = null;
             _star01VFX?.Stop();
             _star01VFX = null;
             _star02VFX?.Stop();
@@ -55,21 +82,30 @@ namespace Nekoyume.UI
                 case 1:
                     starVFX = _star01VFX = VFXController.instance.CreateAndChaseRectTransform<Star01VFX>(starImages[0]);
                     emissionVFX = VFXController.instance.CreateAndChaseRectTransform<StarEmission01VFX>(starImages[0]);
+                    activatedStarImages[0].enabled = true;
                     break;
                 case 2:
                     starVFX = _star02VFX = VFXController.instance.CreateAndChaseRectTransform<Star02VFX>(starImages[1]);
                     emissionVFX = VFXController.instance.CreateAndChaseRectTransform<StarEmission02VFX>(starImages[1]);
+                    activatedStarImages[1].enabled = true;
                     break;
                 case 3:
                     starVFX = _star03VFX = VFXController.instance.CreateAndChaseRectTransform<Star03VFX>(starImages[2]);
                     emissionVFX = VFXController.instance.CreateAndChaseRectTransform<StarEmission03VFX>(starImages[2]);
+                    activatedStarImages[2].enabled = true;
                     break;
                 default:
-                    throw new ArgumentOutOfRangeException();
+                    return;
             }
+
             starVFX?.Play();
             emissionVFX?.Play();
-            slider.value = (float) _currentStar / MaxWave;
+
+            if (!(_smoothenCoroutine is null))
+            {
+                StopCoroutine(_smoothenCoroutine);
+            }
+            _smoothenCoroutine = StartCoroutine(LerpProgressBar((float)_currentStar / MaxWave));
         }
 
         public void SetNextWave(int waveHpSum)
@@ -79,13 +115,48 @@ namespace Nekoyume.UI
 
         public void IncreaseProgress(int hp)
         {
+            if(_stageProgressBarVFX is null)
+            {
+                _stageProgressBarVFX = VFXController.instance.CreateAndChaseRectTransform<StageProgressBarVFX>(vfxOffset);
+                _stageProgressBarVFX.Play();
+            }
+
             _progress -= hp;
-            if(_progress == 0)
+            if (_progress == 0)
             {
                 CompleteWave();
                 return;
             }
-            slider.value = ((float) (_currentWaveHpSum - _progress) / _currentWaveHpSum + _currentStar) / MaxWave;
+
+            var sliderValue = GetProgress(_progress);
+
+            if (!(_smoothenCoroutine is null))
+            {
+                slider.value = GetProgress(_progress + hp);
+                StopCoroutine(_smoothenCoroutine);
+            }
+            _smoothenCoroutine = StartCoroutine(LerpProgressBar(sliderValue));
+        }
+
+        private IEnumerator LerpProgressBar(float value)
+        {
+            float current = slider.value;
+            float t = 0;
+            float duration = (value - current) / (Time.deltaTime * smoothenSpeed);
+
+            while (t < duration)
+            {
+                current = Mathf.Lerp(current, value, Time.deltaTime * smoothenSpeed);
+                slider.value = current;
+                vfxOffset.anchoredPosition = new Vector2(current * _xLength, vfxOffset.anchoredPosition.y);
+                t += Time.deltaTime;
+                yield return null;
+            }
+        }
+
+        private float GetProgress(float progress)
+        {
+            return ((_currentWaveHpSum - progress) / _currentWaveHpSum + _currentStar) / MaxWave;
         }
     }
 }
