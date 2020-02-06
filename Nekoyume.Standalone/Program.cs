4b0405db5ae13776b45d5ac223042500b04caa34
moreal <dev.moreal@gmail.com>
2020-02-06 21:47:15 +0900

    Support standalone build

---
+++ Nekoyume.Standalone/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Cocona;
+using Libplanet;
+using Libplanet.Action;
+using Libplanet.Blockchain;
+using Libplanet.Blockchain.Policies;
+using Libplanet.Crypto;
+using Libplanet.Net;
+using Libplanet.Standalone.Hosting;
+using Nekoyume.Action;
+using Nekoyume.BlockChain;
+using Serilog;
+
+using NekoyumeActionType = Libplanet.Action.PolymorphicAction<Nekoyume.Action.ActionBase>; 
+
+namespace Nekoyume.Standalone
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await CoconaLiteApp.RunAsync<Program>(args);
+        }
+
+        [Command(Description = "Run standalone application with options.")]
+        public async Task Run(
+            int appProtocolVersion,
+            string genesisBlockPath, 
+            bool noMiner,
+            string host = null,
+            ushort? port = null,
+            string privateKeyString = null,
+            string storePath = null,
+            string[] iceServerStrings = null,
+            string[] peerStrings = null
+        )
+        {
+            // Setup logger.
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .MinimumLevel.Debug().CreateLogger();
+
+            var privateKey = string.IsNullOrEmpty(privateKeyString)
+                ? new PrivateKey()
+                : new PrivateKey(ByteUtil.ParseHex(privateKeyString));
+
+            peerStrings ??= Array.Empty<string>();
+            iceServerStrings ??= Array.Empty<string>();
+
+            LibplanetNodeServiceProperties properties = new LibplanetNodeServiceProperties
+            {
+                Host = host,
+                Port = port,
+                AppProtocolVersion = appProtocolVersion,
+                GenesisBlockPath = genesisBlockPath,
+                NoMiner = noMiner,
+                PrivateKey = privateKey,
+                IceServers = iceServerStrings.Select(LoadIceServer),
+                Peers = peerStrings.Select(LoadPeer).ToArray(),
+                StorePath = storePath,
+            };
+
+            var blockAction = new RewardGold { Gold = 1 };
+            var blockPolicy =
+                new BlockPolicy<PolymorphicAction<ActionBase>>(blockAction, TimeSpan.FromSeconds(10), 100000, 2048);
+            Action<BlockChain<NekoyumeActionType>, Swarm<NekoyumeActionType>, PrivateKey> minerLoopAction =
+                (chain, swarm, privateKey) =>
+                {
+                    var miner = new Miner(chain, swarm, privateKey);
+                    while (true)
+                    {
+                        Log.Debug("Miner called.");
+                        try
+                        {
+                            miner.MineBlock();
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Error(ex, "Exception occurred.");
+                        }
+                    }
+                };
+
+            var service = new LibplanetNodeService<NekoyumeActionType>(properties, blockPolicy, minerLoopAction);
+            var cancellationToken = new CancellationToken();
+            await service.StartAsync(cancellationToken);
+        }
+
+        [Command(Description = "Make new private key.")]
+        public void MakePrivateKey()
+        {
+            var privateKey = new PrivateKey();
+            Console.WriteLine("private key: " + ByteUtil.Hex(privateKey.ByteArray));
+            Console.WriteLine("address: " + privateKey.PublicKey.ToAddress().ToHex());
+        }
+
+        private static IceServer LoadIceServer(string iceServerInfo)
+        {
+            var uri = new Uri(iceServerInfo);
+            string[] userInfo = uri.UserInfo.Split(':');
+        
+            return new IceServer(new[] { uri }, userInfo[0], userInfo[1]);
+        }
+        
+        private static BoundPeer LoadPeer(string peerInfo)
+        {
+            var tokens = peerInfo.Split(',');
+            var pubKey = new PublicKey(ByteUtil.ParseHex(tokens[0]));
+            var host = tokens[1];
+            var port = int.Parse(tokens[2]);
+        
+            return new BoundPeer(pubKey, new DnsEndPoint(host, port), 0);
+        }
+    }
+}
