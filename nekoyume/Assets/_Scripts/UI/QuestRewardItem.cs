a09670a71250be6147e62b1f22e6c0fa74cbdc06
김태현 <allone1129@gmail.com>
2020-02-10 16:49:31 +0900

    Change animation timing

--- nekoyume/Assets/_Scripts/UI/QuestRewardItem.cs
+++ nekoyume/Assets/_Scripts/UI/QuestRewardItem.cs
@@ -6,6 +6,7 @@ using Nekoyume.Game;
 using Nekoyume.Helper;
 using DG.Tweening;
 using Nekoyume.Game.Controller;
+using System.Collections;
 
 namespace Nekoyume.UI
 {
@@ -25,27 +26,32 @@ namespace Nekoyume.UI
             var rect = result.RectTransform;
             rect.anchoredPosition = view.gameObject.transform.position.ToCanvasPosition(ActionCamera.instance.Cam, MainCanvas.instance.Canvas);
 
-            result.Play(index);
+            result.StartCoroutine(result.CoPlay(index));
         }
 
-        private void Play(int index)
+        private IEnumerator CoPlay(int index)
         {
             if (Equals(_inventoryTransform,null))
                 UpdateInventoryTransform();
 
-            Sequence seq = DOTween.Sequence();
+            VFXController.instance.Create<ItemMoveVFX>(transform.position);
 
-            seq.AppendCallback(() => VFXController.instance.Create<ItemMoveVFX>(transform.position));
+            Vector3 midPath = new Vector3(_inventoryTransform.position.x + 0.5f * (index + 1), (_inventoryTransform.position.y + transform.position.y) / 2, _inventoryTransform.position.z);
 
-            var midPath = new Vector3(_inventoryTransform.position.x + 0.5f * (index + 1), (_inventoryTransform.position.y + transform.position.y) / 2, _inventoryTransform.position.z);
             Vector3[] path = new Vector3[] { transform.position, midPath, _inventoryTransform.position };
-            seq.Append(transform.DOPath(path, 0.8f + 0.2f * index, PathType.CatmullRom).SetEase(Ease.OutCubic));
 
-            seq.AppendCallback(() => VFXController.instance.Create<ItemMoveVFX>(_inventoryTransform, Vector3.zero));
-            seq.AppendCallback(() => Destroy(gameObject));
-            seq.Append(_inventoryTransform.DOScale(1.2f, 0.1f));
-            seq.Append(_inventoryTransform.DOScale(1.0f, 0.5f).SetEase(Ease.OutBack));
-            seq.Play();
+            Tweener tweenMove;
+            tweenMove = transform.DOPath(path, 0.8f + 0.2f * index, PathType.CatmullRom).SetEase(Ease.OutCubic);
+
+            yield return new WaitForSeconds(0.2f + 0.2f * index);
+
+            if(index == 0)
+                _inventoryTransform.DOScale(1.2f, 0.1f);
+
+            yield return new WaitWhile(tweenMove.IsPlaying);
+            VFXController.instance.Create<ItemMoveVFX>(_inventoryTransform.position);
+            Destroy(gameObject);
+            _inventoryTransform.DOScale(1.0f, 0.5f).SetEase(Ease.OutElastic);
         }
 
         private void UpdateInventoryTransform()
