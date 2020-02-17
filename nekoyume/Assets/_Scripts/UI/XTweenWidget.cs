46f3523513fd6d48710eee420174bba23e7ae8f6
김태현 <allone1129@gmail.com>
2020-02-17 16:52:16 +0900

    Add XTweenWidget class and animations of widgets having AnchoredPositionXTweener

---
+++ nekoyume/Assets/_Scripts/UI/XTweenWidget.cs
@@ -0,0 +1,38 @@
+﻿using DG.Tweening;
+using Nekoyume.UI.Tween;
+using System.Collections;
+using UnityEngine;
+
+namespace Nekoyume.UI
+{
+    public abstract class XTweenWidget:Widget
+    {
+        protected AnchoredPositionXTweener _xTween;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _xTween = GetComponentInChildren<AnchoredPositionXTweener>();
+        }
+
+        public override void Show()
+        { 
+            base.Show();
+            _xTween.Show();
+        }
+
+        public override void Close(bool ignoreCloseAnimation = false)
+        {
+            if (!gameObject.activeSelf)
+                return;
+
+            StartCoroutine(CoClose(ignoreCloseAnimation));
+        }
+
+        private IEnumerator CoClose(bool ignoreCloseAnimation)
+        {
+            yield return new WaitWhile(_xTween.Close().IsPlaying);
+            base.Close(ignoreCloseAnimation);
+        }
+    }
+}
