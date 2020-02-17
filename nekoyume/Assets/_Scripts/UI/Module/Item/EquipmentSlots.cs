a240c471a0fccb0060b54f54fd665382062147ba
Hyun Seungmin <boscohyun@gmail.com>
2020-02-17 17:24:37 +0900

    Cherry pick and resolve conflicts `f83dbac957d32b96700635dfefce68b91e9ba3b5`

--- nekoyume/Assets/_Scripts/UI/Module/Item/EquipmentSlots.cs
+++ nekoyume/Assets/_Scripts/UI/Module/Item/EquipmentSlots.cs
@@ -18,13 +18,10 @@ namespace Nekoyume.UI.Module
             if (slots is null)
                 throw new SerializeFieldNullException();
         }
-
-        // `TryGet()`을 통해서 슬롯을 얻어다가 채우는 방법에서 `TryToEquip()` 정도의 메소드를 통해서 장착을 시도하는 것이 어떨까?
-        // 장착 로직이 밖에 있을 필요는 없어 보이기 때문이다.
-        // 또한 각 슬롯의 정보와 이벤트는 이 클래스에서 다시 정제해서 열어 주는 것이 어떨까?
+        
         public bool TryToEquip(Equipment equipment, Action<EquipmentSlot> onClick, Action<EquipmentSlot> onDoubleClick, bool throwException = true)
         {
-            if (!TryGet(equipment.Data.ItemSubType, out var slot))
+            if (!TryGetToEquip(equipment, out var slot))
             {
                 if (!throwException)
                     return false;
