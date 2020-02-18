adab89d182bb9dcb56f2ce6e909c9e58daedb403
longfin <longfinfunnel@gmail.com>
2020-02-18 09:32:32 +0900

    Restore tx unstaging on start

--- nekoyume/Assets/_Scripts/BlockChain/Agent.cs
+++ nekoyume/Assets/_Scripts/BlockChain/Agent.cs
@@ -168,6 +168,9 @@ namespace Nekoyume.BlockChain
             PrivateKey = privateKey;
             Address = privateKey.PublicKey.ToAddress();
             store = LoadStore(path, storageType);
+            store.UnstageTransactionIds(
+                new HashSet<TxId>(store.IterateStagedTransactionIds())
+            );
 
             try
             {
