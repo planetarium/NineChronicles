cf98e8ad838c6220162f5e6c277ced2b987de862
Hyun Seungmin <boscohyun@gmail.com>
2020-02-28 17:58:07 +0900

    Add test for `Skills`

--- nekoyume/Assets/Tests/EditMode/Battle/SkillsTest.cs
+++ nekoyume/Assets/Tests/EditMode/Battle/SkillsTest.cs
@@ -11,11 +11,20 @@ namespace Tests.EditMode.Battle
     public class SkillsTest
     {
         private TableSheets _tableSheets;
+        private SkillSheet _skillSheet;
+        private SkillBuffSheet _skillBuffSheet;
 
         [SetUp]
         public void SetUp()
         {
             _tableSheets = TableSheetsHelper.MakeTableSheets();
+            Assert.IsNotNull(_tableSheets);
+            _skillSheet = _tableSheets.SkillSheet;
+            Assert.IsNotNull(_skillSheet);
+            Assert.IsTrue(_skillSheet.Any());
+            _skillBuffSheet = _tableSheets.SkillBuffSheet;
+            Assert.IsNotNull(_skillBuffSheet);
+            Assert.IsTrue(_skillBuffSheet.Any());
         }
 
         [TearDown]
@@ -27,15 +36,39 @@ namespace Tests.EditMode.Battle
         [Test]
         public void SelectTest()
         {
-            var skillSheet = _tableSheets.SkillSheet;
-            Assert.IsTrue(skillSheet.Any());
-            var skillRow = skillSheet.First().Value;
+            var skillRow = _skillSheet.First().Value;
+            Assert.IsNotNull(skillRow);
+            
+            var firstSkill = SkillFactory.Get(skillRow, 100, 100);
+            Assert.IsNotNull(firstSkill);
+            
+            var skills = new Skills {firstSkill};
+            var selectedSkill = skills.Select(new Random());
+            Assert.IsNotNull(selectedSkill);
+            Assert.AreEqual(firstSkill, selectedSkill.Skill);
+
+            selectedSkill.Cooldown = 1;
+            selectedSkill = skills.Select(new Random());
+            Assert.IsNull(selectedSkill);
+
+            foreach (var skill in skills)
+            {
+                skill.Cooldown = 0;
+            }
+            
+            selectedSkill = skills.Select(new Random());
+            Assert.IsNotNull(selectedSkill);
+            Assert.AreEqual(firstSkill, selectedSkill.Skill);
+        }
+
+        [Test]
+        public void SelectWithBuffsTest()
+        {
+            var skillRow = _skillSheet.First().Value;
             var firstSkill = SkillFactory.Get(skillRow, 100, 100);
 
-            var skillBuffSheet = _tableSheets.SkillBuffSheet;
-            Assert.IsTrue(skillBuffSheet.Any());
-            var skillBuffRow = skillBuffSheet.First();
-            skillRow = skillSheet.Values.FirstOrDefault(row => row.Id == skillBuffRow.Value.SkillId);
+            var skillBuffRow = _skillBuffSheet.First();
+            skillRow = _skillSheet.Values.FirstOrDefault(row => row.Id == skillBuffRow.Value.SkillId);
             Assert.NotNull(skillRow);
             var firstBuffSkill = SkillFactory.Get(skillRow, 100, 100);
             var buffs = BuffFactory.GetBuffs(firstBuffSkill, _tableSheets.SkillBuffSheet, _tableSheets.BuffSheet)
@@ -45,11 +78,11 @@ namespace Tests.EditMode.Battle
 
             var skills = new Skills {firstSkill};
             var selectedSkill = skills.Select(new Random(), null, _tableSheets.SkillBuffSheet, _tableSheets.BuffSheet);
-            Assert.IsTrue(firstSkill.Equals(selectedSkill));
+            Assert.IsTrue(firstSkill.Equals(selectedSkill.Skill));
 
             skills.Add(firstBuffSkill);
             selectedSkill = skills.Select(new Random(), buffs, _tableSheets.SkillBuffSheet, _tableSheets.BuffSheet);
-            Assert.IsTrue(firstSkill.Equals(selectedSkill));
+            Assert.IsTrue(firstSkill.Equals(selectedSkill.Skill));
         }
     }
 }
