2f3db016f81ac54430a78de7bff0e017a47fa917
moreal <dev.moreal@gmail.com>
2020-03-05 18:09:22 +0900

    Split executable part from NineChronicles.Standalone

---
+++ NineChronicles.Standalone.Executable/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Cocona;
+using Libplanet;
+using Libplanet.Crypto;
+using Libplanet.Net;
+using Libplanet.Standalone.Hosting;
+using Serilog;
+
+using NineChroniclesActionType = Libplanet.Action.PolymorphicAction<Nekoyume.Action.ActionBase>;
+
+namespace NineChronicles.Standalone
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await CoconaLiteApp.RunAsync<Program>(args);
+        }
+
+        [Command(Description = "Run standalone application with options.")]
+        public async Task Run(
+            [Option('V')]
+            int appProtocolVersion,
+            [Option('G')]
+            string genesisBlockPath, 
+            bool noMiner,
+            [Option('H')]
+            string host = null,
+            [Option('P')]
+            ushort? port = null,
+            [Option("private-key")]
+            string privateKeyString = null,
+            string storeType = null,
+            string storePath = null,
+            [Option("ice-server", new [] { 'I', })]
+            string[] iceServerStrings = null,
+            [Option("peer")]
+            string[] peerStrings = null,
+            bool rpcServer = false,
+            string rpcListenHost = "0.0.0.0",
+            int? rpcListenPort = null
+        )
+        {
+            // Setup logger.
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .MinimumLevel.Debug().CreateLogger();
+
+            var privateKey = string.IsNullOrEmpty(privateKeyString)
+                ? new PrivateKey()
+                : new PrivateKey(ByteUtil.ParseHex(privateKeyString));
+
+            peerStrings ??= Array.Empty<string>();
+            iceServerStrings ??= Array.Empty<string>();
+
+            var iceServers = iceServerStrings.Select(LoadIceServer).ToImmutableArray();
+            var peers = peerStrings.Select(LoadPeer).ToImmutableArray();
+
+            LibplanetNodeServiceProperties properties = new LibplanetNodeServiceProperties
+            {
+                Host = host,
+                Port = port,
+                AppProtocolVersion = appProtocolVersion,
+                GenesisBlockPath = genesisBlockPath,
+                NoMiner = noMiner,
+                PrivateKey = privateKey,
+                IceServers = iceServers,
+                Peers = peers,
+                StoreType = storeType,
+                StorePath = storePath,
+            };
+
+            var service = new NineChroniclesNodeService(properties);
+            await service.Run(
+                rpcServer: rpcServer,
+                rpcListenHost: rpcListenHost,
+                rpcListenPort: rpcListenPort
+            );
+        }
+
+        private static IceServer LoadIceServer(string iceServerInfo)
+        {
+            var uri = new Uri(iceServerInfo);
+            string[] userInfo = uri.UserInfo.Split(':');
+        
+            return new IceServer(new[] { uri }, userInfo[0], userInfo[1]);
+        }
+        
+        private static BoundPeer LoadPeer(string peerInfo)
+        {
+            var tokens = peerInfo.Split(',');
+            var pubKey = new PublicKey(ByteUtil.ParseHex(tokens[0]));
+            var host = tokens[1];
+            var port = int.Parse(tokens[2]);
+        
+            return new BoundPeer(pubKey, new DnsEndPoint(host, port), 0);
+        }
+    }
+}
--- NineChronicles.Standalone/NineChroniclesNodeService.cs
+++ NineChronicles.Standalone/NineChroniclesNodeService.cs
@@ -1,12 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Cocona;
 using Grpc.Core;
-using Libplanet;
 using Libplanet.Action;
 using Libplanet.Blockchain;
 using Libplanet.Blockchain.Policies;
@@ -26,100 +23,20 @@ namespace NineChronicles.Standalone
 {
     public class NineChroniclesNodeService
     {
-        static async Task Main(string[] args)
+        private LibplanetNodeServiceProperties Properties { get; }
+
+        public NineChroniclesNodeService(
+            LibplanetNodeServiceProperties properties)
         {
-            await CoconaLiteApp.RunAsync<NineChroniclesNodeService>(args);
-        }
-
-        [Command(Description = "Run standalone application with options.")]
+            Properties = properties;
+        } 
+        
         public async Task Run(
-            [Option('V')]
-            int appProtocolVersion,
-            [Option('G')]
-            string genesisBlockPath, 
-            bool noMiner,
-            [Option('H')]
-            string host = null,
-            [Option('P')]
-            ushort? port = null,
-            [Option("private-key")]
-            string privateKeyString = null,
-            string storeType = null,
-            string storePath = null,
-            [Option("ice-server", new [] { 'I', })]
-            string[] iceServerStrings = null,
-            [Option("peer")]
-            string[] peerStrings = null,
-            bool rpcServer = false,
-            [Option("rpc-listen-host")]
-            string rpcListenHost = "0.0.0.0",
-            [Option("rpc-listen-port")]
-            int? rpcListenPort = null
-        )
-        {
-            // Setup logger.
-            Log.Logger = new LoggerConfiguration()
-                .WriteTo.Console()
-                .MinimumLevel.Debug().CreateLogger();
-
-            var privateKey = string.IsNullOrEmpty(privateKeyString)
-                ? new PrivateKey()
-                : new PrivateKey(ByteUtil.ParseHex(privateKeyString));
-
-            peerStrings ??= Array.Empty<string>();
-            iceServerStrings ??= Array.Empty<string>();
-
-            var iceServers = iceServerStrings.Select(LoadIceServer);
-            var peers = peerStrings.Select(LoadPeer);
-
-            await Run(
-                appProtocolVersion: appProtocolVersion,
-                genesisBlockPath: genesisBlockPath,
-                noMiner: noMiner,
-                privateKey: privateKey,
-                host: host,
-                port: port,
-                storeType: storeType,
-                storePath: storePath,
-                iceServers: iceServers,
-                peers: peers,
-                rpcServer: rpcServer,
-                rpcListenHost: rpcListenHost,
-                rpcListenPort: rpcListenPort
-            );
-        }
-
-        [Ignore]
-        public async Task Run(
-            int appProtocolVersion,
-            string genesisBlockPath, 
-            bool noMiner,
-            PrivateKey privateKey,
-            string host = null,
-            ushort? port = null,
-            string storeType = null,
-            string storePath = null,
-            IEnumerable<IceServer> iceServers = null,
-            IEnumerable<Peer> peers = null,
             bool rpcServer = false,
             string rpcListenHost = null,
             int? rpcListenPort = null,
             CancellationToken cancellationToken = default)
         {
-            LibplanetNodeServiceProperties properties = new LibplanetNodeServiceProperties
-            {
-                Host = host,
-                Port = port,
-                AppProtocolVersion = appProtocolVersion,
-                GenesisBlockPath = genesisBlockPath,
-                NoMiner = noMiner,
-                PrivateKey = privateKey,
-                IceServers = iceServers,
-                Peers = peers,
-                StoreType = storeType,
-                StorePath = storePath,
-            };
-
             // BlockPolicy shared through Lib9c.
             IBlockPolicy<PolymorphicAction<ActionBase>> blockPolicy = BlockPolicy.GetPolicy();
             async Task minerLoopAction(
@@ -144,7 +61,7 @@ namespace NineChronicles.Standalone
             }
 
             var nodeService = new LibplanetNodeService<NineChroniclesActionType>(
-                properties, 
+                Properties, 
                 blockPolicy, 
                 minerLoopAction
             );
@@ -156,14 +73,14 @@ namespace NineChronicles.Standalone
                 if (string.IsNullOrEmpty(rpcListenHost))
                 {
                     throw new CommandExitedException(
-                        "--rpc-host must be required when --rpc had been set.",
+                        "--rpc-listen-host must be required when --rpc-server had been set.",
                         -1
                     );
                 }
                 else if (!(rpcListenPort is int rpcPortValue))
                 {
                     throw new CommandExitedException(
-                        "--rpc-port must be required when --rpc had been set.",
+                        "--rpc-listen-port must be required when --rpc-server had been set.",
                         -1
                     );
                 }
@@ -177,7 +94,7 @@ namespace NineChronicles.Standalone
                         {
                             services.AddHostedService(provider => new ActionEvaluationPublisher(
                                 nodeService.BlockChain,
-                                rpcListenHost,
+                                IPAddress.Loopback.ToString(),
                                 rpcPortValue
                             ));
                         });
@@ -189,24 +106,6 @@ namespace NineChronicles.Standalone
                 services.AddHostedService(provider => nodeService);
                 services.AddSingleton(provider => nodeService.BlockChain);
             }).RunConsoleAsync(cancellationToken);
-        }
-
-        private static IceServer LoadIceServer(string iceServerInfo)
-        {
-            var uri = new Uri(iceServerInfo);
-            string[] userInfo = uri.UserInfo.Split(':');
-        
-            return new IceServer(new[] { uri }, userInfo[0], userInfo[1]);
-        }
-        
-        private static BoundPeer LoadPeer(string peerInfo)
-        {
-            var tokens = peerInfo.Split(',');
-            var pubKey = new PublicKey(ByteUtil.ParseHex(tokens[0]));
-            var host = tokens[1];
-            var port = int.Parse(tokens[2]);
-        
-            return new BoundPeer(pubKey, new DnsEndPoint(host, port), 0);
         }
     }
 }
