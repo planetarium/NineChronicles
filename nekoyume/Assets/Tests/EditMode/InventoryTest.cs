2f27ccbe6b7754a72890203b0108beba1be4e32c
Hyun Seungmin <boscohyun@gmail.com>
2020-03-09 18:52:42 +0900

    Add unit test for Inventory

---
+++ nekoyume/Assets/Tests/EditMode/InventoryTest.cs
@@ -0,0 +1,52 @@
+using System;
+using Nekoyume.Helper;
+using Nekoyume.Model.Item;
+using Nekoyume.TableData;
+using NUnit.Framework;
+using UnityEngine;
+using Inventory = Nekoyume.UI.Model.Inventory;
+
+namespace Tests.EditMode
+{
+    public class InventoryTest
+    {
+        private TableSheets _tableSheets;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _tableSheets = TableSheetsHelper.MakeTableSheets();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _tableSheets = null;
+        }
+
+        [Test]
+        public void AddItem()
+        {
+            if (_tableSheets.EquipmentItemSheet.First is null)
+            {
+                Debug.LogWarning($"{_tableSheets.EquipmentItemSheet} is empty.");
+                return;
+            }
+
+            var inventory = new Inventory();
+            var row = _tableSheets.EquipmentItemSheet.First;
+
+            var equipment = ItemFactory.CreateItemUsable(row, Guid.NewGuid(), -100);
+            inventory.AddItem(equipment);
+            Assert.AreEqual(1, inventory.Equipments.Count);
+            
+            equipment = ItemFactory.CreateItemUsable(row, Guid.NewGuid(), 100);
+            inventory.AddItem(equipment);
+            Assert.AreEqual(1, inventory.Equipments.Count);
+            
+            equipment = ItemFactory.CreateItemUsable(row, Guid.NewGuid(), -100);
+            inventory.AddItem(equipment);
+            Assert.AreEqual(2, inventory.Equipments.Count);
+        }
+    }
+}
