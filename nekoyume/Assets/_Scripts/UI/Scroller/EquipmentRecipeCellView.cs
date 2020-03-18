b28784b66643de519f0e5f6a65c410fabfcc4ed8
Hyun Seungmin <boscohyun@gmail.com>
2020-03-18 09:34:07 +0900

    Fix bug

--- nekoyume/Assets/_Scripts/UI/Scroller/EquipmentRecipeCellView.cs
+++ nekoyume/Assets/_Scripts/UI/Scroller/EquipmentRecipeCellView.cs
@@ -178,7 +178,7 @@ namespace Nekoyume.UI.Scroller
                             break;
                         }
 
-                        if (shouldDimmed)
+                        if (!shouldDimmed)
                         {
                             break;
                         }
