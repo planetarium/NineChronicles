772eedef5a2d75b8f9353def740234098d8a6bc1
moreal <dev.moreal@gmail.com>
2020-03-18 20:28:57 +0900

    Extract GetProtectedPrivateKeys to Lib9c

--- tools/launcher/KeyStore.cs
+++ tools/launcher/KeyStore.cs
@@ -18,42 +18,9 @@ namespace Launcher
 
         public string KeyStorePath { get; }
 
-        public Dictionary<Address, ProtectedPrivateKey> ProtectedPrivateKeys
-        {
-            get {
-                if (!Directory.Exists(KeyStorePath))
-                {
-                    Directory.CreateDirectory(KeyStorePath);
-                }
-
-                var keyPaths = Directory.EnumerateFiles(KeyStorePath);
-
-                var protectedPrivateKeys = new Dictionary<Address, ProtectedPrivateKey>();
-                foreach (var keyPath in keyPaths)
-                {
-                    if (Path.GetFileName(keyPath) is string f && f.StartsWith("."))
-                    {
-                        continue;
-                    }
-
-                    using (Stream stream = new FileStream(keyPath, FileMode.Open))
-                    using (var reader = new StreamReader(stream))
-                    {
-                        try
-                        {
-                            var protectedPrivateKey = ProtectedPrivateKey.FromJson(reader.ReadToEnd());
-                            protectedPrivateKeys[protectedPrivateKey.Address] = protectedPrivateKey;
-                        }
-                        catch (Exception e)
-                        {
-                            Log.Error(e, "The key file {keyPath} is invalid: {exception}", keyPath, e);
-                        }
-                    }
-                }
-
-                return protectedPrivateKeys;
-            }
-        }
+        public Dictionary<Address, ProtectedPrivateKey> ProtectedPrivateKeys =>
+            Nekoyume.KeyStore.GetProtectedPrivateKeys(KeyStorePath)
+                .ToDictionary(pair => pair.Value.Address, pair => pair.Value);
 
         // Of course, it can be replaced with LINQ `Select`. But QML doesn't support it so exists.
         [NotifySignal]
