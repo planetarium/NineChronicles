1efed239434080b67b90927bf878e3b0d75fed53
moreal <dev.moreal@gmail.com>
2020-03-23 21:04:37 +0900

    Initialize Launcher.Updater

---
+++ tools/launcher/Launcher.Updater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Launcher.Updater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
