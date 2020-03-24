cd9c30aa9fd5fcc5218bee304dc55c618fd58c59
Hyun Seungmin <boscohyun@gmail.com>
2020-03-24 11:35:06 +0900

    Rename DodgeBuff to HitBuff

--- nekoyume/Assets/_Scripts/Lib9c/Model/Skill/SkillCategory.cs
+++ nekoyume/Assets/_Scripts/Lib9c/Model/Skill/SkillCategory.cs
@@ -6,15 +6,15 @@ namespace Nekoyume.Model.Skill
         BlowAttack,
         DoubleAttack,
         AreaAttack,
-        
+
         Heal,
-        
+
         // todo: 코드상에서 버프와 디버프를 버프로 함께 구분하고 있는데, 고도화 될 수록 디버프를 구분해주게 될 것으로 보임.
         HPBuff,
         AttackBuff,
         DefenseBuff,
         CriticalBuff,
-        DodgeBuff,
+        HitBuff,
         SpeedBuff,
     }
 }
