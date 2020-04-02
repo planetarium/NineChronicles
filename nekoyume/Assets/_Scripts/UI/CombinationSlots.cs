4ce0272a742afe6bee13605971273075557aedcc
Yang Chun Ung <qooraven@gmail.com>
2020-04-02 16:45:28 +0900

    Subscribe block index

--- nekoyume/Assets/_Scripts/UI/CombinationSlots.cs
+++ nekoyume/Assets/_Scripts/UI/CombinationSlots.cs
@@ -12,20 +12,35 @@ namespace _Scripts.UI
     public class CombinationSlots : XTweenWidget
     {
         public CombinationSlot[] slots;
+        private long _blockIndex;
+        private Dictionary<int, CombinationSlotState> _states;
 
         protected override void Awake()
         {
             base.Awake();
             CombinationSlotStatesSubject.CombinationSlotStates.Subscribe(SetSlots)
                 .AddTo(gameObject);
+            Game.instance.Agent.BlockIndexSubject.ObserveOnMainThread().Subscribe(SubscribeBlockIndex)
+                .AddTo(gameObject);
         }
 
         private void SetSlots(Dictionary<int, CombinationSlotState> states)
         {
-            var blockIndex = Game.instance.Agent.BlockIndex;
-            foreach (var pair in states.Where(pair => !(pair.Value is null)))
+            _states = states;
+            UpdateSlots();
+        }
+
+        private void SubscribeBlockIndex(long blockIndex)
+        {
+            _blockIndex = blockIndex;
+            UpdateSlots();
+        }
+
+        private void UpdateSlots()
+        {
+            foreach (var pair in _states.Where(pair => !(pair.Value is null)))
             {
-                slots[pair.Key].SetData(pair.Value, blockIndex, pair.Key);
+                slots[pair.Key].SetData(pair.Value, _blockIndex, pair.Key);
             }
         }
     }
