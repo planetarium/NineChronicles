40b913421186f78c4342345670c394bb08985f03
Seunghun Lee <waydi1@gmail.com>
2020-04-03 15:23:42 +0900

    Add delay for Dispose

--- nekoyume/Assets/_Scripts/BlockChain/Agent.cs
+++ nekoyume/Assets/_Scripts/BlockChain/Agent.cs
@@ -228,7 +228,13 @@ namespace Nekoyume.BlockChain
         {
             _cancellationTokenSource?.Cancel();
             // `_swarm`의 내부 큐가 비워진 다음 완전히 종료할 때까지 더 기다립니다.
-            Task.Run(async () => { await _swarm?.StopAsync(TimeSpan.FromMilliseconds(SwarmLinger)); })
+            Task.Run(async () =>
+                {
+                    await _swarm?.StopAsync(TimeSpan.FromMilliseconds(SwarmLinger));
+
+                    // 프리로드 중일 경우 StopAsync 에서 딜레이가 없을 수 있어서 딜레이를 추가 합니다.
+                    await Task.Delay(SwarmLinger);
+                })
                 .ContinueWith(_ =>
                 {
                     try
