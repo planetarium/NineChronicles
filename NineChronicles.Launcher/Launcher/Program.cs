573a4470de9bda431d71bf2bcde7560e3b9c5adf
moreal <dev.moreal@gmail.com>
2020-04-07 11:17:49 +0900

    Write logs to file

--- NineChronicles.Launcher/Launcher/Program.cs
+++ NineChronicles.Launcher/Launcher/Program.cs
@@ -13,6 +13,7 @@ namespace Launcher
         {
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.Console()
+                .WriteTo.File(Path.Combine(CurrentPlatform.CurrentWorkingDirectory, "launcher.log"), rollingInterval: RollingInterval.Day)
                 .MinimumLevel.Debug().CreateLogger();
 
             // Set current directory to executable path.
