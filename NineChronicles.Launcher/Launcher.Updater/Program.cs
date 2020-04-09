3c76589d272458ff3f9362dd3f0de9d148f8bd84
Swen Mun <swen@planetariumhq.com>
2020-04-09 17:03:41 +0900

    다운로드 프로그레스바 구현

--- NineChronicles.Launcher/Launcher.Updater/Program.cs
+++ NineChronicles.Launcher/Launcher.Updater/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
@@ -8,7 +8,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Launcher.Common;
 using Launcher.Common.Storage;
-
+using ShellProgressBar;
 using static Launcher.Common.RuntimePlatform.RuntimePlatform;
 
 namespace Launcher.Updater
@@ -40,7 +40,8 @@ namespace Launcher.Updater
             {
                 try
                 {
-                    await DownloadBinariesAsync(u, cts.Token);
+                    string tempPath = await DownloadBinariesAsync(u, cts.Token);
+                    ExtractBinaries(tempPath);
                 }
                 catch (OperationCanceledException)
                 {
@@ -51,51 +52,74 @@ namespace Launcher.Updater
             Process.Start(CurrentPlatform.ExecutableLauncherBinaryPath);
         }
 
-        private static async Task DownloadBinariesAsync(
+        private static async Task<string> DownloadBinariesAsync(
             string gameBinaryDownloadUri,
             CancellationToken cancellationToken
         )
         {
             var tempFilePath = Path.GetTempFileName();
-
+            if (File.Exists(tempFilePath))
+            {
+                File.Delete(tempFilePath);
+            }
             Console.Error.WriteLine($"Start download game binary from '{gameBinaryDownloadUri}' to {tempFilePath}.");
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                Process.Start(
-                    "curl",
-                    $"-o {EscapeShellArgument(tempFilePath)} {EscapeShellArgument(gameBinaryDownloadUri)}"
-                ).WaitForExit();
-            }
-            else
-            {
-                using var httpClient = new HttpClient();
-                httpClient.Timeout = Timeout.InfiniteTimeSpan;
-                var responseMessage = await httpClient.GetAsync(gameBinaryDownloadUri, cancellationToken);
-                if (File.Exists(tempFilePath))
-                {
-                    File.Delete(tempFilePath);
-                }
-                using (var fileStream = new FileStream(tempFilePath, FileMode.Create, FileAccess.Write))
-                {
-                    await responseMessage.Content.CopyToAsync(fileStream);
-                }
-            }
-            Console.Error.WriteLine($"Finished download from '{gameBinaryDownloadUri}'!");
+            
+            using var httpClient = new HttpClient();
+            httpClient.Timeout = Timeout.InfiniteTimeSpan;
+            using var dest = new FileStream(tempFilePath, FileMode.Create, FileAccess.Write);
+            using HttpResponseMessage response = await httpClient.GetAsync(
+                gameBinaryDownloadUri,
+                HttpCompletionOption.ResponseHeadersRead,
+                cancellationToken
+            );
+            using var src = await response.Content.ReadAsStreamAsync();
 
+            var buffer = new byte[8192];
+            long contentLength = response.Content.Headers.ContentLength ?? 0;
+            long totalRead = 0;
+            int bytesRead;
+
+            // FIXME: 표준 출력(stdout)으로 출력하고 있기 때문에, 커맨드라인 인자등을 추가해서 출력을 제어해야합니다.
+            using var progressBar = new ProgressBar(
+                (int)(contentLength / 1024L),
+                "Downloading...",
+                new ProgressBarOptions
+                {
+                    ProgressCharacter = '-',
+                    BackgroundCharacter = '-',
+                    ProgressBarOnBottom = false,
+                    DisplayTimeInRealTime = false,
+                }
+            );
+
+            while ((bytesRead = await src.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false)) != 0)
+            {
+                await dest.WriteAsync(buffer, 0, bytesRead, cancellationToken).ConfigureAwait(false);
+                totalRead += bytesRead;
+                progressBar.Tick((int)(totalRead / 1024L));
+                progressBar.Message = $"Downloading... ({(int)(totalRead / 1024L)}KB/{(int)(contentLength / 1024L)}KB)";
+            }
+            
+            Console.Error.WriteLine($"Finished download from '{gameBinaryDownloadUri}'!");
+            return tempFilePath;
+        }
+
+        private static void ExtractBinaries(string path)
+        {
             // TODO: implement a function to extract with file extension.
             Console.Error.WriteLine("Start to extract game binary.");
             if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
                 var process = Process.Start(
                     "tar",
-                    $"-zxvf {EscapeShellArgument(tempFilePath)} " +
+                    $"-zxvf {EscapeShellArgument(path)} " +
                     $"-C {EscapeShellArgument(CurrentPlatform.CurrentWorkingDirectory)}"
                 );
                 process.WaitForExit();
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                ZipFile.ExtractToDirectory(tempFilePath, CurrentPlatform.CurrentWorkingDirectory);
+                ZipFile.ExtractToDirectory(path, CurrentPlatform.CurrentWorkingDirectory);
             }
             else
             {
