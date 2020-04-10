1c5a1ed4fe12b365ec15061b4e62dea30f47bf0d
Hong Minhee <hong.minhee@planetariumhq.com>
2020-04-10 23:53:01 +0900

    Buy 액션에 오류 로그 추가

--- nekoyume/Assets/_Scripts/Lib9c/Action/Buy.cs
+++ nekoyume/Assets/_Scripts/Lib9c/Action/Buy.cs
@@ -110,45 +110,59 @@ namespace Nekoyume.Action
             }
 
             if (ctx.Signer.Equals(sellerAgentAddress))
-                return states;
+            {
+                return LogError(context, "Aborted as the signer is the seller.");
+            }
 
             var sw = new Stopwatch();
             sw.Start();
             var started = DateTimeOffset.UtcNow;
-            Log.Debug($"Buy exec started.");
+            Log.Debug("Buy exec started.");
 
             if (!states.TryGetAgentAvatarStates(ctx.Signer, buyerAvatarAddress, out var buyerAgentState, out var buyerAvatarState))
             {
-                return states;
+                return LogError(context, "Aborted as the avatar state of the buyer was failed to load.");
             }
             sw.Stop();
-            Log.Debug($"Buy Get Buyer AgentAvatarStates: {sw.Elapsed}");
+            Log.Debug("Buy Get Buyer AgentAvatarStates: {Elapsed}", sw.Elapsed);
             sw.Restart();
 
-            if (!buyerAvatarState.worldInformation.TryGetUnlockedWorldByStageClearedBlockIndex(
-                out var world))
-                return states;
+            if (!buyerAvatarState.worldInformation.TryGetUnlockedWorldByStageClearedBlockIndex(out var world))
+            {
+                return LogError(context, "Aborted as the WorldInformation was failed to load.");
+            }
 
             if (world.StageClearedId < GameConfig.RequireClearedStageLevel.ActionsInShop)
             {
                 // 스테이지 클리어 부족 에러.
-                return states;
+                return LogError(
+                    context,
+                    "Aborted as the signer is not cleared the minimum stage level required to use shop yet: {ClearedLevel} < {RequiredLevel}.",
+                    world.StageClearedId,
+                    GameConfig.RequireClearedStageLevel.ActionsInShop
+                );
             }
 
             if (!states.TryGetState(ShopState.Address, out Bencodex.Types.Dictionary d))
             {
-                return states;
+                return LogError(context, "Aborted as the shop state was failed to load.");
             }
+
             var shopState = new ShopState(d);
             sw.Stop();
-            Log.Debug($"Buy Get ShopState: {sw.Elapsed}");
+            Log.Debug("Buy Get ShopState: {Elapsed}", sw.Elapsed);
             sw.Restart();
 
-            Log.Debug($"Execute Buy. buyer : `{buyerAvatarAddress}` seller: `{sellerAvatarAddress}`");
+            Log.Debug("Execute Buy; buyer: {Buyer} seller: {Seller}", buyerAvatarAddress, sellerAvatarAddress);
             // 상점에서 구매할 아이템을 찾는다.
             if (!shopState.TryGet(sellerAgentAddress, productId, out var outPair))
             {
-                return states;
+                return LogError(
+                    context,
+                    "Aborted as the shop item ({ProductId}) was failed to get from the seller agent ({SellerAgent}).",
+                    productId,
+                    sellerAgentAddress
+                );
             }
             sw.Stop();
             Log.Debug($"Buy Get Item: {sw.Elapsed}");
@@ -156,7 +170,12 @@ namespace Nekoyume.Action
 
             if (!states.TryGetAgentAvatarStates(sellerAgentAddress, sellerAvatarAddress, out var sellerAgentState, out var sellerAvatarState))
             {
-                return states;
+                return LogError(
+                    context,
+                    "Aborted as the seller agent/avatar was filed to load from {SellerAgent}/{SellerAvatar}.",
+                    sellerAgentAddress,
+                    sellerAvatarAddress
+                );
             }
             sw.Stop();
             Log.Debug($"Buy Get Seller AgentAvatarStates: {sw.Elapsed}");
@@ -165,7 +184,13 @@ namespace Nekoyume.Action
             // 돈은 있냐?
             if (buyerAgentState.gold < outPair.Value.Price)
             {
-                return states;
+                return LogError(
+                    context,
+                    "Aborted as the buyer ({Buyer}) has no sufficient gold: {BuyerBalance} < {ItemPrice}",
+                    ctx.Signer,
+                    buyerAgentState.gold,
+                    outPair.Value.Price
+                );
             }
 
             var taxedPrice = decimal.Round(outPair.Value.Price * 0.92m);
@@ -178,7 +203,12 @@ namespace Nekoyume.Action
             // 상점에서 구매할 아이템을 제거한다.
             if (!shopState.Unregister(sellerAgentAddress, outPair.Value))
             {
-                return states;
+                return LogError(
+                    context,
+                    "Aborted as the item ({@ProductId}) was failed to unregister from seller ({Seller})'s inventory.",
+                    outPair.Value.ProductId,
+                    sellerAgentAddress
+                );
             }
 
             // 구매자, 판매자에게 결과 메일 전송
@@ -219,19 +249,19 @@ namespace Nekoyume.Action
 
             states = states.SetState(sellerAvatarAddress, sellerAvatarState.Serialize());
             sw.Stop();
-            Log.Debug($"Buy Set Seller AvatarState: {sw.Elapsed}");
+            Log.Debug("Buy Set Seller AvatarState: {Elapsed}", sw.Elapsed);
             sw.Restart();
 
             states = states.SetState(buyerAvatarAddress, buyerAvatarState.Serialize());
             sw.Stop();
-            Log.Debug($"Buy Set Buyer AvatarState: {sw.Elapsed}");
+            Log.Debug("Buy Set Buyer AvatarState: {Elapsed}", sw.Elapsed);
             sw.Restart();
 
             states = states.SetState(ShopState.Address, shopState.Serialize());
             sw.Stop();
             var ended = DateTimeOffset.UtcNow;
-            Log.Debug($"Buy Set ShopState: {sw.Elapsed}");
-            Log.Debug($"Buy Total Executed Time: {ended - started}");
+            Log.Debug("Buy Set ShopState: {Elapsed}", sw.Elapsed);
+            Log.Debug("Buy Total Executed Time: {Elapsed}", ended - started);
 
             return states
                 .SetState(ctx.Signer, buyerAgentState.Serialize())
