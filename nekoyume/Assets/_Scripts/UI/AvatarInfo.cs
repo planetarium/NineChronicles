7539820ee2642aaa3e9a1b61e53c2e324a4aec39
unengine <unengine@gmail.com>
2020-05-14 20:02:47 +0900

    Implement displaying character stats.

--- nekoyume/Assets/_Scripts/UI/AvatarInfo.cs
+++ nekoyume/Assets/_Scripts/UI/AvatarInfo.cs
@@ -1,8 +1,31 @@
+using Nekoyume.UI.Module;
+using UnityEngine;
 
 namespace Nekoyume.UI
 {
     public class AvatarInfo : Widget
     {
+        [SerializeField]
+        private DetailedStatView[] statViews;
 
+        #region Override
+
+        public override void Show(bool ignoreShowAnimation = false)
+        {
+            base.Show(ignoreShowAnimation);
+
+            var player = Game.Game.instance.Stage.selectedPlayer.Model;
+
+            var statTuples = player.Stats.GetBaseAndAdditionalStats();
+            var idx = 0;
+            foreach (var (statType, value, additionalValue) in statTuples)
+            {
+                var info = statViews[idx];
+                info.Show(statType, value, additionalValue);
+                ++idx;
+            }
+        }
+
+        #endregion
     }
 }
