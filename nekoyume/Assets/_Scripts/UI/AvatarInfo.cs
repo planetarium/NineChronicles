4fc2620aecf0bd28210ea72f4760a026f505bd50
unengine <unengine@gmail.com>
2020-05-15 17:15:49 +0900

    Implement displaying cp, nickname

--- nekoyume/Assets/_Scripts/UI/AvatarInfo.cs
+++ nekoyume/Assets/_Scripts/UI/AvatarInfo.cs
@@ -1,4 +1,6 @@
+using Nekoyume.Battle;
 using Nekoyume.UI.Module;
+using TMPro;
 using UnityEngine;
 
 namespace Nekoyume.UI
@@ -6,7 +8,16 @@ namespace Nekoyume.UI
     public class AvatarInfo : Widget
     {
         [SerializeField]
+        private TextMeshProUGUI nicknameText;
+
+        [SerializeField]
+        private TextMeshProUGUI cpText;
+
+        [SerializeField]
         private DetailedStatView[] statViews;
+
+
+        private const string nicknameTextFormat = "<color=#B38271>Lv.{0}</color=> {1}";
 
         #region Override
 
@@ -14,7 +25,8 @@ namespace Nekoyume.UI
         {
             base.Show(ignoreShowAnimation);
 
-            var player = Game.Game.instance.Stage.selectedPlayer.Model;
+            var gameInstance = Game.Game.instance;
+            var player = gameInstance.Stage.selectedPlayer.Model;
 
             var statTuples = player.Stats.GetBaseAndAdditionalStats();
             var idx = 0;
@@ -24,6 +36,15 @@ namespace Nekoyume.UI
                 info.Show(statType, value, additionalValue);
                 ++idx;
             }
+
+            var currentAvatar = gameInstance.States.CurrentAvatarState;
+
+            nicknameText.text = string.Format(nicknameTextFormat,
+                currentAvatar.level,
+                currentAvatar.NameWithHash);
+
+            cpText.text = CPHelper.GetCP(currentAvatar, gameInstance.TableSheets.CharacterSheet).ToString();
+
         }
 
         #endregion
