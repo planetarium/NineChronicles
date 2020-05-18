302cbb13f0499928356df73adb93e83da63e27ad
Hyun Seungmin <boscohyun@gmail.com>
2020-05-18 11:20:07 +0900

    Add LocalizedText and use this in AvatarTooltip

---
+++ nekoyume/Assets/_Scripts/UI/Module/LocalizedText.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.SimpleLocalization;
+using TMPro;
+using UnityEngine;
+
+namespace Nekoyume.UI.Module
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class LocalizedText : MonoBehaviour
+    {
+        [SerializeField]
+        private string localizationKey = null;
+
+        private TextMeshProUGUI _text;
+        private TextMeshProUGUI Text
+        {
+            get
+            {
+                if (!_text)
+                {
+                    _text = GetComponent<TextMeshProUGUI>();
+                }
+
+                return _text;
+            }
+        }
+
+        private void Awake()
+        {
+            Text.text = LocalizationManager.Localize(localizationKey);
+        }
+    }
+}
