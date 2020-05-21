a07b3eadfb4d36d886a947cd1e41d0e9a6104933
moreal <dev.moreal@gmail.com>
2020-05-21 17:15:42 +0900

    Add test for Launcher.Common

--- NineChronicles.Launcher/Launcher.Common.Tests/UtilsTest.cs
+++ NineChronicles.Launcher/Launcher.Common.Tests/UtilsTest.cs
@@ -1,6 +1,16 @@
+using Xunit;
+
 namespace Launcher.Common.Tests
 {
     public class UtilsTest
     {
+        [Theory]
+        [InlineData("foo", "\"foo\"")]
+        [InlineData("\"bar\"", "\"\\\"bar\\\"\"")]
+        [InlineData("\\\"baz\\\"", "\"\\\\\\\"baz\\\\\\\"\"")]
+        public void EscapeShellArgument(string original, string expected)
+        {
+            Assert.Equal(expected,Utils.EscapeShellArgument(original));
+        }
     }
 }
