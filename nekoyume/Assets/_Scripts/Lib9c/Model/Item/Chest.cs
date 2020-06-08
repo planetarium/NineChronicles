cf47d7de5541edf4a0e722385579866c359f7e96
Yang Chun Ung <qooraven@gmail.com>
2020-06-08 11:10:29 +0900

    Introduce Chest item

---
+++ nekoyume/Assets/_Scripts/Lib9c/Model/Item/Chest.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bencodex.Types;
+using Nekoyume.Model.State;
+using Nekoyume.TableData;
+
+namespace Nekoyume.Model.Item
+{
+    [Serializable]
+    public class Chest : Material
+    {
+        public List<RedeemRewardSheet.RewardInfo> Rewards { get; }
+        public Chest(MaterialItemSheet.Row data, List<RedeemRewardSheet.RewardInfo> rewards) : base(data)
+        {
+            Rewards = rewards ?? new List<RedeemRewardSheet.RewardInfo>();
+        }
+
+        public Chest(Dictionary serialized) : base(serialized)
+        {
+            if (serialized.TryGetValue((Text) "rewards", out var rewards))
+            {
+                Rewards = rewards.ToList(i => new RedeemRewardSheet.RewardInfo((Dictionary) i));
+            }
+        }
+
+        public override IValue Serialize() =>
+            new Dictionary(new Dictionary<IKey, IValue>()
+            {
+                [(Text) "rewards"] = new List(Rewards.Select(r => r.Serialize())),
+            }.Union((Dictionary) base.Serialize()));
+
+        protected bool Equals(Chest other)
+        {
+            return base.Equals(other) && Rewards.SequenceEqual(other.Rewards);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Chest) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (base.GetHashCode() * 397) ^ (Rewards != null ? Rewards.GetHashCode() : 0);
+            }
+        }
+    }
+}
