774f3b2350fd2d8b4ff8c7e8568bb2ca8c3764f4
moreal <dev.moreal@gmail.com>
2020-06-11 17:48:05 +0900

    Use random path keystore instead clear

--- NineChronicles.Standalone.Tests/GraphTypes/GraphQLTestBase.cs
+++ NineChronicles.Standalone.Tests/GraphTypes/GraphQLTestBase.cs
@@ -27,13 +27,8 @@ namespace NineChronicles.Standalone.Tests.GraphTypes
             var blockChain =
                 new BlockChain<PolymorphicAction<ActionBase>>(blockPolicy, store, genesisBlock);
 
-            var tempKeyStorePath = Path.GetTempPath();
+            var tempKeyStorePath = Path.Join(Path.GetTempPath(), Path.GetRandomFileName());
             var keyStore = new Web3KeyStore(tempKeyStorePath);
-            // Path.GetTempPath()가 실행할 때 마다 새로운 경로를 리턴하지 않을 경우를 위해 생성자에서 초기화해줍니다.
-            foreach (var keyId in keyStore.ListIds().ToImmutableList())
-            {
-                keyStore.Remove(keyId);
-            }
 
             StandaloneContextFx = new StandaloneContext
             {
