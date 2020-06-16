ec7f05bf738ebdd209d56bc5834d2202f126d541
Hyun Seungmin <boscohyun@gmail.com>
2020-06-16 18:40:59 +0900

    Add GuidedQuest to Battle

--- nekoyume/Assets/_Scripts/UI/Battle.cs
+++ nekoyume/Assets/_Scripts/UI/Battle.cs
@@ -9,6 +9,10 @@ namespace Nekoyume.UI
     public class Battle : Widget, IToggleListener
     {
         public StageTitle stageTitle;
+
+        [SerializeField]
+        private GuidedQuest guidedQuest = null;
+
         public BossStatus bossStatus;
         public ToggleableButton repeatButton;
         public BossStatus enemyPlayerStatus;
@@ -28,6 +32,7 @@ namespace Nekoyume.UI
         {
             base.Show();
             stageTitle.Show(stageId);
+            guidedQuest.Show(States.Instance.CurrentAvatarState.questList);
             stageProgressBar.Show();
             bossStatus.Close();
             enemyPlayerStatus.Close();
