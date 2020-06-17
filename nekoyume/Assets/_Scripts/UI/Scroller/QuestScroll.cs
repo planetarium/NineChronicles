f890d21b1e8877972ad58fbf4cdcfb3f1e654d31
Hyun Seungmin <boscohyun@gmail.com>
2020-06-17 17:14:56 +0900

    Add forcedScrollSensitivity to QuestScroll to improve scrolling

--- nekoyume/Assets/_Scripts/UI/Scroller/QuestScroll.cs
+++ nekoyume/Assets/_Scripts/UI/Scroller/QuestScroll.cs
@@ -20,6 +20,14 @@ namespace Nekoyume.UI.Scroller
         [SerializeField]
         private float cellSize;
 
+        /// <summary>
+        /// `FancyScrollRect`는 `Scroller.ScrollSensitivity`를 강제로 바꿔버립니다.
+        /// 그렇기 때문에 사용자가 설정한 스크롤 감도가 무시됩니다.
+        /// 이를 강제로 할당하기 윈해서 각 스크롤에서 원하는 감도를 받습니다.
+        /// </summary>
+        [SerializeField]
+        private float forcedScrollSensitivity = 20f;
+
         protected override GameObject CellPrefab => cellPrefab;
 
         protected override float CellSize => cellSize;
@@ -40,6 +48,24 @@ namespace Nekoyume.UI.Scroller
             }
 
             JumpTo(0);
+        }
+
+        protected override void Refresh()
+        {
+            base.Refresh();
+            Scroller.ScrollSensitivity = forcedScrollSensitivity;
+        }
+
+        protected override void Relayout()
+        {
+            base.Relayout();
+            Scroller.ScrollSensitivity = forcedScrollSensitivity;
+        }
+
+        protected override void UpdateContents(IList<QuestModel> items)
+        {
+            base.UpdateContents(items);
+            Scroller.ScrollSensitivity = forcedScrollSensitivity;
         }
     }
 }
