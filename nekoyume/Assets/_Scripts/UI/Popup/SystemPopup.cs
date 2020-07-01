32d072d64c59c6957786d9964ba48c9e0d6b481c
Yang Chun Ung <qooraven@gmail.com>
2020-07-01 13:31:49 +0900

    Add stage exit event

--- nekoyume/Assets/_Scripts/UI/Popup/SystemPopup.cs
+++ nekoyume/Assets/_Scripts/UI/Popup/SystemPopup.cs
@@ -11,6 +11,14 @@ namespace Nekoyume.UI
         public override void Show(string title, string content, string labelOK = "UI_OK", bool localize = true)
         {
             Mixpanel.Track("Unity/SystemPopupImpression");
+            if (Game.Game.instance.Stage.IsInStage)
+            {
+                var props = new Value
+                {
+                    ["StageId"] = Game.Game.instance.Stage.stageId,
+                };
+                Mixpanel.Track("Unity/Stage Exit Crash", props);
+            }
             base.Show(title, content, labelOK, localize);
 #if UNITY_EDITOR
             CloseCallback = UnityEditor.EditorApplication.ExitPlaymode;
