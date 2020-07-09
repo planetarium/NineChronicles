fccfc5ccae4a4019597ba357c1ced2066a1ba463
longfin <longfinfunnel@gmail.com>
2020-07-09 21:24:53 +0900

    유니티 에디터에서는 Mixpanel 초기화 안하게끔

--- nekoyume/Assets/_Scripts/Game/Game.cs
+++ nekoyume/Assets/_Scripts/Game/Game.cs
@@ -60,10 +60,12 @@ namespace Nekoyume.Game
         protected override void Awake()
         {
             // FIXME 이후 사용자가 원치 않으면 정보를 보내지 않게끔 해야 합니다.
+            #if !UNITY_EDITOR
             Mixpanel.SetToken("80a1e14b57d050536185c7459d45195a");
             Mixpanel.Identify(NetworkInterface.GetAllNetworkInterfaces().First().GetPhysicalAddress().ToString());
             Mixpanel.Init();
             Mixpanel.Track("Unity/Started");
+            #endif
 
             Application.targetFrameRate = 60;
             Application.SetStackTraceLogType(LogType.Log, StackTraceLogType.None);
