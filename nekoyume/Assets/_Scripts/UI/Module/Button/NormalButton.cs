9689ce070e11b3d8f3be694cb77f3ee0da4972ec
Yang Chun Ung <qooraven@gmail.com>
2020-08-03 19:06:30 +0900

    Merge pull request #2628 from boscohyun/bug-fix/reorder-bottom-menu-buttons-in-world-map
    Bug fix/reorder bottom menu buttons in world map

--- nekoyume/Assets/_Scripts/UI/Module/Button/NormalButton.cs
+++ nekoyume/Assets/_Scripts/UI/Module/Button/NormalButton.cs
@@ -1,3 +1,4 @@
+using Nekoyume.EnumType;
 using Nekoyume.Game.Controller;
 using Nekoyume.L10n;
 using TMPro;
@@ -22,13 +23,39 @@ namespace Nekoyume.UI.Module
         [SerializeField]
         private Canvas sortingGroup = null;
 
+        private int _originalSortingOrderOffset;
+
         public readonly Subject<NormalButton> OnClick = new Subject<NormalButton>();
 
         #region Mono
 
         protected virtual void Awake()
         {
-            text.text = L10nManager.Localize(string.IsNullOrEmpty(localizationKey) ? "null" : localizationKey);
+            if (sortingGroup)
+            {
+                var widget = GetComponentInParent<Widget>();
+                if (widget is BottomMenu)
+                {
+                    _originalSortingOrderOffset = 0;
+                    sortingGroup.sortingOrder =
+                        MainCanvas.instance.GetLayer(widget.WidgetType).root.sortingOrder;
+                }
+                else if (widget)
+                {
+                    var layerSortingOrder =
+                        MainCanvas.instance.GetLayer(widget.WidgetType).root.sortingOrder;
+                    _originalSortingOrderOffset = sortingGroup.sortingOrder - layerSortingOrder;
+                }
+                else
+                {
+                    _originalSortingOrderOffset = sortingGroup.sortingOrder;
+                }
+            }
+
+            text.text =
+                L10nManager.Localize(string.IsNullOrEmpty(localizationKey)
+                    ? "null"
+                    : localizationKey);
             button.OnClickAsObservable().Subscribe(_ =>
             {
                 AudioController.PlayClick();
@@ -52,12 +79,34 @@ namespace Nekoyume.UI.Module
 
         public void SetSortOrderToTop()
         {
-            sortingGroup.sortingOrder = 100;
+            if (!sortingGroup)
+            {
+                return;
+            }
+
+            var systemInfoSortingOrder =
+                MainCanvas.instance.GetLayer(WidgetType.SystemInfo).root.sortingOrder;
+            sortingGroup.sortingOrder = systemInfoSortingOrder;
         }
 
         public void SetSortOrderToNormal()
         {
-            sortingGroup.sortingOrder = 0;
+            if (!sortingGroup)
+            {
+                return;
+            }
+
+            var widget = GetComponentInParent<Widget>();
+            if (widget)
+            {
+                var layerSortingOrder =
+                    MainCanvas.instance.GetLayer(widget.WidgetType).root.sortingOrder;
+                sortingGroup.sortingOrder = layerSortingOrder + _originalSortingOrderOffset;
+            }
+            else
+            {
+                sortingGroup.sortingOrder = _originalSortingOrderOffset;
+            }
         }
     }
 }
--- nekoyume/Assets/_Scripts/UI/TooltipWidget.cs
+++ nekoyume/Assets/_Scripts/UI/TooltipWidget.cs
@@ -17,7 +17,7 @@ namespace Nekoyume.UI
 
         private readonly List<IDisposable> _disposablesForModel = new List<IDisposable>();
 
-        protected override WidgetType WidgetType => WidgetType.Tooltip;
+        public override WidgetType WidgetType => WidgetType.Tooltip;
 
         public T Model { get; private set; }
 
