b7b14671a87c934084725f245dfab0e6ef88120f
moreal <dev.moreal@gmail.com>
2020-08-05 14:46:28 +0900

    GameConfigState null 인 경우 에러 로그 추가

--- nekoyume/Assets/_Scripts/Lib9c/Action/DailyReward.cs
+++ nekoyume/Assets/_Scripts/Lib9c/Action/DailyReward.cs
@@ -31,6 +31,7 @@ namespace Nekoyume.Action
             var gameConfigState = states.GetGameConfigState();
             if (gameConfigState is null)
             {
+                LogError(context, "Aborted as the game config was failed to load.");
                 return states;
             }
 
