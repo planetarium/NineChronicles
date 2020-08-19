7dc36973b4a44d3680a2e934f573c709df2e3fbf
Hyun Seungmin <boscohyun@gmail.com>
2020-08-19 17:56:48 +0900

    Add more unit test for ShopState

--- .Lib9c.Tests/Model/State/ShopStateTest.cs
+++ .Lib9c.Tests/Model/State/ShopStateTest.cs
@@ -1,6 +1,7 @@
 namespace Lib9c.Tests.Model.State
 {
     using System;
+    using Bencodex.Types;
     using Libplanet;
     using Libplanet.Crypto;
     using Nekoyume.Model.Item;
@@ -10,6 +11,46 @@ namespace Lib9c.Tests.Model.State
 
     public class ShopStateTest
     {
+        [Fact]
+        public void Serialization()
+        {
+            var shopState = new ShopState();
+            var agentAddress = new PrivateKey().ToAddress();
+            var avatarAddress = new PrivateKey().ToAddress();
+            var productId = Guid.NewGuid();
+            var weaponRow = new EquipmentItemSheet.Row();
+            weaponRow.Set(new[]
+            {
+                "10100000", "Weapon", "0", "Normal", "0", "ATK", "1", "2", "10100000",
+            });
+            var itemUsable = new Weapon(
+                weaponRow,
+                Guid.NewGuid(),
+                0);
+            var shopItem = new ShopItem(
+                avatarAddress,
+                productId,
+                itemUsable,
+                0
+            );
+
+            shopState.Register(agentAddress, shopItem);
+            var serialized = (Dictionary)shopState.Serialize();
+            shopState = new ShopState(serialized);
+
+            Assert.Equal(1, shopState.AgentProducts.Count);
+            Assert.True(shopState.AgentProducts.ContainsKey(agentAddress));
+            Assert.Single(shopState.AgentProducts[agentAddress]);
+            Assert.Contains(productId, shopState.AgentProducts[agentAddress]);
+            Assert.Equal(1, shopState.Products.Count);
+            Assert.Contains(productId, shopState.Products);
+            Assert.Equal(shopItem, shopState.Products[productId]);
+            Assert.Equal(1, shopState.ItemSubTypeProducts.Count);
+            Assert.Contains(ItemSubType.Weapon, shopState.ItemSubTypeProducts);
+            Assert.Single(shopState.ItemSubTypeProducts[ItemSubType.Weapon]);
+            Assert.Contains(productId, shopState.ItemSubTypeProducts[ItemSubType.Weapon]);
+        }
+
         [Fact]
         public void Register()
         {
