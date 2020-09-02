8cbacf8f3a644d3352c516557bb479435d7c993a
Hyun Seungmin <boscohyun@gmail.com>
2020-09-02 19:20:52 +0900

    Refactor IToggleGroup

--- nekoyume/Assets/_Scripts/UI/Module/Toggle/ToggleGroup.cs
+++ nekoyume/Assets/_Scripts/UI/Module/Toggle/ToggleGroup.cs
@@ -5,23 +5,50 @@ using UnityEngine;
 
 namespace Nekoyume.UI.Module
 {
-    public class ToggleGroup : IToggleGroup, IToggleListener
+    public class ToggleGroup : IToggleGroup
     {
         private readonly Dictionary<int, IToggleable> _idAndToggleablePairs = new Dictionary<int, IToggleable>();
-        
+
         public readonly Subject<IToggleable> OnToggledOn = new Subject<IToggleable>();
         public readonly Subject<IToggleable> OnToggledOff = new Subject<IToggleable>();
 
         public IEnumerable<IToggleable> Toggleables => _idAndToggleablePairs.Values;
 
         #region IToggleGroup
-        
+
+        public void OnToggle(IToggleable toggleable)
+        {
+            var id = toggleable.GetInstanceID();
+            foreach (var pair in _idAndToggleablePairs.Where(pair => pair.Key != id && pair.Value.IsToggledOn))
+            {
+                pair.Value.SetToggledOff();
+                OnToggledOff.OnNext(pair.Value);
+            }
+
+            if (toggleable.IsToggledOn)
+            {
+                toggleable.SetToggledOff();
+                OnToggledOff.OnNext(toggleable);
+            }
+            else
+            {
+                toggleable.SetToggledOn();
+                OnToggledOn.OnNext(toggleable);
+            }
+        }
+
+        public void RequestToggledOff(IToggleable toggleable)
+        {
+            toggleable.SetToggledOff();
+            OnToggledOff.OnNext(toggleable);
+        }
+
         public void RegisterToggleable(IToggleable toggleable)
         {
             var id = toggleable.GetInstanceID();
             if (_idAndToggleablePairs.ContainsKey(id))
                 return;
-            
+
             _idAndToggleablePairs.Add(id, toggleable);
             toggleable.SetToggleListener(this);
         }
@@ -52,37 +79,6 @@ namespace Nekoyume.UI.Module
             {
                 _idAndToggleablePairs[pair.Key].SetToggledOff();
             }
-        }
-        
-        #endregion
-        
-        #region IToggleListener
-
-        public void OnToggle(IToggleable toggleable)
-        {
-            var id = toggleable.GetInstanceID();
-            foreach (var pair in _idAndToggleablePairs.Where(pair => pair.Key != id && pair.Value.IsToggledOn))
-            {
-                pair.Value.SetToggledOff();
-                OnToggledOff.OnNext(pair.Value);
-            }
-
-            if (toggleable.IsToggledOn)
-            {
-                toggleable.SetToggledOff();
-                OnToggledOff.OnNext(toggleable);
-            }
-            else
-            {
-                toggleable.SetToggledOn();
-                OnToggledOn.OnNext(toggleable);
-            }
-        }
-
-        public void RequestToggledOff(IToggleable toggleable)
-        {
-            toggleable.SetToggledOff();
-            OnToggledOff.OnNext(toggleable);
         }
 
         #endregion
