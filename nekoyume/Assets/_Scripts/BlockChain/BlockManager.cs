0f6f329c00f7a06dbfc284ea2c50d569cc102863
Lee Suho <riemannulus@hitagi.moe>
2020-09-09 18:12:47 +0900

    Remove useless method

--- nekoyume/Assets/_Scripts/BlockChain/BlockManager.cs
+++ nekoyume/Assets/_Scripts/BlockChain/BlockManager.cs
@@ -7,6 +7,7 @@ using Bencodex.Types;
 using Libplanet;
 using Libplanet.Action;
 using Libplanet.Blocks;
+using Nekoyume;
 using Nekoyume.Action;
 using UnityEngine;
 
@@ -58,52 +59,10 @@ namespace Nekoyume.BlockChain
               var tableSheets = Game.Game.GetTableCsvAssets();
               string goldDistributionCsvPath = Path.Combine(Application.streamingAssetsPath, "GoldDistribution.csv");
               GoldDistribution[] goldDistributions = GoldDistribution.LoadInDescendingEndBlockOrder(goldDistributionCsvPath);
-              return BlockHelper.MineGenesisBlock(tableSheets, goldDistributions);
-          }
-
-          /// <summary>
-          /// 블럭의 첫번째 액션의 <see cref="PolymorphicAction{T}.InnerAction"/> 내용을 기준으로 블록을 비교합니다.
-          /// </summary>
-          /// <param name="blockA">블록.</param>
-          /// <param name="blockB">블록.</param>
-          /// <returns>블록이 다르다면 true, 같다면 false를 반환합니다.</returns>
-          public static bool CompareGenesisBlocks(Block<PolymorphicAction<ActionBase>> blockA,
-              Block<PolymorphicAction<ActionBase>> blockB)
-          {
-              return blockA == null || blockB == null ||
-                     !GetHashOfFirstAction(blockA).Equals(GetHashOfFirstAction(blockB));
-          }
-
-          /// <summary>
-          /// 제네시스 블록에 포함되어 있는 <see cref="InitializeStates"/> 액션의
-          /// <see cref="InitializeStates.PlainValue"/>로 부터 <see cref="HashDigest{T}"/> 값을 계산합니다.
-          /// </summary>
-          /// <param name="block"><see cref="InitializeStates"/> 액션만을 포함하고 있는 제네시스 블록.</param>
-          /// <returns><see cref="InitializeStates"/> 액션의 <see cref="InitializeStates.PlainValue"/>
-          /// 중 <see cref="GameAction.Id"/>를 제외하고 계산한 <see cref="HashDigest{T}"/>.</returns>
-          private static HashDigest<SHA256> GetHashOfFirstAction(Block<PolymorphicAction<ActionBase>> block)
-          {
-              var initializeStatesAction = (InitializeStates)block.Transactions.First().Actions[0].InnerAction;
-              Bencodex.Types.Dictionary plainValue = (Bencodex.Types.Dictionary) initializeStatesAction.PlainValue;
-              plainValue = (Bencodex.Types.Dictionary) plainValue.Remove((Text)"id");  // except GameAction.Id.
-              var bytes = plainValue.EncodeIntoChunks().SelectMany(b => b).ToArray();
-              return Hashcash.Hash(bytes);
+              return Nekoyume.BlockHelper.MineGenesisBlock(tableSheets, goldDistributions);
           }
 
           public static string BlockPath(string filename) => Path.Combine(Application.streamingAssetsPath, filename);
 
-          // Copied from Planetarium.Nekoyume.LibplanetEditor.
-          public static void DeleteAllEditor(string path)
-          {
-              DeleteAll(path);
-          }
-
-          private static void DeleteAll(string path)
-          {
-              if (Directory.Exists(path))
-              {
-                  Directory.Delete(path, recursive: true);
-              }
-          }
     }
 }
