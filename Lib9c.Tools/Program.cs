8e33b90bb9000b3398775ce7b98981d326db06c7
Lee Suho <riemannulus@hitagi.moe>
2020-09-14 11:01:38 +0900

    Remove useless method

--- Lib9c.Tools/Program.cs
+++ Lib9c.Tools/Program.cs
@@ -4,11 +4,10 @@ using Lib9c.Tools.SubCommend;
 
 namespace Lib9c.Tools
 {
+    // FIXME more detailed description need 
     [HasSubCommands(typeof(Create), Description = "Create commands")]
     class Program
     {
         static void Main(string[] args) => CoconaLiteApp.Run<Program>(args);
-
-        public void Info() => Console.WriteLine("Show information");
     }
 }
