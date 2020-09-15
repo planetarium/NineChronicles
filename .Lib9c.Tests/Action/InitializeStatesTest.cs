fb52096f9d3f8674555d24053774f4bfbeee94f9
Lee Suho <riemannulus@hitagi.moe>
2020-09-15 11:36:45 +0900

    Activate admin address if `isActivateAdminAddress` is true

--- .Lib9c.Tests/Action/InitializeStatesTest.cs
+++ .Lib9c.Tests/Action/InitializeStatesTest.cs
@@ -1,6 +1,7 @@
 namespace Lib9c.Tests.Action
 {
     using System.Collections.Generic;
+    using System.Collections.Immutable;
     using System.Linq;
     using Bencodex.Types;
     using Libplanet;
@@ -132,6 +133,49 @@ namespace Lib9c.Tests.Action
             Assert.Equal(50, fetchedState.Interval);
             Assert.Equal(1000, fetchedState.ValidUntil);
             Assert.Equal(new[] { default(Address) }, fetchedState.Miners);
+        }
+
+        [Fact]
+        public void ExecuteWithActivateAdminKey()
+        {
+            var gameConfigState = new GameConfigState(_sheets[nameof(GameConfigSheet)]);
+            var redeemCodeListSheet = new RedeemCodeListSheet();
+            redeemCodeListSheet.Set(_sheets[nameof(RedeemCodeListSheet)]);
+            var goldDistributionCsvPath = GoldDistributionTest.CreateFixtureCsvFile();
+            var goldDistributions = GoldDistribution.LoadInDescendingEndBlockOrder(goldDistributionCsvPath);
+            var minterKey = new PrivateKey();
+            var ncg = new Currency("NCG", 2, minterKey.ToAddress());
+            var nonce = new byte[] { 0x00, 0x01, 0x02, 0x03 };
+            var privateKey = new PrivateKey();
+            (ActivationKey activationKey, PendingActivationState pendingActivation) =
+                ActivationKey.Create(privateKey, nonce);
+            var adminAddress = new Address("F9A15F870701268Bd7bBeA6502eB15F4997f32f9");
+
+            var action = new InitializeStates
+            {
+                RankingState = new RankingState(),
+                ShopState = new ShopState(),
+                TableSheets = _sheets,
+                GameConfigState = gameConfigState,
+                RedeemCodeState = new RedeemCodeState(redeemCodeListSheet),
+                AdminAddressState = new AdminState(adminAddress, 1500000),
+                ActivatedAccountsState = new ActivatedAccountsState(ImmutableHashSet<Address>.Empty.Add(adminAddress)),
+                GoldCurrencyState = new GoldCurrencyState(ncg),
+                GoldDistributions = goldDistributions,
+                PendingActivationStates = new[] { pendingActivation },
+            };
+
+            var genesisState = action.Execute(new ActionContext()
+            {
+                BlockIndex = 0,
+                Miner = default,
+                PreviousStates = new State(),
+            });
+
+            var fetchedState = new ActivatedAccountsState(
+                (Dictionary)genesisState.GetState(Addresses.ActivatedAccount));
+
+            Assert.Contains(adminAddress, fetchedState.Accounts);
         }
     }
 }
