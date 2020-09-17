63bdcd59ef0853c1e5b3f5d2dbc287a032120610
Hyun Seungmin <boscohyun@gmail.com>
2020-09-17 18:42:41 +0900

    Remove useless usings

--- nekoyume/Assets/Tests/EditMode/InventoryTest.cs
+++ nekoyume/Assets/Tests/EditMode/InventoryTest.cs
@@ -2,9 +2,7 @@ using System;
 using Nekoyume.Game;
 using Nekoyume.Helper;
 using Nekoyume.Model.Item;
-using Nekoyume.TableData;
 using NUnit.Framework;
-using UnityEngine;
 using Inventory = Nekoyume.UI.Model.Inventory;
 
 namespace Tests.EditMode
@@ -29,18 +27,18 @@ namespace Tests.EditMode
         public void AddItem()
         {
             var inventory = new Inventory();
-            
+
             var row = _tableSheets.EquipmentItemSheet.First;
             Assert.NotNull(row);
 
             var equipment = ItemFactory.CreateItemUsable(row, Guid.NewGuid(), -100);
             inventory.AddItem(equipment);
             Assert.AreEqual(1, inventory.Equipments.Count);
-            
+
             equipment = ItemFactory.CreateItemUsable(row, Guid.NewGuid(), 100);
             inventory.AddItem(equipment);
             Assert.AreEqual(1, inventory.Equipments.Count);
-            
+
             equipment = ItemFactory.CreateItemUsable(row, Guid.NewGuid(), -100);
             inventory.AddItem(equipment);
             Assert.AreEqual(2, inventory.Equipments.Count);
