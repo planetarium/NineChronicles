d9012772601f83dd152b3a242b45deb739fd2f05
Yang Chun Ung <qooraven@gmail.com>
2020-09-28 13:25:55 +0900

    Update Lib9c/Action/CombinationEquipment.cs
    Co-authored-by: Swen Mun <longfinfunnel@gmail.com>

--- Lib9c/Action/CombinationEquipment.cs
+++ Lib9c/Action/CombinationEquipment.cs
@@ -301,7 +301,7 @@ namespace Nekoyume.Action
             var optionSelector = new WeightedSelector<EquipmentItemOptionSheet.Row>(random);
             var optionIds = new HashSet<int>();
 
-            //Skip options sort because sorting in WeightedSelector.Select();
+            // Skip sort subRecipe.Options because it had been already sorted in WeightedSelector.Select();
             foreach (var optionInfo in subRecipe.Options)
             {
                 if (!optionSheet.TryGetValue(optionInfo.Id, out var optionRow))
