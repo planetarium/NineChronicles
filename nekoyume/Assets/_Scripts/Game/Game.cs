37bbdd88763c7776208847b66e94315476d2d891
Lee Suho <riemannulus@hitagi.moe>
2020-09-29 12:08:48 +0900

    Fix language option

--- nekoyume/Assets/_Scripts/Game/Game.cs
+++ nekoyume/Assets/_Scripts/Game/Game.cs
@@ -106,14 +106,14 @@ namespace Nekoyume.Game
 #if UNITY_EDITOR
             if (useSystemLanguage)
             {
-                yield return L10nManager.Initialize(LanguageTypeMapper.ISO396(_options.Language)).ToYieldInstruction();
+                yield return L10nManager.Initialize().ToYieldInstruction();
             }
             else
             {
                 yield return L10nManager.Initialize(languageType).ToYieldInstruction();
             }
 #else
-            yield return L10nManager.Initialize().ToYieldInstruction();
+            yield return L10nManager.Initialize(LanguageTypeMapper.ISO396(_options.Language)).ToYieldInstruction();
 #endif
 
             Widget.Find<Title>().ShowSettingButton();
