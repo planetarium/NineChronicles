8f3d088fa6ab10d00cb7874bdbc19cb6cdea33e5
Ko Chanhyuck <lime_bell@naver.com>
2020-10-05 19:08:36 +0900

    Add ValidatingActionRenderer

---
+++ Lib9c/Renderer/RenderRecord.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Libplanet.Action;
+
+namespace Lib9c.Renderer
+{
+    public abstract class RenderRecord<T>
+        where T : IAction, new()
+    {
+        public long Index;
+
+        public string StackTrace;
+
+        public override string ToString() => $"{Index}.";
+
+        public abstract class ActionBase : RenderRecord<T>
+        {
+            public IAction Action;
+
+            public IActionContext Context;
+
+            public bool Render;
+
+            public bool Unrender
+            {
+                get => !Render;
+                set => Render = !value;
+            }
+
+            public override string ToString() =>
+                $"{base.ToString()} #{Context.BlockIndex} " +
+                (Render ? "Render" : "Unrender") + "Action";
+        }
+
+        public class ActionSuccess : ActionBase
+        {
+            public IAccountStateDelta NextStates;
+
+            public override string ToString() => $"{base.ToString()} [success]";
+        }
+
+        public class ActionError : ActionBase
+        {
+            public Exception Exception;
+
+            public override string ToString() => $"{base.ToString()} [error]";
+        }
+
+        public abstract class BlockBase : RenderRecord<T>
+        {
+            public Libplanet.Blocks.Block<T> OldTip;
+            public Libplanet.Blocks.Block<T> NewTip;
+            public bool Begin;
+
+            public bool End
+            {
+                get => !Begin;
+                set => Begin = !value;
+            }
+
+            public override string ToString() =>
+                $"{base.ToString()} " +
+                $"#{OldTip.Index} {OldTip.Hash} -> #{NewTip.Index} {NewTip.Hash} Render..." +
+                (End ? "End" : string.Empty);
+        }
+
+        public class Block : BlockBase
+        {
+            public override string ToString() =>
+                base.ToString().Replace("Render...", "RenderBlock");
+        }
+
+        public class Reorg : BlockBase
+        {
+            public Libplanet.Blocks.Block<T> Branchpoint;
+
+            public override string ToString() =>
+                base.ToString().Replace("Render...", "RenderReorg") +
+                $" [branchpoint: #{Branchpoint.Index} {Branchpoint.Hash}]";
+        }
+    }
+}
