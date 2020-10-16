26d5f61c17e70531df0ed67d3729c5f746c4e46b
moreal <dev.moreal@gmail.com>
2020-10-16 14:26:35 +0900

    test: correct test mock data

--- .Lib9c.Tests/JsonStatesLoaderTest.cs
+++ .Lib9c.Tests/JsonStatesLoaderTest.cs
@@ -5,9 +5,11 @@ namespace Lib9c.Tests
     using System.IO;
     using System.IO.Abstractions;
     using System.IO.Abstractions.TestingHelpers;
+    using System.Text;
     using Bencodex;
     using Bencodex.Types;
     using Libplanet;
+    using Org.BouncyCastle.Utilities.Encoders;
     using Xunit;
 
     public class JsonStatesLoaderTest
@@ -17,9 +19,21 @@ namespace Lib9c.Tests
         {
             var codec = new Codec();
             Text barValue = (Text)"bar";
+
+            byte[] Base64Encode(byte[] data)
+            {
+                var encoder = new Base64Encoder();
+                using var stream = new MemoryStream();
+                encoder.Encode(data, 0, data.Length, stream);
+                return stream.ToArray();
+            }
+
+            string Base64EncodeString(byte[] data)
+                => Encoding.UTF8.GetString(Base64Encode(data));
+
             IFileSystem fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
             {
-                { "/foo", new MockFileData($"{{\"foo\": \"{ByteUtil.Hex(codec.Encode(barValue))}\"}}") },
+                { "/foo", new MockFileData($"{{\"foo\": \"{Base64EncodeString(codec.Encode(barValue))}\"}}") },
             });
             var loader = new JsonStatesLoader(fileSystem);
             var states = loader.Load("/foo");
