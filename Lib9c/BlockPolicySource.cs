dbbc70275b1435840edd3f2d21c8f043ab891292
Swen Mun <longfinfunnel@gmail.com>
2020-10-16 13:47:03 +0900

    Add FIXME

--- Lib9c/BlockPolicySource.cs
+++ Lib9c/BlockPolicySource.cs
@@ -98,6 +98,8 @@ namespace Nekoyume.BlockChain
             {
                 // It can be caused during `Swarm<T>.PreloadAsync()` because it doesn't fill its 
                 // state right away...
+                // FIXME It should be removed after fix that Libplanet fills its state on IBD.
+                // See also: https://github.com/planetarium/lib9c/pull/151#discussion_r506039478
                 return true;
             }
         }
