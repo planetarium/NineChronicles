674391a3d9261d3608b08f989a57e2159ddef331
Yang Chun Ung <qooraven@gmail.com>
2020-10-19 14:09:56 +0900

    Delete unused method test

--- nekoyume/Assets/Tests/EditMode/StateExtensionsTest.cs
+++ nekoyume/Assets/Tests/EditMode/StateExtensionsTest.cs
@@ -28,22 +28,5 @@ namespace Tests.EditMode
             var serialized = row.ItemId.Serialize();
             Assert.AreEqual(row.ItemId, serialized.ToItemId());
         }
-
-        [Test]
-        public void SerializeChestList()
-        {
-            var tableSheets = TableSheetsHelper.MakeTableSheets();
-            var row = tableSheets.MaterialItemSheet.Values.First(r => r.ItemSubType == ItemSubType.Chest);
-            var chest = ItemFactory.CreateChest(row, null);
-            var rewards = tableSheets.RedeemRewardSheet.Values.First().Rewards;
-            var chest2 = ItemFactory.CreateChest(row, rewards);
-            var chestList = new Dictionary<HashDigest<SHA256>, int>
-            {
-                [chest.ItemId] = 3,
-                [chest2.ItemId] = 4,
-            };
-            var serialized = chestList.Serialize();
-            Assert.AreEqual(chestList, serialized.Deserialize());
-        }
     }
 }
