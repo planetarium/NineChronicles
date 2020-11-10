1332be75aadb30d90b4e0ba24f370bdeaa11769c
Hyun Seungmin <boscohyun@gmail.com>
2020-11-10 14:58:44 +0900

    Extend ShopItem

--- Lib9c/Model/Item/ShopItem.cs
+++ Lib9c/Model/Item/ShopItem.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Numerics;
 using Bencodex.Types;
 using Libplanet;
 using Libplanet.Assets;
@@ -14,21 +13,36 @@ namespace Nekoyume.Model.Item
         public readonly Address SellerAgentAddress;
         public readonly Address SellerAvatarAddress;
         public readonly Guid ProductId;
-        public readonly ItemUsable ItemUsable;
         public readonly FungibleAssetValue Price;
+        public readonly ItemUsable ItemUsable;
+        public readonly Costume Costume;
 
-        public ShopItem(
-            Address sellerAgentAddress,
+        public ShopItem(Address sellerAgentAddress,
             Address sellerAvatarAddress,
             Guid productId,
-            ItemUsable itemUsable,
-            FungibleAssetValue price)
+            FungibleAssetValue price,
+            ItemUsable itemUsable)
         {
             SellerAgentAddress = sellerAgentAddress;
             SellerAvatarAddress = sellerAvatarAddress;
             ProductId = productId;
-            ItemUsable = itemUsable;
             Price = price;
+            ItemUsable = itemUsable;
+            Costume = null;
+        }
+        
+        public ShopItem(Address sellerAgentAddress,
+            Address sellerAvatarAddress,
+            Guid productId,
+            FungibleAssetValue price,
+            Costume costume)
+        {
+            SellerAgentAddress = sellerAgentAddress;
+            SellerAvatarAddress = sellerAvatarAddress;
+            ProductId = productId;
+            Price = price;
+            ItemUsable = null;
+            Costume = costume;
         }
 
         public ShopItem(Dictionary serialized)
@@ -36,10 +50,13 @@ namespace Nekoyume.Model.Item
             SellerAgentAddress = serialized["sellerAgentAddress"].ToAddress();
             SellerAvatarAddress = serialized["sellerAvatarAddress"].ToAddress();
             ProductId = serialized["productId"].ToGuid();
-            ItemUsable = (ItemUsable)ItemFactory.Deserialize(
-                (Dictionary)serialized["itemUsable"]
-            );
             Price = serialized["price"].ToFungibleAssetValue();
+            ItemUsable = serialized.ContainsKey("itemUsable")
+                ? (ItemUsable) ItemFactory.Deserialize((Dictionary) serialized["itemUsable"])
+                : null;
+            Costume = serialized.ContainsKey("costume")
+                ? (Costume) ItemFactory.Deserialize((Dictionary) serialized["costume"])
+                : null;
         }
 
         public IValue Serialize() =>
@@ -48,8 +65,9 @@ namespace Nekoyume.Model.Item
                 [(Text)"sellerAgentAddress"] = SellerAgentAddress.Serialize(),
                 [(Text)"sellerAvatarAddress"] = SellerAvatarAddress.Serialize(),
                 [(Text)"productId"] = ProductId.Serialize(),
-                [(Text)"itemUsable"] = ItemUsable.Serialize(),
                 [(Text)"price"] = Price.Serialize(),
+                [(Text)"itemUsable"] = ItemUsable?.Serialize(),
+                [(Text)"costume"] = Costume?.Serialize(),
             });
 
         protected bool Equals(ShopItem other)
