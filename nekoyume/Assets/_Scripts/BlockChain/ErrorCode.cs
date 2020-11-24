d14bd13ed8b7428fcacc516fdfdb9223a04f255a
Hyun Seungmin <boscohyun@gmail.com>
2020-11-24 15:43:32 +0900

    Fix error code of `CannotToRenderWhenSyncingBlocksException`

--- nekoyume/Assets/_Scripts/BlockChain/ErrorCode.cs
+++ nekoyume/Assets/_Scripts/BlockChain/ErrorCode.cs
@@ -124,7 +124,7 @@ namespace Nekoyume.BlockChain
                     errorMsg += $"\nError Code: {code}";
                     break;
                 case CannotToRenderWhenSyncingBlocksException _:
-                    code = "";
+                    code = "28";
                     key = "ERROR_CANNOT_TO_RENDER_WHEN_SYNCING_BLOCKS";
                     break;
             }
