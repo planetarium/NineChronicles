979796d0a9c76cc479a749046b078f40a47496a1
Hyun Seungmin <boscohyun@gmail.com>
2020-12-04 16:17:21 +0900

    Add unit tests for serialize `Model.Item.Xxx`

--- .Lib9c.Tests/Model/Item/ChestTest.cs
+++ .Lib9c.Tests/Model/Item/ChestTest.cs
@@ -1,7 +1,9 @@
 namespace Lib9c.Tests.Model.Item
 {
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
+    using System.Runtime.Serialization.Formatters.Binary;
     using Nekoyume.Model.Item;
     using Nekoyume.TableData;
     using Xunit;
@@ -12,20 +14,35 @@ namespace Lib9c.Tests.Model.Item
 
         public ChestTest()
         {
-            var dict = TableSheetsImporter.ImportSheets();
-            var tableSheets = new TableSheets(dict);
+            var tableSheets = new TableSheets(TableSheetsImporter.ImportSheets());
             _chestRow = tableSheets.MaterialItemSheet.OrderedList.FirstOrDefault(row =>
                 row.ItemSubType == ItemSubType.Chest);
         }
 
         [Fact]
-        public void Serialization()
+        public void Serialize()
         {
             Assert.NotNull(_chestRow);
 
             var chest = new Chest(_chestRow, new List<RedeemRewardSheet.RewardInfo>());
             var serialized = chest.Serialize();
             var deserialized = new Chest((Bencodex.Types.Dictionary)serialized);
+
+            Assert.Equal(chest, deserialized);
+        }
+
+        [Fact]
+        public void SerializeWithDotNetAPI()
+        {
+            Assert.NotNull(_chestRow);
+
+            var chest = new Chest(_chestRow, new List<RedeemRewardSheet.RewardInfo>());
+            var formatter = new BinaryFormatter();
+            using var ms = new MemoryStream();
+            formatter.Serialize(ms, chest);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            var deserialized = (Chest)formatter.Deserialize(ms);
 
             Assert.Equal(chest, deserialized);
         }
