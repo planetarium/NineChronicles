de34b6b816f39c37a8fc41f8a49bab6da5850bcb
hskim <andykim881028@gmail.com>
2020-12-10 15:40:24 +0900

    Update nekoyume/Assets/_Scripts/State/Subjects/AgentStateSubject.cs
    Co-authored-by: Hyun Seungmin <boscohyun@gmail.com>

--- nekoyume/Assets/_Scripts/State/Subjects/AgentStateSubject.cs
+++ nekoyume/Assets/_Scripts/State/Subjects/AgentStateSubject.cs
@@ -8,7 +8,7 @@ using UniRx;
 namespace Nekoyume.State.Subjects
 {
     /// <summary>
-    /// AgentState가 포함하는 값의 변화를 각각의 ReactiveProperty<T> 필드를 통해 외부에 변화를 알린다.
+    /// The change of the value included in `AgentState` is notified to the outside through each Subject<T> field.
     /// </summary>
     public static class AgentStateSubject
     {
