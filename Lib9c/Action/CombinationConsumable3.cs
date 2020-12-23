ecba1d245bdb2b0585520dca189c092f77b6cb65
Hyun Seungmin <boscohyun@gmail.com>
2020-12-23 16:04:51 +0900

    Add CombinationConsumable3 and unit test

---
+++ Lib9c/Action/CombinationConsumable3.cs
@@ -0,0 +1,213 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+using Bencodex.Types;
+using Libplanet;
+using Libplanet.Action;
+using Nekoyume.Model.Item;
+using Nekoyume.Model.Mail;
+using Nekoyume.Model.State;
+using Nekoyume.TableData;
+using Serilog;
+using Material = Nekoyume.Model.Item.Material;
+
+namespace Nekoyume.Action
+{
+    [Serializable]
+    [ActionType("combination_consumable3")]
+    public class CombinationConsumable3 : GameAction
+    {
+        public Address AvatarAddress;
+        public int recipeId;
+        public int slotIndex;
+
+        protected override IImmutableDictionary<string, IValue> PlainValueInternal
+        {
+            get
+            {
+                var dict = new Dictionary<string, IValue>
+                {
+                    ["recipeId"] = recipeId.Serialize(),
+                    ["avatarAddress"] = AvatarAddress.Serialize(),
+                };
+
+                // slotIndex가 포함되지 않은채 나간 버전과 호환을 위해, 0번째 슬롯을 쓰는 경우엔 보내지 않습니다.
+                if (slotIndex != 0)
+                {
+                    dict["slotIndex"] = slotIndex.Serialize();
+                }
+
+                return dict.ToImmutableDictionary();
+            }
+        }
+
+        public CombinationConsumable3()
+        {
+        }
+
+        protected override void LoadPlainValueInternal(IImmutableDictionary<string, IValue> plainValue)
+        {
+            recipeId = plainValue["recipeId"].ToInteger();
+            AvatarAddress = plainValue["avatarAddress"].ToAddress();
+            if (plainValue.TryGetValue((Text) "slotIndex", out var value))
+            {
+                slotIndex = value.ToInteger();
+            }
+        }
+
+        public override IAccountStateDelta Execute(IActionContext context)
+        {
+            IActionContext ctx = context;
+            var states = ctx.PreviousStates;
+            var slotAddress = AvatarAddress.Derive(
+                string.Format(
+                    CultureInfo.InvariantCulture,
+                    CombinationSlotState.DeriveFormat,
+                    slotIndex
+                )
+            );
+            if (ctx.Rehearsal)
+            {
+                return states
+                    .SetState(AvatarAddress, MarkChanged)
+                    .SetState(ctx.Signer, MarkChanged)
+                    .SetState(slotAddress, MarkChanged);
+            }
+
+            var sw = new Stopwatch();
+            sw.Start();
+            var started = DateTimeOffset.UtcNow;
+            Log.Debug("Combination exec started.");
+
+            if (!states.TryGetAvatarState(ctx.Signer, AvatarAddress, out AvatarState avatarState))
+            {
+                throw new FailedLoadStateException("Aborted as the avatar state of the signer was failed to load.");
+            }
+
+            sw.Stop();
+            Log.Debug("Combination Get AgentAvatarStates: {Elapsed}", sw.Elapsed);
+            sw.Restart();
+
+            if (!avatarState.worldInformation.IsStageCleared(GameConfig.RequireClearedStageLevel.CombinationEquipmentAction))
+            {
+                avatarState.worldInformation.TryGetLastClearedStageId(out var current);
+                throw new NotEnoughClearedStageLevelException(
+                    GameConfig.RequireClearedStageLevel.CombinationEquipmentAction, current);
+            }
+
+            var slotState = states.GetCombinationSlotState(AvatarAddress, slotIndex);
+            if (slotState is null)
+            {
+                throw new FailedLoadStateException($"Aborted as the slot state is failed to load: # {slotIndex}");
+            }
+
+            if(!slotState.Validate(avatarState, ctx.BlockIndex))
+            {
+                throw new CombinationSlotUnlockException(
+                    $"Aborted as the slot state is invalid: {slotState} @ {slotIndex}");
+            }
+
+            Log.Debug("Execute Combination; player: {Player}", AvatarAddress);
+            var consumableItemSheet = states.GetSheet<ConsumableItemSheet>();
+            var recipeRow = states.GetSheet<ConsumableItemRecipeSheet>().Values.FirstOrDefault(r => r.Id == recipeId);
+            if (recipeRow is null)
+            {
+                throw new SheetRowNotFoundException(nameof(ConsumableItemRecipeSheet), recipeId);
+            }
+            var materials = new Dictionary<Material, int>();
+            foreach (var materialInfo in recipeRow.Materials.OrderBy(r => r.Id))
+            {
+                var materialId = materialInfo.Id;
+                var count = materialInfo.Count;
+                if (avatarState.inventory.HasItem(materialId, count))
+                {
+                    avatarState.inventory.TryGetFungibleItem(materialId, out var inventoryItem);
+                    var material = (Material) inventoryItem.item;
+                    materials[material] = count;
+                    avatarState.inventory.RemoveFungibleItem(material, count);
+                }
+                else
+                {
+                    throw new NotEnoughMaterialException(
+                        $"Aborted as the player has no enough material ({materialId} * {count})");
+                }
+            }
+
+            sw.Stop();
+            Log.Debug("Combination Remove Materials: {Elapsed}", sw.Elapsed);
+            sw.Restart();
+
+            var result = new CombinationConsumable.ResultModel
+            {
+                materials = materials,
+                itemType = ItemType.Consumable,
+
+            };
+
+            var costAP = recipeRow.RequiredActionPoint;
+            if (avatarState.actionPoint < costAP)
+            {
+                throw new NotEnoughActionPointException(
+                    $"Aborted due to insufficient action point: {avatarState.actionPoint} < {costAP}"
+                );
+            }
+
+            // ap 차감.
+            avatarState.actionPoint -= costAP;
+            result.actionPoint = costAP;
+
+            var resultConsumableItemId = recipeRow.ResultConsumableItemId;
+            sw.Stop();
+            Log.Debug("Combination Get Food id: {Elapsed}", sw.Elapsed);
+            sw.Restart();
+            result.recipeId = recipeRow.Id;
+
+            if (!consumableItemSheet.TryGetValue(resultConsumableItemId, out var consumableItemRow))
+            {
+                throw new SheetRowNotFoundException(nameof(ConsumableItemSheet), resultConsumableItemId);
+            }
+
+            // 조합 결과 획득.
+            var requiredBlockIndex = ctx.BlockIndex + recipeRow.RequiredBlockIndex;
+            var itemId = ctx.Random.GenerateRandomGuid();
+            var itemUsable = GetFood(consumableItemRow, itemId, requiredBlockIndex);
+            // 액션 결과
+            result.itemUsable = itemUsable;
+            var mail = new CombinationMail(
+                result,
+                ctx.BlockIndex,
+                ctx.Random.GenerateRandomGuid(),
+                requiredBlockIndex
+            );
+            result.id = mail.id;
+            avatarState.UpdateV3(mail);
+            avatarState.UpdateFromCombination(itemUsable);
+            sw.Stop();
+            Log.Debug("Combination Update AvatarState: {Elapsed}", sw.Elapsed);
+            sw.Restart();
+
+            var materialSheet = states.GetSheet<MaterialItemSheet>();
+            avatarState.UpdateQuestRewards(materialSheet);
+
+            avatarState.updatedAt = ctx.BlockIndex;
+            avatarState.blockIndex = ctx.BlockIndex;
+            states = states.SetState(AvatarAddress, avatarState.Serialize());
+            slotState.Update(result, ctx.BlockIndex, requiredBlockIndex);
+            sw.Stop();
+            Log.Debug("Combination Set AvatarState: {Elapsed}", sw.Elapsed);
+            var ended = DateTimeOffset.UtcNow;
+            Log.Debug("Combination Total Executed Time: {Elapsed}", ended - started);
+            return states
+                .SetState(slotAddress, slotState.Serialize());
+        }
+
+        private static ItemUsable GetFood(ConsumableItemSheet.Row equipmentItemRow, Guid itemId, long ctxBlockIndex)
+        {
+            return ItemFactory.CreateItemUsable(equipmentItemRow, itemId, ctxBlockIndex);
+        }
+    }
+}
