705ec38596d654509a2395d1687be0ed67d122cf
hskim881028 <andykim881028@gmail.com>
2021-01-27 18:16:46 +0900

    Add TextTyper

---
+++ nekoyume/Assets/TextTyper/Examples/TextTyperTester.cs
@@ -0,0 +1,123 @@
+﻿namespace RedBlueGames.Tools.TextTyper
+{
+    using UnityEngine;
+    using System.Collections;
+    using System.Collections.Generic;
+    using RedBlueGames.Tools.TextTyper;
+    using UnityEngine.UI;
+    using TMPro;
+
+    /// <summary>
+    /// Class that tests TextTyper and shows how to interface with it.
+    /// </summary>
+    public class TextTyperTester : MonoBehaviour
+    {
+#pragma warning disable 0649 // Ignore "Field is never assigned to" warning, as these are assigned in inspector
+
+
+        [Header("UI References")]
+
+        [SerializeField]
+        private Button printNextButton;
+
+        [SerializeField]
+        private Button printNoSkipButton;
+
+        [SerializeField]
+        private Toggle pauseGameToggle;
+
+        private Queue<string> dialogueLines = new Queue<string>();
+
+        [SerializeField]
+        [Tooltip("The text typer element to test typing with")]
+        private TextTyper testTextTyper;
+
+#pragma warning restore 0649
+        public void Start()
+        {
+            this.testTextTyper.PrintCompleted.AddListener(this.HandlePrintCompleted);
+            this.testTextTyper.CharacterPrinted.AddListener(this.HandleCharacterPrinted);
+
+            this.printNextButton.onClick.AddListener(this.HandlePrintNextClicked);
+            this.printNoSkipButton.onClick.AddListener(this.HandlePrintNoSkipClicked);
+
+            dialogueLines.Enqueue("Hello! My name is... <delay=0.5>NPC</delay>. Got it, <i>bub</i>?");
+            dialogueLines.Enqueue("You can <b>use</b> <i>uGUI</i> <size=40>text</size> <size=20>tag</size> and <color=#ff0000ff>color</color> tag <color=#00ff00ff>like this</color>.");
+            dialogueLines.Enqueue("bold <b>text</b> test <b>bold</b> text <b>test</b>");
+            dialogueLines.Enqueue("Sprites!<sprite index=0><sprite index=1><sprite index=2><sprite index=3>Isn't that neat?");
+            dialogueLines.Enqueue("You can <size=40>size 40</size> and <size=20>size 20</size>");
+            dialogueLines.Enqueue("You can <color=#ff0000ff>color</color> tag <color=#00ff00ff>like this</color>.");
+            dialogueLines.Enqueue("Sample Shake Animations: <anim=lightrot>Light Rotation</anim>, <anim=lightpos>Light Position</anim>, <anim=fullshake>Full Shake</anim>\nSample Curve Animations: <animation=slowsine>Slow Sine</animation>, <animation=bounce>Bounce Bounce</animation>, <animation=crazyflip>Crazy Flip</animation>");
+            ShowScript();
+        }
+
+        public void Update()
+        {
+            UnityEngine.Time.timeScale = this.pauseGameToggle.isOn ? 0.0f : 1.0f;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+                var tag = RichTextTag.ParseNext("blah<color=red>boo</color");
+                LogTag(tag);
+                tag = RichTextTag.ParseNext("<color=blue>blue</color");
+                LogTag(tag);
+                tag = RichTextTag.ParseNext("No tag in here");
+                LogTag(tag);
+                tag = RichTextTag.ParseNext("No <color=blueblue</color tag here either");
+                LogTag(tag);
+                tag = RichTextTag.ParseNext("This tag is a closing tag </bold>");
+                LogTag(tag);
+            }
+        }
+
+        private void HandlePrintNextClicked()
+        {
+            if (this.testTextTyper.IsSkippable() && this.testTextTyper.IsTyping)
+            {
+                this.testTextTyper.Skip();
+            }
+            else
+            {
+                ShowScript();
+            }
+        }
+
+        private void HandlePrintNoSkipClicked()
+        {
+            ShowScript();
+        }
+
+        private void ShowScript()
+        {
+            if (dialogueLines.Count <= 0)
+            {
+                return;
+            }
+
+            this.testTextTyper.TypeText(dialogueLines.Dequeue());
+        }
+
+        private void LogTag(RichTextTag tag)
+        {
+            if (tag != null)
+            {
+                Debug.Log("Tag: " + tag.ToString());
+            }
+        }
+
+        private void HandleCharacterPrinted(string printedCharacter)
+        {
+            // Do not play a sound for whitespace
+            if (printedCharacter == " " || printedCharacter == "\n")
+            {
+                return;
+            }
+        }
+
+        private void HandlePrintCompleted()
+        {
+            Debug.Log("TypeText Complete");
+        }
+    }
+}
