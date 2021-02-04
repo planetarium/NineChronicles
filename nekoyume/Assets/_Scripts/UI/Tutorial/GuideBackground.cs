c77719f86e15aa434c149f6306938181522e9e0a
hskim881028 <andykim881028@gmail.com>
2021-02-04 21:38:19 +0900

    Modified to dynamically resize masks and buttons

--- nekoyume/Assets/_Scripts/UI/Tutorial/GuideBackground.cs
+++ nekoyume/Assets/_Scripts/UI/Tutorial/GuideBackground.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using Coffee.UISoftMask;
 using DG.Tweening;
@@ -17,6 +18,12 @@ namespace Nekoyume.UI
         [SerializeField] private Image background;
 
         private Coroutine _coroutine;
+        private Vector2 _cachedMaskSize;
+
+        private void Awake()
+        {
+            _cachedMaskSize = mask.rectTransform.sizeDelta;
+        }
 
         public override void Play<T>(T data, System.Action callback)
         {
@@ -26,6 +33,7 @@ namespace Nekoyume.UI
                 {
                     StopCoroutine(_coroutine);
                 }
+
                 _coroutine = StartCoroutine(LatePlay(d, callback));
             }
         }
@@ -39,9 +47,12 @@ namespace Nekoyume.UI
         {
             yield return new WaitForSeconds(predelay);
             SetFade(true, data.isExistFadeIn ? fadeDuration : 0.0f);
+            SetButton(data.button, data.target);
+            SetMaskSize(data.target);
 
             mask.rectTransform.position = data.target ? data.target.position : Vector3.zero;
             mask.alpha = data.isEnableMask ? alpha : 0.0f;
+
             float time = 0f;
             float tick = maskDuration / 60.0f;
             while (mask.alpha < alpha - 0.01f)
@@ -53,6 +64,13 @@ namespace Nekoyume.UI
             callback?.Invoke();
         }
 
+        private void SetButton(Button button, RectTransform target)
+        {
+            var rt = button.GetComponent<RectTransform>();
+            rt.position = target ? target.position  : Vector3.zero;
+            rt.sizeDelta = target ? target.sizeDelta : Vector2.one * 2000;
+        }
+
         private void SetFade(bool isIn, float duration, System.Action callback = null)
         {
             background.DOKill();
@@ -62,6 +80,15 @@ namespace Nekoyume.UI
             background.DOFade(isIn ? alpha : 0.0f, duration)
                 .SetEase(fadeCurve)
                 .OnComplete(() => callback?.Invoke());
+        }
+
+        private void SetMaskSize(RectTransform target)
+        {
+            mask.rectTransform.sizeDelta = _cachedMaskSize;
+            if (target && target.sizeDelta.sqrMagnitude > mask.rectTransform.sizeDelta.sqrMagnitude)
+            {
+                target.sizeDelta = mask.rectTransform.sizeDelta * 1.2f;
+            }
         }
     }
 }
