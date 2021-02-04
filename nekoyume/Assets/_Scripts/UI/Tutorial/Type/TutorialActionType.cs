dbee94fe8b173a6f0bbe2a14b04eb2d2c65bb5e8
Hyun Seungmin <boscohyun@gmail.com>
2021-02-04 23:04:22 +0900

    feat(tutorial): add tutorial action types

--- nekoyume/Assets/_Scripts/UI/Tutorial/Type/TutorialActionType.cs
+++ nekoyume/Assets/_Scripts/UI/Tutorial/Type/TutorialActionType.cs
@@ -17,6 +17,9 @@ namespace Nekoyume.UI
         TutorialActionClickBottomMenuBackButton,
         TutorialActionClickFirstCombinationMailSubmitButton,
         TutorialActionClickCombinationResultPopupSubmitButton,
+        TutorialActionClickAvatarInfoFirstInventoryCellView,
+        TutorialActionClickItemInformationTooltipSubmitButton,
+        TutorialActionCloseAvatarInfoWidget,
     }
 
     public class TutorialActionTypeComparer : IEqualityComparer<TutorialActionType>
