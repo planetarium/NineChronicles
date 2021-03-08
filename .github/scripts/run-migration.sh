5b4f30f9eb433cf9e6c301278a53cee7b1a9707a
Lee Suho <riemannulus@hitagi.moe>
2021-03-08 23:09:38 +0900

    fix: run-migration use store command

--- .github/scripts/run-migration.sh
+++ .github/scripts/run-migration.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-dotnet build Lib9c.Tools
+echo "Build Lib9c.Tools"
+dotnet build .Lib9c.Tools
+echo "Run Lib9c.Tools.Store"
 dotnet run \
-    --project Lib9c.Tools \
-    -s _benchmarks_snapshot
-    -d _benchmarks_snapshot_migrate
+    --project .Lib9c.Tools \
+    store \
+    -o _benchmarks_snapshot \
+    -d _benchmarks_snapshot_migrate \
     -t
 
