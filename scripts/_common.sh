de66c91257ed1817093f0a57372b3a19893570d2
Hyun Seungmin <boscohyun@gmail.com>
2021-04-06 12:36:50 +0900

    Fix ulf path

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,14 +9,14 @@ title() {
 }
 
 install_license() {
-    if [[ -f Unity_v2019.x.ulf ]]; then
+    if [[ -f Unity_v2020.x.ulf ]]; then
         # for local debugging
-        cp Unity_v2019.x.ulf /tmp/Unity_v2019.x.ulf
+        cp Unity_v2020.x.ulf /tmp/Unity_v2020.x.ulf
     elif [[ "$ULF" = "" ]]; then
         echo "The ULF environment variable is missing." > /dev/stderr
         exit 1
     else
-        echo -n "$ULF" | base64 -d > /tmp/Unity_v2019.x.ulf
+        echo -n "$ULF" | base64 -d > /tmp/Unity_v2020.x.ulf
     fi
 
     /opt/Unity/Editor/Unity \
