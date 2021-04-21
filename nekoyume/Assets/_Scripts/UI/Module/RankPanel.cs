225ebc346d0f9d71b77324246ccc112608db1d7d
unengine <unengine@gmail.com>
2021-04-21 18:33:11 +0900

    Impelent ranking label.

--- nekoyume/Assets/_Scripts/UI/Module/RankPanel.cs
+++ nekoyume/Assets/_Scripts/UI/Module/RankPanel.cs
@@ -4,6 +4,7 @@ using Nekoyume.State;
 using Nekoyume.UI.Scroller;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UniRx;
 using UnityEngine;
 
@@ -24,34 +25,28 @@ namespace Nekoyume.UI.Module
         private GameObject myInfoPrefab = null;
 
         [SerializeField]
+        private TextMeshProUGUI firstColumnText = null;
+
+        [SerializeField]
+        private TextMeshProUGUI secondColumnText = null;
+
+        [SerializeField]
+        private RankCell myInfoCell = null;
+
+        [SerializeField]
         private int displayCount = 100;
 
         private List<RankCell> _cellViewCache = new List<RankCell>();
 
-        private List<(string, Address, int, int)> _expRankingInfos = null;
+        private List<(string, Address, int, int)> _abilityRankingInfos = null;
 
-        private List<(string, Address, int, int)> ExpRankingInfos {
-            get
-            {
-                if (_expRankingInfos is null)
-                {
-                    LoadRankingInfos();
-                }
+        private (int rank, string name, Address avatarAddress, int cp, int level) _myAbilityRankingInfo;
 
-                if (_expRankingInfos is null)
-                {
-                    Debug.LogError("Failed loading ExpRakingInfos.");
-                }
-
-                return _expRankingInfos;
-            }
-        }
-
-        private readonly List<RankCategory> _rankCategories = new List<RankCategory>()
+        private readonly Dictionary<RankCategory, (string, string)> _rankColumnMap = new Dictionary<RankCategory, (string, string)>
         {
-            RankCategory.Ability,
-            RankCategory.Stage,
-            RankCategory.Mimisburnnr,
+            { RankCategory.Ability, ("UI_CP", "UI_LEVEL") },
+            { RankCategory.Stage, ("UI_STAGE", null)},
+            { RankCategory.Mimisburnnr, ("UI_STAGE", null) },
         };
 
         public void Initialize()
@@ -65,12 +60,12 @@ namespace Nekoyume.UI.Module
                     var subElements = toggleDropdown.items;
                     foreach (var element in subElements)
                     {
-                        innerCategory = currentCategory;
+                        var innerCategory2 = innerCategory;
                         element.onValueChanged.AddListener(value =>
                         {
                             if (value)
                             {
-                                UpdateCategory(innerCategory);
+                                UpdateCategory(innerCategory2);
                             }
                         });
                         ++innerCategory;
@@ -115,7 +110,7 @@ namespace Nekoyume.UI.Module
             CacheCellViews();
         }
 
-        private void LoadRankingInfos()
+        public void LoadRankingInfos()
         {
             var rankingMapStates = States.Instance.RankingMapStates;
             var weeklyArenaState = States.Instance.WeeklyArenaState;
@@ -127,7 +122,7 @@ namespace Nekoyume.UI.Module
                 rankingInfoSet.UnionWith(rankingInfo);
             }
 
-            _expRankingInfos = rankingInfoSet
+            var abilityRankingInfos = rankingInfoSet
                 .OrderByDescending(i =>
                 {
                     var avatarAddress = i.AvatarAddress;
@@ -136,12 +131,33 @@ namespace Nekoyume.UI.Module
                     return arenaInfo is null ? 0 : arenaInfo.CombatPoint;
                 })
                 .ThenByDescending(c => c.Level)
+                .ToList();
+
+            var myAvatarAddress = States.Instance.CurrentAvatarState.address;
+            var myInfoIndex = abilityRankingInfos.FindIndex(i => i.AvatarAddress.Equals(myAvatarAddress));
+            if (myInfoIndex >= 0)
+            {
+                var myInfo = abilityRankingInfos[myInfoIndex];
+                var myArenaInfo = weeklyArenaState.GetArenaInfo(myInfo.AvatarAddress);
+                var avatarNameWithNoHash = myArenaInfo.AvatarName.Split().First();
+
+                _myAbilityRankingInfo = (
+                    myInfoIndex + 1,
+                    avatarNameWithNoHash,
+                    myInfo.AvatarAddress,
+                    myArenaInfo is null ? 0 : myArenaInfo.CombatPoint,
+                    myInfo.Level);
+            }
+
+            _abilityRankingInfos = abilityRankingInfos
                 .Take(displayCount)
                 .Select(i =>
                 {
                     var avatarAddress = i.AvatarAddress;
                     var arenaInfo = weeklyArenaState.GetArenaInfo(avatarAddress);
-                    return (arenaInfo.AvatarName, avatarAddress, arenaInfo.CombatPoint, arenaInfo.Level);
+                    var avatarNameWithNoHash = arenaInfo.AvatarName.Split().First();
+
+                    return (avatarNameWithNoHash, avatarAddress, arenaInfo.CombatPoint, arenaInfo.Level);
                 }).ToList();
         }
 
@@ -194,7 +210,7 @@ namespace Nekoyume.UI.Module
             switch (category)
             {
                 case RankCategory.Ability:
-                    for (int i = 0; i < ExpRankingInfos.Count(); ++i)
+                    for (int i = 0; i < _abilityRankingInfos.Count(); ++i)
                     {
                         if (i >= displayCount)
                         {
@@ -202,14 +218,23 @@ namespace Nekoyume.UI.Module
                         }
 
                         var rank = i + 1;
-                        (string name, Address avatarAddress, int level, int cp) = ExpRankingInfos[i];
-                        _cellViewCache[i].SetDataAsAbility(rank, name, avatarAddress, level, cp);
+                        (string name, Address avatarAddress, int cp, int level) = _abilityRankingInfos[i];
+                        _cellViewCache[i].SetDataAsAbility(rank, name, avatarAddress, cp, level);
                     }
+                    myInfoCell.SetDataAsAbility(
+                        _myAbilityRankingInfo.rank,
+                        _myAbilityRankingInfo.name,
+                        _myAbilityRankingInfo.avatarAddress,
+                        _myAbilityRankingInfo.cp,
+                        _myAbilityRankingInfo.level);
                     break;
                 default:
                     _cellViewCache.ForEach(cell => cell.gameObject.SetActive(false));
                     break;
             }
+
+            firstColumnText.text = _rankColumnMap[category].Item1;
+            secondColumnText.text = _rankColumnMap[category].Item2;
         }
     }
 }
