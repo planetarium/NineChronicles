8fa2e07550392e0f1f4429336149bd55000a1733
unengine <unengine@gmail.com>
2021-05-13 21:05:37 +0900

    Implement animator, add test scene.

---
+++ nekoyume/Assets/_Scripts/UI/Tween/MaskedRectTransformXRoller.cs
@@ -0,0 +1,105 @@
+using Nekoyume.EnumType;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Nekoyume.UI.Tween
+{
+    public class MaskedRectTransformXRoller : MonoBehaviour
+    {
+        [SerializeField]
+        private RectTransform contentRect = null;
+
+        [SerializeField]
+        private float targetAnimationTime = 2f;
+
+        [SerializeField]
+        private AnimationCurve curve = null;
+
+        [SerializeField]
+        private float startDelay = 1.5f;
+
+        [SerializeField]
+        private float endDelay = 1.5f;
+
+        private RectTransform _rectTransform = null;
+
+        private Coroutine _coroutine = null;
+
+        private Vector2 _originalPos;
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+        }
+
+        private void OnEnable()
+        {
+            if (_coroutine != null)
+            {
+                Kill();
+            }
+
+            var fitter = contentRect.GetComponent<ContentSizeFitter>();
+            fitter.SetLayoutHorizontal();
+            if (_rectTransform.rect.width < contentRect.rect.width)
+            {
+                _coroutine = StartCoroutine(CoScrollContent());
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (_coroutine != null)
+            {
+                Kill();
+            }
+        }
+
+        private void Kill()
+        {
+            StopCoroutine(_coroutine);
+            contentRect.anchoredPosition = _originalPos;
+            _coroutine = null;
+        }
+
+        private IEnumerator CoScrollContent()
+        {
+            if (curve is null)
+            {
+                Debug.LogError($"Curve not set.");
+                yield break;
+            }
+
+            _originalPos = contentRect.anchoredPosition;
+
+            var xDiff = contentRect.rect.width - _rectTransform.rect.width;
+            var targetX = _originalPos.x - xDiff;
+            var elapsedTime = 0f;
+
+            yield return new WaitForSeconds(startDelay);
+            while (gameObject.activeSelf)
+            {
+                var t = curve.Evaluate(elapsedTime / targetAnimationTime);
+                var xPos = Mathf.Lerp(_originalPos.x, targetX, t);
+                contentRect.anchoredPosition = new Vector2(xPos, _originalPos.y);
+
+                elapsedTime += Time.deltaTime;
+                if (elapsedTime > targetAnimationTime)
+                {
+                    contentRect.anchoredPosition = new Vector2(targetX, _originalPos.y);
+                    yield return new WaitForSeconds(endDelay);
+
+                    contentRect.anchoredPosition = new Vector2(_originalPos.x, _originalPos.y);
+                    elapsedTime = 0f;
+                    yield return new WaitForSeconds(startDelay);
+                }
+                else
+                {
+                    yield return null;
+                }
+            }
+        }
+    }
+}
