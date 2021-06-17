e40ab19aed43aefd61d42f85b320fe2268204eec
unengine <unengine@gmail.com>
2021-06-17 21:29:53 +0900

    Implement `PageView` and `EventBanner`.

---
+++ nekoyume/Assets/_Scripts/UI/Module/EventBanner.cs
@@ -0,0 +1,36 @@
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Nekoyume.UI.Module
+{
+    public class EventBanner : MonoBehaviour
+    {
+        [SerializeField]
+        private Button playToEarnEventButton;
+
+        private void Awake()
+        {
+            playToEarnEventButton.onClick.AsObservable()
+                .Subscribe(_ => GoToEventPage())
+                .AddTo(gameObject);
+        }
+
+        private void GoToEventPage()
+        {
+            var address = Game.Game.instance.States.CurrentAvatarState.address;
+
+            var confirm = Widget.Find<Confirm>();
+            confirm.CloseCallback = result =>
+            {
+                if (result == ConfirmResult.No)
+                {
+                    return;
+                }
+
+                Application.OpenURL(address.ToString());
+            };
+            confirm.Show("UI_PROCEED_EVENTPAGE", address.ToString(), blurRadius: 2);
+        }
+    }
+}
