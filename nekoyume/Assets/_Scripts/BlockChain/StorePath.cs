ae8d0f62d4ad3a5aab2cc30f59a640e88b45ed06
hskim <andykim881028@gmail.com>
2021-06-24 11:23:37 +0900

    Update nekoyume/Assets/_Scripts/BlockChain/StorePath.cs
    Co-authored-by: Hyun Seungmin <boscohyun@gmail.com>

--- nekoyume/Assets/_Scripts/BlockChain/StorePath.cs
+++ nekoyume/Assets/_Scripts/BlockChain/StorePath.cs
@@ -18,7 +18,7 @@ namespace Nekoyume.BlockChain
         private static readonly IImmutableDictionary<Env, string> DirNames = new Dictionary<Env, string>
         {
             [Env.Production] = "9c",
-            [Env.Development] = "9c-main-partition2",
+            [Env.Development] = "9c-dev",
         }.ToImmutableDictionary();
 
         public static string GetDefaultStoragePath(Env? env = null)
