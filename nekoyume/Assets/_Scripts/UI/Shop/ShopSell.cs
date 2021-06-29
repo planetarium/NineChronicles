01692f86e56cd6d02887744209b728f2f445a69f
hskim881028 <andykim881028@gmail.com>
2021-06-29 12:24:28 +0900

    Add sell popup exception handling

--- nekoyume/Assets/_Scripts/UI/Shop/ShopSell.cs
+++ nekoyume/Assets/_Scripts/UI/Shop/ShopSell.cs
@@ -246,6 +246,15 @@ namespace Nekoyume.UI
                 return;
             }
 
+            if (data.TotalPrice.Value.MinorUnit > 0)
+            {
+                Find<SystemPopup>().Show(L10nManager.Localize("UI_ERROR"),
+                    L10nManager.Localize("UI_TOTAL_PRICE_WARINING"),
+                    L10nManager.Localize("UI_OK"),
+                    false);
+                return;
+            }
+            
             if (data.TotalPrice.Value.Sign * data.TotalPrice.Value.MajorUnit < Model.Shop.MinimumPrice)
             {
                 throw new InvalidSellingPriceException(data);
