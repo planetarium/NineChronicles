6f9c505663a6eb27f41e91debb12af6c0aa9a1b7
hskim881028 <andykim881028@gmail.com>
2021-07-06 18:59:00 +0900

    Add RandomNumberRoulette component

---
+++ nekoyume/Assets/_Scripts/Game/Util/RandomNumberRoulette.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Nekoyume.Game.Util
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class RandomNumberRoulette : MonoBehaviour
+    {
+        [SerializeField] private int digit;
+        [SerializeField] private float cooltime;
+
+        private TextMeshProUGUI _number;
+        private Coroutine _rouletteCoroutine;
+
+        private void Awake()
+        {
+            _number = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void OnEnable()
+        {
+            if (_rouletteCoroutine != null)
+            {
+                StopCoroutine(_rouletteCoroutine);
+            }
+
+            _rouletteCoroutine = StartCoroutine(Play(_number, digit, cooltime));
+        }
+
+        public static IEnumerator Play(TMP_Text effect, int digit, float time)
+        {
+            StringBuilder sb = new StringBuilder();
+            while (true)
+            {
+                sb.Length = 0;
+                for (int i = 0; i < digit; i++)
+                {
+                    sb.Append(Random.Range(0, 10));
+                }
+                effect.text = sb.ToString();
+                yield return new WaitForSeconds(time);
+            }
+        }
+    }
+}
