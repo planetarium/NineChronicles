fdaef854509b4989967a260327d2ee7e1f9b19e6
hskim881028 <andykim881028@gmail.com>
2021-07-28 12:06:42 +0900

    Fix EnhancementCostSheetV2

--- .Lib9c.Tests/TableData/Cost/EnhancementCostSheetV2Test.cs
+++ .Lib9c.Tests/TableData/Cost/EnhancementCostSheetV2Test.cs
@@ -7,9 +7,9 @@
     public class EnhancementCostSheetV2Test
     {
         private const string _csv =
-            @"id,item_sub_type,grade,level,cost,success_ratio,great_success_ratio,fail_ratio,success_required_block_index,great_success_required_block_index,fail_required_block_index
-1,Weapon,1,1,0,0.75,0.25,0,300,700,20
-2,Weapon,1,7,500,0.67,0.23,0.1,300,700,20";
+            @"id,item_sub_type,grade,level,cost,success_ratio,great_success_ratio,fail_ratio,success_required_block_index,great_success_required_block_index,fail_required_block_index,base_stat_growth_min,base_stat_growth_max,extra_stat_growth_min,extra_stat_growth_max,extra_skill_damage_growth_min,extra_skill_damage_growth_max,extra_skill_proc_growth_min,extra_skill_proc_growth_max
+1,Weapon,1,1,0,0.75,0.25,0,300,700,50,0.08,0.12,0.24,0.36,0.24,0.36,0.12,0.18
+2,Weapon,1,2,0,0.75,0.25,0,300,700,50,0.08,0.12,0.08,0.12,0.08,0.12,0.04,0.06";
 
         [Fact]
         public void Set()
@@ -32,21 +32,37 @@
             Assert.Equal(0m, row.FailRatio);
             Assert.Equal(300, row.SuccessRequiredBlockIndex);
             Assert.Equal(700, row.GreatSuccessRequiredBlockIndex);
-            Assert.Equal(20, row.FailRequiredBlockIndex);
+            Assert.Equal(50, row.FailRequiredBlockIndex);
+            Assert.Equal(0.08m, row.BaseStatGrowthMin);
+            Assert.Equal(0.12m, row.BaseStatGrowthMax);
+            Assert.Equal(0.24m, row.ExtraStatGrowthMin);
+            Assert.Equal(0.36m, row.ExtraStatGrowthMax);
+            Assert.Equal(0.24m, row.ExtraSkillDamageGrowthMin);
+            Assert.Equal(0.36m, row.ExtraSkillDamageGrowthMax);
+            Assert.Equal(0.12m, row.ExtraSkillProcGrowthMin);
+            Assert.Equal(0.18m, row.ExtraSkillProcGrowthMax);
 
             row = sheet.Last;
             Assert.Equal(row.Id, row.Key);
             Assert.Equal(2, row.Id);
             Assert.Equal(ItemSubType.Weapon, row.ItemSubType);
             Assert.Equal(1, row.Grade);
-            Assert.Equal(7, row.Level);
-            Assert.Equal(500, row.Cost);
-            Assert.Equal(0.67m, row.SuccessRatio);
-            Assert.Equal(0.23m, row.GreatSuccessRatio);
-            Assert.Equal(0.1m, row.FailRatio);
+            Assert.Equal(2, row.Level);
+            Assert.Equal(0, row.Cost);
+            Assert.Equal(0.75m, row.SuccessRatio);
+            Assert.Equal(0.25m, row.GreatSuccessRatio);
+            Assert.Equal(0m, row.FailRatio);
             Assert.Equal(300, row.SuccessRequiredBlockIndex);
             Assert.Equal(700, row.GreatSuccessRequiredBlockIndex);
-            Assert.Equal(20, row.FailRequiredBlockIndex);
+            Assert.Equal(50, row.FailRequiredBlockIndex);
+            Assert.Equal(0.08m, row.BaseStatGrowthMin);
+            Assert.Equal(0.12m, row.BaseStatGrowthMax);
+            Assert.Equal(0.08m, row.ExtraStatGrowthMin);
+            Assert.Equal(0.12m, row.ExtraStatGrowthMax);
+            Assert.Equal(0.08m, row.ExtraSkillDamageGrowthMin);
+            Assert.Equal(0.12m, row.ExtraSkillDamageGrowthMax);
+            Assert.Equal(0.04m, row.ExtraSkillProcGrowthMin);
+            Assert.Equal(0.06m, row.ExtraSkillProcGrowthMax);
         }
     }
 }
--- Lib9c/TableData/Cost/EnhancementCostSheetV2.cs
+++ Lib9c/TableData/Cost/EnhancementCostSheetV2.cs
@@ -6,7 +6,7 @@ using Nekoyume.Model.Item;
 namespace Nekoyume.TableData
 {
     using static TableExtensions;
-    
+
     public class EnhancementCostSheetV2 : Sheet<int, EnhancementCostSheetV2.Row>
     {
         public class Row : EnhancementCostSheet.Row
@@ -17,6 +17,14 @@ namespace Nekoyume.TableData
             public int SuccessRequiredBlockIndex { get; private set; }
             public int GreatSuccessRequiredBlockIndex { get; private set; }
             public int FailRequiredBlockIndex { get; private set; }
+            public decimal BaseStatGrowthMin { get; private set; }
+            public decimal BaseStatGrowthMax { get; private set; }
+            public decimal ExtraStatGrowthMin { get; private set; }
+            public decimal ExtraStatGrowthMax { get; private set; }
+            public decimal ExtraSkillDamageGrowthMin { get; private set; }
+            public decimal ExtraSkillDamageGrowthMax { get; private set; }
+            public decimal ExtraSkillProcGrowthMin { get; private set; }
+            public decimal ExtraSkillProcGrowthMax { get; private set; }
 
             public override void Set(IReadOnlyList<string> fields)
             {
@@ -27,6 +35,14 @@ namespace Nekoyume.TableData
                 SuccessRequiredBlockIndex = ParseInt(fields[8], 0);
                 GreatSuccessRequiredBlockIndex = ParseInt(fields[9], 0);
                 FailRequiredBlockIndex = ParseInt(fields[10], 0);
+                BaseStatGrowthMin = ParseDecimal(fields[11], 0m);
+                BaseStatGrowthMax = ParseDecimal(fields[12], 0m);
+                ExtraStatGrowthMin = ParseDecimal(fields[13], 0m);
+                ExtraStatGrowthMax = ParseDecimal(fields[14], 0m);
+                ExtraSkillDamageGrowthMin = ParseDecimal(fields[15], 0m);
+                ExtraSkillDamageGrowthMax = ParseDecimal(fields[16], 0m);
+                ExtraSkillProcGrowthMin = ParseDecimal(fields[17], 0m);
+                ExtraSkillProcGrowthMax = ParseDecimal(fields[18], 0m);
             }
         }
 
