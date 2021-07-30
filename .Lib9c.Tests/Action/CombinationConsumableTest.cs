9e2065a73f1a5487880aa972ad065c49c64116f7
boscohyun <boscohyun@gmail.com>
2021-07-30 16:16:22 +0900

    Add "combination_consumable7" as `CombinationConsumable`

--- .Lib9c.Tests/Action/CombinationConsumableTest.cs
+++ .Lib9c.Tests/Action/CombinationConsumableTest.cs
@@ -5,6 +5,8 @@ namespace Lib9c.Tests.Action
     using System.Linq;
     using Libplanet;
     using Libplanet.Action;
+    using Libplanet.Assets;
+    using Libplanet.Crypto;
     using Nekoyume;
     using Nekoyume.Action;
     using Nekoyume.Model.Item;
@@ -17,8 +19,6 @@ namespace Lib9c.Tests.Action
     {
         private readonly Address _agentAddress;
         private readonly Address _avatarAddress;
-        private readonly Address _slotAddress;
-        private readonly Dictionary<string, string> _sheets;
         private readonly IRandom _random;
         private readonly TableSheets _tableSheets;
         private readonly AvatarState _avatarState;
@@ -26,21 +26,22 @@ namespace Lib9c.Tests.Action
 
         public CombinationConsumableTest()
         {
-            _agentAddress = default;
+            _agentAddress = new PrivateKey().ToAddress();
             _avatarAddress = _agentAddress.Derive("avatar");
-            _slotAddress = _avatarAddress.Derive(
+            var slotAddress = _avatarAddress.Derive(
                 string.Format(
                     CultureInfo.InvariantCulture,
                     CombinationSlotState.DeriveFormat,
                     0
                 )
             );
-            _sheets = TableSheetsImporter.ImportSheets();
+            var sheets = TableSheetsImporter.ImportSheets();
             _random = new TestRandom();
-            _tableSheets = new TableSheets(_sheets);
+            _tableSheets = new TableSheets(sheets);
 
             var agentState = new AgentState(_agentAddress);
             agentState.avatarAddresses[0] = _avatarAddress;
+
             var gameConfigState = new GameConfigState();
 
             _avatarState = new AvatarState(
@@ -52,11 +53,19 @@ namespace Lib9c.Tests.Action
                 default
             );
 
+            var gold = new GoldCurrencyState(new Currency("NCG", 2, minter: null));
+
             _initialState = new State()
                 .SetState(_agentAddress, agentState.Serialize())
-                .SetState(_avatarAddress, _avatarState.Serialize());
+                .SetState(_avatarAddress, _avatarState.Serialize())
+                .SetState(
+                    slotAddress,
+                    new CombinationSlotState(
+                        slotAddress,
+                        GameConfig.RequireClearedStageLevel.CombinationConsumableAction).Serialize())
+                .SetState(GameConfigState.Address, gold.Serialize());
 
-            foreach (var (key, value) in _sheets)
+            foreach (var (key, value) in sheets)
             {
                 _initialState =
                     _initialState.SetState(Addresses.TableSheet.Derive(key), value.Serialize());
@@ -90,7 +99,7 @@ namespace Lib9c.Tests.Action
 
             var equipment = ItemFactory.CreateItemUsable(_tableSheets.EquipmentItemSheet.First, default, 0);
 
-            var result = new CombinationConsumable5.ResultModel()
+            var result = new CombinationConsumable5.ResultModel
             {
                 id = default,
                 gold = 0,
@@ -103,10 +112,8 @@ namespace Lib9c.Tests.Action
             for (var i = 0; i < 100; i++)
             {
                 var mail = new CombinationMail(result, i, default, 0);
-                _avatarState.Update(mail);
+                _avatarState.UpdateV4(mail, 0);
             }
-
-            _initialState = _initialState.SetState(_slotAddress, new CombinationSlotState(_slotAddress, requiredStage).Serialize());
 
             if (backward)
             {
@@ -121,14 +128,14 @@ namespace Lib9c.Tests.Action
                     .SetState(_avatarAddress, _avatarState.SerializeV2());
             }
 
-            var action = new CombinationConsumable()
+            var action = new CombinationConsumable
             {
-                AvatarAddress = _avatarAddress,
+                avatarAddress = _avatarAddress,
                 recipeId = row.Id,
                 slotIndex = 0,
             };
 
-            var nextState = action.Execute(new ActionContext()
+            var nextState = action.Execute(new ActionContext
             {
                 PreviousStates = _initialState,
                 Signer = _agentAddress,
@@ -145,7 +152,7 @@ namespace Lib9c.Tests.Action
 
             var nextAvatarState = nextState.GetAvatarStateV2(_avatarAddress);
 
-            Assert.Equal(30, nextAvatarState.mailBox.Count);
+            Assert.Equal(1, nextAvatarState.mailBox.Count);
             Assert.IsType<CombinationMail>(nextAvatarState.mailBox.First());
         }
     }
