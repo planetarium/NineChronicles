4746a40b71f4c6ab1b7ed69f56fea52750097241
Nam yujeong <41294407+Namyujeong@users.noreply.github.com>
2021-08-10 00:10:40 +0900

    Update Buy8.cs
    fix block number

--- Lib9c/Action/Buy8.cs
+++ Lib9c/Action/Buy8.cs
@@ -19,7 +19,7 @@ using static Lib9c.SerializeKeys;
 namespace Nekoyume.Action
 {
     [Serializable]
-    [ActionObsolete(2100000)]
+    [ActionObsolete(2200000)]
     [ActionType("buy8")]
     public class Buy8 : GameAction
     {
@@ -96,7 +96,7 @@ namespace Nekoyume.Action
                     .SetState(ctx.Signer, MarkChanged);
             }
 
-            CheckObsolete(2100000, context);
+            CheckObsolete(2200000, context);
 
             var addressesHex = GetSignerAndOtherAddressesHex(context, buyerAvatarAddress);
 
