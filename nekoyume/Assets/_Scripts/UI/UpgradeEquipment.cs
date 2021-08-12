48eae3463026c33548481f0924914158f0f00a54
hskim881028 <andykim881028@gmail.com>
2021-08-12 21:03:31 +0900

    Link effect animations

--- nekoyume/Assets/_Scripts/UI/UpgradeEquipment.cs
+++ nekoyume/Assets/_Scripts/UI/UpgradeEquipment.cs
@@ -92,12 +92,24 @@ namespace Nekoyume.UI
         private BigInteger _costNcg = 0;
         private string errorMessage;
 
+        private enum  State
+        {
+            Show,
+            Empty,
+            RegisterBase,
+            RegisterMaterial,
+            UnregisterMaterial,
+            Close,
+        }
+
         private static readonly int HashToShow = Animator.StringToHash("Show");
         private static readonly int HashToRegisterBase = Animator.StringToHash("RegisterBase");
         private static readonly int HashToPostRegisterBase = Animator.StringToHash("PostRegisterBase");
         private static readonly int HashToPostRegisterMaterial = Animator.StringToHash("PostRegisterMaterial");
         private static readonly int HashToUnregisterMaterial = Animator.StringToHash("UnregisterMaterial");
         private static readonly int HashToClose = Animator.StringToHash("Close");
+
+        private State _state = State.Empty;
 
         protected override void Awake()
         {
@@ -110,8 +122,6 @@ namespace Nekoyume.UI
         public override void Initialize()
         {
             base.Initialize();
-
-            Clear();
             inventory.SharedModel.SelectedItemView.Subscribe(SubscribeSelectItem).AddTo(gameObject);
             inventory.SharedModel.DeselectItemView();
             inventory.SharedModel.State.Value = ItemSubType.Weapon;
@@ -124,39 +134,73 @@ namespace Nekoyume.UI
             baseSlot.RemoveButton.onClick.AddListener(() =>
             {
                 materialSlot.RemoveButton.onClick.Invoke();
-                inventory.ClearItemState(_baseItem);
-                _baseItem = null;
-                inventory.SharedModel.UpdateDimAndEffectAll();
-                buttonDisabled.SetActive(!IsInteractableButton(_baseItem, _materialItem, _costNcg));
-                ClearInformation();
-                SetActiveContainer(true);
-                Debug.Log("Remove base");
-                _animator.PlayInFixedTime(HashToRegisterBase);
+                UpdateState(State.Empty);
             });
 
             materialSlot.RemoveButton.onClick.AddListener(() =>
             {
-                inventory.ClearItemState(_materialItem);
-                _materialItem = null;
-                inventory.SharedModel.UpdateDimAndEffectAll();
-                buttonDisabled.SetActive(!IsInteractableButton(_baseItem, _materialItem, _costNcg));
-                materialGuideText.text = L10nManager.Localize("UI_SELECT_MATERIAL_TO_UPGRADE");
-                Debug.Log("Remove material");
-                _animator.PlayInFixedTime(HashToUnregisterMaterial);
+                UpdateState(State.UnregisterMaterial);
             });
+        }
+
+        private void UpdateState(State state)
+        {
+            switch (state)
+            {
+                case State.Show:
+                case State.Empty:
+                    inventory.ClearItemState(_baseItem);
+                    baseSlot.RemoveMaterial();
+                    _baseItem = null;
+                    inventory.ClearItemState(_materialItem);
+                    materialSlot.RemoveMaterial();
+                    _materialItem = null;
+                    ClearInformation();
+                    SetActiveContainer(true);
+                    materialGuideText.text = string.Empty;
+                    _animator.Play(state == State.Show ? HashToShow : HashToRegisterBase);
+                    break;
+
+                case State.RegisterBase:
+                case State.UnregisterMaterial:
+                    inventory.ClearItemState(_materialItem);
+                    materialSlot.RemoveMaterial();
+                    _materialItem = null;
+                    SetActiveContainer(false);
+                    materialGuideText.text = L10nManager.Localize("UI_SELECT_MATERIAL_TO_UPGRADE");
+                    if (ItemEnhancement.TryGetRow(_baseItem, _costSheet, out var row))
+                    {
+                        _costNcg = row.Cost;
+                        UpdateInformation(row, _baseItem);
+                    }
+                    _animator.Play(state == State.RegisterBase ? HashToPostRegisterBase : HashToUnregisterMaterial);
+                    break;
+
+                case State.RegisterMaterial:
+                    SetActiveContainer(false);
+                    materialGuideText.text = L10nManager.Localize("UI_UPGRADE_GUIDE");
+                    _animator.Play(HashToPostRegisterMaterial, -1, 0);
+                    break;
+
+                case State.Close:
+                    _animator.Play(HashToClose);
+                    break;
+            }
+
+            buttonDisabled.SetActive(!IsInteractableButton(_baseItem, _materialItem, _costNcg));
+            inventory.SharedModel.UpdateDimAndEffectAll();
         }
 
         public override void Show(bool ignoreShowAnimation = false)
         {
             base.Show(ignoreShowAnimation);
-            _animator.PlayInFixedTime(HashToShow);
-            Clear();
+            UpdateState(State.Show);
             HelpPopup.HelpMe(100017, true);
         }
 
         private void Close()
         {
-            _animator.PlayInFixedTime(HashToClose);
+            UpdateState(State.Close);
             Close(true);
             Find<CombinationMain>().Show();
         }
@@ -277,7 +321,7 @@ namespace Nekoyume.UI
                 .ItemEnhancement(baseGuid, materialGuid, slotIndex)
                 .Subscribe(_ => { }, e => ActionRenderHandler.BackToMain(false, e));
 
-            StartCoroutine(CoCombineNPCAnimation(_baseItem, row.SuccessRequiredBlockIndex, Clear));
+            StartCoroutine(CoCombineNPCAnimation(_baseItem, row.SuccessRequiredBlockIndex, () => UpdateState(State.Empty)));
         }
 
         private IEnumerator CoCombineNPCAnimation(ItemBase itemBase,
@@ -295,7 +339,6 @@ namespace Nekoyume.UI
             var format = L10nManager.Localize("UI_COST_BLOCK");
             var quote = string.Format(format, blockIndex);
             loadingScreen.AnimateNPC(quote);
-            Clear();
         }
 
         private void SubscribeSelectItem(BigInventoryItemView view)
@@ -340,7 +383,6 @@ namespace Nekoyume.UI
                 {
                     return false;
                 }
-
             }
             return true;
         }
@@ -370,44 +412,32 @@ namespace Nekoyume.UI
                 return;
             }
 
-            StageMaterial(view);
-        }
-
-        private void StageMaterial(BigInventoryItemView view)
-        {
-            SetActiveContainer(false);
             if (_baseItem is null)
             {
                 _baseItem = (Equipment)view.Model.ItemBase.Value;
-                if (ItemEnhancement.TryGetRow(_baseItem, _costSheet, out var row))
-                {
-                    _costNcg = row.Cost;
-                    UpdateInformation(row, _baseItem);
-                }
                 baseSlot.AddMaterial(view.Model.ItemBase.Value);
-                materialGuideText.text = L10nManager.Localize("UI_SELECT_MATERIAL_TO_UPGRADE");
                 view.Select(true);
-                _animator.PlayInFixedTime(HashToPostRegisterBase);
+                UpdateState(State.RegisterBase);
             }
             else
             {
-                materialSlot.RemoveButton.onClick.Invoke();
+                if(!(_materialItem is null))
+                {
+                    inventory.ClearItemState(_materialItem);
+                }
+
                 _materialItem = (Equipment)view.Model.ItemBase.Value;
                 materialSlot.AddMaterial(view.Model.ItemBase.Value);
-                materialGuideText.text = L10nManager.Localize("UI_UPGRADE_GUIDE");
                 view.Select(false);
-                _animator.PlayInFixedTime(HashToPostRegisterMaterial);
+                UpdateState(State.RegisterMaterial);
             }
-
-            buttonDisabled.SetActive(!IsInteractableButton(_baseItem, _materialItem, _costNcg));
-            inventory.SharedModel.UpdateDimAndEffectAll();
         }
 
-        private void Clear()
+        private void SetActiveContainer(bool isClear)
         {
-            baseSlot.RemoveButton.onClick.Invoke();
-            ClearInformation();
-            SetActiveContainer(true);
+            noneContainer.SetActive(isClear);
+            itemInformationContainer.SetActive(!isClear);
+            blockInformationContainer.SetActive(!isClear);
         }
 
         private void ClearInformation()
@@ -430,14 +460,6 @@ namespace Nekoyume.UI
             {
                 skill.gameObject.SetActive(false);
             }
-        }
-
-        private void SetActiveContainer(bool isClear)
-        {
-            noneContainer.SetActive(isClear);
-            itemInformationContainer.SetActive(!isClear);
-            blockInformationContainer.SetActive(!isClear);
-            materialGuideText.gameObject.SetActive(!isClear);
         }
 
         private void UpdateInformation(EnhancementCostSheetV2.Row row, Equipment equipment)
