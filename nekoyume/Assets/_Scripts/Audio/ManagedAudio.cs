0ffeab5f3369a8a7e805013345c20998584f591a
sonohoshi <yd111544@gmail.com>
2021-09-01 17:13:37 +0900

    Make new managed audio system
    use it when add sounds in the editor with prefab.

---
+++ nekoyume/Assets/_Scripts/Audio/ManagedAudio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Nekoyume.Game.Controller;
+using UnityEngine;
+
+namespace Nekoyume.Audio
+{
+    public class ManagedAudio : MonoBehaviour
+    {
+        [SerializeField] private AudioSource audioSource;
+
+        public void Play()
+        {
+            audioSource ??= GetComponent<AudioSource>();
+            AudioController.instance.PlaySfx(audioSource.clip.name);
+        }
+    }
+}
