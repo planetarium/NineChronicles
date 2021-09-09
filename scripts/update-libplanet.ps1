ad693029102979285383b0f5c5303b7449e4b5ce
Swen Mun <longfinfunnel@gmail.com>
2021-09-09 19:13:48 +0900

    Add build script for libplanet

---
+++ scripts/update-libplanet.ps1
@@ -0,0 +1,13 @@
+$9cRoot = git rev-parse --show-toplevel
+$LibplanetRoot = Join-Path -Path $9cRoot -ChildPath "nekoyume/Assets/_Scripts/Lib9c/lib9c/.Libplanet"
+$LibplanetRocksDBStorePath = Join-Path -Path $LibplanetRoot -ChildPath "Libplanet.RocksDBStore/"
+$LibplanetRocksDBStoreProj = Join-Path -Path $LibplanetRocksDBStorePath -ChildPath "Libplanet.RocksDBStore.csproj"
+$TempOutputPath = Join-Path -Path $env:TEMP -ChildPath $(New-Guid)
+$PackagePath = Join-Path -Path $9cRoot -ChildPath "nekoyume/Assets/Packages/"
+
+git submodule update --recursive
+dotnet clean $LibplanetRocksDBStoreProj
+dotnet build $LibplanetRocksDBStoreProj -c Release -o $TempOutputPath
+
+Copy-Item -Path $(Join-Path -Path $TempOutputPath -ChildPath "*") -Include "*.dll", "*.xml" -Destination $PackagePath
+Remove-Item $TempOutputPath -Recurse
