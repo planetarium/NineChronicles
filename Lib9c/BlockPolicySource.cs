225f3fcca6ffb3d7dafea7b0c30098b344b517a3
hskim881028 <andykim881028@gmail.com>
2021-09-13 21:09:57 +0900

    Fix

--- Lib9c/BlockPolicySource.cs
+++ Lib9c/BlockPolicySource.cs
@@ -126,7 +126,7 @@ namespace Nekoyume.BlockChain
         {
             // Avoid NRE when genesis block appended
             long index = blockChain.Tip?.Index ?? 0;
-            if (transaction.Actions.Count > 1)
+            if (transaction.Actions.Count > 0 || IsObsolete(transaction, index))
             {
                 return false;
             }
