5cd1bc3b2e9cc25e41cbaddf710fd5e1dc1c237c
sonohoshi <yd111544@gmail.com>
2021-10-20 18:23:47 +0900

    Fix broken test codes about mimis brunnr

--- .Lib9c.Tests/Action/MimisbrunnrBattle0Test.cs
+++ .Lib9c.Tests/Action/MimisbrunnrBattle0Test.cs
@@ -102,10 +102,28 @@
                 throw new SheetRowNotFoundException("MimisbrunnrSheet", stageId);
             }
 
+            var equipments = new List<Guid>();
+
             var equipmentRow =
-                _tableSheets.EquipmentItemSheet.Values.First(x => x.ElementalType == ElementalType.Fire);
-            var equipment = ItemFactory.CreateItemUsable(equipmentRow, default, 0);
-            previousAvatarState.inventory.AddItem2(equipment);
+                _tableSheets.EquipmentItemSheet.Values.Last(x => x.Id == 10151001);
+            var equipment = ItemFactory.CreateItemUsable(equipmentRow, Guid.NewGuid(), 0);
+            previousAvatarState.inventory.AddItem(equipment);
+
+            var mailEquipmentRow = _tableSheets.EquipmentItemSheet.Values.Last(x => x.Id == 10251001);
+            var mailEquipment = ItemFactory.CreateItemUsable(mailEquipmentRow, Guid.NewGuid(), 0);
+            previousAvatarState.inventory.AddItem(mailEquipment);
+
+            var beltEquipment = ItemFactory.CreateItemUsable(
+                _tableSheets.EquipmentItemSheet.Values.Last(x => x.Id == 10351000), Guid.NewGuid(), 0);
+            previousAvatarState.inventory.AddItem(beltEquipment);
+
+            var necklaceEquipment = ItemFactory.CreateItemUsable(
+                _tableSheets.EquipmentItemSheet.Values.Last(x => x.Id == 10451000), Guid.NewGuid(), 0);
+            previousAvatarState.inventory.AddItem(necklaceEquipment);
+            equipments.Add(equipment.ItemId);
+            equipments.Add(mailEquipment.ItemId);
+            equipments.Add(beltEquipment.ItemId);
+            equipments.Add(necklaceEquipment.ItemId);
 
             foreach (var equipmentId in previousAvatarState.inventory.Equipments)
             {
@@ -136,7 +154,7 @@
             var action = new MimisbrunnrBattle0()
             {
                 costumes = new List<Guid> { ((Costume)costume).ItemId },
-                equipments = new List<Guid>() { equipment.ItemId },
+                equipments = equipments,
                 foods = new List<Guid>(),
                 worldId = worldId,
                 stageId = stageId,
