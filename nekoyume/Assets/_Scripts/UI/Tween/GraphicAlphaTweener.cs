b6fddbf01ffe63172fe3892359558e3b3a8b3d61
sonohoshi <yd111544@gmail.com>
2021-10-25 18:51:08 +0900

    Implement DOTweenBase to GraphicAlphaTweener

--- nekoyume/Assets/_Scripts/UI/Tween/GraphicAlphaTweener.cs
+++ nekoyume/Assets/_Scripts/UI/Tween/GraphicAlphaTweener.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 namespace Nekoyume.UI.Tween
 {
     [RequireComponent(typeof(Graphic))]
-    public class GraphicAlphaTweener : MonoBehaviour
+    public class GraphicAlphaTweener : DOTweenBase
     {
         [SerializeField]
         private float beginValue = 0f;
@@ -16,13 +16,7 @@ namespace Nekoyume.UI.Tween
         private float endValue = 1f;
 
         [SerializeField]
-        private float duration = 1f;
-
-        [SerializeField]
         private bool infiniteLoop = false;
-
-        [SerializeField]
-        private Ease ease = Ease.Linear;
 
         [SerializeField]
         private LoopType loopType = LoopType.Yoyo;
@@ -30,43 +24,28 @@ namespace Nekoyume.UI.Tween
         private Graphic _graphic;
         private Color _originColor;
 
-        protected Tweener Tweener { get; set; }
-
-        private void Awake()
+        protected override void Awake()
         {
+            base.Awake();
             _graphic = GetComponent<Graphic>();
             _originColor = _graphic.color;
         }
 
         private void OnEnable()
         {
-            var color = new Color(_originColor.r, _originColor.g, _originColor.b, beginValue);
-            _graphic.color = color;
-
-            color.a = endValue;
-            Tweener = _graphic.DOColor(color, duration)
-                .SetEase(ease);
+            _graphic.DOFade(beginValue, 0f);
+            currentTween = _graphic.DOFade(endValue, duration).SetEase(ease);
 
             if (infiniteLoop)
             {
-                Tweener = Tweener.SetLoops(-1, loopType);
+                currentTween = currentTween.SetLoops(-1, loopType);
             }
         }
 
         private void OnDisable()
         {
-            KillTween();
+            Stop();
             _graphic.color = _originColor;
-        }
-
-        public void KillTween()
-        {
-            if (Tweener?.IsPlaying() ?? false)
-            {
-                Tweener?.Kill();
-            }
-
-            Tweener = null;
         }
     }
 }
