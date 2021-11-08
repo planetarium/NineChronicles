8a392cf3d981f71d4b189def374eb7586ee7b944
hskim881028 <andykim881028@gmail.com>
2021-11-08 14:05:11 +0900

    Reflect review

--- nekoyume/Assets/_Scripts/UI/Scroller/GridScrollDefaultContext.cs
+++ nekoyume/Assets/_Scripts/UI/Scroller/GridScrollDefaultContext.cs
@@ -6,12 +6,10 @@ namespace Nekoyume.UI.Scroller
 {
     public class GridScrollDefaultContext : IDisposable, IFancyGridViewContext
     {
-        private IFancyGridViewContext _fancyGridViewContextImplementation;
         public ScrollDirection ScrollDirection { get; set; }
         public Func<(float ScrollSize, float ReuseMargin)> CalculateScrollSize { get; set; }
         public GameObject CellTemplate { get; set; }
         public Func<int> GetGroupCount { get; set; }
-
         public Func<float> GetStartAxisSpacing { get; set; }
         public Func<float> GetCellSize { get; set; }
 
