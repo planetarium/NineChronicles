cc1ca8fc5c84d2d4f97082aca87d0b6abf8245ed
boscohyun <boscohyun@gmail.com>
2021-11-19 19:41:50 +0900

    Introduce `MixpanelValueFactory`

---
+++ nekoyume/Assets/PackageExtensions/Mixpanel/MixpanelValueFactory.cs
@@ -0,0 +1,42 @@
+﻿using mixpanel;
+using UnityEngine;
+
+namespace PackageExtensions.Mixpanel
+{
+    public static class MixpanelValueFactory
+    {
+        private static bool _initialized;
+        private static string _clientHost;
+        private static string _clientHash;
+
+        private static void Initialize()
+        {
+            if (_initialized)
+            {
+                return;
+            }
+
+            _clientHost = Resources.Load<TextAsset>("MixpanelClientHost").text;
+            _clientHash = Resources.Load<TextAsset>("MixpanelClientHash").text;
+            _initialized = true;
+        }
+
+        public static Value GetValue(params (string key, string value)[] properties)
+        {
+            Initialize();
+
+            var value = new Value
+            {
+                ["client-host"] = _clientHost,
+                ["client-hash"] = _clientHash,
+            };
+
+            foreach (var (key, v) in properties)
+            {
+                value[key] = v;
+            }
+
+            return value;
+        }
+    }
+}
