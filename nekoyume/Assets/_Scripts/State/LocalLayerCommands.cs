2c41cb449357e75bcb29e92728337e79f142cb46
boscohyun <boscohyun@gmail.com>
2021-12-03 22:43:11 +0900

    Introduce `LocalLayerCommands`

---
+++ nekoyume/Assets/_Scripts/State/LocalLayerCommands.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Libplanet;
+using Nekoyume.BlockChain;
+using Nekoyume.Game;
+
+namespace Nekoyume.State
+{
+    public class LocalLayerCommands
+    {
+        #region Singleton
+
+        private static class Singleton
+        {
+            internal static readonly LocalLayerCommands Value = new LocalLayerCommands();
+        }
+
+        public static LocalLayerCommands Instance => Singleton.Value;
+
+        private LocalLayerCommands()
+        {
+        }
+
+        #endregion
+
+        public void Apply(
+            IAgent agent,
+            States states,
+            TableSheets tableSheets,
+            IReadOnlyList<Address> updatedAddresses,
+            bool ignoreNotify = false)
+        {
+            // TODO: implement.
+        }
+    }
+}
