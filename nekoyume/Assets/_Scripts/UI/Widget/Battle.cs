4f5583e92d2c8f54b7d8e9b50314bf172351e090
Kim sm <dev.sonohoshi@gmail.com>
2021-12-14 14:30:45 +0900

    Change access modifier of background in `Stage`
    and add method `DestroyBackground`

--- nekoyume/Assets/_Scripts/UI/Widget/Battle.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/Battle.cs
@@ -99,8 +99,7 @@ namespace Nekoyume.UI
             guidedQuest.Hide(ignoreCloseAnimation);
             enemyPlayerStatus.Close(ignoreCloseAnimation);
             Find<HeaderMenuStatic>().Close();
-            Destroy(Game.Game.instance.Stage.background);
-            Game.Game.instance.Stage.background = null;
+            Game.Game.instance.Stage.DestroyBackground();
             base.Close(ignoreCloseAnimation);
         }
 
