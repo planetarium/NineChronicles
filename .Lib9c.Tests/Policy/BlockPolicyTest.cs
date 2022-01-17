7495e16f81fa956e63c3439128b9b35644d71948
Yang Chun Ung <qooraven@gmail.com>
2022-01-17 21:38:40 +0900

    Delete Obsolte_actions

--- .Lib9c.Tests/Policy/BlockPolicyTest.cs
+++ .Lib9c.Tests/Policy/BlockPolicyTest.cs
@@ -605,18 +605,6 @@ namespace Lib9c.Tests
         }
 
         [Fact]
-        public void Obsolete_Actions()
-        {
-            Assert.Empty(Assembly.GetAssembly(typeof(ActionBase))!.GetTypes().Where(
-                type => type.Namespace is { } @namespace &&
-                        @namespace.StartsWith($"{nameof(Nekoyume)}.{nameof(Nekoyume.Action)}") &&
-                        typeof(ActionBase).IsAssignableFrom(type) &&
-                        !type.IsAbstract &&
-                        Regex.IsMatch(type.Name, @"\d+$") &&
-                        !type.IsDefined(typeof(ActionObsoleteAttribute), false)));
-        }
-
-        [Fact]
         public void ValidateNextBlockWithManyTransactionsPerSigner()
         {
             var adminPrivateKey = new PrivateKey();
