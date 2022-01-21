00b9aec1f44b6c8e904a7d001bd6b5f38c4dc491
tyrosine1153 <tyrosine1153@gmail.com>
2022-01-21 16:10:59 +0900

    Extend ReactiveProperty to Subscribe LanguageType on Inspector

---
+++ nekoyume/Assets/_Scripts/L10n/LanguageTypeReactiveProperty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UniRx;
+
+namespace Nekoyume.L10n
+{
+    [Serializable]
+    public class LanguageTypeReactiveProperty : ReactiveProperty<LanguageType>
+    {
+        public LanguageTypeReactiveProperty() : base()
+        {
+        }
+        
+        public LanguageTypeReactiveProperty(LanguageType initialValue) : base(initialValue)
+        {
+        }
+    }
+}
