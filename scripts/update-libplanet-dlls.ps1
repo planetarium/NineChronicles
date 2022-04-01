3c0c4be546fe3874bb21cee43c6b77133150fbba
Hong Minhee <hong.minhee@planetariumhq.com>
2022-04-01 13:19:23 +0900

    Download Libplanet dlls rather than building them

--- scripts/update-libplanet-dlls.ps1
+++ scripts/update-libplanet-dlls.ps1
@@ -1,15 +1,38 @@
 #!/usr/bin/env pwsh
+$InformationPreference = "Continue"
+
+function New-TemporaryDirectory {
+  $parent = [System.IO.Path]::GetTempPath()
+  [string] $name = [System.Guid]::NewGuid()
+  New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
+
 $9cRoot = git rev-parse --show-toplevel
-$LibplanetRoot = Join-Path -Path $9cRoot -ChildPath "nekoyume/Assets/_Scripts/Lib9c/lib9c/.Libplanet"
-$LibplanetRocksDBStorePath = Join-Path -Path $LibplanetRoot -ChildPath "Libplanet.RocksDBStore/"
-$LibplanetRocksDBStoreProj = Join-Path -Path $LibplanetRocksDBStorePath -ChildPath "Libplanet.RocksDBStore.csproj"
-$tempDir = [System.IO.Path]::GetTempPath()
-$TempOutputPath = Join-Path -Path $tempDir -ChildPath $(New-Guid)
-$PackagePath = Join-Path -Path $9cRoot -ChildPath "nekoyume/Assets/Packages/"
+$PackagePath = Join-Path $9cRoot "nekoyume" "Assets" "Packages"
+$LibplanetProject = Join-Path $9cRoot `
+  "nekoyume" "Assets" "_Scripts" "Lib9c" "lib9c" `
+  ".Libplanet" "Libplanet" "Libplanet.csproj"
+$LibplanetVersion = (Select-Xml `
+  "/Project/PropertyGroup/VersionPrefix/text()" `
+  -LiteralPath $LibplanetProject).Node.Value
+$LibplanetDownloadUrlBase = `
+  "https://github.com/planetarium/libplanet/releases/download"
 
-git submodule update --recursive
-dotnet clean $LibplanetRocksDBStoreProj
-dotnet build $LibplanetRocksDBStoreProj -c Release -o $TempOutputPath
+Write-Information "Libplanet version: $LibplanetVersion"
+foreach ($file in Get-ChildItem $PackagePath -Filter Libplanet*.dll) {
+  $packageName = $file.Basename
+  Write-Information "Downloading $packageName $LibplanetVersion..."
+  $nupkgName = "$packageName.$LibplanetVersion"
+  $nupkgFile = "$nupkgName.nupkg"
+  $nupkgUrl = "$LibplanetDownloadUrlBase/$LibplanetVersion/$nupkgFile"
+  $tmpDir = New-TemporaryDirectory
+  Push-Location "$tmpDir"
+  Invoke-WebRequest -Uri $nupkgUrl -OutFile $nupkgFile
+  Expand-Archive $nupkgFile
+  Pop-Location
+  $assembliesDir = Join-Path $tmpDir $nupkgName "lib" "netstandard2.0"
+  foreach ($f in Get-ChildItem $assembliesDir) {
+    Move-Item $f -Force -Destination (Join-Path $PackagePath $f.Name)
+  }
+}
 
-Copy-Item -Path $(Join-Path -Path $TempOutputPath -ChildPath "*") -Include "*.dll", "*.xml" -Destination $PackagePath
-Remove-Item $TempOutputPath -Recurse
