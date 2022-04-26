97e652f5ed78d99bff5e9c55575f6cd9fb1ba932
Hyun Seungmin <boscohyun@gmail.com>
2022-04-26 20:07:51 +0900

    Obsolete RankingBattle10

--- Lib9c/Policy/BlockPolicySource.cs
+++ Lib9c/Policy/BlockPolicySource.cs
@@ -54,12 +54,6 @@ namespace Nekoyume.BlockChain.Policy
 
         public const long V100089ObsoleteIndex = 2_908_000;
 
-        /// <summary>
-        /// Current date: 2021/12/14 16:21
-        /// Current block index: 2_965_521
-        /// Temporary target date : 2021/12/20 12:00
-        /// Temporary block index:
-        /// </summary>
         public const long V100093ObsoleteIndex = 3_050_000;
 
         public const long V100095ObsoleteIndex = 3_317_632;
@@ -67,6 +61,8 @@ namespace Nekoyume.BlockChain.Policy
         public const long V100096ObsoleteIndex = 3_317_632;
         
         public const long V100170ObsoleteIndex = 3_810_000;
+
+        public const long V100190ObsoleteIndex = 4_000_000;
 
         public const long PermissionedMiningStartIndex = 2_225_500;
 
