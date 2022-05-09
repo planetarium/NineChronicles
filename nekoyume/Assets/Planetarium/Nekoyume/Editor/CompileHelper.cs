db04b2d98f076e9d57f03b817d73af280f3b9e40
Hyun Seungmin <boscohyun@gmail.com>
2022-05-09 17:09:35 +0900

    Introduce CompileHelper

---
+++ nekoyume/Assets/Planetarium/Nekoyume/Editor/CompileHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Planetarium.Nekoyume.Editor
+{
+    public static class CompileHelper
+    {
+        public static readonly string[] UselessObjDirectoryPaths = {
+            Path.Combine(Application.dataPath, "_Scripts", "Lib9c", "lib9c", "Lib9c.DevExtensions", "obj"),
+        };
+
+        [MenuItem("Tools/Compile Helper/Cleanup Useless Obj Files", true)]
+        public static bool IsNotPlayMode() => !Application.isPlaying;
+        
+        [MenuItem("Tools/Compile Helper/Cleanup Useless Obj Files")]
+        public static void CleanupUselessObjFiles()
+        {
+            foreach (var uselessObjDirectoryPath in UselessObjDirectoryPaths)
+            {
+                if (Directory.Exists(uselessObjDirectoryPath))
+                {
+                    Directory.Delete(uselessObjDirectoryPath, true);
+                }
+            }
+        }
+    }
+}
