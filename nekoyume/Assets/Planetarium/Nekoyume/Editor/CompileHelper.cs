31e962e0a866699a141d0fb40f757f03f30b617e
Hyun Seungmin <boscohyun@gmail.com>
2022-05-09 17:42:28 +0900

    Refactor minor

--- nekoyume/Assets/Planetarium/Nekoyume/Editor/CompileHelper.cs
+++ nekoyume/Assets/Planetarium/Nekoyume/Editor/CompileHelper.cs
@@ -6,14 +6,17 @@ namespace Planetarium.Nekoyume.Editor
 {
     public static class CompileHelper
     {
+        private const string CleanupUselessObjFilesMenu =
+            "Tools/Compile Helper/Cleanup Useless Obj Files";
+
         public static readonly string[] UselessObjDirectoryPaths = {
             Path.Combine(Application.dataPath, "_Scripts", "Lib9c", "lib9c", "Lib9c.DevExtensions", "obj"),
         };
 
-        [MenuItem("Tools/Compile Helper/Cleanup Useless Obj Files", true)]
+        [MenuItem(CleanupUselessObjFilesMenu, true)]
         public static bool IsNotPlayMode() => !Application.isPlaying;
         
-        [MenuItem("Tools/Compile Helper/Cleanup Useless Obj Files")]
+        [MenuItem(CleanupUselessObjFilesMenu)]
         public static void CleanupUselessObjFiles()
         {
             foreach (var uselessObjDirectoryPath in UselessObjDirectoryPaths)
@@ -23,6 +26,10 @@ namespace Planetarium.Nekoyume.Editor
                     Directory.Delete(uselessObjDirectoryPath, true);
                 }
             }
+            
+            // AssetDatabase.Refresh();
+            AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
+            Debug.Log($"\"{CleanupUselessObjFilesMenu}\" Finished.");
         }
     }
 }
