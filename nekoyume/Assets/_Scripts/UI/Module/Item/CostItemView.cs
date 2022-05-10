6d07490917e79bdfe26380d5ff877c5553af0ded
unengine <unengine@gmail.com>
2022-05-10 18:02:49 +0900

    Rename prefabs

---
+++ nekoyume/Assets/_Scripts/UI/Module/Item/CostItemView.cs
@@ -0,0 +1,11 @@
+using Nekoyume.UI.Model;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nekoyume.UI.Module
+{
+    public class CostItemView : CountableItemView<CountableItem>
+    {
+    }
+}
