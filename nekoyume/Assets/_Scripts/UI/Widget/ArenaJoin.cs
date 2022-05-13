a1c9ac337b7e1c03f0f81482c89f7154c0c665bd
Hyun Seungmin <boscohyun@gmail.com>
2022-05-13 18:24:44 +0900

    Introduce ArenaJoin UI

---
+++ nekoyume/Assets/_Scripts/UI/Widget/ArenaJoin.cs
@@ -0,0 +1,17 @@
+﻿using Nekoyume.UI.Module.Arena;
+using UnityEngine;
+
+namespace Nekoyume.UI
+{
+    public class ArenaJoin : Widget
+    {
+        [SerializeField]
+        private ArenaJoinSeasonScroll _scroll;
+
+        [SerializeField]
+        private ArenaJoinSeasonInfo _info;
+
+        [SerializeField]
+        private ArenaJoinBottomButtons _buttons;
+    }
+}
