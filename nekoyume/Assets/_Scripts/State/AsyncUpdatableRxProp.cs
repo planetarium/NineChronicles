edf9224f6062cf07ae748c404a329d90857d1c10
Hyun Seungmin <boscohyun@gmail.com>
2022-05-18 15:38:33 +0900

    Introduce `RxProps`

---
+++ nekoyume/Assets/_Scripts/State/AsyncUpdatableRxProp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+
+namespace Nekoyume.State
+{
+    using UniRx;
+
+    public class AsyncUpdatableRxProp<TValue> :
+        ReactiveProperty<TValue>,
+        IAsyncUpdatableReadOnlyRxProp<TValue>
+    {
+        private readonly Func<TValue, UniTask<TValue>> _updateAsyncFunc;
+
+        public AsyncUpdatableRxProp(Func<TValue, UniTask<TValue>> updateAsyncFunc) :
+            this(default, updateAsyncFunc)
+        {
+        }
+
+        public AsyncUpdatableRxProp(TValue defaultValue, Func<TValue, UniTask<TValue>> updateAsyncFunc)
+        {
+            Value = defaultValue;
+            _updateAsyncFunc = updateAsyncFunc
+                               ?? throw new ArgumentNullException(nameof(updateAsyncFunc));
+        }
+
+        public async UniTaskVoid UpdateAsync(bool forceNotify)
+        {
+            var t = await _updateAsyncFunc(Value);
+            SetValue(t, forceNotify);
+        }
+
+        public IObservable<TValue> UpdateAsObservable(bool forceNotify)
+        {
+            var task = _updateAsyncFunc(Value);
+            var observe = task.ToObservable();
+            observe.First().Subscribe(t => SetValue(t, forceNotify));
+            return observe;
+        }
+
+        public IDisposable SubscribeWithUpdateOnce(Action<TValue> onNext)
+        {
+            UpdateAsync(false).Forget();
+            return this.Subscribe(onNext);
+        }
+
+        private void SetValue(TValue value, bool forceNotify)
+        {
+            if (forceNotify)
+            {
+                SetValueAndForceNotify(value);
+            }
+            else
+            {
+                SetValue(value);
+            }
+        }
+    }
+}
---
+++ nekoyume/Assets/_Scripts/State/IAsyncUpdatableReadOnlyRxProp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UniRx;
+
+namespace Nekoyume.State
+{
+    public interface IAsyncUpdatableReadOnlyRxProp<T> : IReadOnlyReactiveProperty<T>
+    {
+        UniTaskVoid UpdateAsync(bool forceNotify);
+
+        IObservable<T> UpdateAsObservable(bool forceNotify);
+
+        IDisposable SubscribeWithUpdateOnce(Action<T> onNext);
+    }
+}
