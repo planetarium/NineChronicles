26dba023e888756a7ea86818d51874fbb8910a6a
Hyun Seungmin <boscohyun@gmail.com>
2022-05-18 16:58:09 +0900

    Use RxProps in ArenaMenu UI

--- nekoyume/Assets/_Scripts/UI/Module/Lobby/ArenaMenu.cs
+++ nekoyume/Assets/_Scripts/UI/Module/Lobby/ArenaMenu.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using Bencodex.Types;
-using Cysharp.Threading.Tasks;
-using Nekoyume.Action;
-using Nekoyume.Model.State;
 using Nekoyume.State;
 using Nekoyume.UI.Module.Arena;
 using TMPro;
@@ -23,15 +19,6 @@ namespace Nekoyume.UI.Module.Lobby
 
         private readonly List<IDisposable> _disposables = new List<IDisposable>();
 
-        private long _beginningBlockIndex;
-
-        private long _endBlockIndex;
-
-        private long _progressBlockIndex;
-
-        private readonly Subject<(long bedinning, long end, long progress)> _subject
-            = new Subject<(long bedinning, long end, long progress)>();
-
         private void OnEnable()
         {
             var agent = Game.Game.instance.Agent;
@@ -41,59 +28,17 @@ namespace Nekoyume.UI.Module.Lobby
                 return;
             }
 
-            agent.BlockIndexSubject
-                .ObserveOnMainThread()
-                .Subscribe(UpdateProgressBar)
+            _progressBar.ResumeOrShow(RxProps.ArenaProgress);
+            _ticketCount.text = string.Empty;
+            RxProps.ArenaInfo.SubscribeWithUpdateOnce(info =>
+                    _ticketCount.text = info.DailyChallengeCount.ToString())
                 .AddTo(_disposables);
-            _progressBar.ResumeOrShow(_subject);
-            UpdateProgressBar(agent.BlockIndex);
-            UpdateTicketCountAsync().Forget();
         }
 
         private void OnDisable()
         {
             _progressBar.Pause();
             _disposables.DisposeAllAndClear();
-        }
-
-        private void UpdateProgressBar(long blockIndex)
-        {
-            var cas = States.Instance.CurrentAvatarState;
-            if (cas is null)
-            {
-                return;
-            }
-
-            _beginningBlockIndex = cas.dailyRewardReceivedIndex;
-            var gcs = States.Instance.GameConfigState;
-            if (gcs is null)
-            {
-                return;
-            }
-
-            _endBlockIndex = _beginningBlockIndex + gcs.DailyArenaInterval;
-            _progressBlockIndex = blockIndex - _beginningBlockIndex;
-            _subject.OnNext((_beginningBlockIndex, _endBlockIndex, _progressBlockIndex));
-        }
-
-        private async UniTaskVoid UpdateTicketCountAsync()
-        {
-            _ticketCount.text = string.Empty;
-
-            var currentAddress = States.Instance.CurrentAvatarState?.address;
-            if (!currentAddress.HasValue)
-            {
-                return;
-            }
-
-            var avatarAddress = currentAddress.Value;
-            var infoAddress = States.Instance.WeeklyArenaState.address.Derive(avatarAddress.ToByteArray());
-            var rawInfo = await UniTask.Run(async () =>
-                await Game.Game.instance.Agent.GetStateAsync(infoAddress));
-            if (rawInfo is Dictionary dictionary)
-            {
-                _ticketCount.text = dictionary["dailyChallengeCount"].ToInteger().ToString();
-            }
         }
     }
 }
