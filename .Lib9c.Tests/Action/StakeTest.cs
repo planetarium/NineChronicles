b241e88bd4749f5dfebdda671deb13bb8a2c82b7
moreal <dev.moreal@gmail.com>
2022-05-23 18:28:31 +0900

    Complement `Stake` test to update amount

--- .Lib9c.Tests/Action/StakeTest.cs
+++ .Lib9c.Tests/Action/StakeTest.cs
@@ -132,6 +132,40 @@ namespace Lib9c.Tests.Action
         }
 
         [Fact]
+        public void Update()
+        {
+            var action = new Stake(50);
+            var states = action.Execute(new ActionContext
+            {
+                PreviousStates = _initialState,
+                Signer = _signerAddress,
+                BlockIndex = 0,
+            });
+
+            states.TryGetStakeState(_signerAddress, out StakeState stakeState);
+            Assert.Equal(0, stakeState.StartedBlockIndex);
+            Assert.Equal(0 + StakeState.LockupInterval, stakeState.CancellableBlockIndex);
+            Assert.Equal(0, stakeState.ReceivedBlockIndex);
+            Assert.Equal(_currency * 50, states.GetBalance(stakeState.address, _currency));
+            Assert.Equal(_currency * 50, states.GetBalance(_signerAddress, _currency));
+
+            var updateAction = new Stake(100);
+            states = updateAction.Execute(new ActionContext
+            {
+                PreviousStates = states,
+                Signer = _signerAddress,
+                BlockIndex = 1,
+            });
+
+            states.TryGetStakeState(_signerAddress, out stakeState);
+            Assert.Equal(1, stakeState.StartedBlockIndex);
+            Assert.Equal(1 + StakeState.LockupInterval, stakeState.CancellableBlockIndex);
+            Assert.Equal(0, stakeState.ReceivedBlockIndex);
+            Assert.Equal(_currency * 100, states.GetBalance(stakeState.address, _currency));
+            Assert.Equal(_currency * 0, states.GetBalance(_signerAddress, _currency));
+        }
+
+        [Fact]
         public void Serialization()
         {
             var action = new Stake(100);
