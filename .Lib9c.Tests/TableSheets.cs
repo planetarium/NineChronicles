be49cd93459cf8e8c2727459a97060a3b6693701
Kim sm <dev.sonohoshi@gmail.com>
2022-05-24 15:27:29 +0900

    Add sheets to TableSheets.cs

--- .Lib9c.Tests/TableSheets.cs
+++ .Lib9c.Tests/TableSheets.cs
@@ -147,6 +147,10 @@ namespace Lib9c.Tests
 
         public CrystalMaterialCostSheet CrystalMaterialCostSheet { get; private set; }
 
+        public CrystalStageBuffGachaSheet CrystalStageBuffGachaSheet { get; private set; }
+
+        public CrystalRandomBuffSheet CrystalRandomBuffSheet { get; private set; }
+
         public ArenaSheet ArenaSheet { get; private set; }
 
         public void ItemSheetInitialize()
