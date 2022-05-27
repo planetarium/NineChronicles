f76173c5306e0a23aa994390841b233841041fc2
Hyun Seungmin <boscohyun@gmail.com>
2022-05-27 14:42:45 +0900

    Introduce ArenaSheetExtensions

---
+++ nekoyume/Assets/_Scripts/Extension/ArenaSheetExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Nekoyume.Model.EnumType;
+using Nekoyume.TableData;
+
+namespace Nekoyume
+{
+    public static class ArenaSheetExtensions
+    {
+        public static bool TryGetSeasonNumber(this ArenaSheet.Row row, int round, out int seasonNumber)
+        {
+            seasonNumber = 0;
+            foreach (var roundData in row.Round)
+            {
+                if (roundData.ArenaType == ArenaType.Season)
+                {
+                    seasonNumber++;
+                }
+
+                if (roundData.Round == round)
+                {
+                    return roundData.ArenaType == ArenaType.Season;
+                }
+            }
+
+            return false;
+        }
+    }
+}
