e7e1291e1e895fee4f940344da4dadca5173685b
tyrosine1153 <tyrosine1153@gmail.com>
2022-06-13 19:32:31 +0900

    Add L10n keys

--- nekoyume/Assets/_Scripts/UI/Widget/Popup/StakingPopupNone.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/Popup/StakingPopupNone.cs
@@ -1,5 +1,6 @@
 using Nekoyume.Game.Controller;
 using Nekoyume.UI.Module;
+using TMPro;
 using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,12 +9,16 @@ namespace Nekoyume.UI
 {
     public class StakingPopupNone : PopupWidget
     {
+        [SerializeField] private TextMeshProUGUI startStakingUnitText;
         [SerializeField] private ConditionalButton uploadButton;
         [SerializeField] private Button closeButton;
 
         protected override void Awake()
         {
             base.Awake();
+
+            Game.Game.instance.TableSheets.StakeRegularRewardSheet.TryGetValue(1, out var row);
+            startStakingUnitText.text = (row?.RequiredGold ?? 0).ToString();
 
             uploadButton.OnSubmitSubject.Subscribe(_ =>
             {
