9a7c196ba23e8c674ddc8c996c51450b5ba35a79
sonohoshi <dev.sonohoshi@gmail.com>
2022-06-30 11:21:23 +0900

    Introduce ToCurrencyNotation() extension method

---
+++ nekoyume/Assets/_Scripts/Extension/BigIntegerExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+
+namespace Nekoyume
+{
+    public static class BigIntegerExtensions
+    {
+        public static string ToCurrencyNotation(this BigInteger num)
+        {
+            var absoluteValue = BigInteger.Abs(num);
+            var exponent = BigInteger.Log10(BigInteger.Abs(num));
+            if (absoluteValue >= BigInteger.One)
+            {
+                switch ((long) Math.Floor(exponent))
+                {
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                        return num.ToString();
+                    case 4:
+                    case 5:
+                        return BigInteger.Divide(num, (BigInteger)1e3) + "K";
+                    case 6:
+                    case 7:
+                    case 8:
+                    case 9:
+                        return BigInteger.Divide(num, (BigInteger)1e6) + "M";
+                    default:
+                        return BigInteger.Divide(num, (BigInteger)1e9) + "B";
+                }
+            }
+
+            return num.ToString();
+        }
+    }
+}
