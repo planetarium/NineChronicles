55fc542e8b08f8a2fceed94457844f5a88ceaed1
Hyun Seungmin <boscohyun@gmail.com>
2022-07-04 15:29:54 +0900

    Remove useless using

--- nekoyume/Assets/_Scripts/State/RxProps.Arena.cs
+++ nekoyume/Assets/_Scripts/State/RxProps.Arena.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Bencodex.Types;
-using Cysharp.Threading.Tasks;
 using Libplanet;
 using Nekoyume.Action;
 using Nekoyume.Arena;
