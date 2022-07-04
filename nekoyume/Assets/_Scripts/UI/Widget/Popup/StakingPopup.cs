357afe01206d6b28504d8f5d664f8d355204f3b0
sonohoshi <dev.sonohoshi@gmail.com>
2022-07-04 18:37:57 +0900

    Refactor miner in StakingPopup

--- nekoyume/Assets/_Scripts/UI/Widget/Popup/StakingPopup.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/Popup/StakingPopup.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using Nekoyume.Game;
 using Nekoyume.Game.Controller;
 using Nekoyume.Helper;
+using Nekoyume.L10n;
 using Nekoyume.Model.Item;
 using Nekoyume.State;
 using Nekoyume.UI.Module;
@@ -15,19 +16,26 @@ namespace Nekoyume.UI
 {
     public class StakingPopup : PopupWidget
     {
-        [SerializeField] private Button closeButton;
+        [SerializeField]
+        private Button closeButton;
 
-        [SerializeField] private ConditionalButton stakingButton;
+        [SerializeField]
+        private ConditionalButton stakingButton;
 
-        [SerializeField] private Image stepImage;
+        [SerializeField]
+        private Image stepImage;
 
-        [SerializeField] private TextMeshProUGUI stepText;
+        [SerializeField]
+        private TextMeshProUGUI stepText;
 
-        [SerializeField] private TextMeshProUGUI stepScoreText;
+        [SerializeField]
+        private TextMeshProUGUI stepScoreText;
 
-        [SerializeField] private StakingItemView[] regularItemViews;
+        [SerializeField]
+        private StakingItemView[] regularItemViews;
 
-        [SerializeField] private RectTransform tooltipRectTransform;
+        [SerializeField]
+        private RectTransform tooltipRectTransform;
 
         [SerializeField]
         private StakingItemView systemRewardView;
@@ -59,7 +67,8 @@ namespace Nekoyume.UI
             tooltipRectTransform.gameObject.SetActive(false);
 
             this.UpdateAsObservable()
-                .Where(_ => tooltipRectTransform.gameObject.activeSelf && Input.GetMouseButtonDown(0))
+                .Where(_ => tooltipRectTransform.gameObject.activeSelf
+                            && Input.GetMouseButtonDown(0))
                 .Subscribe(_ => tooltipRectTransform.gameObject.SetActive(false))
                 .AddTo(gameObject);
         }
@@ -68,9 +77,9 @@ namespace Nekoyume.UI
         {
             var level = States.Instance.StakingLevel;
             var deposit = States.Instance.StakedBalanceState?.Gold.MajorUnit ?? 0;
-            var regularSheet = TableSheets.Instance.StakeRegularRewardSheet;
-            var regularFixedSheet = TableSheets.Instance.StakeRegularFixedRewardSheet;
             var sheets = TableSheets.Instance;
+            var regularSheet = sheets.StakeRegularRewardSheet;
+            var regularFixedSheet = sheets.StakeRegularFixedRewardSheet;
             var stakingMultiplierSheet = sheets.CrystalMonsterCollectionMultiplierSheet;
 
             stepImage.sprite = SpriteHelper.GetStakingIcon(level);
@@ -80,9 +89,9 @@ namespace Nekoyume.UI
             if (regularSheet.TryGetValue(level, out var regular)
                 && regularFixedSheet.TryGetValue(level, out var regularFixed))
             {
-                var materialSheet = Game.Game.instance.TableSheets.MaterialItemSheet;
+                var materialSheet = sheets.MaterialItemSheet;
                 var rewardsCount = Mathf.Min(regular.Rewards.Count, regularItemViews.Length);
-                for (int i = 0; i < rewardsCount; i++)
+                for (var i = 0; i < rewardsCount; i++)
                 {
                     var item = ItemFactory.CreateMaterial(materialSheet, regular.Rewards[i].ItemId);
 
@@ -94,7 +103,7 @@ namespace Nekoyume.UI
                     var view = regularItemViews[i];
                     view.Set(item, result, itemBase =>
                     {
-                        ShowToolTip(itemBase, view.gameObject.transform);
+                        ShowToolTip(itemBase, view.transform);
                     });
                 }
 
