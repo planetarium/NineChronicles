0448ad224a2cbc5aa51968f0f158c02145ca9a64
Hyun Seungmin <boscohyun@gmail.com>
2022-07-15 18:40:39 +0900

    Introduce `EventDungeonExtensions`

---
+++ Lib9c/Extensions/EventDungeonExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Nekoyume.Extensions
+{
+    public static class EventDungeonExtensions
+    {
+        public static int ToEventScheduleId(this int eventDungeonId)
+        {
+            if (eventDungeonId < 10_000_000 ||
+                eventDungeonId > 99_999_999)
+            {
+                throw new ArgumentException(
+                    "DungeonId must be between 10000000 and 99999999.");
+            }
+
+            return eventDungeonId / 10_000;
+        }
+    }
+}
