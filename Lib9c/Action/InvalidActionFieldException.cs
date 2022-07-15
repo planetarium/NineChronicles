d34d10d989143c4bfd6f939d152a616a29cd7022
Hyun Seungmin <boscohyun@gmail.com>
2022-07-15 18:24:10 +0900

    Introduce `InvalideActionFieldsException`

---
+++ Lib9c/Action/InvalidActionFieldException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Nekoyume.Action
+{
+    [Serializable]
+    public class InvalidActionFieldException : Exception
+    {
+        public InvalidActionFieldException()
+        {
+        }
+
+        public InvalidActionFieldException(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidActionFieldException(
+            string message,
+            Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public InvalidActionFieldException(
+            string actionType,
+            string fieldName,
+            string addressesHex,
+            string message = "")
+            : base($"[{actionType}] {fieldName} [{addressesHex}]: {message}")
+        {
+        }
+
+        protected InvalidActionFieldException(
+            SerializationInfo info,
+            StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
