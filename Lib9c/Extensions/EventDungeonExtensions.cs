f5928eef0a0a17d93833dade8bcedb3a2e78727b
Hyun Seungmin <boscohyun@gmail.com>
2022-07-21 22:15:56 +0900

    Introduce `EventDungeonExtensions`

--- Lib9c/Extensions/EventDungeonExtensions.cs
+++ Lib9c/Extensions/EventDungeonExtensions.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nekoyume.Model.Event;
+using Nekoyume.TableData;
+using Nekoyume.TableData.Event;
 
 namespace Nekoyume.Extensions
 {
@@ -15,6 +20,150 @@ namespace Nekoyume.Extensions
             }
 
             return eventDungeonId / 10_000;
+        }
+
+        public static int ToEventDungeonStageNumber(this int eventDungeonStageId)
+        {
+            if (eventDungeonStageId < 10_000_000 ||
+                eventDungeonStageId > 99_999_999)
+            {
+                throw new ArgumentException(
+                    $"{nameof(eventDungeonStageId)}({eventDungeonStageId}) must be" +
+                    " between 10,000,000 and 99,999,999.");
+            }
+
+            return eventDungeonStageId % 10_000;
+        }
+
+        public static bool TryGetRowForDungeon(
+            this EventScheduleSheet sheet,
+            long blockIndex,
+            out EventScheduleSheet.Row row)
+        {
+            if (sheet is null)
+            {
+                row = null;
+                return false;
+            }
+
+            row = sheet.OrderedList.FirstOrDefault(row =>
+                row.StartBlockIndex <= blockIndex &&
+                row.DungeonEndBlockIndex >= blockIndex);
+            return row != null;
+        }
+
+        public static bool TryGetRowForRecipe(
+            this EventScheduleSheet sheet,
+            long blockIndex,
+            out EventScheduleSheet.Row row)
+        {
+            if (sheet is null)
+            {
+                row = null;
+                return false;
+            }
+
+            row = sheet.OrderedList.FirstOrDefault(row =>
+                row.StartBlockIndex <= blockIndex &&
+                row.RecipeEndBlockIndex >= blockIndex);
+            return row != null;
+        }
+
+        public static bool TryGetRowByEventScheduleId(
+            this EventDungeonSheet sheet,
+            int eventScheduleId,
+            out EventDungeonSheet.Row row)
+        {
+            if (sheet is null)
+            {
+                row = null;
+                return false;
+            }
+
+            row = sheet.OrderedList.FirstOrDefault(row =>
+                row.Id.ToEventScheduleId() == eventScheduleId);
+            return row != null;
+        }
+
+        public static bool TryGetRowByEventDungeonStageId(
+            this EventDungeonSheet sheet,
+            int eventDungeonStageId,
+            out EventDungeonSheet.Row row)
+        {
+            foreach (var dungeonRow in sheet.OrderedList)
+            {
+                if (eventDungeonStageId < dungeonRow.StageBegin || eventDungeonStageId > dungeonRow.StageEnd)
+                {
+                    continue;
+                }
+
+                row = dungeonRow;
+                return true;
+            }
+
+            row = null;
+            return false;
+        }
+
+        public static List<EventDungeonStageSheet.Row> GetStageRows(
+            this EventDungeonStageSheet sheet,
+            int beginningStageId,
+            int endStageId)
+        {
+            if (sheet is null)
+            {
+                return new List<EventDungeonStageSheet.Row>();
+            }
+
+            return sheet.OrderedList
+                .Where(row =>
+                    row.Id >= beginningStageId &&
+                    row.Id <= endStageId)
+                .ToList();
+        }
+
+        public static int GetStageNumber(this EventDungeonStageSheet.Row row) =>
+            row.Id.ToEventDungeonStageNumber();
+        
+        public static List<EventDungeonStageWaveSheet.Row> GetStageWaveRows(
+            this EventDungeonStageWaveSheet sheet,
+            int beginningStageId,
+            int endStageId)
+        {
+            if (sheet is null)
+            {
+                return new List<EventDungeonStageWaveSheet.Row>();
+            }
+
+            return sheet.OrderedList
+                .Where(row =>
+                    row.StageId >= beginningStageId &&
+                    row.StageId <= endStageId)
+                .ToList();
+        }
+
+        public static int GetRemainingTicketsConsiderReset(
+            this EventDungeonInfo info,
+            EventScheduleSheet.Row eventScheduleRow,
+            long currentBlockIndex)
+        {
+            if (info is null ||
+                eventScheduleRow is null)
+            {
+                return 0;
+            }
+
+            var blockRange = currentBlockIndex - eventScheduleRow.StartBlockIndex;
+            if (blockRange <= 0)
+            {
+                return 0;
+            }
+
+            var interval =
+                (int)(blockRange / eventScheduleRow.DungeonTicketsResetIntervalBlockRange);
+            return interval > info.ResetTicketsInterval
+                ? eventScheduleRow.DungeonTicketsMax
+                : 0;
         }
     }
 }
