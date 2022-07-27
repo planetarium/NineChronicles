f4753aaa4acca5b67ea1bbd8334b62c01a6640af
sonohoshi <dev.sonohoshi@gmail.com>
2022-07-27 16:52:51 +0900

    Version up HackAndSlash to 16

--- Lib9c/Action/HackAndSlash15.cs
+++ Lib9c/Action/HackAndSlash15.cs
@@ -8,6 +8,7 @@ using Libplanet;
 using Libplanet.Action;
 using Nekoyume.Battle;
 using Nekoyume.Extensions;
+using Nekoyume.Helper;
 using Nekoyume.Model.State;
 using Nekoyume.TableData;
 using Nekoyume.TableData.Crystal;
@@ -17,13 +18,12 @@ using static Lib9c.SerializeKeys;
 namespace Nekoyume.Action
 {
     /// <summary>
-    /// Hard forked at https://github.com/planetarium/lib9c/pull/1225
+    /// Hard forked at https://github.com/planetarium/lib9c/pull/1222
     /// Updated at https://github.com/planetarium/lib9c/pull/1225
     /// </summary>
     [Serializable]
     [ActionType("hack_and_slash15")]
-    [ActionType("hack_and_slash16")]
-    public class HackAndSlash : GameAction
+    public class HackAndSlash15 : GameAction
     {
         public List<Guid> costumes;
         public List<Guid> equipments;
@@ -129,87 +129,21 @@ namespace Nekoyume.Action
             sw.Stop();
             Log.Verbose("{AddressesHex}HAS Get Sheets: {Elapsed}", addressesHex, sw.Elapsed);
 
-            var worldSheet = sheets.GetSheet<WorldSheet>();
-            if (!worldSheet.TryGetValue(worldId, out var worldRow, false))
-            {
-                throw new SheetRowNotFoundException(addressesHex, nameof(WorldSheet), worldId);
-            }
-
-            if (stageId < worldRow.StageBegin ||
-                stageId > worldRow.StageEnd)
-            {
-                throw new SheetRowColumnException(
-                    $"{addressesHex}{worldId} world is not contains {worldRow.Id} stage: " +
-                    $"{worldRow.StageBegin}-{worldRow.StageEnd}");
-            }
-
-            sw.Restart();
-            if (!sheets.GetSheet<StageSheet>().TryGetValue(stageId, out var stageRow))
-            {
-                throw new SheetRowNotFoundException(addressesHex, nameof(StageSheet), stageId);
-            }
-
-            sw.Stop();
-            Log.Verbose("{AddressesHex}HAS Get StageSheet: {Elapsed}", addressesHex, sw.Elapsed);
-
-            sw.Restart();
-            var worldInformation = avatarState.worldInformation;
-            if (!worldInformation.TryGetWorld(worldId, out var world))
-            {
-                // NOTE: Add new World from WorldSheet
-                worldInformation.AddAndUnlockNewWorld(worldRow, blockIndex, worldSheet);
-            }
-
-            if (!world.IsUnlocked)
-            {
-                throw new InvalidWorldException($"{addressesHex}{worldId} is locked.");
-            }
-
-            if (world.StageBegin != worldRow.StageBegin ||
-                world.StageEnd != worldRow.StageEnd)
-            {
-                worldInformation.UpdateWorld(worldRow);
-            }
-
-            if (world.IsStageCleared && stageId > world.StageClearedId + 1 ||
-                !world.IsStageCleared && stageId != world.StageBegin)
-            {
-                throw new InvalidStageException(
-                    $"{addressesHex}Aborted as the stage ({worldId}/{stageId}) is not cleared; " +
-                    $"cleared stage: {world.StageClearedId}"
-                );
-            }
-
-            sw.Stop();
-            Log.Verbose("{AddressesHex}HAS Validate World: {Elapsed}", addressesHex, sw.Elapsed);
-
-            sw.Restart();
-            var equipmentList = avatarState.ValidateEquipmentsV2(equipments, blockIndex);
-            var foodIds = avatarState.ValidateConsumable(foods, blockIndex);
-            var costumeIds = avatarState.ValidateCostume(costumes);
-            sw.Stop();
-            Log.Verbose("{AddressesHex}HAS Validate Items: {Elapsed}", addressesHex, sw.Elapsed);
-
-            if (avatarState.actionPoint < stageRow.CostAP)
-            {
-                throw new NotEnoughActionPointException(
-                    $"{addressesHex}Aborted due to insufficient action point: " +
-                    $"{avatarState.actionPoint} < cost({stageRow.CostAP}))"
-                );
-            }
+            // Validate about avatar state.
+            Validator.ValidateForHackAndSlash(avatarState,
+                sheets,
+                worldId,
+                stageId,
+                equipments,
+                costumes,
+                foods,
+                sw,
+                blockIndex,
+                addressesHex);
 
             var items = equipments.Concat(costumes);
             avatarState.EquipItems(items);
-            avatarState.ValidateItemRequirement(
-                costumeIds.Concat(foodIds).ToList(),
-                equipmentList,
-                sheets.GetSheet<ItemRequirementSheet>(),
-                sheets.GetSheet<EquipmentItemRecipeSheet>(),
-                sheets.GetSheet<EquipmentItemSubRecipeSheetV2>(),
-                sheets.GetSheet<EquipmentItemOptionSheet>(),
-                addressesHex);
-
-            avatarState.actionPoint -= stageRow.CostAP;
+            avatarState.actionPoint -= sheets.GetSheet<StageSheet>()[stageId].CostAP;
             sw.Stop();
             Log.Verbose("{AddressesHex}HAS Unequip items: {Elapsed}", addressesHex, sw.Elapsed);
 
@@ -228,7 +162,6 @@ namespace Nekoyume.Action
                     sheets.GetSheet<QuestRewardSheet>(),
                     sheets.GetSheet<QuestItemRewardSheet>(),
                     sheets.GetSheet<EquipmentItemRecipeSheet>());
-
                 sw.Stop();
                 Log.Verbose("{AddressesHex}HAS Update QuestList: {Elapsed}", addressesHex, sw.Elapsed);
             }
@@ -237,7 +170,7 @@ namespace Nekoyume.Action
 
             var skillStateAddress = Addresses.GetSkillStateAddressFromAvatarAddress(avatarAddress);
             CrystalRandomSkillState skillState = null;
-            var isNotClearedStage = !worldInformation.IsStageCleared(stageId);
+            var isNotClearedStage = !avatarState.worldInformation.IsStageCleared(stageId);
             var skillsOnWaveStart = new List<Model.Skill.Skill>();
             if (isNotClearedStage)
             {
@@ -313,7 +246,7 @@ namespace Nekoyume.Action
                     worldId,
                     stageId,
                     blockIndex,
-                    worldSheet,
+                    sheets.GetSheet<WorldSheet>(),
                     sheets.GetSheet<WorldUnlockSheet>()
                 );
                 sw.Stop();
