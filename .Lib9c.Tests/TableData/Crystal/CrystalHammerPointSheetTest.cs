95170dab13aa44fdb3237d2efd2bec8d993ac26b
sonohoshi <dev.sonohoshi@gmail.com>
2022-08-04 17:23:14 +0900

    Add CrystalHammerPointSheet
    for new combination equipment feature

---
+++ .Lib9c.Tests/TableData/Crystal/CrystalHammerPointSheetTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Lib9c.Tests.TableData.Crystal
+{
+    using System.Linq;
+    using Nekoyume.TableData.Crystal;
+    using Xunit;
+
+    public class CrystalHammerPointSheetTest
+    {
+        [Fact]
+        public void Set()
+        {
+            var csv = @"recipe_ID,max_hammer_point,crystal_cost
+1,3,10";
+            var sheet = new CrystalHammerPointSheet();
+            sheet.Set(csv);
+            var row = sheet.First().Value;
+
+            Assert.Equal(1, row.RecipeId);
+            Assert.Equal(3, row.MaxPoint);
+            Assert.Equal(10, row.CRYSTAL);
+        }
+    }
+}
