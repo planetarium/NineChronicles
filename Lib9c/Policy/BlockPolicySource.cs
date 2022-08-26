40b0cc83c7ebc9db410c2df1aa3ce9db034c2a09
Seo Myunggyun (Jonathan) <tkiapril@users.noreply.github.com>
2022-08-26 16:34:47 +0900

    Bump libplanet to 0.41.0

--- Lib9c/Policy/BlockPolicySource.cs
+++ Lib9c/Policy/BlockPolicySource.cs
@@ -296,15 +296,15 @@ namespace Nekoyume.BlockChain.Policy
             if (transaction.Actions.Count > 1)
             {
                 return new TxPolicyViolationException(
-                    transaction.Id,
                     $"Transaction {transaction.Id} has too many actions: " +
-                    $"{transaction.Actions.Count}");
+                    $"{transaction.Actions.Count}",
+                    transaction.Id);
             }
             else if (IsObsolete(transaction, index))
             {
                 return new TxPolicyViolationException(
-                    transaction.Id,
-                    $"Transaction {transaction.Id} is obsolete.");
+                    $"Transaction {transaction.Id} is obsolete.",
+                    transaction.Id);
             }
 
             try
@@ -317,9 +317,9 @@ namespace Nekoyume.BlockChain.Policy
                     // any actions.
                     return transaction.Actions.Any()
                         ? new TxPolicyViolationException(
-                            transaction.Id,
                             $"Transaction {transaction.Id} by an authorized miner should not " +
-                            $"have any action: {transaction.Actions.Count}")
+                            $"have any action: {transaction.Actions.Count}",
+                            transaction.Id)
                         : null;
                 }
 
@@ -333,8 +333,8 @@ namespace Nekoyume.BlockChain.Policy
                         new PendingActivationState(rawPending).Verify(aa.GetSignature())
                         ? null
                         : new TxPolicyViolationException(
-                            transaction.Id,
-                            $"Transaction {transaction.Id} has an invalid activate action.");
+                            $"Transaction {transaction.Id} has an invalid activate action.",
+                            transaction.Id);
                 }
 
                 // Check admin
@@ -356,9 +356,9 @@ namespace Nekoyume.BlockChain.Policy
                                 activatedAccounts.Contains(transaction.Signer)
                                 ? null
                                 : new TxPolicyViolationException(
-                                    transaction.Id,
                                     $"Transaction {transaction.Id} is by a signer " +
-                                    $"without account activation: {transaction.Signer}");
+                                    $"without account activation: {transaction.Signer}",
+                                    transaction.Id);
                         }
                         return null;
                     case Bencodex.Types.Boolean _:
@@ -370,8 +370,8 @@ namespace Nekoyume.BlockChain.Policy
             catch (InvalidSignatureException)
             {
                 return new TxPolicyViolationException(
-                    transaction.Id,
-                    $"Transaction {transaction.Id} has invalid signautre.");
+                    $"Transaction {transaction.Id} has invalid signautre.",
+                    transaction.Id);
             }
             catch (IncompleteBlockStatesException)
             {
