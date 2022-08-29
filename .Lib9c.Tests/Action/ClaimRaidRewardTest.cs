e4987015b495446dc88e6dae7f342e27bfa5caa3
unengine <unengine@gmail.com>
2022-08-29 16:00:50 +0900

    Fix inline data

--- .Lib9c.Tests/Action/ClaimRaidRewardTest.cs
+++ .Lib9c.Tests/Action/ClaimRaidRewardTest.cs
@@ -33,14 +33,14 @@ namespace Lib9c.Tests.Action
         // Already Claim.
         [InlineData(typeof(NotEnoughRankException), 10_000, 1, 0, 0)]
         // Skip previous reward.
-        [InlineData(null, 50_000, 1, 537_500, 545)]
+        [InlineData(null, 50_000, 1, 645_000, 1_100)]
         // Claim all reward.
-        [InlineData(null, 10_000, 0, 25_000, 50)]
-        [InlineData(null, 20_000, 0, 81_250, 130)]
-        [InlineData(null, 30_000, 0, 181_250, 250)]
-        [InlineData(null, 40_000, 0, 337_500, 405)]
-        [InlineData(null, 50_000, 0, 562_500, 595)]
-        [InlineData(null, 90_000, 0, 562_500, 595)]
+        [InlineData(null, 10_000, 0, 30_000, 50)]
+        [InlineData(null, 20_000, 0, 97_500, 150)]
+        [InlineData(null, 30_000, 0, 217_500, 350)]
+        [InlineData(null, 40_000, 0, 405_000, 650)]
+        [InlineData(null, 50_000, 0, 675_000, 1_150)]
+        [InlineData(null, 90_000, 0, 675_000, 1_150)]
         public void Execute(Type exc, int highScore, int latestRank, int expectedCrystal, int expectedRune)
         {
             Address avatarAddress = default;
