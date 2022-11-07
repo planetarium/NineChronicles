9b8756e1ee8c74c5d345e973033ec22f6a50263a
sonohoshi <dev.sonohoshi@gmail.com>
2022-11-07 15:06:12 +0900

    Add sheets about grand finale
    GrandFinaleScheduleSheet, GrandFinaleParticipantsSheet

---
+++ Lib9c/TableData/GrandFinale/GrandFinaleParticipantsSheet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Libplanet;
+
+namespace Nekoyume.TableData.GrandFinale
+{
+    using static TableExtensions;
+
+    [Serializable]
+    public class GrandFinaleParticipantsSheet : Sheet<int, GrandFinaleParticipantsSheet.Row>
+    {
+        [Serializable]
+        public class Row : SheetRow<int>
+        {
+            public override int Key => GrandFinaleId;
+
+            public int GrandFinaleId { get; private set; }
+
+            public List<Address> Participants { get; private set; }
+
+            public override void Set(IReadOnlyList<string> fields)
+            {
+                GrandFinaleId = ParseInt(fields[0]);
+                Participants = new List<Address> {new Address(fields[1])};
+            }
+        }
+
+        protected override void AddRow(int key, Row value)
+        {
+            if (!TryGetValue(key, out var row))
+            {
+                Add(key, value);
+
+                return;
+            }
+
+            if (!value.Participants.Any())
+            {
+                return;
+            }
+
+            row.Participants.Add(value.Participants[0]);
+        }
+
+        public GrandFinaleParticipantsSheet() : base(nameof(GrandFinaleParticipantsSheet))
+        {
+        }
+    }
+}
