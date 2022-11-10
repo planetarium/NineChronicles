20e6cebb3c5fdcd8f37ba1d2e305615c570be832
sonohoshi <dev.sonohoshi@gmail.com>
2022-11-10 14:40:25 +0900

    Add GrandFinaleInformation
    model and unit test

---
+++ .Lib9c.Tests/Model/GrandFinale/GrandFinaleInformationTest.cs
@@ -0,0 +1,67 @@
+﻿namespace Lib9c.Tests.Model.GrandFinale
+{
+    using Bencodex.Types;
+    using Libplanet;
+    using Libplanet.Crypto;
+    using Nekoyume.Model.GrandFinale;
+    using Xunit;
+
+    public class GrandFinaleInformationTest
+    {
+        [Fact]
+        public void Serialize()
+        {
+            var avatarAddress = new PrivateKey().ToAddress();
+            var addr1 = new PrivateKey().ToAddress();
+            var addr2 = new PrivateKey().ToAddress();
+            var state = new GrandFinaleInformation(avatarAddress, 1, 1);
+            state.UpdateRecord(addr1, true);
+            state.UpdateRecord(addr2, false);
+            var serialized = (List)state.Serialize();
+            var deserialized = new GrandFinaleInformation(serialized);
+
+            Assert.Equal(state.Address, deserialized.Address);
+            Assert.Equal(state.Ticket, deserialized.Ticket);
+            Assert.True(state.TryGetBattleRecord(addr1, out var win));
+            Assert.True(deserialized.TryGetBattleRecord(addr1, out var deserializedWin));
+            Assert.Equal(win, deserializedWin);
+
+            Assert.True(state.TryGetBattleRecord(addr2, out win));
+            Assert.True(deserialized.TryGetBattleRecord(addr2, out deserializedWin));
+            Assert.Equal(win, deserializedWin);
+        }
+
+        [Fact]
+        public void UseTicket()
+        {
+            var avatarAddress = new PrivateKey().ToAddress();
+            var state = new GrandFinaleInformation(avatarAddress, 1, 1);
+            Assert.Equal(1, state.Ticket);
+            state.UseTicket();
+            var serialized = (List)state.Serialize();
+            var deserialized = new GrandFinaleInformation(serialized);
+
+            Assert.Equal(state.Address, deserialized.Address);
+            Assert.Equal(state.Ticket, deserialized.Ticket);
+            Assert.Equal(0, deserialized.Ticket);
+        }
+
+        [Fact]
+        public void UpdateBattleRecord()
+        {
+            var avatarAddress = new PrivateKey().ToAddress();
+            var state = new GrandFinaleInformation(avatarAddress, 1, 1);
+            Assert.Equal(1, state.Ticket);
+
+            var enemyAddr = new PrivateKey().ToAddress();
+            state.UpdateRecord(enemyAddr, true);
+            var serialized = (List)state.Serialize();
+            var deserialized = new GrandFinaleInformation(serialized);
+
+            Assert.Equal(state.Address, deserialized.Address);
+            Assert.True(state.TryGetBattleRecord(enemyAddr, out var win));
+            Assert.True(win);
+            Assert.False(state.TryGetBattleRecord(new PrivateKey().ToAddress(), out _));
+        }
+    }
+}
---
+++ Lib9c/Model/GrandFinale/GrandFinaleInformation.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using Bencodex.Types;
+using Libplanet;
+using Nekoyume.Action;
+using Nekoyume.Model.Arena;
+using Nekoyume.Model.State;
+
+namespace Nekoyume.Model.GrandFinale
+{
+    using System.Linq;
+    public class GrandFinaleInformation : IState
+    {
+        public static Address DeriveAddress(Address avatarAddress, int grandFinaleId) =>
+            avatarAddress.Derive($"arena_information_{grandFinaleId}");
+
+        public Address Address;
+        public int Ticket { get; private set; }
+        private Dictionary<Address, bool> BattleRecordDictionary { get; }
+
+        public GrandFinaleInformation(Address avatarAddress, int grandFinaleId, int ticket)
+        {
+            Address = DeriveAddress(avatarAddress, grandFinaleId);
+            Ticket = ticket;
+            BattleRecordDictionary = new Dictionary<Address, bool>();
+        }
+
+        public GrandFinaleInformation(List serialized)
+        {
+            Address = serialized[0].ToAddress();
+            Ticket = (Integer)serialized[1];
+            BattleRecordDictionary =
+                ((Dictionary)serialized[2]).ToDictionary(pair => pair.Key.ToAddress(),
+                    pair => pair.Value.ToBoolean());
+        }
+
+        public IValue Serialize()
+        {
+            var battleRecordDict = new Dictionary(BattleRecordDictionary
+                .OrderBy(pair => pair.Key)
+                .Select(pair =>
+                    new KeyValuePair<IKey, IValue>(
+                        (IKey)pair.Key.Serialize(),
+                        pair.Value.Serialize()))
+            );
+            return List.Empty
+                .Add(Address.Serialize())
+                .Add(Ticket)
+                .Add(battleRecordDict);
+        }
+
+        public void UseTicket()
+        {
+            if (Ticket <= 0)
+            {
+                throw new NotEnoughTicketException(
+                    $"[{nameof(GrandFinaleInformation)}] have({Ticket})");
+            }
+
+            Ticket -= 1;
+        }
+
+        public void UpdateRecord(Address enemyAddress, bool win)
+        {
+            BattleRecordDictionary.TryAdd(enemyAddress, win);
+        }
+
+        public bool TryGetBattleRecord(Address enemyAddress, out bool win) =>
+            BattleRecordDictionary.TryGetValue(enemyAddress, out win);
+    }
+}
