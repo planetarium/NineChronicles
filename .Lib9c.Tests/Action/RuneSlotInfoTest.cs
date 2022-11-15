616589983f43b5b2372664086121e0926f6ea495
Yang Chun Ung <qooraven@gmail.com>
2022-11-15 15:06:59 +0900

    Fix RuneSlotInfo serialize

---
+++ .Lib9c.Tests/Action/RuneSlotInfoTest.cs
@@ -0,0 +1,21 @@
+namespace Lib9c.Tests.Action
+{
+    using Bencodex.Types;
+    using Nekoyume.Action;
+    using Xunit;
+
+    public class RuneSlotInfoTest
+    {
+        [Fact]
+        public void Serialize()
+        {
+            var info = new RuneSlotInfo(1, 1, 1);
+            var serialized = info.Serialize();
+            var deserialized = new RuneSlotInfo((List)serialized);
+
+            Assert.Equal(1, deserialized.Level);
+            Assert.Equal(1, deserialized.RuneId);
+            Assert.Equal(1, deserialized.SlotIndex);
+        }
+    }
+}
