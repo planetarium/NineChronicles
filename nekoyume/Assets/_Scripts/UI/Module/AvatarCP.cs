22f3c70dc5e1f2dd4d1c179ba3dc918e9b3c7d9e
hskim881028 <andykim881028@gmail.com>
2022-11-17 14:43:44 +0900

    Add decrease cp effect

--- nekoyume/Assets/_Scripts/UI/Module/AvatarCP.cs
+++ nekoyume/Assets/_Scripts/UI/Module/AvatarCP.cs
@@ -1,7 +1,7 @@
-using System;
 using System.Collections;
 using Nekoyume.UI.Tween;
 using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace Nekoyume.UI.Module
@@ -15,10 +15,16 @@ namespace Nekoyume.UI.Module
         private DigitTextTweener cpTextValueTweener = null;
 
         [SerializeField]
-        private GameObject additionalCpArea = null;
+        private GameObject increaseCpArea = null;
+
+        [SerializeField]
+        private GameObject decreaseCpArea = null;
 
         [SerializeField]
         private TextMeshProUGUI additionalCpText = null;
+
+        [SerializeField]
+        private TextMeshProUGUI decreaseCpText = null;
 
         private Coroutine _disableCpTween;
 
@@ -28,7 +34,8 @@ namespace Nekoyume.UI.Module
             {
                 StopCoroutine(_disableCpTween);
             }
-            additionalCpArea.gameObject.SetActive(false);
+
+            increaseCpArea.gameObject.SetActive(false);
         }
 
         public void UpdateCP(int cp)
@@ -44,22 +51,27 @@ namespace Nekoyume.UI.Module
             }
 
             cpTextValueTweener.Play(prevCp, currentCp);
+            increaseCpArea.gameObject.SetActive(false);
+            decreaseCpArea.gameObject.SetActive(false);
             if (prevCp < currentCp)
             {
-                additionalCpArea.gameObject.SetActive(true);
+                increaseCpArea.gameObject.SetActive(true);
                 additionalCpText.text = (currentCp - prevCp).ToString();
-                _disableCpTween = StartCoroutine(CoDisableIncreasedCp());
             }
-            else
+            else if (prevCp > currentCp)
             {
-                additionalCpArea.gameObject.SetActive(false);
+                decreaseCpArea.gameObject.SetActive(true);
+                decreaseCpText.text = (math.abs(currentCp - prevCp)).ToString();
             }
+
+            _disableCpTween = StartCoroutine(CoDisableAdditionalCp());
         }
 
-        private IEnumerator CoDisableIncreasedCp()
+        private IEnumerator CoDisableAdditionalCp()
         {
             yield return new WaitForSeconds(1.5f);
-            additionalCpArea.gameObject.SetActive(false);
+            increaseCpArea.gameObject.SetActive(false);
+            decreaseCpArea.gameObject.SetActive(false);
         }
     }
 }
