099a8c7c1afd2a758ab0de0bfd89f36dc3a6bae1
sonohoshi <dev.sonohoshi@gmail.com>
2022-11-21 20:51:55 +0900

    Change slider of schedule ui

--- nekoyume/Assets/_Scripts/UI/Module/GrandFinaleJoin.cs
+++ nekoyume/Assets/_Scripts/UI/Module/GrandFinaleJoin.cs
@@ -2,6 +2,8 @@
 using Nekoyume.Game.Controller;
 using Nekoyume.Helper;
 using Nekoyume.State;
+using Nekoyume.TableData;
+using Nekoyume.TableData.GrandFinale;
 using Nekoyume.UI.Module.Arena.Join;
 using Nekoyume.ValueControlComponents.Shader;
 using TMPro;
@@ -13,11 +15,6 @@ namespace Nekoyume.UI.Module
 {
     public class GrandFinaleJoin : MonoBehaviour
     {
-        private const string OffSeasonString = "off-season";
-
-        [SerializeField]
-        private ArenaJoinSeasonInfo arenaJoinSeasonInfo;
-
         [SerializeField]
         private ConditionalButton arenaJoinButton;
 
@@ -25,24 +22,21 @@ namespace Nekoyume.UI.Module
         private ConditionalButton grandFinaleJoinButton;
 
         [SerializeField]
-        private ShaderPropertySlider seasonProgressSlider;
+        private Image arenaProgressFillImage;
 
         [SerializeField]
-        private Image seasonProgressFillImage;
+        private TextMeshProUGUI arenaProgressSliderFillText;
 
         [SerializeField]
-        private TextMeshProUGUI seasonProgressSliderFillText;
+        private Image grandFinaleProgressFillImage;
+
+        [SerializeField]
+        private TextMeshProUGUI grandFinaleProgressSliderFillText;
 
         public void Set(System.Action onClickJoinArena)
         {
             var blockIndex = Game.Game.instance.Agent.BlockIndex;
             var roundData = TableSheets.Instance.ArenaSheet.GetRoundByBlockIndex(blockIndex);
-            arenaJoinSeasonInfo.SetData(
-                OffSeasonString,
-                roundData,
-                ArenaJoinSeasonInfo.RewardType.Food,
-                null
-            );
             arenaJoinButton.OnClickSubject.Subscribe(_ => onClickJoinArena.Invoke()).AddTo(gameObject);
 
             var grandFinaleRow =
@@ -55,32 +49,71 @@ namespace Nekoyume.UI.Module
                     .Show(grandFinaleRow, States.Instance.GrandFinaleStates.GrandFinaleParticipants);
             }).AddTo(gameObject);
 
+            SetGrandFinaleInfo(grandFinaleRow, blockIndex);
+            SetArenaInfo(roundData, blockIndex);
+        }
+
+        private void SetGrandFinaleInfo(
+            GrandFinaleScheduleSheet.Row grandFinaleRow,
+            long blockIndex)
+        {
             var (beginning, end, current) = (grandFinaleRow.StartBlockIndex,
                 grandFinaleRow.EndBlockIndex, blockIndex);
             if (current > end)
             {
-                seasonProgressFillImage.enabled = false;
-                seasonProgressSliderFillText.enabled = false;
+                grandFinaleProgressFillImage.enabled = false;
+                grandFinaleProgressSliderFillText.enabled = false;
 
                 return;
             }
 
             if (current < beginning)
             {
-                seasonProgressFillImage.enabled = false;
-                seasonProgressSliderFillText.text = Util.GetBlockToTime(beginning - current);
-                seasonProgressSliderFillText.enabled = true;
+                grandFinaleProgressFillImage.enabled = false;
+                grandFinaleProgressSliderFillText.text = Util.GetBlockToTime(beginning - current);
+                grandFinaleProgressSliderFillText.enabled = true;
 
                 return;
             }
 
             var range = end - beginning;
             var progress = current - beginning;
-            var sliderNormalizedValue = (float)progress / range;
-            seasonProgressSlider.NormalizedValue = sliderNormalizedValue;
-            seasonProgressFillImage.enabled = true;
-            seasonProgressSliderFillText.text = Util.GetBlockToTime(range - progress);
-            seasonProgressSliderFillText.enabled = true;
+            var sliderNormalizedValue = (float) progress / range;
+            grandFinaleProgressFillImage.fillAmount = sliderNormalizedValue;
+            grandFinaleProgressFillImage.enabled = true;
+            grandFinaleProgressSliderFillText.text = Util.GetBlockToTime(range - progress);
+            grandFinaleProgressSliderFillText.enabled = true;
+        }
+
+        private void SetArenaInfo(
+            ArenaSheet.RoundData roundData,
+            long blockIndex)
+        {
+            var (beginning, end, current) = roundData.GetSeasonProgress(blockIndex);
+            if (current > end)
+            {
+                arenaProgressFillImage.enabled = false;
+                arenaProgressSliderFillText.enabled = false;
+
+                return;
+            }
+
+            if (current < beginning)
+            {
+                arenaProgressFillImage.enabled = false;
+                arenaProgressSliderFillText.text = Util.GetBlockToTime(beginning - current);
+                arenaProgressSliderFillText.enabled = true;
+
+                return;
+            }
+
+            var range = end - beginning;
+            var progress = current - beginning;
+            var sliderNormalizedValue = (float) progress / range;
+            arenaProgressFillImage.fillAmount = sliderNormalizedValue;
+            arenaProgressFillImage.enabled = true;
+            arenaProgressSliderFillText.text = Util.GetBlockToTime(range - progress);
+            arenaProgressSliderFillText.enabled = true;
         }
     }
 }
