500dc116b1041704551e6ff830b3faabc7ea6590
Say Cheong <greymistcube@gmail.com>
2022-12-08 14:04:30 +0900

    Adjusted mainnet policy to default

--- Lib9c/Policy/MinBlockProtocolVersionPolicy.cs
+++ Lib9c/Policy/MinBlockProtocolVersionPolicy.cs
@@ -18,9 +18,6 @@ namespace Nekoyume.BlockChain.Policy
             new MinBlockProtocolVersionPolicy(0);
 
         public static IVariableSubPolicy<int> Mainnet =>
-            Default
-                .Add(new SpannedSubPolicy<int>(
-                    startIndex: 4_000L,
-                    value: 4));
+            Default;
     }
 }
