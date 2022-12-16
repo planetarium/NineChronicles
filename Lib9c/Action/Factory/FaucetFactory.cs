054e8938ba461e1ccb9a2abaa98f2d569bdb80b8
hyeon <hyeon@planetariumhq.com>
2022-12-16 17:00:24 +0900

    Update FaucetFactory to create FaucetRune

--- Lib9c/Action/Factory/FaucetFactory.cs
+++ Lib9c/Action/Factory/FaucetFactory.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Libplanet;
 using Nekoyume.Action.Interface;
 
@@ -5,13 +6,19 @@ namespace Nekoyume.Action.Factory
 {
     public class FaucetFactory
     {
-        public static IFaucet CreateFaucet(
+        public static IFaucet CreateFaucetCurrency(
             Address agentAddress,
             int faucetNcg,
             int faucetCrystal
         )
         {
-            return new Faucet(agentAddress, faucetNcg, faucetCrystal);
+            return new FaucetCurrency(agentAddress, faucetNcg, faucetCrystal);
+        }
+
+        public static IFaucet CreateFaucetRune(
+            Address avatarAddress, Dictionary<int, int> faucetRunes)
+        {
+            return new FaucetRune(avatarAddress, faucetRunes);
         }
     }
 }
