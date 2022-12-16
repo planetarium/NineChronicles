6d9e08da4632b23c5ecb7c2afb683004268e069a
hyeon <hyeon@planetariumhq.com>
2022-12-16 16:45:13 +0900

    Update faucet action: add faucet rune

--- Lib9c/Action/Faucet.cs
+++ Lib9c/Action/Faucet.cs
@@ -1,27 +1,27 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using Bencodex.Types;
 using Libplanet;
 using Libplanet.Action;
 using Libplanet.Assets;
 using Nekoyume.Action.Interface;
+using Nekoyume.Helper;
 using Nekoyume.Model.State;
+using Nekoyume.TableData;
 
 namespace Nekoyume.Action
 {
     [Serializable]
-    [ActionType("faucet")]
-    public class Faucet : GameAction, IFaucet
+    [ActionType("faucet_currency")]
+    public class FaucetCurrency : GameAction, IFaucet
     {
         public Address AgentAddress;
         public int FaucetNcg;
         public int FaucetCrystal;
 
-        // public Address AvatarAddress;
-        // public Dictionary<int, int> Runes = new Dictionary<int, int>();
-
-
-        public Faucet(Address agentAddress, int faucetNcg = 0, int faucetCrystal = 0)
+        public FaucetCurrency(Address agentAddress, int faucetNcg = 0, int faucetCrystal = 0)
         {
             AgentAddress = agentAddress;
             FaucetNcg = faucetNcg;
@@ -49,6 +49,7 @@ namespace Nekoyume.Action
                 states = states.MintAsset(AgentAddress, crystal * FaucetCrystal);
             }
 
+
             return states;
 // #endif
             throw new ActionUnavailableException("This action is only for test.");
@@ -73,6 +74,58 @@ namespace Nekoyume.Action
             // AvatarAddress = plainValue["avatar_address"].ToAddress();
             // Runes = ((Dictionary)plainValue["runes"]).ToDictionary(pair =>
             // pair.Key.ToInteger(), pair => pair.Value.ToInteger());
+        }
+    }
+
+    [Serializable]
+    [ActionType("faucet_rune")]
+    public class FaucetRune : GameAction, IFaucet
+    {
+        public Address AvatarAddress;
+        public Dictionary<int, int> FaucetRunes;
+
+        public FaucetRune(Address avatarAddress, Dictionary<int, int> faucetRunes)
+        {
+            AvatarAddress = avatarAddress;
+            FaucetRunes = faucetRunes;
+        }
+
+        public override IAccountStateDelta Execute(IActionContext context)
+        {
+// #if TEST_9C
+            if (context.Rehearsal)
+            {
+                return context.PreviousStates;
+            }
+
+            var states = context.PreviousStates;
+            if (!(FaucetRunes is null))
+            {
+                RuneSheet runeSheet = states.GetSheet<RuneSheet>();
+                if (runeSheet.OrderedList != null)
+                {
+                    foreach (var rune in FaucetRunes.OrderBy(x => x.Key))
+                    {
+                        states = states.MintAsset(AvatarAddress, RuneHelper.ToFungibleAssetValue(
+                            runeSheet.OrderedList.First(r => r.Id == rune.Key),
+                            rune.Value
+                        ));
+                    }
+                }
+            }
+
+            return states;
+
+// #endif
+            throw new ActionUnavailableException("This action is only for test.");
+        }
+
+        protected override IImmutableDictionary<string, IValue> PlainValueInternal { get; }
+
+        protected override void LoadPlainValueInternal(
+            IImmutableDictionary<string, IValue> plainValue)
+        {
+            throw new NotImplementedException();
         }
     }
 }
