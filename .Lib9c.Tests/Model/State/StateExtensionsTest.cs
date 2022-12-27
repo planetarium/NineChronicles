b674661e6e12ac0c26daedc25ff51eff62cc724b
Hyun Seungmin <boscohyun@gmail.com>
2022-12-27 14:03:09 +0900

    Add unit tests minor

--- .Lib9c.Tests/Model/State/StateExtensionsTest.cs
+++ .Lib9c.Tests/Model/State/StateExtensionsTest.cs
@@ -1,15 +1,52 @@
 namespace Lib9c.Tests.Model.State
 {
+    using System.Collections.Generic;
     using System.Numerics;
+    using Libplanet;
+    using Libplanet.Crypto;
     using Nekoyume.Model.State;
     using Xunit;
 
     public class StateExtensionsTest
     {
+        public static IEnumerable<object[]> Get_Long_MemberData()
+        {
+            yield return new object[] { long.MinValue };
+            yield return new object[] { 0L };
+            yield return new object[] { long.MaxValue };
+        }
+
+        public static IEnumerable<object[]> Get_BigInteger_MemberData()
+        {
+            yield return new object[] { (BigInteger)decimal.MinValue };
+            yield return new object[] { (BigInteger)0 };
+            yield return new object[] { (BigInteger)decimal.MaxValue };
+        }
+
+        [Fact]
+        public void Address()
+        {
+            var addr = new PrivateKey().ToAddress();
+            var ser = addr.Serialize();
+            var de = ser.ToAddress();
+            Assert.Equal(addr, de);
+            var ser2 = de.Serialize();
+            Assert.Equal(ser, ser2);
+        }
+
+        [Fact]
+        public void NullableAddress()
+        {
+            Address? addr = new PrivateKey().ToAddress();
+            var ser = addr.Serialize();
+            var de = ser.ToNullableAddress();
+            Assert.Equal(addr, de);
+            var ser2 = de.Serialize();
+            Assert.Equal(ser, ser2);
+        }
+
         [Theory]
-        [InlineData(long.MinValue)]
-        [InlineData(0L)]
-        [InlineData(long.MaxValue)]
+        [MemberData(nameof(Get_Long_MemberData))]
         public void Long(long value)
         {
             var ser = value.Serialize();
@@ -20,9 +57,7 @@ namespace Lib9c.Tests.Model.State
         }
 
         [Theory]
-        [InlineData(long.MinValue)]
-        [InlineData(0L)]
-        [InlineData(long.MaxValue)]
+        [MemberData(nameof(Get_Long_MemberData))]
         public void NullableLong(long? value)
         {
             var ser = value.Serialize();
@@ -32,38 +67,26 @@ namespace Lib9c.Tests.Model.State
             Assert.Equal(ser, ser2);
         }
 
-        [Fact]
-        public void SerializeBigInteger()
+        [Theory]
+        [MemberData(nameof(Get_BigInteger_MemberData))]
+        public void BigInteger(BigInteger value)
         {
-            Assert.Equal(
-                (Bencodex.Types.Integer)123,
-                StateExtensions.Serialize((BigInteger)123)
-            );
-            Assert.Equal(
-                (Bencodex.Types.Integer)456,
-                StateExtensions.Serialize((BigInteger?)456)
-            );
-            Assert.Equal(
-                Bencodex.Types.Null.Value,
-                StateExtensions.Serialize((BigInteger?)null)
-            );
+            var ser = value.Serialize();
+            var de = ser.ToBigInteger();
+            Assert.Equal(value, de);
+            var ser2 = de.Serialize();
+            Assert.Equal(ser, ser2);
         }
 
-        [Fact]
-        public void DeserializeBigInteger()
+        [Theory]
+        [MemberData(nameof(Get_BigInteger_MemberData))]
+        public void NullableBigInteger(BigInteger? value)
         {
-            Assert.Equal(
-                (BigInteger)123,
-                new Bencodex.Types.Integer(123).ToBigInteger()
-            );
-            Assert.Equal(
-                (BigInteger?)123,
-                new Bencodex.Types.Integer(123).ToNullableBigInteger()
-            );
-            Assert.Equal(
-                (BigInteger?)null,
-                Bencodex.Types.Null.Value.ToNullableBigInteger()
-            );
+            var ser = value.Serialize();
+            var de = ser.ToNullableBigInteger();
+            Assert.Equal(value, de);
+            var ser2 = de.Serialize();
+            Assert.Equal(ser, ser2);
         }
     }
 }
