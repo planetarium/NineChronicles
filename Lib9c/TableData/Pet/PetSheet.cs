b45a80e69365318f10bc0dbccab858bf02f252d7
sonohoshi <dev.sonohoshi@gmail.com>
2023-01-25 15:38:28 +0900

    Make TableSheets about pet system
    PetSheet, PerCostSheet, PerOptionSheet, PerOptionType:Enum

---
+++ Lib9c/TableData/Pet/PetSheet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using static Nekoyume.TableData.TableExtensions;
+
+namespace Nekoyume.TableData.Pet
+{
+    public class PetSheet : Sheet<int, PetSheet.Row>
+    {
+        public class Row : SheetRow<int>
+        {
+            public int Id;
+            public int Grade;
+            public string SoulStoneTicker;
+            public override int Key => Id;
+            public override void Set(IReadOnlyList<string> fields)
+            {
+                Id = ParseInt(fields[0]);
+                Grade = ParseInt(fields[1]);
+                SoulStoneTicker = fields[2];
+            }
+        }
+
+        public PetSheet() : base(nameof(PetSheet))
+        {
+        }
+    }
+}
