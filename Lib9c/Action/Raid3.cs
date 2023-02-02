cf3c68fe2393334716be56ad5c9b33c06e9fff3d
moreal <dev.moreal@gmail.com>
2023-02-03 06:13:04 +0900

    Implement `IRaidV2`

--- Lib9c/Action/Raid3.cs
+++ Lib9c/Action/Raid3.cs
@@ -26,7 +26,7 @@ namespace Nekoyume.Action
     [Serializable]
     [ActionObsolete(BlockPolicySource.V100360ObsoleteIndex)]
     [ActionType("raid3")]
-    public class Raid3 : GameAction
+    public class Raid3 : GameAction, IRaidV2
     {
         public Address AvatarAddress;
         public List<Guid> EquipmentIds;
@@ -34,6 +34,13 @@ namespace Nekoyume.Action
         public List<Guid> FoodIds;
         public List<RuneSlotInfo> RuneInfos;
         public bool PayNcg;
+
+        Address IRaidV2.AvatarAddress => AvatarAddress;
+        IEnumerable<Guid> IRaidV2.EquipmentIds => EquipmentIds;
+        IEnumerable<Guid> IRaidV2.CostumeIds => CostumeIds;
+        IEnumerable<Guid> IRaidV2.FoodIds => FoodIds;
+        IEnumerable<IValue> IRaidV2.RuneSlotInfos => RuneInfos.Select(x => x.Serialize());
+        bool IRaidV2.PayNcg => PayNcg;
 
         public override IAccountStateDelta Execute(IActionContext context)
         {
