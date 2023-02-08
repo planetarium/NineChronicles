722671b048be1004dd8481726219642c79bd9646
hyeon <hyeon@planetariumhq.com>
2023-02-08 10:12:56 +0900

    Add consumable crafting test

--- .Lib9c.Tests/Action/Scenario/ItemCraftTest.cs
+++ .Lib9c.Tests/Action/Scenario/ItemCraftTest.cs
@@ -135,6 +135,7 @@ namespace Lib9c.Tests.Action.Scenario
                 });
                 var slotState = stateV2.GetCombinationSlotState(_avatarAddr, i);
                 // TEST: requiredBlock
+                // TODO: Check reduced required block when pet comes in
                 Assert.Equal(equipmentRecipe.RequiredBlockIndex, slotState.RequiredBlockIndex);
             }
 
@@ -144,6 +145,84 @@ namespace Lib9c.Tests.Action.Scenario
             foreach (var itemId in targetItemIdList)
             {
                 // TEST: Created equipment should match with targetItemList
+                Assert.NotNull(inventoryState.Items.Where(e => e.item.Id == itemId));
+            }
+        }
+
+        [Theory]
+        [InlineData(1, new[] { 200000 })] // 맛없는 요리
+        public void CraftConsumableTest(int randomSeed, int[] targetItemIdList)
+        {
+            // Disable all quests to prevent contamination by quest reward
+            var (stateV1, stateV2) = QuestUtil.DisableQuestList(
+                _initialStatesWithAvatarStateV1,
+                _initialStatesWithAvatarStateV2,
+                _avatarAddr
+            );
+
+            // Setup requirements
+            var random = new TestRandom(randomSeed);
+            var recipeList = _tableSheets.ConsumableItemRecipeSheet.OrderedList.Where(
+                recipe => targetItemIdList.Contains(recipe.ResultConsumableItemId)
+            ).ToList();
+            var allMateriallist = new List<EquipmentItemSubRecipeSheet.MaterialInfo>();
+            foreach (var recipe in recipeList)
+            {
+                allMateriallist = allMateriallist.Concat(recipe.GetAllMaterials()).ToList();
+            }
+
+            // Prepare combination slot
+            for (var i = 0; i < targetItemIdList.Length; i++)
+            {
+                stateV2 = CraftUtil.PrepareCombinationSlot(stateV2, _avatarAddr, i);
+            }
+
+            // Initial inventory must be empty
+            var inventoryAddress = _avatarAddr.Derive(LegacyInventoryKey);
+            var inventoryState = new Inventory((List)stateV2.GetState(inventoryAddress));
+            Assert.Equal(0, inventoryState.Items.Count);
+
+            // Add materials to inventory
+            var avatarState = stateV2.GetAvatarStateV2(_avatarAddr);
+            foreach (var material in allMateriallist)
+            {
+                var materialRow = _tableSheets.MaterialItemSheet[material.Id];
+                var materialItem = ItemFactory.CreateItem(materialRow, random);
+                avatarState.inventory.AddItem(materialItem, material.Count);
+            }
+
+            stateV2 = stateV2.SetState(inventoryAddress, avatarState.inventory.Serialize());
+
+            for (var i = 0; i < recipeList.Count; i++)
+            {
+                // Do combination action
+                var recipe = recipeList[i];
+                var action = new CombinationConsumable
+                {
+                    avatarAddress = _avatarAddr,
+                    slotIndex = i,
+                    recipeId = recipe.Id,
+                };
+
+                stateV2 = action.Execute(new ActionContext
+                {
+                    PreviousStates = stateV2,
+                    Signer = _agentAddr,
+                    BlockIndex = 0L,
+                    Random = random,
+                });
+                var slotState = stateV2.GetCombinationSlotState(_avatarAddr, i);
+                // TEST: requiredBlockIndex
+                // TODO: Check reduced required block when pet comens in
+                Assert.Equal(recipe.RequiredBlockIndex, slotState.RequiredBlockIndex);
+            }
+
+            inventoryState = new Inventory((List)stateV2.GetState(inventoryAddress));
+            // TEST: Only created items should remain in inventory
+            Assert.Equal(recipeList.Count, inventoryState.Items.Count);
+            foreach (var itemId in targetItemIdList)
+            {
+                // TEST: Created consumables should be match with targetItemList
                 Assert.NotNull(inventoryState.Items.Where(e => e.item.Id == itemId));
             }
         }
