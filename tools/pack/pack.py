b7fe92a04633ec9d478536b267066adb07a4df97
Atralupus <me@atralupus.com>
2023-02-28 03:32:51 +0900

    feat: zip for android

--- tools/pack/pack.py
+++ tools/pack/pack.py
@@ -59,7 +59,22 @@ def zip_for_windows(build_result_dir: str, out_dir: str):
                 logging.info('Added: %s <- %s', relname, fullname)
 
 
+def zip_for_android(build_result_dir: str, out_dir: str):
+    archive_path = os.path.join(out_dir, 'Android.zip')
+    with zipfile.ZipFile(archive_path, 'w', ZIP_DEFLATED) as archive:
+        basepath = os.path.abspath(build_result_dir) + os.sep
+        for path, dirs, files in os.walk(build_result_dir):
+            logging.debug('Walk: %r, %r, %r', path, dirs, files)
+            for name in files + dirs:
+                fullname = os.path.abspath(os.path.join(path, name))
+                assert fullname.startswith(basepath)
+                relname = fullname[len(basepath):]
+                archive.write(fullname, relname)
+                logging.info('Added: %s <- %s', relname, fullname)
+
+
 ZIP = {
+    "Android": zip_for_android,
     "StandaloneOSX": zip_for_macos,
     "StandaloneLinux64": zip_for_linux,
     "StandaloneWindows": zip_for_windows,
