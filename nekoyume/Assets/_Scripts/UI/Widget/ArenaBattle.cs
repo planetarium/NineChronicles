e8649555bc81ec5c05f32bd534fb8a4b85830b67
sonohoshi <dev.sonohoshi@gmail.com>
2023-02-27 18:35:56 +0900

    add Status and EventBanner closing to battle ui

--- nekoyume/Assets/_Scripts/UI/Widget/ArenaBattle.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/ArenaBattle.cs
@@ -46,6 +46,8 @@ namespace Nekoyume.UI
             bool ignoreShowAnimation = false)
         {
             Find<HeaderMenuStatic>().Close(true);
+            Find<Status>().Close(true);
+            Find<EventBanner>().Close(true);
             SetStatus(myDigest, myStatus);
             SetStatus(enemyDigest, enemyStatus);
             comboText.comboMax = AttackCountHelper.GetCountMax(myDigest.Level);
