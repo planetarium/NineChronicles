f1e071299bc642963ac972a539111bce39ad9e20
sonohoshi <dev.sonohoshi@gmail.com>
2023-02-27 17:45:43 +0900

    add notification to DccMain ui

--- nekoyume/Assets/_Scripts/UI/Widget/DccMain.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/DccMain.cs
@@ -1,9 +1,16 @@
-﻿using Nekoyume.UI.Module;
+﻿using System;
+using System.Collections.Generic;
+using Nekoyume.Game;
+using Nekoyume.Helper;
+using Nekoyume.State.Subjects;
+using Nekoyume.UI.Module;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Nekoyume.UI
 {
+    using UniRx;
+
     public class DccMain : Widget
     {
         [SerializeField]
@@ -11,6 +18,39 @@ namespace Nekoyume.UI
 
         [SerializeField]
         private Button backButton;
+
+        [SerializeField]
+        private GameObject notification;
+
+        private readonly List<IDisposable> _disposables = new();
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            void SetNotification()
+            {
+                var hasNotification = false;
+                foreach (var row in TableSheets.Instance.PetSheet)
+                {
+                    hasNotification |= PetRenderingHelper.HasNotification(row.Id);
+                }
+
+                notification.SetActive(hasNotification);
+            }
+
+            SetNotification();
+            AgentStateSubject.Gold.Subscribe(_ =>
+            {
+                SetNotification();
+            }).AddTo(_disposables);
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            _disposables.DisposeAllAndClear();
+        }
 
         protected override void Awake()
         {
