0c8b9be14426629c686d067ba2440e73e3d94ba1
ulismoon <ulismoon@gmail.com>
2023-03-17 16:23:27 +0900

    Setup appsettings.local.json from example file

--- nekoyume/Assets/Planetarium/Nekoyume/Editor/HeadlessTool.cs
+++ nekoyume/Assets/Planetarium/Nekoyume/Editor/HeadlessTool.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 using System.IO;
-using System.Net.Mime;
+using Nekoyume.BlockChain;
 using UnityEditor;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
@@ -20,16 +20,17 @@ namespace Planetarium.Nekoyume.Editor
 
         private static string _genesisPath = Application.streamingAssetsPath;
 
+        private static string SetDirectory(string title)
+        {
+            return EditorUtility.OpenFolderPanel(title, _docsRoot, "");
+        }
+
         [MenuItem("Tools/Headless/Setup NineChronicles.Headless repository")]
         private static void SetupHeadlessRepository()
         {
             Debug.LogFormat($"Current project directory is: {_currentDir}");
             Debug.LogFormat($"Docs root directory is: {_docsRoot}");
-            _docsRoot =
-                EditorUtility.OpenFolderPanel(
-                    "Select directory to put headless repository code",
-                    _docsRoot, ""
-                );
+            _docsRoot = SetDirectory("Select directory to put headless repository code");
             Debug.LogFormat($"Docs root directory is changed to: {_docsRoot}");
             _headlessPath = Path.Join(_docsRoot, "NineChronicles.Headless");
 
@@ -55,6 +56,35 @@ namespace Planetarium.Nekoyume.Editor
             process = Process.Start(startInfo);
             process.WaitForExit();
             Debug.Log("Submodules updated.");
+        }
+
+        [MenuItem("Tools/Headless/Delete and re-create genesis block")]
+        private static void ResetGenesisBlock()
+        {
+            // TODO: Select option to use local genesis or download genesis from URL (like use mainnet genesis)
+            Debug.Log($"Delete and create genesis-block to {BlockManager.GenesisBlockPath()}");
+            LibplanetEditor.DeleteAllEditorAndMakeGenesisBlock();
+        }
+
+        [MenuItem("Tools/Headless/Setup headless config")]
+        private static void SetupAppsettingsJson()
+        {
+            var appsettings = System.IO.File.ReadAllText(Path.Combine("Assets", "Planetarium",
+                "Nekoyume", "Editor", "appsettings.example.json"));
+            Debug.Log($"{appsettings.Length} text read from appsettings.");
+            if (string.IsNullOrEmpty(_headlessPath))
+            {
+                _headlessPath = SetDirectory("Please select NineChronicles.Headless directory");
+            }
+
+            Debug.Log(_headlessPath);
+            File.WriteAllText(
+                Path.Combine(
+                    _headlessPath,
+                    "NineChronicles.Headless.Executable",
+                    "appsettings.local.json"
+                ), appsettings);
+            Debug.Log("appsettings.local.json set.");
         }
     }
 }
