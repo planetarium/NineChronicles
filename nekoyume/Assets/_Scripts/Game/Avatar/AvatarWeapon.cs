c572145e37975e359d920e20a48cbd5ee7a40c20
hskim881028 <andykim881028@gmail.com>
2023-03-21 21:10:41 +0900

    Add AvatarWeapon for UI avatar.

---
+++ nekoyume/Assets/_Scripts/Game/Avatar/AvatarWeapon.cs
@@ -0,0 +1,30 @@
+﻿using Nekoyume.Helper;
+using Spine.Unity;
+using Spine.Unity.AttachmentTools;
+using UnityEngine;
+
+namespace Nekoyume.Game.Avatar
+{
+    public class AvatarWeapon : MonoBehaviour
+    {
+        private const string DefaultPmaShader = "Spine/Skeleton";
+        private const string WeaponSlot = "weapon";
+
+        [SerializeField]
+        private int weaponId = 10100000;
+
+        [SerializeField]
+        private SkeletonGraphic skeletonGraphic;
+
+        private void Awake()
+        {
+            var shader = Shader.Find(DefaultPmaShader);
+            var weaponSlotIndex = skeletonGraphic.Skeleton.FindSlotIndex(WeaponSlot);
+            var weaponSprite = SpriteHelper.GetPlayerSpineTextureWeapon(weaponId);
+            var newWeapon = weaponSprite.ToRegionAttachmentPMAClone(shader);
+            skeletonGraphic.Skeleton.Data.DefaultSkin
+                .SetAttachment(weaponSlotIndex, WeaponSlot, newWeapon);
+            skeletonGraphic.Skeleton.SetSlotsToSetupPose();
+        }
+    }
+}
