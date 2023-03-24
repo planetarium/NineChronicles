15a33fd0f6640cc8614d3a15324764c7f85da090
ulismoon <ulismoon@gmail.com>
2023-03-24 17:38:55 +0900

    Make headless tool work
    - Use symlink to share Lib9c on headless from 9c
    - Run headless in process (Separate needed)
    - Delete prev. store when regen genesis
    - Fix path separator

--- nekoyume/Assets/Planetarium/Nekoyume/Editor/HeadlessTool.cs
+++ nekoyume/Assets/Planetarium/Nekoyume/Editor/HeadlessTool.cs
@@ -1,5 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 using Nekoyume.BlockChain;
 using UnityEditor;
 using UnityEngine;
@@ -13,16 +15,30 @@ namespace Planetarium.Nekoyume.Editor
     {
         private static string _currentDir = Directory.GetCurrentDirectory();
 
+        private static string _lib9cDir =
+            Path.Combine(_currentDir, "Assets", "_Scripts", "Lib9c", "lib9c");
+
         private static string _docsRoot =
-            Directory.GetParent(Directory.GetParent(_currentDir).ToString()).ToString();
+            PlayerPrefs.HasKey("docsRoot")
+                ? PlayerPrefs.GetString("docsRoot")
+                : Directory.GetParent(Directory.GetParent(_currentDir).ToString()).ToString();
 
-        private static string _headlessPath = "";
+        private static string _headlessPath = PlayerPrefs.HasKey("headlessPath")
+            ? PlayerPrefs.GetString("headlessPath")
+            : "";
 
-        private static string _genesisPath = Application.streamingAssetsPath;
+        private static string _storeName = "9c_dev";
+        private static string _genesisPath = FixPath(Application.streamingAssetsPath);
+
+
+        private static string FixPath(string path) => path.Replace("/",
+            (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                ? Path.DirectorySeparatorChar
+                : Path.AltDirectorySeparatorChar).ToString());
 
         private static string SetDirectory(string title)
         {
-            return EditorUtility.OpenFolderPanel(title, _docsRoot, "");
+            return FixPath(EditorUtility.OpenFolderPanel(title, _docsRoot, ""));
         }
 
         [MenuItem("Tools/Headless/Setup NineChronicles.Headless repository")]
@@ -31,8 +47,10 @@ namespace Planetarium.Nekoyume.Editor
             Debug.LogFormat($"Current project directory is: {_currentDir}");
             Debug.LogFormat($"Docs root directory is: {_docsRoot}");
             _docsRoot = SetDirectory("Select directory to put headless repository code");
+            PlayerPrefs.SetString("docsRoot", _docsRoot);
             Debug.LogFormat($"Docs root directory is changed to: {_docsRoot}");
-            _headlessPath = Path.Join(_docsRoot, "NineChronicles.Headless");
+            _headlessPath = Path.Combine(_docsRoot, "NineChronicles.Headless_Unity-runner");
+            PlayerPrefs.SetString("headlessPath", _headlessPath);
 
             Debug.LogFormat("Cloning Repository...");
             var process = Process.Start(
@@ -40,22 +58,53 @@ namespace Planetarium.Nekoyume.Editor
                 $@"clone https://github.com/planetarium/NineChronicles.Headless {_headlessPath}"
             );
             process.WaitForExit();
-            Debug.Log("Headless repository cloned.");
+            Debug.Log("Headless repository cloned for unity.");
 
-            var startInfo = new ProcessStartInfo("git", "checkout development");
-            startInfo.WorkingDirectory = _headlessPath;
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "git",
+                Arguments = @"submodule update --init --recursive NineChronicles.RPC.Shared",
+                WorkingDirectory = _headlessPath,
+            };
             process = Process.Start(startInfo);
             process.WaitForExit();
+            Debug.Log($"NineChronicles.RPC.Shared submodule updated : {process.ExitCode}");
 
-            startInfo.Arguments = "pull origin development";
-            startInfo.WorkingDirectory = _headlessPath;
-            process = Process.Start(startInfo);
+            if (Directory.Exists(Path.Combine(_headlessPath, "Lib9c")))
+            {
+                new DirectoryInfo(Path.Combine(_headlessPath, "Lib9c")).Delete(true);
+            }
 
-            process.WaitForExit();
-            startInfo.Arguments = "submodule update --init --recursive";
-            process = Process.Start(startInfo);
-            process.WaitForExit();
-            Debug.Log("Submodules updated.");
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                startInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments =
+                        $@"/c mklink /d {Path.Combine(_headlessPath, "Lib9c")} {_lib9cDir}",
+                    Verb = "runas",
+                    CreateNoWindow = true,
+                };
+                process = Process.Start(startInfo);
+                process.WaitForExit();
+                Debug.Log($"Lib9c symlink created. with {process.ExitCode}");
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                startInfo = new ProcessStartInfo
+                {
+                    FileName = "ln",
+                    Arguments = @$"-s {_lib9cDir} {Path.Combine(_headlessPath, "Lib9c")}"
+                };
+                process = Process.Start(startInfo);
+                process.WaitForExit();
+                Debug.Log($"Lib9c symlink created. with {process.ExitCode}");
+            }
+            else
+            {
+                throw new NotSupportedException(
+                    "Current OS is not in Windows|OSX|Linux. Please use supported one");
+            }
         }
 
         [MenuItem("Tools/Headless/Delete and re-create genesis block")]
@@ -63,6 +112,7 @@ namespace Planetarium.Nekoyume.Editor
         {
             // TODO: Select option to use local genesis or download genesis from URL (like use mainnet genesis)
             Debug.Log($"Delete and create genesis-block to {BlockManager.GenesisBlockPath()}");
+            Directory.Delete(Path.Combine(_genesisPath, _storeName), true);
             LibplanetEditor.DeleteAllEditorAndMakeGenesisBlock();
         }
 
@@ -85,6 +135,36 @@ namespace Planetarium.Nekoyume.Editor
                     "appsettings.local.json"
                 ), appsettings);
             Debug.Log("appsettings.local.json set.");
+        }
+
+        [MenuItem("Tools/Headless/Run local headless node")]
+        public static void Initialize()
+        {
+            if (string.IsNullOrEmpty(_headlessPath) ||
+                !File.Exists(Path.Combine(_headlessPath, "NineChronicles.Headless.Executable",
+                    "appsettings.local.json")))
+            {
+                SetupAppsettingsJson();
+            }
+
+            Debug.Log(Path.Combine(_genesisPath, "genesis-block"));
+            Debug.Log(Path.Combine(_genesisPath, _storeName));
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments =
+                    @$"run --project NineChronicles.Headless.Executable -C appsettings.local.json \
+                           --genesis-block-path {Path.Combine(_genesisPath, "genesis-block")} \
+                           --store-path {Path.Combine(_genesisPath, _storeName)} \
+                           --store-type memory",
+            };
+            Debug.Log(startInfo.Arguments);
+            startInfo.WorkingDirectory = _headlessPath;
+            Debug.Log($"WorkingDirectory: {startInfo.WorkingDirectory}");
+            var process = Process.Start(startInfo);
+            // FIXME: Can I wait here?
+            process.WaitForExit();
+            Debug.Log($"Headless done: {process.ExitCode}");
         }
     }
 }
