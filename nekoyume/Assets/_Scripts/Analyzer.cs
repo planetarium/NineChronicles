53784b1d10300b0c91de576bb9c0a85b0acc5f2c
sonohoshi <dev.sonohoshi@gmail.com>
2023-04-03 13:37:58 +0900

    enable Mixpanel tracking

--- nekoyume/Assets/_Scripts/Analyzer.cs
+++ nekoyume/Assets/_Scripts/Analyzer.cs
@@ -31,40 +31,40 @@ namespace Nekoyume
             string rpcServerHost = null,
             bool isTrackable = false)
         {
-            //_isTrackable = isTrackable;
-            //if (!_isTrackable)
-            //{
-            //    Debug.Log($"Analyzer does not track: {nameof(isTrackable)} is false");
-            //    return;
-            //}
+            _isTrackable = isTrackable;
+            if (!_isTrackable)
+            {
+                Debug.Log($"Analyzer does not track: {nameof(isTrackable)} is false");
+                return;
+            }
 
-            //var clientHost = Resources.Load<TextAsset>("ClientHost")?.text ?? "no-host";
-            //var clientHash = Resources.Load<TextAsset>("ClientHash")?.text ?? "no-hash";
-            //var targetNetwork = Resources.Load<TextAsset>("TargetNetwork")?.text ?? "no-target";
+            var clientHost = Resources.Load<TextAsset>("ClientHost")?.text ?? "no-host";
+            var clientHash = Resources.Load<TextAsset>("ClientHash")?.text ?? "no-hash";
+            var targetNetwork = Resources.Load<TextAsset>("TargetNetwork")?.text ?? "no-target";
 
-            //InitializeSentry(
-            //    clientHost,
-            //    clientHash,
-            //    targetNetwork,
-            //    rpcServerHost,
-            //    uniqueId,
-            //    _isTrackable);
+            InitializeSentry(
+                clientHost,
+                clientHash,
+                targetNetwork,
+                rpcServerHost,
+                uniqueId,
+                _isTrackable);
 
-            //_mixpanelValueFactory = new MixpanelValueFactory(
-            //    clientHost,
-            //    clientHash,
-            //    targetNetwork,
-            //    rpcServerHost,
-            //    uniqueId);
+            _mixpanelValueFactory = new MixpanelValueFactory(
+                clientHost,
+                clientHash,
+                targetNetwork,
+                rpcServerHost,
+                uniqueId);
 
-            //Mixpanel.SetToken("80a1e14b57d050536185c7459d45195a");
-            //Mixpanel.Identify(uniqueId);
-            //Mixpanel.Register("AgentAddress", uniqueId);
-            //Mixpanel.People.Set("AgentAddress", uniqueId);
-            //Mixpanel.People.Name = uniqueId;
-            //Mixpanel.Init();
+            Mixpanel.SetToken("80a1e14b57d050536185c7459d45195a");
+            Mixpanel.Identify(uniqueId);
+            Mixpanel.Register("AgentAddress", uniqueId);
+            Mixpanel.People.Set("AgentAddress", uniqueId);
+            Mixpanel.People.Name = uniqueId;
+            Mixpanel.Init();
 
-            //Debug.Log($"Analyzer initialized: {uniqueId}");
+            Debug.Log($"Analyzer initialized: {uniqueId}");
         }
 
         private void InitializeSentry(
@@ -75,22 +75,22 @@ namespace Nekoyume
             string uniqueId = "none",
             bool isTrackable = false)
         {
-            //if (!isTrackable)
-            //{
-            //    return;
-            //}
-
-            //SentrySdk.ConfigureScope(scope =>
-            //{
-            //    scope.User = new User()
-            //    {
-            //        Id = uniqueId
-            //    };
-            //    scope.SetTag("client-host", clientHost);
-            //    scope.SetTag("client-hash", clientHash);
-            //    scope.SetTag("target-network", targetNetwork);
-            //    scope.SetTag("rpc-server-host", rpcServerHost);
-            //});
+            // if (!isTrackable)
+            // {
+            //     return;
+            // }
+            //
+            // SentrySdk.ConfigureScope(scope =>
+            // {
+            //     scope.User = new User()
+            //     {
+            //         Id = uniqueId
+            //     };
+            //     scope.SetTag("client-host", clientHost);
+            //     scope.SetTag("client-hash", clientHash);
+            //     scope.SetTag("target-network", targetNetwork);
+            //     scope.SetTag("rpc-server-host", rpcServerHost);
+            // });
         }
 
         //private ITransaction CreateTrace(string eventName, Dictionary<string, string> properties)
@@ -117,10 +117,10 @@ namespace Nekoyume
 
         public void Track(string eventName, params (string key, string value)[] properties)
         {
-            //if (!_isTrackable)
-            //{
-            //    return;
-            //}
+            if (!_isTrackable)
+            {
+                return;
+            }
 
             //ITransaction sentryTrace = CreateTrace(
             //    eventName,
@@ -129,14 +129,14 @@ namespace Nekoyume
             //        prop => prop.value));
             //Instance.FinishTrace(sentryTrace);
 
-            //if (properties.Length == 0)
-            //{
-            //    Mixpanel.Track(eventName);
-            //    return;
-            //}
+            if (properties.Length == 0)
+            {
+                Mixpanel.Track(eventName);
+                return;
+            }
 
-            //var value = _mixpanelValueFactory.GetValue(properties);
-            //Mixpanel.Track(eventName, value);
+            var value = _mixpanelValueFactory.GetValue(properties);
+            Mixpanel.Track(eventName, value);
         }
 
         public ITransaction Track(
@@ -144,40 +144,38 @@ namespace Nekoyume
             Dictionary<string, Value> valueDict,
             bool returnTrace = false)
         {
+            if (!_isTrackable)
+            {
+                return null;
+            }
+
+            // var sentryTrace = CreateTrace(
+            //     eventName,
+            //     valueDict.ToDictionary(
+            //         item => item.Key,
+            //         item => item.Value.ToString()));
+
+            var value = new Value(valueDict);
+            value = _mixpanelValueFactory.UpdateValue(value);
+            Mixpanel.Track(eventName, value);
+
+            // if (returnTrace)
+            // {
+            //     return sentryTrace;
+            // }
+            //
+            // Instance.FinishTrace(sentryTrace);
             return null;
-            //if (!_isTrackable)
-            //{
-            //    return null;
-            //}
-
-            //var sentryTrace =  CreateTrace(
-            //    eventName,
-            //    valueDict.ToDictionary(
-            //        item => item.Key,
-            //        item => item.Value.ToString()));
-
-            //Value value = new Value(valueDict);
-
-            //value = _mixpanelValueFactory.UpdateValue(value);
-            //Mixpanel.Track(eventName, value);
-
-
-            //if (returnTrace)
-            //{
-            //    return sentryTrace;
-            //}
-            //Instance.FinishTrace(sentryTrace);
-            //return null;
         }
 
         public void Flush()
         {
-            //if (!_isTrackable)
-            //{
-            //    return;
-            //}
+            if (!_isTrackable)
+            {
+                return;
+            }
 
-            //Mixpanel.Flush();
+            Mixpanel.Flush();
         }
     }
 }
