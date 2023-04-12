6e5f971e4cbc5bbb97ecaed1b6b426429d0df4bf
Hyun Seungmin <boscohyun@gmail.com>
2023-04-12 15:05:27 +0900

    Add toolbar to select the feature mode

--- nekoyume/Assets/Plugins/StateViewer/Editor/StateViewerWindow.cs
+++ nekoyume/Assets/Plugins/StateViewer/Editor/StateViewerWindow.cs
@@ -8,7 +8,20 @@ namespace StateViewer.Editor
 {
     public class StateViewerWindow : EditorWindow
     {
+        public enum FeatureMode
+        {
+            StateAndBalance, // FIXME: Separate this mode to State and Balance.
+            SetInventory,
+        }
+
+        private string[] _featureModeNames = {
+            "State & Balance",
+            "Set Inventory",
+        };
+        private int _selectedFeatureIndex;
+
         private StateAndBalanceFeature _stateAndBalanceFeature;
+        private SetInventoryFeature _setInventoryFeature;
 
         [SerializeField]
         private bool initialized;
@@ -20,7 +33,9 @@ namespace StateViewer.Editor
         private void OnEnable()
         {
             minSize = new Vector2(800f, 400f);
+            _selectedFeatureIndex = 0;
             _stateAndBalanceFeature = new StateAndBalanceFeature(this);
+            _setInventoryFeature = new SetInventoryFeature(this);
             initialized = true;
         }
 
@@ -31,7 +46,18 @@ namespace StateViewer.Editor
                 return;
             }
 
-            _stateAndBalanceFeature.OnGUI();
+            _selectedFeatureIndex = GUILayout.Toolbar(
+                _selectedFeatureIndex,
+                _featureModeNames);
+            switch (_selectedFeatureIndex)
+            {
+                case 0:
+                    _stateAndBalanceFeature.OnGUI();
+                    break;
+                case 1:
+                    _setInventoryFeature.OnGUI();
+                    break;
+            }
         }
     }
 }
