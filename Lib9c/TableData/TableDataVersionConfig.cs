e6304c6a09e8f10ace1b6c74b0dbffdd9a136550
hyeon <hyeon@planetariumhq.com>
2023-04-17 23:20:45 +0900

    Update StakeState
    - Splits stake V1 and V2 steps using version control config
    - Conventions

---
+++ Lib9c/TableData/TableDataVersionConfig.cs
@@ -0,0 +1,9 @@
+namespace Nekoyume.TableData
+{
+    public static class TableDataVersionConfig
+    {
+        // PatchTableSheet version update without changing model version
+        // FIXME: Set block index to real block index to change reward version
+        public const long StakeRewardSheetV2Index = 7_000_000L;
+    }
+}
