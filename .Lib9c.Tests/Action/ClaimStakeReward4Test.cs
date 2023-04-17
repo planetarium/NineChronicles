d8f3d1211e5df1313cb5de4ea44af8b78e0cb2aa
hyeon <hyeon@planetariumhq.com>
2023-04-17 23:22:59 +0900

    Introduce ClaimStakeReward4 action

---
+++ .Lib9c.Tests/Action/ClaimStakeReward4Test.cs
@@ -0,0 +1,250 @@
+namespace Lib9c.Tests.Action
+{
+    using System.Linq;
+    using Libplanet;
+    using Libplanet.Action;
+    using Libplanet.Assets;
+    using Libplanet.Crypto;
+    using Nekoyume;
+    using Nekoyume.Action;
+    using Nekoyume.Helper;
+    using Nekoyume.Model.State;
+    using Nekoyume.TableData;
+    using Serilog;
+    using Xunit;
+    using Xunit.Abstractions;
+    using static Lib9c.SerializeKeys;
+
+    public class ClaimStakeReward4Test
+    {
+        private readonly IAccountStateDelta _initialState;
+        private readonly Currency _currency;
+        private readonly Address _signerAddress;
+        private readonly Address _avatarAddress;
+        private readonly Address _avatarAddressForBackwardCompatibility;
+        private readonly Address _stakeStateAddress;
+
+        public ClaimStakeReward4Test(ITestOutputHelper outputHelper)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.TestOutput(outputHelper)
+                .CreateLogger();
+
+            _initialState = new State();
+
+            var sheets = TableSheetsImporter.ImportSheets();
+            foreach (var (key, value) in sheets)
+            {
+                _initialState = _initialState
+                    .SetState(Addresses.TableSheet.Derive(key), value.Serialize());
+            }
+
+            var tableSheets = new TableSheets(sheets);
+
+#pragma warning disable CS0618
+            // Use of obsolete method Currency.Legacy(): https://github.com/planetarium/lib9c/discussions/1319
+            _currency = Currency.Legacy("NCG", 2, null);
+#pragma warning restore CS0618
+            var goldCurrencyState = new GoldCurrencyState(_currency);
+
+            _signerAddress = new PrivateKey().ToAddress();
+            _stakeStateAddress = StakeState.DeriveAddress(_signerAddress);
+            var agentState = new AgentState(_signerAddress);
+            _avatarAddress = _signerAddress.Derive("0");
+            agentState.avatarAddresses.Add(0, _avatarAddress);
+            var avatarState = new AvatarState(
+                _avatarAddress,
+                _signerAddress,
+                0,
+                tableSheets.GetAvatarSheets(),
+                new GameConfigState(sheets[nameof(GameConfigSheet)]),
+                new PrivateKey().ToAddress()
+            )
+            {
+                level = 100,
+            };
+
+            _avatarAddressForBackwardCompatibility = _signerAddress.Derive("1");
+            agentState.avatarAddresses.Add(1, _avatarAddressForBackwardCompatibility);
+            var avatarStateForBackwardCompatibility = new AvatarState(
+                _avatarAddressForBackwardCompatibility,
+                _signerAddress,
+                0,
+                tableSheets.GetAvatarSheets(),
+                new GameConfigState(sheets[nameof(GameConfigSheet)]),
+                new PrivateKey().ToAddress()
+            )
+            {
+                level = 100,
+            };
+
+            _initialState = _initialState
+                .SetState(_signerAddress, agentState.Serialize())
+                .SetState(_avatarAddress, avatarState.SerializeV2())
+                .SetState(
+                    _avatarAddress.Derive(LegacyInventoryKey),
+                    avatarState.inventory.Serialize())
+                .SetState(
+                    _avatarAddress.Derive(LegacyWorldInformationKey),
+                    avatarState.worldInformation.Serialize())
+                .SetState(
+                    _avatarAddress.Derive(LegacyQuestListKey),
+                    avatarState.questList.Serialize())
+                .SetState(
+                    _avatarAddressForBackwardCompatibility,
+                    avatarStateForBackwardCompatibility.Serialize())
+                .SetState(GoldCurrencyState.Address, goldCurrencyState.Serialize());
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            var action = new ClaimStakeReward(_avatarAddress);
+            var deserialized = new ClaimStakeReward();
+            deserialized.LoadPlainValue(action.PlainValue);
+            Assert.Equal(action.AvatarAddress, deserialized.AvatarAddress);
+        }
+
+        [Theory]
+        [InlineData(ClaimStakeReward3.ObsoletedIndex)]
+        [InlineData(ClaimStakeReward3.ObsoletedIndex - 1)]
+        public void Execute_Throw_ActionUnAvailableException(long blockIndex)
+        {
+            var action = new ClaimStakeReward(_avatarAddress);
+            Assert.Throws<ActionUnavailableException>(() => action.Execute(new ActionContext
+            {
+                PreviousStates = _initialState,
+                Signer = _signerAddress,
+                BlockIndex = blockIndex,
+            }));
+        }
+
+        [Theory]
+        [InlineData(
+            ClaimStakeReward3.ObsoletedIndex,
+            100,
+            null,
+            ClaimStakeReward3.ObsoletedIndex + StakeState.LockupInterval,
+            40,
+            4,
+            0
+        )]
+        [InlineData(
+            ClaimStakeReward3.ObsoletedIndex,
+            6000,
+            null,
+            ClaimStakeReward3.ObsoletedIndex + StakeState.LockupInterval,
+            4800,
+            36,
+            4
+        )]
+        // Calculate rune start from hard fork index
+        [InlineData(
+            0L,
+            6000,
+            0L,
+            ClaimStakeReward3.ObsoletedIndex + StakeState.LockupInterval,
+            170400,
+            1278,
+            32
+        )]
+        // Stake reward v2
+        // Stake before v2, prev. receive v1, receive v1 & v2
+        [InlineData(
+            TableDataVersionConfig.StakeRewardSheetV2Index - StakeState.RewardInterval * 2,
+            50,
+            TableDataVersionConfig.StakeRewardSheetV2Index - StakeState.RewardInterval,
+            TableDataVersionConfig.StakeRewardSheetV2Index + 1,
+            5,
+            1,
+            0
+        )]
+        // Stake before v2, prev. receive v2, receive v2
+        [InlineData(
+            TableDataVersionConfig.StakeRewardSheetV2Index - StakeState.RewardInterval,
+            50,
+            TableDataVersionConfig.StakeRewardSheetV2Index,
+            TableDataVersionConfig.StakeRewardSheetV2Index + StakeState.RewardInterval,
+            5,
+            1,
+            0
+        )]
+        // Stake after v2, no prev. receive, receive v2
+        [InlineData(
+            TableDataVersionConfig.StakeRewardSheetV2Index,
+            6000,
+            null,
+            TableDataVersionConfig.StakeRewardSheetV2Index + StakeState.RewardInterval,
+            1200,
+            9,
+            1
+        )]
+        // stake after v2, prev. receive v2, receive v2
+        [InlineData(
+            TableDataVersionConfig.StakeRewardSheetV2Index,
+            50,
+            TableDataVersionConfig.StakeRewardSheetV2Index + StakeState.RewardInterval,
+            TableDataVersionConfig.StakeRewardSheetV2Index + StakeState.RewardInterval * 2,
+            5,
+            1,
+            0
+        )]
+        public void Execute_Success(
+            long startedBlockIndex,
+            int stakeAmount,
+            long? previousRewardReceiveIndex,
+            long blockIndex,
+            int expectedHourglass,
+            int expectedApStone,
+            int expectedRune)
+        {
+            Execute(
+                _avatarAddress,
+                startedBlockIndex,
+                stakeAmount,
+                previousRewardReceiveIndex,
+                blockIndex,
+                expectedHourglass,
+                expectedApStone,
+                expectedRune);
+        }
+
+        [Fact]
+        public void Execute_With_Old_AvatarState_Success()
+        {
+            Execute(_avatarAddressForBackwardCompatibility, ClaimStakeReward3.ObsoletedIndex, 100, 0L, ClaimStakeReward3.ObsoletedIndex + StakeState.LockupInterval, 40, 4, 0);
+        }
+
+        private void Execute(Address avatarAddress, long startedBlockIndex, int stakeAmount, long? previousRewardReceiveIndex, long blockIndex, int expectedHourglass, int expectedApStone, int expectedRune)
+        {
+            var state = _initialState;
+            var initialStakeState = new StakeState(_stakeStateAddress, startedBlockIndex);
+            if (!(previousRewardReceiveIndex is null))
+            {
+                initialStakeState.Claim((long)previousRewardReceiveIndex);
+            }
+
+            state = state
+                .SetState(_stakeStateAddress, initialStakeState.Serialize())
+                .MintAsset(_stakeStateAddress, _currency * stakeAmount);
+
+            var action = new ClaimStakeReward(avatarAddress);
+            var states = action.Execute(new ActionContext
+            {
+                PreviousStates = state,
+                Signer = _signerAddress,
+                BlockIndex = blockIndex,
+            });
+
+            AvatarState avatarState = states.GetAvatarStateV2(avatarAddress);
+            Assert.Equal(expectedHourglass, avatarState.inventory.Items.First(x => x.item.Id == 400000).count);
+            // It must be never added into the inventory if the amount is 0.
+            Assert.Equal(expectedApStone, avatarState.inventory.Items.First(x => x.item.Id == 500000).count);
+            Assert.Equal(expectedRune * RuneHelper.StakeRune, states.GetBalance(avatarAddress, RuneHelper.StakeRune));
+
+            Assert.True(states.TryGetStakeState(_signerAddress, out StakeState stakeState));
+            Assert.Equal(blockIndex, stakeState.ReceivedBlockIndex);
+        }
+    }
+}
---
+++ Lib9c/Action/ClaimStakeReward.cs
@@ -0,0 +1,265 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using Bencodex.Types;
+using Lib9c.Abstractions;
+using Libplanet;
+using Libplanet.Action;
+using Libplanet.Assets;
+using Microsoft.VisualBasic;
+using Nekoyume.Extensions;
+using Nekoyume.Helper;
+using Nekoyume.Model.Item;
+using Nekoyume.Model.State;
+using Nekoyume.TableData;
+using static Lib9c.SerializeKeys;
+
+namespace Nekoyume.Action
+{
+    /// <summary>
+    /// Hard forked at https://github.com/planetarium/lib9c/pull/1458
+    /// </summary>
+    [ActionType(ActionTypeText)]
+    public class ClaimStakeReward : GameAction, IClaimStakeReward, IClaimStakeRewardV1
+    {
+        private const string ActionTypeText = "claim_stake_reward4";
+
+        private const string StakeRegularRewardSheetV1Data =
+            @"level,required_gold,item_id,rate,type
+1,50,400000,10,Item
+1,50,500000,800,Item
+1,50,20001,6000,Rune
+2,500,400000,8,Item
+2,500,500000,800,Item
+2,500,20001,6000,Rune
+3,5000,400000,5,Item
+3,5000,500000,800,Item
+3,5000,20001,6000,Rune
+4,50000,400000,5,Item
+4,50000,500000,800,Item
+4,50000,20001,6000,Rune
+5,500000,400000,5,Item
+5,500000,500000,800,Item
+5,500000,20001,6000,Rune";
+
+        private const string StakeRegularFixedRewardSheetV1Data =
+            @"level,required_gold,item_id,count
+1,50,500000,1
+2,500,500000,2
+3,5000,500000,2
+4,50000,500000,2
+5,500000,500000,2";
+
+        private readonly ImmutableSortedDictionary<string,
+                ImmutableSortedDictionary<int, IStakeRewardSheet>>
+            _stakeRewardHistoryDict;
+
+        internal Address AvatarAddress { get; private set; }
+
+        Address IClaimStakeRewardV1.AvatarAddress => AvatarAddress;
+
+        public ClaimStakeReward(Address avatarAddress) : this()
+        {
+            AvatarAddress = avatarAddress;
+        }
+
+        public ClaimStakeReward()
+        {
+            var regularRewardSheetV1 = new StakeRegularRewardSheet();
+            regularRewardSheetV1.Set(StakeRegularRewardSheetV1Data);
+            var fixedRewardSheetV1 = new StakeRegularFixedRewardSheet();
+            fixedRewardSheetV1.Set(StakeRegularFixedRewardSheetV1Data);
+            _stakeRewardHistoryDict =
+                new Dictionary<string, ImmutableSortedDictionary<int, IStakeRewardSheet>>
+                {
+                    {
+                        "StakeRegularRewardSheet", new Dictionary<int, IStakeRewardSheet>
+                        {
+                            { 1, regularRewardSheetV1 },
+                        }.ToImmutableSortedDictionary()
+                    },
+                    {
+                        "StakeRegularFixedRewardSheet",
+                        new Dictionary<int, IStakeRewardSheet>
+                        {
+                            { 1, fixedRewardSheetV1 }
+                        }.ToImmutableSortedDictionary()
+                    },
+                }.ToImmutableSortedDictionary();
+        }
+
+        private IAccountStateDelta ProcessReward(IActionContext context, IAccountStateDelta states,
+            ref AvatarState avatarState,
+            ItemSheet itemSheet, FungibleAssetValue stakedAmount,
+            int rewardStep, int runeRewardStep,
+            List<StakeRegularFixedRewardSheet.RewardInfo> fixedReward,
+            List<StakeRegularRewardSheet.RewardInfo> regularReward)
+        {
+            var currency = stakedAmount.Currency;
+
+            // Regular Reward
+            foreach (var reward in regularReward)
+            {
+                switch (reward.Type)
+                {
+                    case StakeRegularRewardSheet.StakeRewardType.Item:
+                        var (quantity, _) = stakedAmount.DivRem(currency * reward.Rate);
+                        if (quantity < 1)
+                        {
+                            // If the quantity is zero, it doesn't add the item into inventory.
+                            continue;
+                        }
+
+                        ItemSheet.Row row = itemSheet[reward.ItemId];
+                        ItemBase item = row is MaterialItemSheet.Row materialRow
+                            ? ItemFactory.CreateTradableMaterial(materialRow)
+                            : ItemFactory.CreateItem(row, context.Random);
+                        avatarState.inventory.AddItem(item, (int)quantity * rewardStep);
+                        break;
+                    case StakeRegularRewardSheet.StakeRewardType.Rune:
+                        var runeReward = runeRewardStep *
+                                         RuneHelper.CalculateStakeReward(stakedAmount, reward.Rate);
+                        if (runeReward < 1 * RuneHelper.StakeRune)
+                        {
+                            continue;
+                        }
+
+                        states = states.MintAsset(AvatarAddress, runeReward);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            // Fixed Reward
+            foreach (var reward in fixedReward)
+            {
+                ItemSheet.Row row = itemSheet[reward.ItemId];
+                ItemBase item = row is MaterialItemSheet.Row materialRow
+                    ? ItemFactory.CreateTradableMaterial(materialRow)
+                    : ItemFactory.CreateItem(row, context.Random);
+                avatarState.inventory.AddItem(item, reward.Count * rewardStep);
+            }
+
+            return states;
+        }
+
+        public override IAccountStateDelta Execute(IActionContext context)
+        {
+            if (context.Rehearsal)
+            {
+                return context.PreviousStates;
+            }
+
+            var states = context.PreviousStates;
+            CheckActionAvailable(ClaimStakeReward3.ObsoletedIndex, context);
+            // TODO: Uncomment this when new version of action is created
+            // CheckObsolete(ObsoletedIndex, context);
+            var addressesHex = GetSignerAndOtherAddressesHex(context, AvatarAddress);
+            if (!states.TryGetStakeState(context.Signer, out StakeState stakeState))
+            {
+                throw new FailedLoadStateException(
+                    ActionTypeText,
+                    addressesHex,
+                    typeof(StakeState),
+                    StakeState.DeriveAddress(context.Signer));
+            }
+
+            if (!stakeState.IsClaimable(context.BlockIndex, out _, out _))
+            {
+                throw new RequiredBlockIndexException(
+                    ActionTypeText,
+                    addressesHex,
+                    context.BlockIndex);
+            }
+
+            if (!states.TryGetAvatarStateV2(
+                    context.Signer,
+                    AvatarAddress,
+                    out var avatarState,
+                    out var migrationRequired))
+            {
+                throw new FailedLoadStateException(
+                    ActionTypeText,
+                    addressesHex,
+                    typeof(AvatarState),
+                    AvatarAddress);
+            }
+
+            var sheets = states.GetSheets(sheetTypes: new[]
+            {
+                typeof(StakeRegularRewardSheet),
+                typeof(ConsumableItemSheet),
+                typeof(CostumeItemSheet),
+                typeof(EquipmentItemSheet),
+                typeof(MaterialItemSheet),
+            });
+
+            var currency = states.GetGoldCurrency();
+            var stakedAmount = states.GetBalance(stakeState.address, currency);
+            var stakeRegularRewardSheet = sheets.GetSheet<StakeRegularRewardSheet>();
+            int level =
+                stakeRegularRewardSheet.FindLevelByStakedAmount(context.Signer, stakedAmount);
+            var rewards = stakeRegularRewardSheet[level].Rewards;
+            ItemSheet itemSheet = sheets.GetItemSheet();
+            var accumulatedRewards =
+                stakeState.CalculateAccumulatedRewards(context.BlockIndex, out var v1Step,
+                    out var v2Step);
+            var accumulatedRuneRewards =
+                stakeState.CalculateAccumulatedRuneRewards(context.BlockIndex, out var runeV1Step,
+                    out var runeV2Step);
+            if (v1Step > 0)
+            {
+                var fixedReward =
+                    ((StakeRegularFixedRewardSheet)_stakeRewardHistoryDict[
+                        "StakeRegularFixedRewardSheet"][1])
+                    [level].Rewards;
+                var regularReward =
+                    ((StakeRegularRewardSheet)_stakeRewardHistoryDict["StakeRegularRewardSheet"][1])
+                    [level].Rewards;
+                states = ProcessReward(context, states, ref avatarState, itemSheet,
+                    stakedAmount, v1Step, runeV1Step, fixedReward, regularReward);
+            }
+
+            if (v2Step > 0)
+            {
+                var fixedReward =
+                    states.TryGetSheet<StakeRegularFixedRewardSheet>(out var fixedRewardSheet)
+                        ? fixedRewardSheet[level].Rewards
+                        : new List<StakeRegularFixedRewardSheet.RewardInfo>();
+                var regularReward = sheets.GetSheet<StakeRegularRewardSheet>()[level].Rewards;
+                states = ProcessReward(context, states, ref avatarState, itemSheet,
+                    stakedAmount, v2Step, runeV2Step, fixedReward, regularReward);
+            }
+
+            stakeState.Claim(context.BlockIndex);
+
+            if (migrationRequired)
+            {
+                states = states
+                    .SetState(avatarState.address, avatarState.SerializeV2())
+                    .SetState(
+                        avatarState.address.Derive(LegacyWorldInformationKey),
+                        avatarState.worldInformation.Serialize())
+                    .SetState(
+                        avatarState.address.Derive(LegacyQuestListKey),
+                        avatarState.questList.Serialize());
+            }
+
+            return states
+                .SetState(stakeState.address, stakeState.Serialize())
+                .SetState(
+                    avatarState.address.Derive(LegacyInventoryKey),
+                    avatarState.inventory.Serialize());
+        }
+
+        protected override IImmutableDictionary<string, IValue> PlainValueInternal =>
+            ImmutableDictionary<string, IValue>.Empty
+                .Add(AvatarAddressKey, AvatarAddress.Serialize());
+
+        protected override void LoadPlainValueInternal(
+            IImmutableDictionary<string, IValue> plainValue)
+        {
+            AvatarAddress = plainValue[AvatarAddressKey].ToAddress();
+        }
+    }
+}
