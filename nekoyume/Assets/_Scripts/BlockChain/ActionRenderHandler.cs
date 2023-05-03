b0430bdda33773b6855b916a0e5a816d9a2c64ec
unengine <unengine@gmail.com>
2023-05-03 12:32:07 +0900

    Remove useless casting

--- nekoyume/Assets/_Scripts/BlockChain/ActionRenderHandler.cs
+++ nekoyume/Assets/_Scripts/BlockChain/ActionRenderHandler.cs
@@ -99,7 +99,7 @@ namespace Nekoyume.BlockChain
                         Analyzer.Instance.Track("Unity/ActionRender",
                             new Dictionary<string, Value>()
                             {
-                                ["ActionType"] = (Value)actionType.TypeIdentifier,
+                                ["ActionType"] = actionType.TypeIdentifier,
                                 ["Elapsed"] = elapsed,
                                 ["AvatarAddress"] =
                                     States.Instance.CurrentAvatarState.address.ToString(),
