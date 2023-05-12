96612648b6b9016e12409c87feeb93d010f04fa6
sonohoshi <dev.sonohoshi@gmail.com>
2023-05-12 22:02:03 +0900

    add command line options handling at Builder.cs

--- nekoyume/Assets/Editor/Builder.cs
+++ nekoyume/Assets/Editor/Builder.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Lib9c.DevExtensions;
@@ -237,8 +239,10 @@ namespace Editor
                 targetDirName,
                 buildTarget switch
                 {
-                    BuildTarget.StandaloneWindows or BuildTarget.StandaloneWindows64 => $"{PlayerName}.exe",
-                    BuildTarget.Android => $"{PlayerName}.{(EditorUserBuildSettings.buildAppBundle ? "aab" : "apk")}",
+                    BuildTarget.StandaloneWindows or BuildTarget.StandaloneWindows64 =>
+                        $"{PlayerName}.exe",
+                    BuildTarget.Android =>
+                        $"{PlayerName}.{(EditorUserBuildSettings.buildAppBundle ? "aab" : "apk")}",
                     _ => PlayerName,
                 }
             );
@@ -253,11 +257,58 @@ namespace Editor
                     : options,
             };
 
+            // This code snippets from: https://github.com/game-ci/documentation/blob/main/example/BuildScript.cs
             if (buildTarget == BuildTarget.Android)
             {
                 // Due to executable size issue, we can't use script debugging for Android at least 2021.3.5f1.
                 buildPlayerOptions.options &= ~BuildOptions.AllowDebugging;
                 buildPlayerOptions.options |= BuildOptions.CompressWithLz4HC;
+
+                var cliOptions = new Dictionary<string, string>();
+                var args = Environment.GetCommandLineArgs();
+
+                // Extract flags with optional values
+                for (int current = 0, next = 1; current < args.Length; current++, next++)
+                {
+                    // Parse flag
+                    var isFlag = args[current].StartsWith("-");
+                    if (!isFlag)
+                    {
+                        continue;
+                    }
+
+                    var flag = args[current].TrimStart('-');
+
+                    // Parse optional value
+                    var flagHasValue = next < args.Length && !args[next].StartsWith("-");
+                    var value = flagHasValue ? args[next].TrimStart('-') : "";
+                    cliOptions.Add(flag, value);
+
+                    if (cliOptions.TryGetValue("androidKeystoreName", out var keystoreName) &&
+                        !string.IsNullOrEmpty(keystoreName))
+                    {
+                        PlayerSettings.Android.useCustomKeystore = true;
+                        PlayerSettings.Android.keystoreName = keystoreName;
+                    }
+
+                    if (cliOptions.TryGetValue("androidKeystorePass", out var keystorePass) &&
+                        !string.IsNullOrEmpty(keystorePass))
+                    {
+                        PlayerSettings.Android.keystorePass = keystorePass;
+                    }
+
+                    if (cliOptions.TryGetValue("androidKeyaliasName", out var keyaliasName) &&
+                        !string.IsNullOrEmpty(keyaliasName))
+                    {
+                        PlayerSettings.Android.keyaliasName = keyaliasName;
+                    }
+
+                    if (cliOptions.TryGetValue("androidKeyaliasPass", out var keyaliasPass) &&
+                        !string.IsNullOrEmpty(keyaliasPass))
+                    {
+                        PlayerSettings.Android.keyaliasPass = keyaliasPass;
+                    }
+                }
             }
 
             UpdateDefines(useDevExtension);
