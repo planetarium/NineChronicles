1ef18b320371f7a6509112243d1e3dad06769ae1
tyrosine1153 <tyrosine1153@gmail.com>
2023-06-13 16:28:33 +0900

    Add Costume Color ScriptableObject

---
+++ nekoyume/Assets/Editor/ColorSelectScriptableObjectEditor.cs
@@ -0,0 +1,13 @@
+﻿using Nekoyume;
+using Nekoyume.EnumType;
+using Nekoyume.Game.Util;
+using UnityEditor;
+
+namespace Editor
+{
+    [CustomEditor(typeof(CostumeColorScriptableObject))]
+    public class ColorSelectScriptableObjectEditor : EnumGeneratorEditor<ColorSelectType>
+    {
+
+    }
+}
