e8d5aa88dd085f1492fda5b2810ce3aaa4898080
Hyun Seungmin <boscohyun@gmail.com>
2023-06-15 20:59:20 +0900

    Introduce GarageWallet address

--- Lib9c/Addresses.cs
+++ Lib9c/Addresses.cs
@@ -36,6 +36,7 @@ namespace Nekoyume
         public static readonly Address Raid                  = new Address("0000000000000000000000000000000000000015");
         public static readonly Address Rune                  = new Address("0000000000000000000000000000000000000016");
         public static readonly Address Market                = new Address("0000000000000000000000000000000000000017");
+        public static readonly Address GarageWallet          = new Address("0000000000000000000000000000000000000018");
 
         public static Address GetSheetAddress<T>() where T : ISheet => GetSheetAddress(typeof(T).Name);
 
