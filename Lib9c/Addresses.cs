a2166625ef32517787e0bb9a1e65604e629e26f0
Hyun Seungmin <boscohyun@gmail.com>
2023-06-15 16:30:31 +0900

    Implement some methods to Addresses

--- Lib9c/Addresses.cs
+++ Lib9c/Addresses.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Globalization;
 using System.Linq;
+using System.Security.Cryptography;
 using Libplanet;
 using Nekoyume.Action;
 using Nekoyume.Model.State;
 using Nekoyume.TableData;
+using static Lib9c.SerializeKeys;
 
 namespace Nekoyume
 {
@@ -80,27 +82,64 @@ namespace Nekoyume
                     $"Index must be between 0 and {Nekoyume.GameConfig.SlotCount - 1}.");
             }
 
-            return agentAddr.Derive(
-                string.Format(
-                    CultureInfo.InvariantCulture,
-                    CreateAvatar.DeriveFormat,
-                    index
-                ));
+            var deriveKey = string.Format(
+                CultureInfo.InvariantCulture,
+                CreateAvatar.DeriveFormat,
+                index);
+            return agentAddr.Derive(deriveKey);
+        }
+
+        public static Address GetInventoryAddress(Address agentAddr, int avatarIndex)
+        {
+            return GetAvatarAddress(agentAddr, avatarIndex)
+                .Derive(LegacyInventoryKey);
         }
 
         public static Address GetCombinationSlotAddress(Address avatarAddr, int index)
         {
-            return avatarAddr.Derive(
-                string.Format(
-                    CultureInfo.InvariantCulture,
-                    CombinationSlotState.DeriveFormat,
-                    index
-                ));
+            var deriveKey = string.Format(
+                CultureInfo.InvariantCulture,
+                CombinationSlotState.DeriveFormat,
+                index);
+            return avatarAddr.Derive(deriveKey);
         }
 
-        public static bool IsContainedInAgent(Address agentAddr, Address avatarAddr) =>
+        public static Address GetGarageBalanceAddress(Address agentAddr)
+        {
+            return agentAddr.Derive("garage-balance");
+        }
+
+        public static Address GetGarageAddress(
+            Address agentAddr,
+            HashDigest<SHA256> fungibleId)
+        {
+            return agentAddr
+                .Derive("garage")
+                .Derive(fungibleId.ToString());
+        }
+
+        public static bool CheckAvatarAddrIsContainedInAgent(
+            Address agentAddr,
+            Address avatarAddr) =>
             Enumerable.Range(0, Nekoyume.GameConfig.SlotCount)
                 .Select(index => GetAvatarAddress(agentAddr, index))
                 .Contains(avatarAddr);
+
+        public static bool CheckAgentHasPermissionOnBalanceAddr(
+            Address agentAddr,
+            Address balanceAddr) =>
+            agentAddr == balanceAddr ||
+            Enumerable.Range(0, Nekoyume.GameConfig.SlotCount)
+                .Select(index => GetAvatarAddress(agentAddr, index))
+                .Contains(balanceAddr);
+
+        public static bool CheckInventoryAddrIsContainedInAgent(
+            Address agentAddr,
+            Address inventoryAddr) =>
+            Enumerable.Range(0, Nekoyume.GameConfig.SlotCount)
+                .Select(index => GetInventoryAddress(agentAddr, index))
+                .Contains(inventoryAddr);
+
+
     }
 }
