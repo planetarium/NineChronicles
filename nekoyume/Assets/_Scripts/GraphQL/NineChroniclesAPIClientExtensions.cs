4ab1943163a795502d6be73a34fccd0c0ac3b2e5
Hyun Seungmin <boscohyun@gmail.com>
2023-06-16 20:00:14 +0900

    Implement extensions method to get block hash

---
+++ nekoyume/Assets/_Scripts/GraphQL/NineChroniclesAPIClientExtensions.cs
@@ -0,0 +1,43 @@
+using System.Threading.Tasks;
+using Libplanet.Blocks;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+namespace Nekoyume.GraphQL
+{
+    public static class NineChroniclesAPIClientExtensions
+    {
+        public static async Task<BlockHash?> GetBlockHashAsync(
+            this NineChroniclesAPIClient client,
+            long blockIndex)
+        {
+            var query = $@"
+query {{
+    chainQuery {{
+        blockQuery {{
+            blocks(offset: {blockIndex}, limit: 1) {{
+                hash
+            }}
+        }}
+    }}
+}}";
+            var response = await client.GetObjectAsync<JObject>(query);
+            if (response is null)
+            {
+                Debug.LogError($"Failed getting response : {nameof(JObject)}");
+                return null;
+            }
+
+            try
+            {
+                var hash = response["chainQuery"]["blockQuery"]["blocks"][0]["hash"].ToString();
+                return BlockHash.FromString(hash);
+            }
+            catch
+            {
+                Debug.LogError(response.ToString());
+                return null;
+            }
+        }
+    }
+}
