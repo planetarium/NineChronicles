bd19d2cfd226ec8768a69fbc0758350508dc733f
Say Cheong <greymistcube@gmail.com>
2023-07-03 19:06:29 +0900

    Removed unsupported methods

--- Lib9c.Renderers/Renderers/BlockRenderer.cs
+++ Lib9c.Renderers/Renderers/BlockRenderer.cs
@@ -1,8 +1,5 @@
-using System;
-using Libplanet.Action;
 using Libplanet.Blockchain.Renderers;
 using Libplanet.Blocks;
-using Nekoyume.Action;
 #if UNITY_EDITOR || UNITY_STANDALONE
 using UniRx;
 #else
@@ -18,12 +15,6 @@ namespace Lib9c.Renderers
     {
         public readonly Subject<(NCBlock OldTip, NCBlock NewTip)> BlockSubject =
             new Subject<(NCBlock OldTip, NCBlock NewTip)>();
-
-        public readonly Subject<(NCBlock OldTip, NCBlock NewTip, NCBlock Branchpoint)> ReorgSubject =
-            new Subject<(NCBlock OldTip, NCBlock NewTip, NCBlock Branchpoint)>();
-
-        public readonly Subject<(NCBlock OldTip, NCBlock NewTip, NCBlock Branchpoint)> ReorgEndSubject =
-            new Subject<(NCBlock OldTip, NCBlock NewTip, NCBlock Branchpoint)>();
 
         public void RenderBlock(NCBlock oldTip, NCBlock newTip) =>
             BlockSubject.OnNext((oldTip, newTip));
