46866cf6c408c3279f95b2c65976cd5c313f3282
Jonny <jonny@planetariumhq.com>
2023-07-17 17:29:34 +0900

    add client commit hash

--- nekoyume/Assets/_Scripts/UI/Widget/System/VersionSystem.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/System/VersionSystem.cs
@@ -2,6 +2,7 @@ using Libplanet;
 using Libplanet.Blocks;
 using TMPro;
 using UniRx;
+using UnityEngine;
 
 namespace Nekoyume.UI
 {
@@ -11,12 +12,16 @@ namespace Nekoyume.UI
         private int _version;
         private long _blockIndex;
         private BlockHash _hash;
+        private string _clientCommitHash;
 
         protected override void Awake()
         {
             base.Awake();
             Game.Game.instance.Agent.BlockIndexSubject.Subscribe(SubscribeBlockIndex).AddTo(gameObject);
             Game.Game.instance.Agent.BlockTipHashSubject.Subscribe(SubscribeBlockHash).AddTo(gameObject);
+
+            _clientCommitHash = Resources.Load<TextAsset>("ClientHash")?.text[..8] ?? string.Empty;
+
 #if UNITY_ANDROID || UNITY_IOS
             UpdateText();
 #endif
@@ -44,10 +49,11 @@ namespace Nekoyume.UI
         {
             var hash = _hash.ToString();
             hash = hash.Length >= 4 ? hash.Substring(0, 4) : "...";
+
 #if UNITY_ANDROID || UNITY_IOS
-            informationText.text = $"APV: {_version} / #{_blockIndex} / Hash: {hash} / ver: {UnityEngine.Application.version}";
+            informationText.text = $"APV: {_version} / #{_blockIndex} / Hash: {hash} / ver: {UnityEngine.Application.version}({_clientCommitHash})";
 #else
-            informationText.text = $"APV: {_version} / #{_blockIndex} / Hash: {hash}";
+            informationText.text = $"APV: {_version} / #{_blockIndex} / Hash: {hash} / ver: {clientCommitHash}";
 #endif
         }
     }
