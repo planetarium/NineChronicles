984acc9afe6c6110af4176e75c1e85e26c588fcc
XxshiftxX <qjfrntop12@gmail.com>
2023-07-24 16:05:20 +0900

    Add code for loading currentBlock and actions data

--- Dashboard/src/App.tsx
+++ Dashboard/src/App.tsx
@@ -1,4 +1,42 @@
+import { useQueries } from "@tanstack/react-query";
+import axios from "axios";
+import { useMemo } from "react";
+
+interface Block { index: number }
+interface Action { id: string, obsoleteAt: number }
+
+const fetchLatestBlock = async () => {
+  const response = await axios.get<Block>(`${import.meta.env.VITE_API_PATH}/api/blocks/latest`);
+  return response.data;
+};
+
+const fetchActions = async () => {
+  const response = await axios.get<Action[]>(import.meta.env.VITE_OBSOLETE_DATA_PATH);
+  return response.data;
+}
+
 function App() {
+  const [latestBlockQuery, actionsQuery] = useQueries({
+    queries: [
+      { queryKey: ['latestBlock'], queryFn: fetchLatestBlock },
+      { queryKey: ['actions'], queryFn: fetchActions },
+    ],
+  });
+  const isLoading = latestBlockQuery.isLoading || actionsQuery.isLoading;
+  const isError = latestBlockQuery.isError || actionsQuery.isError;
+
+  const currentBlock = useMemo(
+    () => latestBlockQuery.data?.index ?? 0,
+    [latestBlockQuery.data],
+  );
+  const actions = useMemo(
+    () => actionsQuery.data?.sort((a, b) => b.obsoleteAt - a.obsoleteAt) ?? [],
+    [actionsQuery.data],
+  );
+
+  if (isLoading) return <></>;
+  if (isError) return <></>;
+
   return <></>;
 }
 
