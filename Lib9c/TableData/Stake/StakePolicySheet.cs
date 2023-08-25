94ebaa50cdd3fd356cf2dcec9f381c2b41183614
moreal <dev.moreal@gmail.com>
2023-08-25 14:31:55 +0900

    Introduce `StakePolicySheet`

---
+++ Lib9c/TableData/Stake/StakePolicySheet.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace Nekoyume.TableData.Stake
+{
+    public class StakePolicySheet : Sheet<string, StakePolicySheet.Row>
+    {
+        [Serializable]
+        public class Row : SheetRow<string>
+        {
+            public override string Key => TableName;
+
+            public string TableTypeName { get; private set; }
+
+            public string TableName { get; private set; }
+
+            public override void Set(IReadOnlyList<string> fields)
+            {
+                TableTypeName = fields[0];
+                TableName = fields[1];
+            }
+        }
+
+        public StakePolicySheet() : base(nameof(StakePolicySheet))
+        {
+        }
+    }
+}
