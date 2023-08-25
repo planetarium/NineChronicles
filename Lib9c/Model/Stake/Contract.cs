50e85c919a98f5c3af27243d78b288ebb969f407
moreal <dev.moreal@gmail.com>
2023-08-25 15:09:21 +0900

    Introduce StakeStateV2

---
+++ Lib9c/Model/Stake/Contract.cs
@@ -0,0 +1,77 @@
+using System;
+using Bencodex.Types;
+
+namespace Lib9c.Model.Stake
+{
+    public class Contract
+    {
+        public const string StateTypeName = "stake_contract";
+        public const long StateTypeVersion = 1;
+
+        public const string StakeRegularFixedRewardSheetPrefix
+            = "StakeRegularFixedRewardSheet_";
+        public const string StakeRegularRewardSheetPrefix
+            = "StakeRegularRewardSheet_";
+
+        public string StakeRegularFixedRewardSheetTableName { get; init; }
+       public string StakeRegularRewardSheetTableName { get; init; }
+
+       public Contract(
+            string stakeRegularFixedRewardSheetTableName,
+            string stakeRegularRewardSheetTableName)
+       {
+            if (!stakeRegularFixedRewardSheetTableName.StartsWith(StakeRegularFixedRewardSheetPrefix))
+            {
+                throw new ArgumentException(nameof(stakeRegularFixedRewardSheetTableName));
+            }
+
+            if (!stakeRegularRewardSheetTableName.StartsWith(StakeRegularRewardSheetPrefix))
+            {
+                throw new ArgumentException(nameof(stakeRegularRewardSheetTableName));
+            }
+
+            StakeRegularFixedRewardSheetTableName = stakeRegularFixedRewardSheetTableName;
+            StakeRegularRewardSheetTableName = stakeRegularRewardSheetTableName;
+       }
+       
+       public Contract(IValue serialized)
+       {
+            if (serialized is not List list)
+            {
+                throw new ArgumentException(
+                    nameof(serialized),
+                    $"{nameof(serialized)} is not List");
+            }
+
+            if (list[0] is not Text typeName ||
+                (string)typeName != StateTypeName)
+            {
+                throw new ArgumentException(
+                    nameof(serialized),
+                    $"State type name is not {StateTypeName}");
+            }
+
+            if (list[1] is not Integer typeVersion ||
+                (long)typeVersion != StateTypeVersion)
+            {
+                throw new ArgumentException(
+                    nameof(serialized),
+                    $"State type version is not {StateTypeVersion}");
+            }
+
+            const int reservedCount = 2;
+            StakeRegularFixedRewardSheetTableName = (Text)list[reservedCount];
+            StakeRegularRewardSheetTableName = (Text)list[reservedCount + 1];
+        }
+
+       public List Serialize()
+       {
+            return new List(
+                (Text)StateTypeName,
+                (Integer)StateTypeVersion,
+                (Text)StakeRegularFixedRewardSheetTableName,
+                (Text)StakeRegularRewardSheetTableName
+            );
+       }
+    }
+}
---
+++ Lib9c/Model/Stake/StakeStateV2.cs
@@ -0,0 +1,84 @@
+using System;
+using Bencodex.Types;
+using Lib9c.Model.Stake;
+using Nekoyume.Model.State;
+
+namespace Lib9c.Model.State
+{
+    public readonly struct StakeStateV2 : IState
+    {
+        public const string StateTypeName = "stake_state";
+        public const int StateTypeVersion = 2;
+
+        public readonly Contract Contract;
+        public readonly long StartedBlockIndex;
+        public readonly long ClaimedBlockIndex;
+
+        public long CancellableBlockIndex =>
+            StartedBlockIndex + StakeState.LockupInterval;
+        public long ClaimableBlockIndex =>
+            ClaimedBlockIndex + StakeState.RewardInterval;
+
+        public StakeStateV2(
+            Contract contract,
+            long startedBlockIndex)
+            : this(contract, startedBlockIndex, startedBlockIndex)
+        {
+        }
+
+        public StakeStateV2(
+            Contract contract,
+            long startedBlockIndex,
+            long claimedBlockIndex)
+        {
+            Contract = contract;
+            StartedBlockIndex = startedBlockIndex;
+            ClaimedBlockIndex = claimedBlockIndex;
+        }
+
+        public StakeStateV2(
+            StakeState stakeState,
+            Contract contract
+        ) : this(
+            contract,
+            stakeState.StartedBlockIndex,
+            stakeState.GetClaimableBlockIndex(long.MaxValue) - StakeState.RewardInterval
+        )
+        {
+        }
+
+        public StakeStateV2(IValue serialized)
+        {
+            if (serialized is not List list)
+            {
+                throw new ArgumentException(
+                    nameof(serialized),
+                    $"{nameof(serialized)} should be List type.");
+            }
+
+            if (list[0] is not Text stateTypeNameValue ||
+                stateTypeNameValue != StateTypeName ||
+                list[1] is not Integer stateTypeVersionValue ||
+                stateTypeVersionValue.Value != StateTypeVersion)
+            {
+                throw new ArgumentException(
+                    nameof(serialized),
+                    $"{nameof(serialized)} doesn't have valid header.");
+            }
+
+            const int reservedCount = 2;
+
+            Contract = new Contract(list[reservedCount]);
+            StartedBlockIndex = (Integer)list[reservedCount + 1];
+            ClaimedBlockIndex = (Integer)list[reservedCount + 2];
+        }
+
+        public IValue Serialize() => new List(
+            (Text)StateTypeName,
+            (Integer)StateTypeVersion,
+            Contract.Serialize(),
+            (Integer)StartedBlockIndex,
+            (Integer)ClaimedBlockIndex
+        );
+    }
+}
