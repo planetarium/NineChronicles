3a099452f2aa850da846c440b3ab38d256e54f99
Hyun Seungmin <boscohyun@gmail.com>
2023-09-01 20:56:32 +0900

    add GameConfigSheetFixtures

---
+++ .Lib9c.Tests/Fixtures/TableCSV/GameConfigSheetFixtures.cs
@@ -0,0 +1,25 @@
+namespace Lib9c.Tests.Fixtures.TableCSV
+{
+    public static class GameConfigSheetFixtures
+    {
+        public const string Default = @"key,value
+hourglass_per_block,3
+action_point_max,120
+daily_reward_interval,1700
+daily_arena_interval,5040
+weekly_arena_interval,56000
+required_appraise_block,10
+battle_arena_interval,4
+rune_stat_slot_unlock_cost,100
+rune_skill_slot_unlock_cost,1000
+daily_rune_reward_amount,1
+daily_worldboss_interval,7200
+worldboss_required_interval,5
+stake_regular_fixed_reward_sheet_v2_start_block_index,6700000
+stake_regular_reward_sheet_v2_start_block_index,5510416
+stake_regular_reward_sheet_v3_start_block_index,6700000
+stake_regular_reward_sheet_v4_start_block_index,6910000
+stake_regular_reward_sheet_v5_start_block_index,7650000
+";
+    }
+}
