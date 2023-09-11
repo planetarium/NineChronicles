e596a30a27cd99fae07908e327ec187f776ef3ca
jonny <jonny@planetariumhq.com>
2023-09-11 18:00:53 +0900

    add ShopDynamicGridLayoutView

---
+++ nekoyume/Assets/_Scripts/UI/Module/IAP/ShopDynamicGridLayoutView.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+namespace Nekoyume.UI.Module
+{
+    public class ShopDynamicGridLayoutView : MonoBehaviour
+    {
+        [SerializeField]
+        float space;
+
+        public void OnEnable()
+        {
+            Refresh();
+        }
+
+        public void Refresh()
+        {
+            var rectTrans = GetComponent<RectTransform>();
+            rectTrans.pivot = new Vector2(0, 1);
+
+            var children = GetComponentsInChildren<RectTransform>(false).ToList();
+            children.Remove(rectTrans);
+            children.Sort(Compare);
+
+            Vector2 lastPos = new Vector2(space, -space);
+            float minHeight = 0;
+            for (int i = 0; i < children.Count; i++)
+            {
+                children[i].pivot = new Vector2(0, 1);
+                children[i].anchorMin = new Vector2(0, 1);
+                children[i].anchorMax = new Vector2(0, 1);
+
+                children[i].SetSiblingIndex(i);
+                children[i].localPosition = lastPos;
+                if (lastPos.x + children[i].rect.width + space > rectTrans.rect.width ||
+                    lastPos.x + children[i].rect.width + space + children[Mathf.Min((i + 1), (children.Count - 1))].rect.width + space > rectTrans.rect.width)
+                {
+                    lastPos.x = space;
+                    lastPos.y -= (children[i].rect.height + space);
+
+                    for (int z = i; z >= 0; z--)
+                    {
+                        if (children[z].anchoredPosition.y - children[z].rect.height < lastPos.y)
+                            lastPos.x += children[z].rect.width + space;
+                    }
+                }
+                else
+                {
+                    lastPos.x += children[i].rect.width + space;
+                }
+
+                if ((lastPos.y - children[i].rect.height) < minHeight)
+                {
+                    minHeight = (lastPos.y - children[i].rect.height);
+                }
+            }
+
+            rectTrans.sizeDelta = new Vector2(rectTrans.sizeDelta.x, Mathf.Abs(minHeight) + space);
+        }
+
+        private static int Compare(RectTransform lhs, RectTransform rhs)
+        {
+            if (lhs == rhs) return 0;
+
+            var rectLhs = lhs.rect;
+            var rectRhs = rhs.rect;
+
+            if (rectLhs.height < rectRhs.height) return 1;
+            if (rectLhs.height > rectRhs.height) return -1;
+            return 0;
+        }
+    }
+}
