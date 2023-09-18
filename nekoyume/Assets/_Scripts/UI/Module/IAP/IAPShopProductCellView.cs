61323be06e8c4d060ed232a121b3276992bbdff6
Jonny <jonny@planetariumhq.com>
2023-09-18 19:39:59 +0900

    connect shop ui element

--- nekoyume/Assets/_Scripts/UI/Module/IAP/IAPShopProductCellView.cs
+++ nekoyume/Assets/_Scripts/UI/Module/IAP/IAPShopProductCellView.cs
@@ -2,18 +2,135 @@ using NineChronicles.ExternalServices.IAPService.Runtime.Models;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
 
 namespace Nekoyume.UI.Module
 {
     public class IAPShopProductCellView : MonoBehaviour
     {
+        [SerializeField]
+        private TextMeshProUGUI productName;
+
+        [SerializeField]
+        private TextMeshProUGUI discount;
+
+        [SerializeField]
+        private TextMeshProUGUI[] preDiscountPrice;
+
+        [SerializeField]
+        private TextMeshProUGUI[] price;
+
+        [SerializeField]
+        private TextMeshProUGUI buyLimitDescription;
+
+        [SerializeField]
+        private GameObject[] discountObjs;
+
+        [SerializeField]
+        private GameObject recommended;
+
+        [SerializeField]
+        private Button buyButton;
+
+        [SerializeField]
+        private Image backgroundImage;
+
         private RectTransform _rect;
         private ProductSchema _data;
+        private UnityEngine.Purchasing.Product _puchasingData;
 
-        public void SetData(ProductSchema data)
+        private void Awake()
+        {
+            buyButton.onClick.AddListener(()=> {
+                if (_data == null || !_data.Buyable)
+                    return;
+
+                Widget.Find<ShopListPopup>().Show(_data, _puchasingData);
+            });
+        }
+
+        public void SetData(ProductSchema data, bool isRecommended)
         {
             _data = data;
             _rect = GetComponent<RectTransform>();
+
+            productName.text = data.Name;
+
+
+            var isDiscount = false;
+
+            _puchasingData = Game.Game.instance.IAPStoreManager.IAPProducts.First(p => p.definition.id == data.GoogleSku);
+
+            foreach (var item in price)
+            {
+                item.text = _puchasingData.metadata.localizedPriceString;
+            }
+
+            foreach (var item in discountObjs)
+            {
+                item.SetActive(isDiscount);
+            }
+            //discount
+            if (isDiscount)
+            {
+                //discount.text = $"{data.DIscount}%";
+                foreach (var item in preDiscountPrice)
+                {
+                    //item.text = 
+                }
+            }
+
+
+            buyButton.interactable = data.Buyable;
+
+            recommended.SetActive(isRecommended);
+            /*            if (isOn)
+            {
+                Analyzer.Instance.Track(
+                    "Unity/Shop/IAP/Tab/Click",
+                    ("product-id", tab.ProductId));
+                var product = products?.FirstOrDefault(p => p.GoogleSku == tab.ProductId);
+                if (product is null)
+                {
+                    return;
+                }
+
+                var storeProduct = Game.Game.instance.IAPStoreManager.IAPProducts.First(p =>
+                    p.definition.id == tab.ProductId);
+
+                _selectedProductId = tab.ProductId;
+                view.PriceTexts.ForEach(text => text.text = storeProduct.metadata.localizedPriceString);
+                view.ProductImage.sprite =
+                    _productImageDictionary[GetProductImageNameFromProductId(tab.ProductId)];
+                view.PurchaseButton.interactable = product.Buyable;
+                var limit = product.DailyLimit ?? product.WeeklyLimit;
+                view.LimitCountObjects.ForEach(obj => obj.SetActive(limit.HasValue));
+                if (limit.HasValue)
+                {
+                    var remain = limit - product.PurchaseCount;
+                    view.BuyLimitCountText.ForEach(text => text.text = $"{remain}/{limit}");
+                }
+
+                view.RewardViews.ForEach(v => v.gameObject.SetActive(false));
+                foreach (var fungibleItemSchema in product.FungibleItemList)
+                {
+                    var rewardView =
+                        view.RewardViews.First(v => !v.gameObject.activeSelf);
+                    rewardView.RewardName.text =
+                        L10nManager.LocalizeItemName(fungibleItemSchema.SheetItemId);
+                    rewardView.RewardImage.sprite =
+                        SpriteHelper.GetItemIcon(fungibleItemSchema.SheetItemId);
+                    rewardView.RewardCount.text = $"x{fungibleItemSchema.Amount}";
+                    rewardView.gameObject.SetActive(true);
+                }
+
+                var messageKey = product.DailyLimit.HasValue
+                    ? "UI_MS_BUT_LIMIT_MESSAGE_DAY"
+                    : "UI_MS_BUT_LIMIT_MESSAGE_WEEK";
+                view.BuyLimitMessageText.text = L10nManager.Localize(messageKey);
+            }*/
         }
     }
 }
--- nekoyume/Assets/_Scripts/UI/Widget/Popup/ShopListPopup.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/Popup/ShopListPopup.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using Nekoyume.UI.Module;
+using NineChronicles.ExternalServices.IAPService.Runtime.Models;
+using Nekoyume.Helper;
+using System.Numerics;
 
 namespace Nekoyume.UI
 {
@@ -11,9 +14,11 @@ namespace Nekoyume.UI
         private Image productBgImage;
 
         [SerializeField]
-        private GameObject discountObj;
+        private GameObject[] discountObjs;
         [SerializeField]
         private TextMeshProUGUI discountText;
+        [SerializeField]
+        private TextMeshProUGUI[] preDiscountPrice;
 
         [SerializeField]
         private IAPRewardView[] iapRewards;
@@ -29,6 +34,12 @@ namespace Nekoyume.UI
         [SerializeField]
         private Button closeButton;
 
+        [SerializeField]
+        private TextMeshProUGUI[] priceTexts;
+
+        private ProductSchema _data;
+        private UnityEngine.Purchasing.Product _puchasingData;
+
         protected override void Awake()
         {
             base.Awake();
@@ -37,10 +48,70 @@ namespace Nekoyume.UI
             CloseWidget = () => Close();
         }
 
-        public override void Show(bool ignoreShowAnimation = false)
+        public void Show(ProductSchema data, UnityEngine.Purchasing.Product puchasingData,bool ignoreShowAnimation = false)
         {
-            base.Show(ignoreShowAnimation);
+            _data = data;
+            _puchasingData = puchasingData;
 
+            var isDiscount = false;
+
+            foreach (var item in priceTexts)
+            {
+                item.text = _puchasingData.metadata.localizedPriceString;
+            }
+
+            int iapRewardIndex = 0;
+            for (int i = 0; i < _data.FavList.Length; i++)
+            {
+                if(iapRewardIndex < iapRewards.Length)
+                {
+                    iapRewards[iapRewardIndex].gameObject.SetActive(true);
+                    iapRewards[iapRewardIndex].RewardImage.sprite = SpriteHelper.GetFavIcon(_data.FavList[i].Ticker.ToString());
+                    iapRewards[iapRewardIndex].RewardCount.text = ((BigInteger)_data.FavList[i].Amount).ToCurrencyNotation();
+                    iapRewards[iapRewardIndex].RewardGrade.sprite = SpriteHelper.GetItemBackground(Util.GetTickerGrade(_data.FavList[i].Ticker.ToString()));
+                    iapRewards[i].gameObject.SetActive(true);
+                    iapRewardIndex++;
+                }
+            }
+            for (int i = 0; i < _data.FungibleItemList.Length; i++)
+            {
+                if (iapRewardIndex < iapRewards.Length)
+                {
+                    iapRewards[iapRewardIndex].gameObject.SetActive(true);
+                    iapRewards[iapRewardIndex].RewardImage.sprite = SpriteHelper.GetItemIcon(_data.FungibleItemList[i].SheetItemId);
+                    iapRewards[iapRewardIndex].RewardCount.text = $"x{_data.FungibleItemList[i].Amount}";
+                    try
+                    {
+                        iapRewards[iapRewardIndex].RewardGrade.sprite = SpriteHelper.GetItemBackground(Game.Game.instance.TableSheets.ItemSheet[_data.FungibleItemList[i].SheetItemId].Grade);
+                    }
+                    catch
+                    {
+                        Debug.LogError($"Can't Find Item ID {_data.FungibleItemList[i].SheetItemId} in ItemSheet");
+                    }
+                    iapRewards[i].gameObject.SetActive(true);
+                    iapRewardIndex++;
+                }
+            }
+            for (; iapRewardIndex < iapRewards.Length; iapRewardIndex++)
+            {
+                iapRewards[iapRewardIndex].gameObject.SetActive(true);
+            }
+
+            foreach (var item in discountObjs)
+            {
+                item.SetActive(isDiscount);
+            }
+
+            if (isDiscount)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            base.Show(ignoreShowAnimation);
         }
     }
 }
