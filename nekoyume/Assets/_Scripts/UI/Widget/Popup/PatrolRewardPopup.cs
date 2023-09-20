3591093598f38794c5dfb808bcf6eabfdb6a2392
tyrosine1153 <tyrosine1153@gmail.com>
2023-09-20 18:25:27 +0900

    Edit PatrolRewardPopup
    for service model

--- nekoyume/Assets/_Scripts/UI/Widget/Popup/PatrolRewardPopup.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/Popup/PatrolRewardPopup.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Nekoyume.Game.Controller;
 using Nekoyume.L10n;
+using Nekoyume.UI.Model;
 using Nekoyume.UI.Module;
-using Nekoyume.UI.Module.Lobby;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -24,20 +24,12 @@ namespace Nekoyume.UI
         [Serializable]
         private class RewardData
         {
-            public CostType costType;
+            public PatrolRewardType rewardType;
             public RewardView cumulativeReward;
             public RewardView rewardPerTime;
         }
 
-        [Serializable]
-        private struct PatrolTimeStruct
-        {
-            public int hour;
-            public int minute;
-        }
-
         [SerializeField] private Button closeButton;
-
         [SerializeField] private RewardData[] rewardViews;
         [SerializeField] private TextMeshProUGUI patrolTimeText;
         [SerializeField] private Image patrolTimeGauge;
@@ -45,56 +37,23 @@ namespace Nekoyume.UI
 
         private static readonly TimeSpan MaxTime = new(24, 0, 0);
         private static readonly TimeSpan MinTime = new(0, 10, 0);
+        private readonly Dictionary<PatrolRewardType, int> _rewards = new();
         private readonly List<IDisposable> _disposables = new();
 
-        private Dictionary<CostType, int> _rewards;
-        private Dictionary<CostType, int> _rewardsPerTime;
-        private DateTime _lastRewardTime;
         private TimeSpan PatrolTime
         {
             get
             {
-                var patrolTime = DateTime.Now - _lastRewardTime;
+                var patrolTime = DateTime.Now - SharedModel.LastRewardTime.Value;
                 return patrolTime > MaxTime ? MaxTime : patrolTime;
             }
         }
 
-        // 임시 데이터
-        [SerializeField] private int menuLevelForTest;
-        [SerializeField] private int levelForTest;
-        [SerializeField] private PatrolTimeStruct patrolTimeForTest;
-        private static readonly Dictionary<int, Dictionary<CostType, int>> RewardTable = new()
-        {
-            [1] = new Dictionary<CostType, int>
-            {
-                [CostType.Crystal] = 50,
-                [CostType.ActionPoint] = 1,
-                [CostType.SilverDust] = 50,
-                [CostType.GoldDust] = 1,
-            },
-            [150] = new Dictionary<CostType, int>
-            {
-                [CostType.Crystal] = 125,
-                [CostType.ActionPoint] = 1,
-                [CostType.SilverDust] = 60,
-                [CostType.GoldDust] = 1,
-            },
-            [250] = new Dictionary<CostType, int>
-            {
-                [CostType.Crystal] = 250,
-                [CostType.ActionPoint] = 1,
-                [CostType.SilverDust] = 70,
-                [CostType.GoldDust] = 2,
-            },
-        };
+        public bool Notification => PatrolTime >= MinTime;
 
-        private static readonly Dictionary<CostType, TimeSpan> UnitTable = new()
-        {
-            [CostType.GoldDust] = new TimeSpan(8, 0, 0),
-            [CostType.SilverDust] = new TimeSpan(6, 0, 0),
-            [CostType.ActionPoint] = new TimeSpan(24, 0, 0),
-            [CostType.Crystal] = new TimeSpan(0, 5, 0)
-        };
+        public readonly PatrolReward SharedModel = new();
+
+        private bool _initialized;
 
         protected override void Awake()
         {
@@ -112,108 +71,114 @@ namespace Nekoyume.UI
         {
             base.Show(ignoreShowAnimation);
 
-            _disposables.DisposeAllAndClear();
-            _lastRewardTime = DateTime.Today; // Todo : Fill this
-
-            FindObjectOfType<PatrolRewardMenu>().SetLevel(menuLevelForTest, false);
-
-            var patrolTime = new TimeSpan(patrolTimeForTest.hour, patrolTimeForTest.minute, 0);
-            Observable
-                .Timer(TimeSpan.FromMinutes(0), TimeSpan.FromMinutes(1))
-                .Subscribe(_ => patrolTime = patrolTime.Add(TimeSpan.FromMinutes(1)))
-                // .Subscribe(_ => _patrolTime = DateTime.Now - lastRewardTime)
-                .AddTo(_disposables);
-
-            // var level = States.Instance.CurrentAvatarState.level;
-            var level = levelForTest;
-            var rewardLevel = 0;
-            foreach (var key in RewardTable.Keys)
+            if (!SharedModel.Initialized)
             {
-                if (level >= key)
-                {
-                    rewardLevel = key;
-                }
-                else
-                {
-                    break;
-                }
+                SharedModel.Initialize();
             }
-            _rewardsPerTime = RewardTable[rewardLevel];
 
-            receiveButton.OnSubmitSubject
-                .Subscribe(_ => ReceiveReward(_rewards))
-                .AddTo(_disposables);
+            if (!_initialized)
+            {
+                Init();
+            }
+
+            OnChangeTime(PatrolTime);
+
+            var level = Game.Game.instance.States.CurrentAvatarState.level;
+            if (SharedModel.NextLevel <= level)
+            {
+                // PatrolReward.LoadPolicyInfo
+                SharedModel.LoadPolicyInfo(level);
+            }
         }
 
-        // rewardPerTime
-        private void OnChangeRewardsPerTime(Dictionary<CostType, int> rewardsPerTime)
+        private void Init()
+        {
+            _disposables.DisposeAllAndClear();
+
+            SharedModel.RewardModels
+                .Subscribe(OnChangeRewardModels)
+                .AddTo(_disposables);
+
+            SharedModel.LastRewardTime
+                .Subscribe(_ => OnChangeTime(PatrolTime))
+                .AddTo(_disposables);
+
+            Observable
+                .Timer(TimeSpan.FromMinutes(0), TimeSpan.FromMinutes(1))
+                .Where(_ => gameObject.activeSelf && SharedModel.Initialized)
+                .Subscribe(_ => OnChangeTime(PatrolTime))
+                .AddTo(_disposables);
+
+            receiveButton.OnSubmitSubject
+                .Where(_ => SharedModel.Initialized)
+                .Subscribe(_ => ClaimReward(_rewards))
+                .AddTo(_disposables);
+
+            _initialized = true;
+        }
+
+        // rewardModels
+        private void OnChangeRewardModels(List<PatrolRewardModel> rewardModels)
         {
             foreach (var bonusRewardView in rewardViews)
             {
                 bonusRewardView.rewardPerTime.container.SetActive(false);
             }
 
-            foreach (var (costType, value) in rewardsPerTime)
+            foreach (var reward in rewardModels)
             {
-                var view = rewardViews.FirstOrDefault(view => view.costType == costType);
+                var rewardType = GetRewardType(reward);
+                var view = rewardViews.FirstOrDefault(view => view.rewardType == rewardType);
                 if (view == null)
                 {
                     continue;
                 }
 
-                var unitTime = UnitTable[costType];
-                var unitText = unitTime.TotalHours > 1
-                    ? $"{unitTime.TotalHours}h"
-                    : $"{unitTime.TotalMinutes}m";
+                var interval = reward.RewardInterval;
+                var intervalText = interval.TotalHours > 1
+                    ? $"{interval.TotalHours}h"
+                    : $"{interval.TotalMinutes}m";
                 view.rewardPerTime.container.SetActive(true);
-                view.rewardPerTime.text.text = $"{value}/{unitText}";
+                view.rewardPerTime.text.text = $"{reward.PerInterval}/{intervalText}";
             }
 
-            SetRewards(rewardsPerTime, PatrolTime);
+            SetRewards(rewardModels, PatrolTime);
         }
 
         // time
-        private void OnChangeTime()
+        private void OnChangeTime(TimeSpan patrolTime)
         {
-            var patrolTime = PatrolTime;
-
+            var hourText = patrolTime.TotalHours >= 1
+                ? $"{(int)patrolTime.TotalHours}h "
+                : string.Empty;
+            var minuteText = patrolTime.TotalHours < 1 || patrolTime.Minutes >= 1
+                ? $"{patrolTime.Minutes}m"
+                : string.Empty;
             patrolTimeText.text = L10nManager.Localize(
-                "UI_PATROL_TIME_FORMAT",
-                $"{patrolTime.TotalHours}h {patrolTime.Minutes}m");
+                "UI_PATROL_TIME_FORMAT", $"{hourText}{minuteText}");
             patrolTimeGauge.fillAmount = (float)(patrolTime / MaxTime);
 
-            var remainTime = MinTime - patrolTime;
+            var ticksWithoutSeconds = patrolTime.Ticks / TimeSpan.TicksPerMinute * TimeSpan.TicksPerMinute;
+            var remainTime = MinTime - new TimeSpan(ticksWithoutSeconds);
             var canReceive = remainTime <= TimeSpan.Zero;
             receiveButton.Interactable = canReceive;
             receiveButton.Text = canReceive
                 ? L10nManager.Localize("UI_GET_REWARD")
-                : L10nManager.Localize("UI_REMAINING_TIME", remainTime.ToString(@"m'm'"));
+                : L10nManager.Localize("UI_REMAINING_TIME", $"{remainTime.Minutes}m");
 
-            SetRewards(_rewardsPerTime, patrolTime);
+            SetRewards(SharedModel.RewardModels.Value, patrolTime);
         }
 
-        // rewardPerTime, patrolTime
-        private void SetRewards(Dictionary<CostType, int> rewardsPerTime, TimeSpan patrolTime)
-        {
-            _rewards.Clear();
-
-            var clampedPatrolTime = patrolTime > MaxTime ? MaxTime : patrolTime;
-            foreach (var (costType, value) in rewardsPerTime)
-            {
-                _rewards[costType] = value * (int)(clampedPatrolTime / UnitTable[costType]);
-            }
-        }
-
-        private void OnChangeRewards(Dictionary<CostType, int> rewards)
+        private void OnChangeRewards(Dictionary<PatrolRewardType, int> rewards)
         {
             foreach (var bonusRewardView in rewardViews)
             {
                 bonusRewardView.cumulativeReward.container.SetActive(false);
             }
 
-            foreach (var (costType, value) in rewards)
+            foreach (var (rewardType, value) in rewards)
             {
-                var view = rewardViews.FirstOrDefault(view => view.costType == costType);
+                var view = rewardViews.FirstOrDefault(view => view.rewardType == rewardType);
                 if (view == null)
                 {
                     continue;
@@ -224,7 +189,40 @@ namespace Nekoyume.UI
             }
         }
 
-        private void ReceiveReward(Dictionary<CostType, int> rewards)
+        private void SetRewards(List<PatrolRewardModel> rewardModels, TimeSpan patrolTime)
+        {
+            _rewards.Clear();
+
+            foreach (var reward in rewardModels)
+            {
+                var rewardType = GetRewardType(reward);
+                _rewards[rewardType] = reward.PerInterval * (int)(patrolTime / reward.RewardInterval);
+            }
+
+            OnChangeRewards(_rewards);
+        }
+
+        private static PatrolRewardType GetRewardType(PatrolRewardModel reward)
+        {
+            if (reward.ItemId != null)
+            {
+                return reward.ItemId switch
+                {
+                    500000 => PatrolRewardType.ApStone,
+                    600201 => PatrolRewardType.GoldPowder,
+                    800201 => PatrolRewardType.SilverPowder,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+            }
+
+            return reward.Currency switch
+            {
+                "CRYSTAL" => PatrolRewardType.Crystal,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        private void ClaimReward(Dictionary<PatrolRewardType, int> rewards)
         {
             foreach (var reward in rewards)
             {
@@ -233,6 +231,7 @@ namespace Nekoyume.UI
 
             Close();
             // PatrolRewardMenu.DailyRewardAnimation
+            SharedModel.ClaimReward();
         }
     }
 }
