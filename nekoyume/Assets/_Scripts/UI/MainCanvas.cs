69fd65e96b21634083cfbcd8bd5c2af9b0a91f6b
jonny <jonny@planetariumhq.com>
2023-09-21 18:48:05 +0900

    fix compile error

--- nekoyume/Assets/_Scripts/UI/MainCanvas.cs
+++ nekoyume/Assets/_Scripts/UI/MainCanvas.cs
@@ -379,7 +379,7 @@ namespace Nekoyume.UI
             yield return null;
             _secondWidgets.Add(Widget.Create<SummonDetailPopup>());
             yield return null;
-            secondWidgets.Add(Widget.Create<ShopListPopup>());
+            _secondWidgets.Add(Widget.Create<ShopListPopup>());
             yield return null;
 
             // tooltip
