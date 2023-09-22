1f09d239e3ca8fdbae564c3620ba6e5afdc3da7e
tyrosine1153 <tyrosine1153@gmail.com>
2023-09-22 18:33:01 +0900

    Fix patrol reward

--- nekoyume/Assets/_Scripts/UI/Module/Lobby/PatrolRewardMenu.cs
+++ nekoyume/Assets/_Scripts/UI/Module/Lobby/PatrolRewardMenu.cs
@@ -73,11 +73,16 @@ namespace Nekoyume.UI.Module.Lobby
                 {
                     SetCanClaim(false, true);
                 }
-                else
-                {
-                    ClaimRewardAnimation();
-                }
             }).AddTo(_disposables);
+
+            this.UpdateAsObservable()  // For internal Test
+                .Where(_ => Input.GetKeyDown(KeyCode.W))
+                .Subscribe(x =>
+                {
+                    var avatarAddress = Game.Game.instance.States.CurrentAvatarState.address;
+                    var agentAddress = Game.Game.instance.States.AgentState.address;
+                    patrolReward.LoadAvatarInfo(avatarAddress.ToHex(), agentAddress.ToHex());
+                });
         }
 
         private void SetCanClaim(bool patrolling, bool claiming)
@@ -93,7 +98,7 @@ namespace Nekoyume.UI.Module.Lobby
             dimmedText.text = L10nManager.Localize(messageKey);
         }
 
-        private void ClaimRewardAnimation()
+        public void ClaimRewardAnimation()
         {
             var target = Widget.Find<HeaderMenuStatic>()
                 .GetToggle(HeaderMenuStatic.ToggleType.AvatarInfo);
