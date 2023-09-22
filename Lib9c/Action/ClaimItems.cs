5a1a257c70191d57549627a529025603f8222b0e
XxshiftxX <qjfrntop12@gmail.com>
2023-09-22 10:12:37 +0900

    refactor to seperate methods

--- Lib9c/Action/ClaimItems.cs
+++ Lib9c/Action/ClaimItems.cs
@@ -8,6 +8,7 @@ using Libplanet.Action;
 using Libplanet.Action.State;
 using Libplanet.Crypto;
 using Libplanet.Types.Assets;
+using Nekoyume.Action;
 using Nekoyume.Extensions;
 using Nekoyume.Model.Item;
 using Nekoyume.Model.State;
@@ -23,9 +24,12 @@ namespace Nekoyume.Action
         public IEnumerable<Address> AvatarAddresses { get; private set; }
         public IEnumerable<FungibleAssetValue> Amounts { get; private set; }
 
-        public ClaimItems() {}
+        public ClaimItems()
+        {
+        }
 
-        public ClaimItems(IEnumerable<Address> avatarAddresses, IEnumerable<FungibleAssetValue> amounts)
+        public ClaimItems(IEnumerable<Address> avatarAddresses,
+            IEnumerable<FungibleAssetValue> amounts)
         {
             AvatarAddresses = avatarAddresses;
             Amounts = amounts;
@@ -48,18 +52,50 @@ namespace Nekoyume.Action
             context.UseGas(1);
 
             var states = context.PreviousState;
-            var itemSheet = states.GetSheets(containItemSheet: true).GetItemSheet();
-            var inventoryAddresses =
-                AvatarAddresses.Select(avatarAddress => avatarAddress.Derive(LegacyInventoryKey)).ToList();
+            states = BurnAssets(context, states);
 
-            var inventories = inventoryAddresses.Select(inventoryAddress =>
+            var items = BuildItems(context, states);
+            var inventoryDictionary = BuildInventoryDictionary(context, states);
+
+            foreach (var (item, amount) in items)
             {
-                var addressHex = GetSignerAndOtherAddressesHex(context, inventoryAddress);
-                return states.GetInventory(inventoryAddress)
-                    ?? throw new FailedLoadStateException(ActionTypeText, addressHex, typeof(Inventory), inventoryAddress);
-            }).ToList();
+                foreach (var inventory in inventoryDictionary.Values)
+                {
+                    inventory.AddItem(item, amount);
+                }
+            }
 
+            return inventoryDictionary
+                .OrderBy(keyValuePair => keyValuePair.Key)
+                .Aggregate(states, (current, inventoryKeyValue) => current.SetState(
+                    inventoryKeyValue.Key,
+                    inventoryKeyValue.Value.Serialize()));
+        }
+
+        private IAccount BurnAssets(IActionContext context, IAccount states)
+        {
             foreach (var fungibleAssetValue in Amounts)
+            {
+                var decimalPlaces = fungibleAssetValue.Currency.DecimalPlaces;
+                if (decimalPlaces != 0)
+                {
+                    throw new ArgumentException(
+                        "DecimalPlaces of fungibleAssetValue for claimItems are not 0");
+                }
+
+                states = states.BurnAsset(context, context.Signer,
+                    fungibleAssetValue * AvatarAddresses.Count());
+            }
+
+            return states;
+        }
+
+        private List<(ItemBase item, int amount)> BuildItems(IActionContext context,
+            IAccount states)
+        {
+            var itemSheet = states.GetSheets(containItemSheet: true).GetItemSheet();
+
+            return Amounts.Select(fungibleAssetValue =>
             {
                 var ticker = fungibleAssetValue.Currency.Ticker;
                 if (!ticker.StartsWith("IT_") ||
@@ -68,15 +104,6 @@ namespace Nekoyume.Action
                     throw new ArgumentException($"Format of Amount currency's ticker is invalid");
                 }
 
-                var decimalPlaces = fungibleAssetValue.Currency.DecimalPlaces;
-                if (decimalPlaces != 0)
-                {
-                    throw new ArgumentException(
-                        "DecimalPlaces of fungibleAssetValue for claimItems are not 0");
-                }
-
-                states = states.BurnAsset(context, context.Signer, fungibleAssetValue * inventories.Count);
-
                 var item = itemSheet[itemId] switch
                 {
                     MaterialItemSheet.Row materialRow =>
@@ -84,18 +111,24 @@ namespace Nekoyume.Action
                     var itemRow => ItemFactory.CreateItem(itemRow, context.Random)
                 };
 
-                foreach (var inventory in inventories)
-                {
-                    inventory.AddItem(item, (int)fungibleAssetValue.RawValue);
-                }
-            }
+                return (item, 0);
+            }).ToList();
+        }
 
-            foreach (var (inventoryAddress, i) in inventoryAddresses.Select((x, i) => (x, i)))
-            {
-                states = states.SetState(inventoryAddress, inventories[i].Serialize());
-            }
-
-            return states;
+        private Dictionary<Address, Inventory> BuildInventoryDictionary(
+            IActionContext context,
+            IAccountState states)
+        {
+            return AvatarAddresses
+                .Select(avatarAddress => avatarAddress.Derive(LegacyInventoryKey))
+                .ToDictionary(
+                    inventoryAddress => inventoryAddress,
+                    inventoryAddress => states.GetInventory(inventoryAddress) ??
+                                        throw new FailedLoadStateException(
+                                            ActionTypeText,
+                                            GetSignerAndOtherAddressesHex(context, inventoryAddress),
+                                            typeof(Inventory),
+                                            inventoryAddress));
         }
     }
 }
