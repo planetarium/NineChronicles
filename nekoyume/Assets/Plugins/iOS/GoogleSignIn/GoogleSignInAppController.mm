85265de5c7d44f06364346ac380aa2cb7aa92f35
Daniel Bae <daniel.bae@devcra.com>
2023-09-25 19:43:22 +0900

    fix: update GoogleSignin

--- nekoyume/Assets/Plugins/iOS/GoogleSignIn/GoogleSignInAppController.mm
+++ nekoyume/Assets/Plugins/iOS/GoogleSignIn/GoogleSignInAppController.mm
@@ -59,29 +59,9 @@ GoogleSignInHandler *gsiHandler;
   method_exchangeImplementations(original, swizzled);
 }
 
-- (BOOL)GoogleSignInAppController:(UIApplication *)application
-    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-  // IMPORTANT: IF you are not supplying a GoogleService-Info.plist in your
-  // project that contains the client id, you need to set the client id here.
-
-  NSString *path = [[NSBundle mainBundle] pathForResource:@"GoogleService-Info"
-                                                   ofType:@"plist"];
-  NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:path];
-  NSString *clientId = [dict objectForKey:@"CLIENT_ID"];
-
-  gsiHandler = [GoogleSignInHandler alloc];
-
-  // Setup the Sign-In instance.
-  GIDSignIn *signIn = [GIDSignIn sharedInstance];
-  signIn.clientID = clientId;
-  signIn.uiDelegate = gsiHandler;
-  signIn.delegate = gsiHandler;
-
-  // looks like it's just calling itself, but the implementations were swapped
-  // so we're actually calling the original once we're done
-  return [self GoogleSignInAppController:application
-           didFinishLaunchingWithOptions:launchOptions];
+- (BOOL)GoogleSignInAppController:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  NSLog(@"GSI application:didFinishLaunchingWithOption:");
+  return [self GoogleSignInAppController:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 /**
@@ -95,11 +75,8 @@ GoogleSignInHandler *gsiHandler;
                                          openURL:url
                                sourceApplication:sourceApplication
                                       annotation:annotation];
-
-  return [[GIDSignIn sharedInstance] handleURL:url
-                             sourceApplication:sourceApplication
-                                    annotation:annotation] ||
-         handled;
+  NSLog(@"GSI application:openURL:sourceApplication:annotation: %s", [url.absoluteString UTF8String]);
+  return [[GIDSignIn sharedInstance] handleURL:url] || handled;
 }
 
 /**
@@ -108,17 +85,9 @@ GoogleSignInHandler *gsiHandler;
 - (BOOL)GoogleSignInAppController:(UIApplication *)app
                           openURL:(NSURL *)url
                           options:(NSDictionary *)options {
-
-  BOOL handled =
-      [self GoogleSignInAppController:app openURL:url options:options];
-
-  return [[GIDSignIn sharedInstance]
-                     handleURL:url
-             sourceApplication:
-                 options[UIApplicationOpenURLOptionsSourceApplicationKey]
-                    annotation:
-                        options[UIApplicationOpenURLOptionsAnnotationKey]] ||
-         handled;
+  BOOL handled = [self GoogleSignInAppController:app openURL:url options:options];
+  NSLog(@"GSI application:openURL:options: %s", [url.absoluteString UTF8String]);
+  return [[GIDSignIn sharedInstance] handleURL:url] || handled;
 }
 
 @end
