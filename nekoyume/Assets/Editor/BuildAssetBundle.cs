9aa81b9095083aee2abd6214fc042d2d20d0ce57
uitae <uitae.kim@devcra.com>
2023-10-04 03:23:53 +0900

    implemented utils editor scripts

---
+++ nekoyume/Assets/Editor/BuildAssetBundle.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildAssetBundle
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory,
+            BuildAssetBundleOptions.None,
+            BuildTarget.StandaloneWindows);
+    }
+}
