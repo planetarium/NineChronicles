630bc04bd209c7fb7d4ffeb5c1413e3f569ea67c
Suho Lee <iam@suho.io>
2023-10-10 17:36:42 +0900

    refactor: use `GetRandom` instead of `Random`

--- Lib9c/Action/CreateAvatar.cs
+++ Lib9c/Action/CreateAvatar.cs
@@ -196,6 +196,7 @@ namespace Nekoyume.Action
             var skillSheet = states.GetSheet<SkillSheet>();
             var characterLevelSheet = states.GetSheet<CharacterLevelSheet>();
             var enhancementCostSheet = states.GetSheet<EnhancementCostSheetV2>();
+            var random = context.GetRandom();
 
             avatarState.level = 300;
             avatarState.exp = characterLevelSheet[300].Exp;
@@ -210,7 +211,7 @@ namespace Nekoyume.Action
 
                 var equipment = (Equipment)ItemFactory.CreateItemUsable(
                     equipmentRow,
-                    context.Random.GenerateRandomGuid(),
+                    random.GenerateRandomGuid(),
                     0L,
                     madeWithMimisbrunnrRecipe: subRecipeRow.IsMimisbrunnrSubRecipe ?? false);
 
@@ -250,7 +251,7 @@ namespace Nekoyume.Action
                         .First(x => x.ItemSubType == subType &&
                                     x.Grade == grade &&
                                     x.Level == i);
-                    equipment.LevelUp(ctx.Random, costRow, true);
+                    equipment.LevelUp(random, costRow, true);
                 }
 
                 avatarState.inventory.AddItem(equipment);
