1914670b399a9d786563ae17e1679c9e057da38e
Suho Lee <iam@suho.io>
2023-10-10 16:09:17 +0900

    test: fix test for `ActionEvaluationSerializer`

--- .Libplanet.Extensions.ActionEvaluatorCommonComponents.Tests/ActionEvaluationSerializerTest.cs
+++ .Libplanet.Extensions.ActionEvaluatorCommonComponents.Tests/ActionEvaluationSerializerTest.cs
@@ -1,7 +1,10 @@
 using System.Collections.Immutable;
 using Bencodex.Types;
 using Libplanet.Action;
+using Libplanet.Action.State;
 using Libplanet.Crypto;
+using Libplanet.Store;
+using Libplanet.Store.Trie;
 
 namespace Libplanet.Extensions.ActionEvaluatorCommonComponents.Tests;
 
@@ -10,30 +13,43 @@ public class ActionEvaluationSerializerTest
     [Fact]
     public void Serialization()
     {
+        IKeyValueStore keyValueStore = new MemoryKeyValueStore();
+        IStateStore stateStore = new TrieStateStore(keyValueStore);
         var addresses = Enumerable.Repeat(0, 4).Select(_ => new PrivateKey().ToAddress()).ToImmutableList();
         AccountStateDelta outputStates = (AccountStateDelta)new AccountStateDelta()
             .SetState(addresses[0], Null.Value)
             .SetState(addresses[1], (Text)"foo")
             .SetState(addresses[2], new List((Text)"bar"));
+        var previousTrie = stateStore.GetStateRoot(null);
+        var nextTrie = previousTrie;
+        foreach (var kv in outputStates.Delta.ToRawDelta())
+        {
+            nextTrie = nextTrie.Set(kv.Key, kv.Value);
+        }
+        nextTrie = stateStore.Commit(nextTrie);
 
-        var previousStates = new AccountStateDelta();
-
-        var actionEvaluation = new ActionEvaluation(
-            Null.Value,
-            new ActionContext(null,
-                addresses[0],
+        var committed = new CommittedActionEvaluation(
+            action: Null.Value,
+            inputContext: new CommittedActionContext(
+                signer: addresses[0],
+                txId: null,
+                miner: addresses[1],
+                blockIndex: 0,
+                blockProtocolVersion: 0,
+                rehearsal: false,
+                previousState: previousTrie.Hash,
+                randomSeed: 123,
+                blockAction: true),
+            outputState: nextTrie.Hash,
+            exception: new UnexpectedlyTerminatedActionException(
+                "",
                 null,
-                addresses[1],
-                0,
-                0,
-                false,
-                previousStates,
-                123,
                 null,
-                true),
-            outputStates,
-            new Libplanet.Action.UnexpectedlyTerminatedActionException("", null, null, null, null, new NullAction(), null));
-        var serialized = ActionEvaluationMarshaller.Serialize(actionEvaluation);
+                null,
+                null,
+                new NullAction(),
+                new Exception()));
+        var serialized = ActionEvaluationMarshaller.Serialize(committed);
         var deserialized = ActionEvaluationMarshaller.Deserialize(serialized);
 
         Assert.Equal(Null.Value, deserialized.Action);
@@ -42,8 +58,7 @@ public class ActionEvaluationSerializerTest
         Assert.Equal(0, deserialized.InputContext.BlockProtocolVersion);
         Assert.Equal(addresses[0], deserialized.InputContext.Signer);
         Assert.Equal(addresses[1], deserialized.InputContext.Miner);
-        Assert.Equal(Null.Value, deserialized.OutputState.GetState(addresses[0]));
-        Assert.Equal((Text)"foo", deserialized.OutputState.GetState(addresses[1]));
-        Assert.Equal(new List((Text)"bar"), deserialized.OutputState.GetState(addresses[2]));
+        Assert.Equal(previousTrie.Hash, deserialized.InputContext.PreviousState);
+        Assert.Equal(nextTrie.Hash, deserialized.OutputState);
     }
 }
