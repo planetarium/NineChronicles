583be008626bf76a3eef7bae6a427411efe2b849
uitae <uitae.kim@devcra.com>
2023-10-20 13:29:15 +0900

    Implemented async loader

--- nekoyume/Assets/_Scripts/AssetBundleHelper/AssetBundleLoader.cs
+++ nekoyume/Assets/_Scripts/AssetBundleHelper/AssetBundleLoader.cs
@@ -1,9 +1,7 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Nekoyume.AssetBundleHelper
@@ -12,6 +10,67 @@ namespace Nekoyume.AssetBundleHelper
     {
         private static readonly bool useCache = true;
         private static readonly Dictionary<string, AssetBundle> loadedAssetBundleCache = new();
+
+        #region ASYNC_LOADING
+
+        public static IEnumerator LoadAllAssetBundleAsync<T>(string bundleName,
+            Action<float> onProgress, Action<T[]> onFinished) where T : UnityEngine.Object
+        {
+            return LoadFromFileAsync(bundleName, onProgress, assetBundle =>
+            {
+                var assets = assetBundle.LoadAllAssets<T>();
+                onFinished(assets);
+            });
+        }
+
+        public static IEnumerator LoadAssetBundleAsync<T>(string bundleName, string objectName,
+            Action<float> onProgress, Action<T> onFinished) where T : UnityEngine.Object
+        {
+            return LoadFromFileAsync(bundleName, onProgress, assetBundle =>
+            {
+                var asset = assetBundle.LoadAsset<T>(objectName);
+                onFinished(asset);
+            });
+        }
+
+        private static IEnumerator LoadFromFileAsync(string bundleName, Action<float> onProgress,
+            Action<AssetBundle> onFinished)
+        {
+            if (useCache && loadedAssetBundleCache.ContainsKey(bundleName))
+            {
+                onFinished(loadedAssetBundleCache[bundleName]);
+                yield break;
+            }
+
+            var path = Path.Combine(Application.streamingAssetsPath, bundleName);
+            var request = AssetBundle.LoadFromFileAsync(path);
+            while
+                (request.progress <
+                 0.95) // condition checks like [progress < 1] does not work properly due to the Unity engine bug
+            {
+                onProgress(request.progress);
+                yield return null;
+            }
+
+            yield return request;
+            var loadedBundle = request.assetBundle;
+            if (loadedBundle == null)
+            {
+                Debug.LogError($"Failed to load Assetbundle at ${path} - ${bundleName}");
+                yield break;
+            }
+
+            if (useCache)
+            {
+                loadedAssetBundleCache[bundleName] = loadedBundle;
+            }
+
+            onFinished.Invoke(loadedBundle);
+        }
+
+        #endregion
+
+        #region STATIC_LOADING
 
         public static void LoadVFXCache()
         {
@@ -56,7 +115,6 @@ namespace Nekoyume.AssetBundleHelper
 
         private static bool LoadFromFile(string bundleName)
         {
-            // TODO: change path to downloaded assetbundle using UWR
             var path = Path.Combine(Application.streamingAssetsPath, bundleName);
             var loadedBundle = AssetBundle.LoadFromFile(path);
             if (loadedBundle == null)
@@ -72,5 +130,7 @@ namespace Nekoyume.AssetBundleHelper
 
             return true;
         }
+
+        #endregion
     }
 }
