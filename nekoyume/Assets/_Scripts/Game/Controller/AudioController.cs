53857ac75b4a27fb4bb2388276e5bab5e98b2014
yujeong <yujeong@planetariumhq.com>
2023-11-02 17:33:22 +0900

    change audiocontroller string

--- nekoyume/Assets/_Scripts/Game/Controller/AudioController.cs
+++ nekoyume/Assets/_Scripts/Game/Controller/AudioController.cs
@@ -36,7 +36,7 @@ namespace Nekoyume.Game.Controller
             public const string Main = "bgm_main";
             public const string Shop = "bgm_shop";
             public const string Ranking = "bgm_ranking";
-            public const string Combination = "bgm_combination";
+            public const string Combination = "bgm_workshop";
             public const string StageGreen = "bgm_stage_green";
             public const string StageBlue = "bgm_stage_blue";
             public const string StageOrange = "bgm_stage_orange";
