183af592edf4aca51f1ec5a6034375435a7c64cf
XxshiftxX <qjfrntop12@gmail.com>
2023-11-06 16:35:19 +0900

    Add test case for testing success case

--- .Lib9c.Tests/Action/Garages/UnloadFromGaragesTest.cs
+++ .Lib9c.Tests/Action/Garages/UnloadFromGaragesTest.cs
@@ -5,11 +5,16 @@ namespace Lib9c.Tests.Action.Garages
     using System.Collections.Generic;
     using System.Linq;
     using System.Security.Cryptography;
+    using Lib9c.Tests.Util;
+    using Libplanet.Action.State;
     using Libplanet.Common;
     using Libplanet.Crypto;
     using Libplanet.Types.Assets;
     using Nekoyume;
+    using Nekoyume.Action;
     using Nekoyume.Action.Garages;
+    using Nekoyume.Model.Garages;
+    using Nekoyume.Model.Item;
     using Xunit;
 
     public class UnloadFromGaragesTest
@@ -18,10 +23,28 @@ namespace Lib9c.Tests.Action.Garages
 
         private static readonly Address AgentAddress = new PrivateKey().ToAddress();
 
+        private static readonly Address AvatarAddress =
+            Addresses.GetAvatarAddress(AgentAddress, AvatarIndex);
+
+        private readonly TableSheets _tableSheets;
+        private readonly Currency _ncg;
+        private readonly IAccount _previousStates;
+
+        public UnloadFromGaragesTest()
+        {
+            var initializeStates = InitializeUtil.InitializeStates(
+                agentAddr: AgentAddress,
+                avatarIndex: AvatarIndex);
+            _tableSheets = initializeStates.tableSheets;
+            _previousStates = initializeStates.initialStatesWithAvatarStateV2;
+            _ncg = initializeStates.initialStatesWithAvatarStateV2.GetGoldCurrency();
+        }
+
         public static IEnumerable<object[]> Get_Sample_PlainValue()
         {
             var avatarAddress = Addresses.GetAvatarAddress(AgentAddress, AvatarIndex);
-            var fungibleAssetValues = GetFungibleAssetValues(AgentAddress, avatarAddress);
+            var fungibleAssetValues = GetFungibleAssetValues(AgentAddress, avatarAddress)
+                as IEnumerable<(Address balanceAddr, FungibleAssetValue value)>;
             var hex = string.Join(
                 string.Empty,
                 Enumerable.Range(0, 64).Select(i => (i % 10).ToString()));
@@ -83,7 +106,52 @@ namespace Lib9c.Tests.Action.Garages
             }
         }
 
-        private static IEnumerable<(Address balanceAddr, FungibleAssetValue value)>
+        [Fact]
+        public void Execute_Success()
+        {
+            const long blockIndex = 0L;
+            var (states, unloadDataEnumerable) = RegisterPlainValue(_previousStates);
+            var action = new UnloadFromGarages(new[] { unloadDataEnumerable });
+            states = action.Execute(new ActionContext
+            {
+                Signer = AgentAddress,
+                BlockIndex = blockIndex,
+                Rehearsal = false,
+                PreviousState = states,
+                RandomSeed = new TestRandom().Seed,
+            });
+
+            // Test fungibleAssetValues
+            var unloadData = action.UnloadData.ToArray();
+            var garageBalanceAddress = Addresses.GetGarageBalanceAddress(AgentAddress);
+            if (unloadData[0].fungibleAssetValues is { } fungibleAssetValues)
+            {
+                foreach (var (balanceAddress, value) in fungibleAssetValues)
+                {
+                    Assert.Equal(value, states.GetBalance(balanceAddress, value.Currency));
+                    Assert.Equal(
+                        value.Currency * 0,
+                        states.GetBalance(garageBalanceAddress, value.Currency));
+                }
+            }
+
+            // Test fungibleItems
+            if (unloadData[0].fungibleIdAndCounts is { } fungibleIdAndCounts)
+            {
+                var inventoryAddress = unloadData[0].recipientAvatarAddress
+                    .Derive(SerializeKeys.LegacyInventoryKey);
+                var inventory = states.GetInventory(inventoryAddress);
+
+                foreach (var (fungibleId, count) in fungibleIdAndCounts)
+                {
+                    var garageAddress = Addresses.GetGarageAddress(AgentAddress, fungibleId);
+                    Assert.Equal(0, new FungibleItemGarage(states.GetState(garageAddress)).Count);
+                    Assert.True(inventory.HasFungibleItem(fungibleId, blockIndex: 0, count));
+                }
+            }
+        }
+
+        private static (Address balanceAddr, FungibleAssetValue value)[]
             GetFungibleAssetValues(
                 Address agentAddr,
                 Address avatarAddr)
@@ -102,6 +170,60 @@ namespace Lib9c.Tests.Action.Garages
                     return (agentAddr, fav);
                 })
                 .ToArray();
+        }
+
+        private (IAccount states, (
+            Address recipientAvatarAddress,
+            IEnumerable<(Address balanceAddress, FungibleAssetValue value)>? fungibleAssetValues,
+            IEnumerable<(HashDigest<SHA256> fungibleId, int count)>? fungibleIdAndCounts,
+            string? memo))
+            RegisterPlainValue(IAccount previousStates)
+        {
+            var states = previousStates;
+
+            var recipientAvatarAddress = AvatarAddress;
+            var fungibleAssetValues = GetFungibleAssetValues(AgentAddress, AvatarAddress);
+            var fungibleItemAndCounts = _tableSheets.MaterialItemSheet.OrderedList!
+                .Take(3)
+                .Select(ItemFactory.CreateMaterial)
+                .Select((material, index) =>
+                {
+                    var garageAddress =
+                        Addresses.GetGarageAddress(AgentAddress, material.FungibleId);
+                    var count = index + 1;
+                    var garage = new FungibleItemGarage(material, count);
+                    states = states.SetState(garageAddress, garage.Serialize());
+                    return (FungibleItem: (IFungibleItem)material, count);
+                })
+                .ToArray();
+            var fungibleItemIdAndCounts = fungibleItemAndCounts
+                .Select(tuple => (fungibleId: tuple.FungibleItem.FungibleId, tuple.count))
+                .ToArray();
+
+            var actionContext = new ActionContext { Signer = Addresses.Admin };
+            var garageBalanceAddress = Addresses.GetGarageBalanceAddress(AgentAddress);
+            foreach (var (_, value) in fungibleAssetValues)
+            {
+                if (value.Currency.Equals(_ncg))
+                {
+                    states = states.TransferAsset(
+                        actionContext,
+                        Addresses.Admin,
+                        garageBalanceAddress,
+                        value);
+                }
+                else
+                {
+                    states = states.MintAsset(
+                        actionContext,
+                        garageBalanceAddress,
+                        value);
+                }
+            }
+
+            return (
+                states,
+                (recipientAvatarAddress, fungibleAssetValues, fungibleItemIdAndCounts, "memo"));
         }
     }
 }
