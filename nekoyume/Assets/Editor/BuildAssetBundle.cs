8fc92d292e5bc4b3eefc0c9125541204414cd3c6
UiTae Kim <taejja09@gmail.com>
2023-11-08 08:18:07 +0900

    cleaned code

--- nekoyume/Assets/Editor/BuildAssetBundle.cs
+++ nekoyume/Assets/Editor/BuildAssetBundle.cs
@@ -4,47 +4,50 @@ using System.IO;
 using UnityEditor;
 using UnityEngine;
 
-public class BuildAssetBundle
+namespace Editor
 {
-    private static readonly string assetBundleDirectory = "Assets/AssetBundles";
-
-    [MenuItem("Assets/Build AssetBundles/All")]
-    public static void BuildAllAssetBundles()
+    public static class BuildAssetBundle
     {
-        BuildAllAssetBundlesIOS();
-        BuildAllAssetBundlesAndroid();
-        BuildAllAssetBundlesWindows();
-    }
+        private static readonly string assetBundleDirectory = "Assets/AssetBundles";
 
-    [MenuItem("Assets/Build AssetBundles/iOS")]
-    public static void BuildAllAssetBundlesIOS()
-    {
-        BuildAllAssetBundles($"{assetBundleDirectory}/{Application.version}/iOS", BuildTarget.iOS);
-    }
-
-    [MenuItem("Assets/Build AssetBundles/Android")]
-    public static void BuildAllAssetBundlesAndroid()
-    {
-        BuildAllAssetBundles($"{assetBundleDirectory}/{Application.version}/Android",
-            BuildTarget.Android);
-    }
-
-    [MenuItem("Assets/Build AssetBundles/Windows")]
-    public static void BuildAllAssetBundlesWindows()
-    {
-        BuildAllAssetBundles($"{assetBundleDirectory}/{Application.version}/Windows",
-            BuildTarget.StandaloneWindows);
-    }
-
-    private static void BuildAllAssetBundles(string path, BuildTarget target)
-    {
-        if (!Directory.Exists(path))
+        [MenuItem("Build/AssetBundles/All")]
+        public static void BuildAllAssetBundles()
         {
-            Directory.CreateDirectory(path);
+            BuildAllAssetBundlesIOS();
+            BuildAllAssetBundlesAndroid();
+            BuildAllAssetBundlesWindows();
         }
 
-        BuildPipeline.BuildAssetBundles(path,
-            BuildAssetBundleOptions.None,
-            target);
+        [MenuItem("Build/AssetBundles/iOS")]
+        public static void BuildAllAssetBundlesIOS()
+        {
+            BuildAllAssetBundles($"{assetBundleDirectory}/{Application.version}/iOS", BuildTarget.iOS);
+        }
+
+        [MenuItem("Build/AssetBundles/Android")]
+        public static void BuildAllAssetBundlesAndroid()
+        {
+            BuildAllAssetBundles($"{assetBundleDirectory}/{Application.version}/Android",
+                BuildTarget.Android);
+        }
+
+        [MenuItem("Build/AssetBundles/Windows")]
+        public static void BuildAllAssetBundlesWindows()
+        {
+            BuildAllAssetBundles($"{assetBundleDirectory}/{Application.version}/Windows",
+                BuildTarget.StandaloneWindows);
+        }
+
+        private static void BuildAllAssetBundles(string path, BuildTarget target)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            BuildPipeline.BuildAssetBundles(path,
+                BuildAssetBundleOptions.None,
+                target);
+        }
     }
 }
