9d76c8024292c0b5e6109252038330fe7e424a8f
Hyun Seungmin <boscohyun@gmail.com>
2023-11-08 20:42:28 +0900

    improve DimmedLoadingScreen with prefab

--- nekoyume/Assets/_Scripts/UI/Widget/Screen/DimmedLoadingScreen.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/Screen/DimmedLoadingScreen.cs
@@ -3,15 +3,63 @@ using Nekoyume.EnumType;
 using Nekoyume.L10n;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Nekoyume.UI
 {
     public class DimmedLoadingScreen : ScreenWidget
     {
+        public enum Case
+        {
+            ConnectingToTheNetworks = 0,
+            WaitingForSocialAuthenticating,
+            WaitingForPortalAuthenticating,
+            WaitingForPlanetAccountInfoSyncing,
+        }
+
         public override WidgetType WidgetType => WidgetType.System;
 
         [SerializeField]
-        private TMP_Text messageText;
+        private TMP_Text titleText;
+
+        [SerializeField]
+        private TMP_Text subText;
+
+        public void Show(
+            Case enumCase = Case.ConnectingToTheNetworks,
+            bool ignoreShowAnimation = false)
+        {
+            UpdateContent(enumCase);
+            if (IsActive())
+            {
+                return;
+            }
+
+            Show(ignoreShowAnimation);
+        }
+
+        private void UpdateContent(Case enumCase)
+        {
+            switch (enumCase)
+            {
+                case Case.ConnectingToTheNetworks:
+                    titleText.text = L10nManager.Localize("LOADING_TO_CONNECT_TO_NETWORKS");
+                    subText.text = L10nManager.Localize("LOADING_TO_CONNECT_TO_NETWORKS_DESC");
+                    break;
+                case Case.WaitingForSocialAuthenticating:
+                    titleText.text = L10nManager.Localize("LOADING_FOR_SOCIAL_AUTHENTICATION");
+                    subText.text = string.Empty;
+                    break;
+                case Case.WaitingForPortalAuthenticating:
+                    titleText.text = L10nManager.Localize("LOADING_FOR_PORTAL_AUTHENTICATION");
+                    subText.text = string.Empty;
+                    break;
+                case Case.WaitingForPlanetAccountInfoSyncing:
+                    titleText.text = L10nManager.Localize("LOADING_TO_SYNCHRONIZE_PLANET_ACCOUNT_INFORMATION");
+                    subText.text = string.Empty;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(enumCase), enumCase, null);
+            }
+        }
     }
 }
