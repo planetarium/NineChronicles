419b4fa017e14eee79f037937b195d61051f5bba
Hyun Seungmin <boscohyun@gmail.com>
2023-11-21 18:03:31 +0900

    add comment temporary

--- nekoyume/Assets/_Scripts/Multiplanetary/PlanetContext.cs
+++ nekoyume/Assets/_Scripts/Multiplanetary/PlanetContext.cs
@@ -5,6 +5,10 @@ using Nekoyume.Helper;
 
 namespace Nekoyume.Multiplanetary
 {
+    // TODO: PlanetRegistry와 PlanetAccountInfos를 외부에서 셔플해서 사용하고 있는데,
+    //       이 안에서 한 번만 셔플하도록 해야 할 것 같다. 이때, 둘이 같은 순서가 보장되게끔.
+    //       또 첫 실행 시 PlanetSelector.DefaultPlanet 대신 셔플한 순서의 첫 번째가 선택되도록 하면 좋겠다.
+    //       위의 내용 모두 PlanetSelector에 위임해야 하겠다.
     public class PlanetContext
     {
         public readonly CommandLineOptions CommandLineOptions;
