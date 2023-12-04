524ba0a19cb28b0aea27dc1260479afcb730a504
tyrosine1153 <tyrosine1153@gmail.com>
2023-12-04 21:17:15 +0900

    Apply RuneSummon action to client

--- nekoyume/Assets/_Scripts/Blockchain/ActionManager.cs
+++ nekoyume/Assets/_Scripts/Blockchain/ActionManager.cs
@@ -1652,6 +1652,44 @@ namespace Nekoyume.Blockchain
                 });
         }
 
+        public IObservable<ActionEvaluation<RuneSummon>> RuneSummon(int groupId, int summonCount)
+        {
+            // analytics
+            var sentryTx = Analyzer.Instance.Track(
+                "",
+                new Dictionary<string, Value>()
+                {
+                }, true);
+
+            var avatarState = States.Instance.CurrentAvatarState;
+            var avatarAddress = avatarState.address;
+
+            // check material enough
+            var tableSheets = Game.Game.instance.TableSheets;
+            var summonRow = tableSheets.SummonSheet[groupId];
+            var materialRow = tableSheets.MaterialItemSheet[summonRow.CostMaterial];
+            var count = summonRow.CostMaterialCount * summonCount;
+            LocalLayerModifier.RemoveItem(avatarAddress, materialRow.ItemId, count);
+
+            var action = new RuneSummon
+            {
+                AvatarAddress = avatarAddress,
+                GroupId = groupId,
+                SummonCount = summonCount,
+            };
+            ProcessAction(action);
+
+            return _agent.ActionRenderer.EveryRender<RuneSummon>()
+                .Timeout(ActionTimeout)
+                .Where(eval => eval.Action.Id.Equals(action.Id))
+                .First()
+                .ObserveOnMainThread()
+                .DoOnError(e =>
+                {
+                    Game.Game.BackToMainAsync(HandleException(action.Id, e)).Forget();
+                });
+        }
+
         public IObservable<ActionEvaluation<ClaimItems>> ClaimItems(
             params (Address, IReadOnlyList<FungibleAssetValue>)[] claimData)
         {
