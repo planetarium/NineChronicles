9e21a6a437468545cdc8bfbc3785acabd07807d9
Jonny <jonny@planetariumhq.com>
2024-01-05 18:19:19 +0900

    deleted copied file check create directory add summary

--- nekoyume/Assets/_Scripts/Game/Game.cs
+++ nekoyume/Assets/_Scripts/Game/Game.cs
@@ -199,22 +199,45 @@ namespace Nekoyume.Game
         }
 #endif
 
-#if !UNITY_EDITOR && UNITY_ANDROID
+
         [RuntimeInitializeOnLoadMethod]
         static void OnRuntimeMethodLoad()
         {
+#if !UNITY_EDITOR && UNITY_ANDROID
             Debug.Log("[OnRuntimeMethodLoad] After Scene is loaded and game is running");
             AndroidKeyStorePathChange();
+#endif
         }
 
+        /// <summary>
+        /// Changes the path of the Android key store by copying the contents from 
+        /// "storage/emulated/0/Documents/NineChronicles" to the platform's persistent data path.
+        /// After copying, it deletes any files in the new location that have the same names as those in the original path.
+        /// This method is typically used for data migration or backup purposes in Android applications.
+        /// </summary>
         static void AndroidKeyStorePathChange()
         {
             CopyFolder("storage/emulated/0/Documents/NineChronicles", Platform.PersistentDataPath);
+            DeleteSameFileName(Platform.PersistentDataPath, "storage/emulated/0/Documents/NineChronicles");
         }
 
+        /// <summary>
+        /// Copies a folder, including its files and subdirectories, from a specified source path to a target path.
+        /// If the target directory does not exist, it is created. Each file copy operation is logged for debugging purposes.
+        /// This method employs recursion to handle subdirectories, ensuring a complete copy of the folder structure.
+        /// </summary>
+        /// <param name="path">The source path of the folder to be copied.</param>
+        /// <param name="target">The target path where the folder will be copied to.</param>
         static void CopyFolder(string path, string target)
         {
             Debug.Log($"[CopyFolder] path:{path}    target:{target}");
+
+            if (!Directory.Exists(target))
+            {
+                Directory.CreateDirectory(target);
+                Debug.Log($"[Directory.CreateDirectory] target: {target}");
+            }
+
             foreach (string file in Directory.GetFiles(path))
             {
                 File.Copy(file, Path.Combine(target, Path.GetFileName(file)));
@@ -222,9 +245,34 @@ namespace Nekoyume.Game
             }
 
             foreach (string directory in Directory.GetDirectories(path))
+            {
                 CopyFolder(directory, Path.Combine(target, Path.GetFileName(directory)));
+            }
         }
-#endif
+
+        /// <summary>
+        /// Deletes files in the target directory that have the same names as those in the specified source directory.
+        /// The method iterates over all files in the source path, including its subdirectories.
+        /// For each file, it checks if a file with the same relative path exists in the target directory.
+        /// If such a file exists, it is deleted. Each deletion operation is logged for debugging purposes.
+        /// </summary>
+        /// <param name="path">The source directory path to compare files from.</param>
+        /// <param name="target">The target directory path where files will be checked and deleted if a match is found.</param>
+        static void DeleteSameFileName(string path, string target)
+        {
+            Debug.Log($"[DeleteCopiedFile] path:{path}    target:{target}");
+            foreach (string file in Directory.GetFiles(path, "*", SearchOption.AllDirectories))
+            {
+                string relativePath = file.Substring(path.Length + 1);
+                string targetFile = Path.Combine(target, relativePath);
+
+                if (File.Exists(targetFile))
+                {
+                    File.Delete(targetFile);
+                    Debug.Log($"[File.Delete] Deleted file: {targetFile}");
+                }
+            }
+        }
 
         protected override void Awake()
         {
