082f9f17dc389c5f056cb4bd3bd9c686289b0c18
sonohoshi <dev.sonohoshi@gmail.com>
2024-01-30 11:39:26 +0900

    add generate keystore by raw pk feature to KeystoreEditor

--- nekoyume/Assets/Editor/KeyStoreEditor.cs
+++ nekoyume/Assets/Editor/KeyStoreEditor.cs
@@ -11,6 +11,7 @@ namespace Editor
         private static string _addr;
         private static string _originPw;
         private static string _newPw;
+        private static string _rawPrivateKeyString;
 
         [MenuItem("Tools/Show Keystore Editor")]
         private static void Init()
@@ -18,6 +19,7 @@ namespace Editor
             _addr = "0xABCD";
             _originPw = "origin password";
             _newPw = "new password";
+            _rawPrivateKeyString = "raw privatekey";
             var window = GetWindow(typeof(KeyStoreEditor));
             window.Show();
         }
@@ -40,6 +42,20 @@ namespace Editor
                 }
 
                 KeyManager.Instance.TryChangePassphrase(new Address(_addr), _originPw, _newPw);
+            }
+
+            if (GUILayout.Button("generate keystore by raw pk, use password \'new password input\'"))
+            {
+                Debug.Log("Trying to generate keystore...");
+                if (!KeyManager.Instance.IsInitialized)
+                {
+                    KeyManager.Instance.Initialize(
+                        keyStorePath: null,
+                        encryptPassphraseFunc: Util.AesEncrypt,
+                        decryptPassphraseFunc: Util.AesDecrypt);
+                }
+
+                KeyManager.Instance.Register(new PrivateKey(_rawPrivateKeyString), _newPw);
             }
         }
     }
