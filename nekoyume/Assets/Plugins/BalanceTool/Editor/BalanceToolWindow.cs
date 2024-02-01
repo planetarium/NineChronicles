4c297e2aa30c0cab4f05cdc2d7a0afce5e527b7b
Ko Chanhyuck <limeelbee@gmail.com>
2024-02-01 13:18:40 +0900

    chore: fix build errors due to editor feature

--- nekoyume/Assets/Plugins/BalanceTool/Editor/BalanceToolWindow.cs
+++ nekoyume/Assets/Plugins/BalanceTool/Editor/BalanceToolWindow.cs
@@ -1,5 +1,5 @@
 using System;
-using BalanceTool.Util;
+using BalanceTool.Runtime.Util;
 using Libplanet.Action.State;
 using Libplanet.Crypto;
 using UnityEditor;
@@ -12,7 +12,7 @@ namespace BalanceTool.Editor
         private const string PlayDataCsvHeader =
             "world_id,stage_id,avatar_level,equipment_00_id,equipment_00_level,equipment_01_id,equipment_01_level,equipment_02_id,equipment_02_level,equipment_03_id,equipment_03_level,equipment_04_id,equipment_04_level,equipment_05_id,equipment_05_level,food_00_id,food_00_count,food_01_id,food_01_count,food_02_id,food_02_count,food_03_id,food_03_count,costume_00_id,costume_01_id,costume_02_id,costume_03_id,costume_04_id,costume_05_id,rune_00_id,rune_00_level,rune_01_id,rune_01_level,rune_02_id,rune_02_level,rune_03_id,rune_03_level,crystal_random_buff_id,play_count";
 
-        private IAccount _prevStates;
+        private IWorld _prevStates;
         private Address _agentAddr;
         private int _avatarIndex;
 
