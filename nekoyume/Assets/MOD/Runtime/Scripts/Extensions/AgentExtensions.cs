f6d2df0a9588f4372f965e6a74bdb2634d6b57b0
Hyun Seungmin <boscohyun@gmail.com>
2024-02-05 11:59:54 +0900

    apply libplanet 4

--- nekoyume/Assets/MOD/Runtime/Scripts/Extensions/AgentExtensions.cs
+++ nekoyume/Assets/MOD/Runtime/Scripts/Extensions/AgentExtensions.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Bencodex.Types;
 using Cysharp.Threading.Tasks;
+using Libplanet.Action.State;
 using Libplanet.Crypto;
 using Nekoyume.Action;
 using Nekoyume.Blockchain;
@@ -17,7 +18,9 @@ namespace NineChronicles.MOD
             Address avatarAddress)
         {
             var arenaItemSlotAddress = ItemSlotState.DeriveAddress(avatarAddress, BattleType.Arena);
-            var arenaItemSlotValue = await agent.GetStateAsync(arenaItemSlotAddress);
+            var arenaItemSlotValue = await agent.GetStateAsync(
+                ReservedAddresses.LegacyAccount,
+                arenaItemSlotAddress);
             return arenaItemSlotValue is List l
                 ? new ItemSlotState(l)
                 : new ItemSlotState(BattleType.Arena);
@@ -28,25 +31,31 @@ namespace NineChronicles.MOD
             Address avatarAddress,
             IEnumerable<RuneSlotInfo> runeSlotInfos)
         {
-            IEnumerable<Address> runeStateAddresses;
+            Address[] runeStateAddresses;
             if (runeSlotInfos is null)
             {
                 var arenaRuneSlotAddress = RuneSlotState.DeriveAddress(avatarAddress, BattleType.Arena);
-                var arenaRuneSlotValue = await agent.GetStateAsync(arenaRuneSlotAddress);
+                var arenaRuneSlotValue = await agent.GetStateAsync(
+                    ReservedAddresses.LegacyAccount,
+                    arenaRuneSlotAddress);
                 var arenaRuneSlotState = arenaRuneSlotValue is List l
                     ? new RuneSlotState(l)
                     : new RuneSlotState(BattleType.Arena);
 
-                runeStateAddresses = arenaRuneSlotState.GetEquippedRuneSlotInfos().Select(slotInfo =>
-                    RuneState.DeriveAddress(avatarAddress, slotInfo.RuneId));
+                runeStateAddresses = arenaRuneSlotState.GetEquippedRuneSlotInfos()
+                    .Select(slotInfo => RuneState.DeriveAddress(avatarAddress, slotInfo.RuneId))
+                    .ToArray();
             }
             else
             {
-                runeStateAddresses = runeSlotInfos.Select(info =>
-                                RuneState.DeriveAddress(avatarAddress, info.RuneId));
+                runeStateAddresses = runeSlotInfos
+                    .Select(info => RuneState.DeriveAddress(avatarAddress, info.RuneId))
+                    .ToArray();
             }
 
-            var runeStateValues = await agent.GetStateBulkAsync(runeStateAddresses);
+            var runeStateValues = await agent.GetStateBulkAsync(
+                ReservedAddresses.LegacyAccount,
+                runeStateAddresses);
             var runeStates = new List<RuneState>();
             foreach (var runeStateAddress in runeStateAddresses)
             {
