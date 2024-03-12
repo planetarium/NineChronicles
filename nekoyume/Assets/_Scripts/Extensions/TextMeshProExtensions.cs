3b58a59363c290654261c64f59b3e1a141ea512a
sonohoshi <dev.sonohoshi@gmail.com>
2024-03-12 16:23:25 +0900

    make TextMeshProExtensions
    introduce SubscribeForClickLink()

---
+++ nekoyume/Assets/_Scripts/Extensions/TextMeshProExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Nekoyume
+{
+    using UniRx;
+    public static class TextMeshProExtensions
+    {
+        private static readonly Camera Camera;
+
+        static TextMeshProExtensions()
+        {
+            Camera = Camera.main;
+        }
+
+        private static void OnClickTextWithLink(TMP_Text text, Action<TMP_LinkInfo> onClick)
+        {
+            if (text == null)
+            {
+                return;
+            }
+
+            var linkIndex = TMP_TextUtilities.FindIntersectingLink(text, Input.mousePosition, Camera);
+            if (linkIndex != -1)
+            {
+                onClick.Invoke(text.textInfo.linkInfo[linkIndex]);
+            }
+        }
+
+        public static IDisposable SubscribeForClickLink(this TextMeshProUGUI text,
+            Action<TMP_LinkInfo> onClick)
+        {
+            return Observable.EveryUpdate().Where(_ => Input.GetKeyDown(KeyCode.Mouse0))
+                .Subscribe(_ => OnClickTextWithLink(text, onClick));
+        }
+    }
+}
