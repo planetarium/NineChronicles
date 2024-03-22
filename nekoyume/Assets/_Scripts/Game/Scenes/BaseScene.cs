7494f8c24f3d845fc3b747296e277e7d4d0ddb18
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2024-03-22 16:39:49 +0900

    Add SceneManager

---
+++ nekoyume/Assets/_Scripts/Game/Scenes/BaseScene.cs
@@ -0,0 +1,35 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Nekoyume.Game.Scenes
+{
+    public abstract class BaseScene : MonoBehaviour
+    {
+        public bool IsInitialized { get; protected set; } = false;
+
+        /// <summary>
+        /// 씬의 모든 오브젝트가 초기화된 뒤 씬 객체를 초기화하기 위해
+        /// Awake가 아닌 Start타이밍에 초기화를 진행함
+        /// </summary>
+        private void Start()
+        {
+            WaitUntilInitialized().Forget();
+        }
+
+        private async UniTask WaitUntilInitialized()
+        {
+            await UniTask.WaitUntil(() => Game.instance.IsInitialized);
+            await LoadSceneAssets();
+            Initialize();
+        }
+
+        protected abstract UniTask LoadSceneAssets();
+
+        protected virtual void Initialize()
+        {
+            IsInitialized = true;
+        }
+
+        public abstract void Clear();
+    }
+}
---
+++ nekoyume/Assets/_Scripts/Game/Scenes/NCSceneManager.cs
@@ -0,0 +1,87 @@
+using System;
+using Cysharp.Threading.Tasks;
+using Nekoyume.UI;
+using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
+
+namespace Nekoyume.Game.Scenes
+{
+    public enum SceneType
+    {
+        Intro,
+        Lobby,
+        Battle,
+        Loading,
+    }
+
+    public class NcSceneManager
+    {
+        private const float ChangeSceneDelay = 500f;
+
+        private static class Singleton
+        {
+            internal static readonly NcSceneManager Value = new();
+        }
+        public static NcSceneManager Instance => Singleton.Value;
+
+        public SceneType ESceneType { get; private set; } = SceneType.Intro;
+
+        private BaseScene _currentScene;
+
+        private NcSceneManager()
+        {
+            SceneManager.sceneUnloaded += OnSceneUnLoaded;
+        }
+
+        public async UniTask LoadScene(SceneType type)
+        {
+            // TODO: 씬 타입에 따라 로딩 스크린을 다르게 보여줄 수 있도록 수정
+            Widget.Find<LoadingScreen>().Show(LoadingScreen.LoadingType.WorldBoss);
+
+            ESceneType = type;
+            ClearScene();
+            await SceneManager.LoadSceneAsync(GetSceneName(SceneType.Loading)).ToUniTask();
+            await UniTask.Delay(TimeSpan.FromMilliseconds(ChangeSceneDelay));
+            await SceneManager.LoadSceneAsync(GetSceneName(type)).ToUniTask();
+            await UniTask.NextFrame();
+            _currentScene = Object.FindObjectOfType<BaseScene>();
+            await UniTask.WaitUntil(() => _currentScene.IsInitialized);
+
+            Widget.Find<LoadingScreen>().Close();
+        }
+
+        // TODO: Addressable로 변경
+        private string GetSceneName(SceneType type)
+        {
+            switch (type)
+            {
+                case SceneType.Intro:
+                    break;
+                case SceneType.Lobby:
+                    return "EnterBattleTestScene";
+                case SceneType.Battle:
+                    break;
+                case SceneType.Loading:
+                    break;
+            }
+
+            return string.Empty;
+        }
+
+        private void ClearScene()
+        {
+            if (_currentScene != null)
+                _currentScene.Clear();
+            _currentScene = null;
+
+            // TODO: Clear UI
+
+            // TODO: after apply addressable
+            // resourceManager?.ReleaseAll();
+        }
+
+        private void OnSceneUnLoaded(Scene scene)
+        {
+        }
+    }
+}
