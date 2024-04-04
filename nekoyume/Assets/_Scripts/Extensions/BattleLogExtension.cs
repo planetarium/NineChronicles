33a0b7d34d687543ccefb62b2da10cb7a19200d2
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2024-04-04 21:50:41 +0900

    Add GetMonsterIds Extension

---
+++ nekoyume/Assets/_Scripts/Extensions/BattleLogExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Nekoyume.Model.BattleStatus;
+
+namespace Nekoyume
+{
+    public static class BattleLogExtension
+    {
+        public static List<int> GetMonsterIds(this BattleLog battleLog)
+        {
+            var monsterIds = new List<int>();
+            foreach (var currentEvent in battleLog)
+            {
+                if (currentEvent is SpawnWave spawnWave)
+                {
+                    monsterIds.AddRange(spawnWave.Enemies.Select(enemy => enemy.CharacterId));
+                }
+            }
+
+            return monsterIds;
+        }
+    }
+}
