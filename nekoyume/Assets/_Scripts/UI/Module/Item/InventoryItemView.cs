7853f89e9d7733382bd229a997e849449c606494
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2024-04-18 19:08:28 +0900

    apply tradeable object setting to sell inventory item view
    (cherry picked from commit 9e16804c0362afd0da17be1ec1235da4ad77bb7f)

--- nekoyume/Assets/_Scripts/UI/Module/Item/InventoryItemView.cs
+++ nekoyume/Assets/_Scripts/UI/Module/Item/InventoryItemView.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Nekoyume.Helper;
 using Nekoyume.Model.Item;
-using Nekoyume.Model.State;
 using Nekoyume.UI.Model;
 using Nekoyume.UI.Scroller;
 using UnityEngine;
@@ -16,9 +15,9 @@ namespace Nekoyume.UI.Module
     public class InventoryItemView : MonoBehaviour
     {
         [SerializeField]
-        private BaseItemView baseItemView;
+        protected BaseItemView baseItemView;
 
-        private readonly List<IDisposable> _disposables = new List<IDisposable>();
+        protected readonly List<IDisposable> Disposables = new List<IDisposable>();
 
         public void Set(InventoryItem model, InventoryScroll.ContextModel context)
         {
@@ -43,9 +42,9 @@ namespace Nekoyume.UI.Module
             }
         }
 
-        private void UpdateItem(InventoryItem model, InventoryScroll.ContextModel context)
+        protected virtual void UpdateItem(InventoryItem model, InventoryScroll.ContextModel context)
         {
-            _disposables.DisposeAllAndClear();
+            Disposables.DisposeAllAndClear();
             baseItemView.Container.SetActive(true);
             baseItemView.EmptyObject.SetActive(false);
             baseItemView.EnoughObject.SetActive(false);
@@ -99,34 +98,34 @@ namespace Nekoyume.UI.Module
             baseItemView.CountText.text = model.Count.Value.ToString();
 
             model.Equipped.Subscribe(b => baseItemView.EquippedObject.SetActive(b))
-                .AddTo(_disposables);
+                .AddTo(Disposables);
             model.LevelLimited.Subscribe(b => baseItemView.LevelLimitObject.SetActive(b))
-                .AddTo(_disposables);
+                .AddTo(Disposables);
             model.DimObjectEnabled.Subscribe(b => baseItemView.DimObject.SetActive(b))
-                .AddTo(_disposables);
+                .AddTo(Disposables);
             model.Tradable.Subscribe(b => baseItemView.TradableObject.SetActive(b))
-                .AddTo(_disposables);
+                .AddTo(Disposables);
             model.Selected.Subscribe(b => baseItemView.SelectObject.SetActive(b))
-                .AddTo(_disposables);
-            model.Focused.Subscribe(b => baseItemView.FocusObject.SetActive(b)).AddTo(_disposables);
+                .AddTo(Disposables);
+            model.Focused.Subscribe(b => baseItemView.FocusObject.SetActive(b)).AddTo(Disposables);
             model.HasNotification.Subscribe(b => baseItemView.NotificationObject.SetActive(b))
-                .AddTo(_disposables);
+                .AddTo(Disposables);
             model.GrindingCountEnabled
                 .Subscribe(b => baseItemView.GrindingCountObject.SetActive(b))
-                .AddTo(_disposables);
+                .AddTo(Disposables);
             model.CollectionSelected
                 .Subscribe(b => baseItemView.SelectCollectionObject.SetActive(b))
-                .AddTo(_disposables);
+                .AddTo(Disposables);
 
             baseItemView.TouchHandler.OnClick.Select(_ => model)
-                .Subscribe(context.OnClick.OnNext).AddTo(_disposables);
+                .Subscribe(context.OnClick.OnNext).AddTo(Disposables);
             baseItemView.TouchHandler.OnDoubleClick.Select(_ => model)
-                .Subscribe(context.OnDoubleClick.OnNext).AddTo(_disposables);
+                .Subscribe(context.OnDoubleClick.OnNext).AddTo(Disposables);
         }
 
-        private void UpdateRune(InventoryItem model, InventoryScroll.ContextModel context)
+        protected virtual void UpdateRune(InventoryItem model, InventoryScroll.ContextModel context)
         {
-            _disposables.DisposeAllAndClear();
+            Disposables.DisposeAllAndClear();
             baseItemView.Container.SetActive(true);
             baseItemView.EmptyObject.SetActive(false);
             baseItemView.EnoughObject.SetActive(false);
@@ -184,32 +183,33 @@ namespace Nekoyume.UI.Module
             }
 
             model.Equipped.Subscribe(b => baseItemView.EquippedObject.SetActive(b))
-                .AddTo(_disposables);
+                .AddTo(Disposables);
             model.Selected.Subscribe(b => baseItemView.SelectObject.SetActive(b))
-                .AddTo(_disposables);
-            model.Focused.Subscribe(b => baseItemView.FocusObject.SetActive(b)).AddTo(_disposables);
+                .AddTo(Disposables);
+            model.Focused.Subscribe(b => baseItemView.FocusObject.SetActive(b)).AddTo(Disposables);
             model.DimObjectEnabled.Subscribe(b => baseItemView.DimObject.SetActive(b))
-                .AddTo(_disposables);
+                .AddTo(Disposables);
             model.HasNotification.Subscribe(b => baseItemView.NotificationObject.SetActive(b))
-                .AddTo(_disposables);
+                .AddTo(Disposables);
             model.CollectionSelected
                 .Subscribe(b => baseItemView.SelectCollectionObject.SetActive(b))
-                .AddTo(_disposables);
+                .AddTo(Disposables);
 
             baseItemView.TouchHandler.OnClick.Select(_ => model)
-                .Subscribe(context.OnClick.OnNext).AddTo(_disposables);
+                .Subscribe(context.OnClick.OnNext).AddTo(Disposables);
             baseItemView.TouchHandler.OnDoubleClick.Select(_ => model)
-                .Subscribe(context.OnDoubleClick.OnNext).AddTo(_disposables);
+                .Subscribe(context.OnDoubleClick.OnNext).AddTo(Disposables);
         }
 
-        private void UpdateFungibleAsset(InventoryItem model, InventoryScroll.ContextModel context)
+        protected virtual void UpdateFungibleAsset(InventoryItem model, InventoryScroll.ContextModel context)
         {
-            _disposables.DisposeAllAndClear();
+            Disposables.DisposeAllAndClear();
             baseItemView.Container.SetActive(true);
             baseItemView.EmptyObject.SetActive(false);
             baseItemView.EnoughObject.SetActive(false);
             baseItemView.MinusObject.SetActive(false);
             baseItemView.ExpiredObject.SetActive(false);
+            baseItemView.TradableObject.SetActive(false);
             baseItemView.SelectBaseItemObject.SetActive(false);
             baseItemView.SelectMaterialItemObject.SetActive(false);
             baseItemView.LockObject.SetActive(false);
@@ -217,7 +217,6 @@ namespace Nekoyume.UI.Module
             baseItemView.PriceText.gameObject.SetActive(false);
             baseItemView.LoadingObject.SetActive(false);
             baseItemView.LevelLimitObject.SetActive(false);
-            baseItemView.TradableObject.SetActive(false);
             baseItemView.GrindingCountObject.SetActive(false);
             baseItemView.EnhancementText.gameObject.SetActive(false);
             baseItemView.EnhancementImage.gameObject.SetActive(false);
@@ -257,22 +256,22 @@ namespace Nekoyume.UI.Module
             baseItemView.GradeHsv.value = data.GradeHsvValue;
 
             model.Equipped.Subscribe(b => baseItemView.EquippedObject.SetActive(b))
-                .AddTo(_disposables);
+                .AddTo(Disposables);
             model.Selected.Subscribe(b => baseItemView.SelectObject.SetActive(b))
-                .AddTo(_disposables);
-            model.Focused.Subscribe(b => baseItemView.FocusObject.SetActive(b)).AddTo(_disposables);
+                .AddTo(Disposables);
+            model.Focused.Subscribe(b => baseItemView.FocusObject.SetActive(b)).AddTo(Disposables);
             model.DimObjectEnabled.Subscribe(b => baseItemView.DimObject.SetActive(b))
-                .AddTo(_disposables);
+                .AddTo(Disposables);
             model.HasNotification.Subscribe(b => baseItemView.NotificationObject.SetActive(b))
-                .AddTo(_disposables);
+                .AddTo(Disposables);
             model.CollectionSelected
                 .Subscribe(b => baseItemView.SelectCollectionObject.SetActive(b))
-                .AddTo(_disposables);
+                .AddTo(Disposables);
 
             baseItemView.TouchHandler.OnClick.Select(_ => model)
-                .Subscribe(context.OnClick.OnNext).AddTo(_disposables);
+                .Subscribe(context.OnClick.OnNext).AddTo(Disposables);
             baseItemView.TouchHandler.OnDoubleClick.Select(_ => model)
-                .Subscribe(context.OnDoubleClick.OnNext).AddTo(_disposables);
+                .Subscribe(context.OnDoubleClick.OnNext).AddTo(Disposables);
         }
     }
 }
