4db9e0ef580d424dc27f2f7d54de8d8cd4166e81
jonny <jonny@planetariumhq.com>
2024-05-12 17:35:45 +0900

    fix script name

--- nekoyume/Assets/_Scripts/UI/MainCanvas.cs
+++ nekoyume/Assets/_Scripts/UI/MainCanvas.cs
@@ -488,7 +488,7 @@ namespace Nekoyume.UI
             _secondWidgets.Add(Widget.Create<AdventureBossEnterBountyPopup>());
             _secondWidgets.Add(Widget.Create<AdventureBossRewardPopup>());
             _secondWidgets.Add(Widget.Create<AdventureBossStartNotificationPopup>());
-            _secondWidgets.Add(Widget.Create<AdventureBossUnlockLockedFloorPopup>());
+            _secondWidgets.Add(Widget.Create<AdventureBoss_UnlockLockedFloorPopup>());
 #endif
         }
 
