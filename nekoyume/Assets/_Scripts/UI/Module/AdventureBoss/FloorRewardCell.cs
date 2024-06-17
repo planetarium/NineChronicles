a7d97cae1c4989ca09e35b9826b27c9b40a7ae07
jonny <jonny@planetariumhq.com>
2024-06-17 19:07:52 +0900

    reward info popup fix

--- nekoyume/Assets/_Scripts/UI/Module/AdventureBoss/FloorRewardCell.cs
+++ nekoyume/Assets/_Scripts/UI/Module/AdventureBoss/FloorRewardCell.cs
@@ -1,5 +1,5 @@
+using Lib9c;
 using Nekoyume.L10n;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -13,9 +13,48 @@ namespace Nekoyume
         [SerializeField] private BaseItemView[] rewardItemViews;
         [SerializeField] private GameObject receivedObj;
 
-        public void SetData(int floor)
+        public void SetData(int floor, List<TableData.AdventureBoss.AdventureBossSheet.RewardAmountData> rewards)
         {
+            if (Game.Game.instance.AdventureBossData.ExploreInfo.Value == null)
+            {
+                receivedObj.SetActive(false);
+            }
+            else
+            {
+                receivedObj.SetActive(Game.Game.instance.AdventureBossData.ExploreInfo.Value.Floor >= floor);
+            }
+
             floorText.text = $"{floor}F";
+
+            for (int i = 0; i < rewardItemViews.Length; i++)
+            {
+                if(i < rewards.Count)
+                {
+                    rewardItemViews[i].gameObject.SetActive(true);
+                    var reward = rewards[i];
+                    switch (reward.ItemType)
+                    {
+                        case "Material":
+                            rewardItemViews[i].ItemViewSetItemData(reward.ItemId, reward.Amount);
+                            break;
+                        case "Rune":
+                            rewardItemViews[i].ItemViewSetCurrencyData(reward.ItemId, reward.Amount);
+                            break;
+                        case "Crystal":
+                            rewardItemViews[i].ItemViewSetCurrencyData(Currencies.Crystal.Ticker, rewards[i].Amount);
+                            break;
+                        default:
+                            NcDebug.LogError("Invalid ItemType: " + reward.ItemType);
+                            break;
+                    }
+                }
+                else
+                {
+                    rewardItemViews[i].gameObject.SetActive(false);
+                }
+            }
+
+
             floorDescText.text = L10nManager.Localize("UI_ADVENTUREBOSS_FLOOR_REWARD_DESC", floor);
         }
     }
--- nekoyume/Assets/_Scripts/UI/Widget/Popup/AdventureBossRewardInfoPopup.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/Popup/AdventureBossRewardInfoPopup.cs
@@ -71,6 +71,60 @@ namespace Nekoyume.UI
             toggleFloor.onValueChanged.AddListener((isOn) =>
             {
                 contentsFloor.SetActive(isOn);
+                if (!isOn)
+                {
+                    return;
+                }
+                if(Game.instance.AdventureBossData.SeasonInfo.Value == null)
+                {
+                    NcDebug.LogError("SeasonInfo is null");
+                    foreach (var item in floorRewardCells)
+                    {
+                        item.gameObject.SetActive(false);
+                    }
+                    return;
+                }
+                var seasonInfo = Game.instance.AdventureBossData.SeasonInfo.Value;
+                var tableSheets = TableSheets.Instance;
+                
+                var bossRow = tableSheets.AdventureBossSheet.Values.FirstOrDefault(row => row.BossId == seasonInfo.BossId);
+                if (bossRow == null)
+                {
+                    NcDebug.LogError($"BossSheet is not found. BossId: {seasonInfo.BossId}");
+                    foreach (var item in floorRewardCells)
+                    {
+                        item.gameObject.SetActive(false);
+                    }
+                    return;
+                }
+
+                var floorRows = tableSheets.AdventureBossFloorSheet.Values.Where(row =>
+                                            row.AdventureBossId == bossRow.Id
+                                            && (row.Floor == 6
+                                                || row.Floor == 11
+                                                || row.Floor == 16
+                                                || row.Floor == 20
+                                                ));
+                var floorRewardDatas = TableSheets.Instance.AdventureBossFloorFirstRewardSheet.Values.Join(floorRows,
+                    rewardRow => rewardRow.FloorId,
+                    floorRow => floorRow.Id,
+                    (rewardRow, floorRow) => new
+                    {
+                        floorRow.Floor,
+                        rewardRow.Rewards
+                    }).OrderByDescending(row => row.Floor).ToList();
+
+                for (int i = 0; i < floorRewardCells.Length; i++)
+                {
+                    if (i >= floorRewardDatas.Count)
+                    {
+                        floorRewardCells[i].gameObject.SetActive(false);
+                        NcDebug.LogError($"FloorRewardData is not enough. Index: {i}");
+                        continue;
+                    }
+                    floorRewardCells[i].gameObject.SetActive(true);
+                    floorRewardCells[i].SetData(floorRewardDatas[i].Floor, floorRewardDatas[i].Rewards);
+                }
             });
             toggleOperational.onValueChanged.AddListener((isOn) =>
             {
