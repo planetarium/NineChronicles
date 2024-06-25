5d30d84ab0b9be15f51117ec69019abdb30a1d88
jonny <jonny@planetariumhq.com>
2024-06-25 13:24:55 +0900

    fix adventureboss rewardpopup data refresh issue

--- nekoyume/Assets/_Scripts/UI/Widget/Popup/AdventureBossRewardPopup.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/Popup/AdventureBossRewardPopup.cs
@@ -211,7 +211,6 @@ namespace Nekoyume.UI
                                                     TableSheets.Instance.AdventureBossNcgRewardRatioSheet,
                                                     States.Instance.GameConfigState.AdventureBossNcgRuneRatio,
                                                     out var wantedReward);
-                        RefreshWithSeasonInfo(exploreBoard, exploreInfo, bountyBoard);
                         if (_lastSeasonId < seasonInfo.Season)
                         {
                             _lastSeasonId = seasonInfo.Season;
@@ -234,6 +233,7 @@ namespace Nekoyume.UI
                             _lastSeasonId = seasonInfo.Season;
                         }
                     }
+                    RefreshWithSeasonInfo(exploreBoard, exploreInfo, bountyBoard);
                 }
                 catch (Exception e)
                 {
@@ -360,7 +360,10 @@ namespace Nekoyume.UI
                 double myScoreRatio = (double)exploreInfo.Score / (double)exploreBoard.TotalPoint;
                 myScoreRatioText.text = $"{myScoreRatio * 100:F2}%";
             }
-            SetExploreInfoVIew(false);
+            else
+            {
+                SetExploreInfoVIew(false);
+            }
             if (bountyBoard != null)
             {
                 var bountyInfo = bountyBoard.Investors.
@@ -372,8 +375,11 @@ namespace Nekoyume.UI
                     SetBountyInfoView(true);
                     return;
                 }
+                else
+                {
+                    SetBountyInfoView(false);
+                }
             }
-            SetBountyInfoView(false);
         }
 
         private void SetBountyInfoView(bool visible)
