9a96bc21cc0edb8f3f153e039ddd129ea1a3fc6b
tyrosine1153 <tyrosine1153@gmail.com>
2024-06-26 19:27:12 +0900

    refactor update state exception in BlockRenderHandler

--- nekoyume/Assets/_Scripts/Blockchain/BlockRenderHandler.cs
+++ nekoyume/Assets/_Scripts/Blockchain/BlockRenderHandler.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Bencodex.Types;
 using Cysharp.Threading.Tasks;
@@ -7,15 +8,13 @@ using Lib9c;
 using Lib9c.Renderers;
 using Libplanet.Types.Assets;
 using Nekoyume.Exceptions;
+using Nekoyume.Helper;
 using Nekoyume.Model.State;
 using Nekoyume.State;
 using Nekoyume.State.Subjects;
-using UnityEngine;
-using static Lib9c.SerializeKeys;
 
 namespace Nekoyume.Blockchain
 {
-    using UI.Scroller;
     using UniRx;
 
     public class BlockRenderHandler
@@ -73,35 +72,35 @@ namespace Nekoyume.Blockchain
             var agentState = States.Instance.AgentState;
             if (agentState != null && _balanceUpdateRequired)
             {
-                var (hasException, exception) =
-                    await UniTask.Run<(bool hasException, Exception exception)>(async () =>
+                var exception = await UniTask.Run(async () =>
+                {
+                    FungibleAssetValue gold;
+                    FungibleAssetValue crystal;
+                    FungibleAssetValue garage;
+                    try
                     {
-                        FungibleAssetValue gold;
-                        FungibleAssetValue crystal;
-                        FungibleAssetValue garage;
-                        try
-                        {
-                            var ncg = States.Instance.GoldBalanceState.Gold.Currency;
-                            var favArr = await Task.WhenAll(
-                                agent.GetBalanceAsync(agentState.address, ncg),
-                                agent.GetBalanceAsync(agentState.address, Currencies.Crystal),
-                                agent.GetBalanceAsync(agentState.address, Currencies.Garage));
-                            gold = favArr[0];
-                            crystal = favArr[1];
-                            garage = favArr[2];
-                        }
-                        catch (Exception e)
-                        {
-                            return (true, e);
-                        }
+                        var ncg = States.Instance.GoldBalanceState.Gold.Currency;
+                        var favArr = await Task.WhenAll(
+                            agent.GetBalanceAsync(agentState.address, ncg),
+                            agent.GetBalanceAsync(agentState.address, Currencies.Crystal),
+                            agent.GetBalanceAsync(agentState.address, Currencies.Garage));
+                        gold = favArr[0];
+                        crystal = favArr[1];
+                        garage = favArr[2];
+                    }
+                    catch (Exception e)
+                    {
+                        return e;
+                    }
 
-                        AgentStateSubject.OnNextGold(gold);
-                        AgentStateSubject.OnNextCrystal(crystal);
-                        AgentStateSubject.OnNextGarage(garage);
-                        _balanceUpdateRequired = false;
-                        return (false, null);
-                    });
-                if (hasException && exception is not OperationCanceledException)
+                    AgentStateSubject.OnNextGold(gold);
+                    AgentStateSubject.OnNextCrystal(crystal);
+                    AgentStateSubject.OnNextGarage(garage);
+
+                    _balanceUpdateRequired = false;
+                    return null;
+                });
+                if (exception is not null && exception is not OperationCanceledException)
                 {
                     NcDebug.LogException(exception);
                 }
@@ -110,37 +109,36 @@ namespace Nekoyume.Blockchain
             var currentAvatarState = States.Instance.CurrentAvatarState;
             if (currentAvatarState != null && _avatarUpdateRequired)
             {
-                var (hasException, exception) =
-                    await UniTask.Run<(bool hasException, Exception exception)>(async () =>
+                var exception = await UniTask.Run(async () =>
+                {
+                    AvatarState avatarState;
+                    try
                     {
-                        AvatarState avatarState;
-                        try
-                        {
-                            avatarState = (await agent.GetAvatarStatesAsync(
-                                new[] { currentAvatarState.address }))[currentAvatarState.address];
-                        }
-                        catch (Exception e)
-                        {
-                            return (true, e);
-                        }
+                        avatarState = (await agent.GetAvatarStatesAsync(
+                            new[] { currentAvatarState.address }))[currentAvatarState.address];
+                    }
+                    catch (Exception e)
+                    {
+                        return e;
+                    }
 
-                        if (avatarState is null)
-                        {
-                            return (true, new StateNullException(
-                                $"Given address {currentAvatarState.address} is empty."));
-                        }
+                    if (avatarState is null)
+                    {
+                        return new StateNullException(
+                            $"Given address {currentAvatarState.address} is empty.");
+                    }
 
-                        var states = await Task.WhenAll(
-                            agent.GetStateAsync(Addresses.ActionPoint, avatarState.address),
-                            agent.GetStateAsync(Addresses.DailyReward, avatarState.address));
-                        var ap = states[0] is Integer apValue ? (long)apValue : avatarState.actionPoint;
-                        ReactiveAvatarState.UpdateActionPoint(ap);
-                        var dri = states[1] is Integer driValue ? (long)driValue : avatarState.dailyRewardReceivedIndex;
-                        ReactiveAvatarState.UpdateDailyRewardReceivedIndex(dri);
-                        _avatarUpdateRequired = false;
-                        return (false, null);
-                    });
-                if (hasException && !(exception is OperationCanceledException))
+                    var states = await Task.WhenAll(
+                        agent.GetStateAsync(Addresses.ActionPoint, avatarState.address),
+                        agent.GetStateAsync(Addresses.DailyReward, avatarState.address));
+                    var ap = states[0] is Integer apValue ? (long)apValue : avatarState.actionPoint;
+                    ReactiveAvatarState.UpdateActionPoint(ap);
+                    var dri = states[1] is Integer driValue ? (long)driValue : avatarState.dailyRewardReceivedIndex;
+                    ReactiveAvatarState.UpdateDailyRewardReceivedIndex(dri);
+                    _avatarUpdateRequired = false;
+                    return null;
+                });
+                if (exception is not null && exception is not OperationCanceledException)
                 {
                     NcDebug.LogException(exception);
                 }
