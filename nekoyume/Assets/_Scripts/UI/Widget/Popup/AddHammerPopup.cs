d6394b1ff1a4db92b3f0cd34e544bfd9d5c5d9f3
tyrosine1153 <tyrosine1153@gmail.com>
2024-07-08 22:50:34 +0900

    create scripts AddHammerPopup

---
+++ nekoyume/Assets/_Scripts/UI/Widget/Popup/AddHammerPopup.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Nekoyume.UI
+{
+    public class AddHammerPopup : PopupWidget
+    {
+        [SerializeField]
+        private Button closeButton;
+    }
+}
