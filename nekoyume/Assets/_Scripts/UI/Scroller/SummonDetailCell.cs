81c13a7bbff882b1a4811446d210ad44553ad868
tyrosine1153 <tyrosine1153@gmail.com>
2024-07-19 14:51:45 +0900

    Revert "Add prefabs, scripts UI SummonDetailPopup, SummonSkillsPopup"
    This reverts commit bd49158485c092bb931ee3486eb7ec986c72d229.
    
    # Conflicts:
    #	nekoyume/Assets/AddressableAssets/UI/Module/CellView/SummonDetailCell.prefab
    #	nekoyume/Assets/AddressableAssets/UI/Module/CellView/SummonSkillsCell.prefab
    #	nekoyume/Assets/Resources/UI/Prefabs/UI_SummonDetailPopup.prefab
    #	nekoyume/Assets/StreamingAssets/Localization/common.csv
    #	nekoyume/Assets/_Scripts/UI/Scroller/SummonDetailCell.cs
    #	nekoyume/Assets/_Scripts/UI/Scroller/SummonSkillsCell.cs
    #	nekoyume/Assets/_Scripts/UI/Scroller/SummonSkillsScroll.cs
    #	nekoyume/Assets/_Scripts/UI/Widget/Popup/SummonDetailPopup.cs
    #	nekoyume/Assets/_Scripts/UI/Widget/Popup/SummonSkillsPopup.cs

--- nekoyume/Assets/_Scripts/UI/Scroller/SummonDetailCell.cs
+++ nekoyume/Assets/_Scripts/UI/Scroller/SummonDetailCell.cs
@@ -1,43 +1,64 @@
 using Nekoyume.Helper;
 using Nekoyume.L10n;
+using System;
+using System.Collections.Generic;
+using Nekoyume.Game.Controller;
 using Nekoyume.TableData;
+using Nekoyume.UI.Module;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Nekoyume.UI.Scroller
 {
+    using UniRx;
+
     public class SummonDetailCell : RectCell<SummonDetailCell.Model, SummonDetailScroll.ContextModel>
     {
         public class Model
         {
             public EquipmentItemSheet.Row EquipmentRow;
+            public List<EquipmentItemSubRecipeSheetV2.OptionInfo> Options;
             public string RuneTicker;
             public float Ratio;
         }
 
-        [SerializeField] private Image iconImage;
+        [SerializeField] private Button button;
+        [SerializeField] private SummonItemView view;
         [SerializeField] private TextMeshProUGUI nameText;
+        [SerializeField] private TextMeshProUGUI infoText;
         [SerializeField] private TextMeshProUGUI percentText;
+        [SerializeField] private GameObject selected;
+
+        private readonly List<IDisposable> _disposables = new();
 
         public override void UpdateContent(Model itemData)
         {
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(() =>
+            {
+                AudioController.PlayClick();
+                Context.OnClick.OnNext(itemData);
+            });
+
             if (itemData.EquipmentRow is not null)
             {
-                iconImage.overrideSprite = SpriteHelper.GetItemIcon(itemData.EquipmentRow.Id);
+                view.SetData(itemData.EquipmentRow);
                 nameText.text = itemData.EquipmentRow.GetLocalizedName(true, false);
+                infoText.text = itemData.EquipmentRow.GetLocalizedInformation();
             }
 
-            if (!string.IsNullOrEmpty(itemData.RuneTicker) &&
-                RuneFrontHelper.TryGetRuneData(itemData.RuneTicker, out var runeData) &&
-                Game.Game.instance.TableSheets.RuneListSheet.TryGetValue(runeData.id, out var row))
+            if (!string.IsNullOrEmpty(itemData.RuneTicker))
             {
-                nameText.text = $"<color=#{LocalizationExtensions.GetColorHexByGrade(row.Grade)}>" +
-                    $"{L10nManager.Localize($"RUNE_NAME_{row.Id}")}</color>";
-                iconImage.overrideSprite = runeData.icon;
+                nameText.text = LocalizationExtensions.GetLocalizedFavName(itemData.RuneTicker);
+                infoText.text = LocalizationExtensions.GetLocalizedInformation(itemData.RuneTicker);
             }
 
             percentText.text = itemData.Ratio.ToString("0.####%");
+
+            _disposables.DisposeAllAndClear();
+            Context.OnClick.Subscribe(row =>
+                selected.SetActive(row.Equals(itemData))).AddTo(_disposables);
         }
     }
 }
--- nekoyume/Assets/_Scripts/UI/Widget/Popup/SummonDetailPopup.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/Popup/SummonDetailPopup.cs
@@ -1,26 +1,47 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using Nekoyume.Helper;
+using Nekoyume.Model.EnumType;
+using Nekoyume.Model.Item;
 using Nekoyume.TableData;
 using Nekoyume.TableData.Summon;
+using Nekoyume.UI.Module;
 using Nekoyume.UI.Scroller;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Nekoyume.UI
 {
+    using UniRx;
+
     public class SummonDetailPopup : PopupWidget
     {
-        [SerializeField]
-        private Button closeButton;
+        [SerializeField] private Button closeButton;
+        [SerializeField] private TextMeshProUGUI titleText;
 
-        [SerializeField]
-        private SummonDetailScroll scroll;
+        [SerializeField] private TextMeshProUGUI[] mainStatTexts;
+        [SerializeField] private RecipeOptionView recipeOptionView;
+
+        [SerializeField] private SummonDetailScroll scroll;
+
+        private readonly List<IDisposable> _disposables = new();
+
+        private const string StatTextFormat = "{0} {1}";
 
         protected override void Awake()
         {
             base.Awake();
 
-            closeButton.onClick.AddListener(() => Close());
-            CloseWidget = closeButton.onClick.Invoke;
+            closeButton.onClick.AddListener(() =>
+            {
+                Close();
+            });
+            CloseWidget = () =>
+            {
+                Close(true);
+            };
         }
 
         public void Show(SummonSheet.Row summonRow)
@@ -29,6 +50,7 @@ namespace Nekoyume.UI
             var equipmentItemSheet = tableSheets.EquipmentItemSheet;
             var equipmentItemRecipeSheet = tableSheets.EquipmentItemRecipeSheet;
             var runeSheet = tableSheets.RuneSheet;
+            var equipmentItemSubRecipeSheet = tableSheets.EquipmentItemSubRecipeSheetV2;
 
             float ratioSum = summonRow.Recipes.Sum(pair => pair.Item2);
             var models = summonRow.Recipes.Where(pair => pair.Item1 > 0).Select(pair =>
@@ -36,9 +58,11 @@ namespace Nekoyume.UI
                 var (recipeId, ratio) = pair;
 
                 EquipmentItemSheet.Row equipmentRow = null;
+                List<EquipmentItemSubRecipeSheetV2.OptionInfo> optionInfos = null;
                 if (equipmentItemRecipeSheet.TryGetValue(recipeId, out var recipeRow))
                 {
                     equipmentRow = equipmentItemSheet[recipeRow.ResultEquipmentId];
+                    optionInfos = equipmentItemSubRecipeSheet[recipeRow.SubRecipeIds[0]].Options;
                 }
 
                 string runeTicker = null;
@@ -51,13 +75,54 @@ namespace Nekoyume.UI
                 {
                     EquipmentRow = equipmentRow,
                     RuneTicker = runeTicker,
+                    Options = optionInfos,
                     Ratio = ratio / ratioSum
                 };
             }).OrderBy(model => model.Ratio);
 
+            _disposables.DisposeAllAndClear();
+            scroll.OnClick.Subscribe(PreviewDetail).AddTo(_disposables);
             scroll.UpdateData(models, true);
 
+            titleText.text = summonRow.GetLocalizedName();
             base.Show();
+        }
+
+        private void PreviewDetail(SummonDetailCell.Model model)
+        {
+            // CharacterView
+            SetCharacter(model.EquipmentRow);
+
+            // MainStatText
+            var mainStatText = mainStatTexts[0];
+            var stat = model.EquipmentRow.GetUniqueStat();
+            var statValueText = stat.StatType.ValueToString((int)stat.TotalValue);
+            mainStatText.text = string.Format(StatTextFormat, stat.StatType, statValueText);
+            mainStatText.gameObject.SetActive(true);
+
+            // OptionView
+            recipeOptionView.SetOptions(model.Options, false);
+        }
+
+        private static void SetCharacter(EquipmentItemSheet.Row equipmentRow)
+        {
+            var game = Game.Game.instance;
+
+            var maxLevel = game.TableSheets.EnhancementCostSheetV3.Values
+                .Where(row =>
+                    row.ItemSubType == equipmentRow.ItemSubType &&
+                    row.Grade == equipmentRow.Grade)
+                .Max(row => row.Level);
+            var resultItem = (Equipment)ItemFactory.CreateItemUsable(
+                    equipmentRow, Guid.NewGuid(), 0L, maxLevel);
+
+            var (equipments, costumes) = game.States.GetEquippedItems(BattleType.Adventure);
+            var sameType = equipments.FirstOrDefault(e => e.ItemSubType == equipmentRow.ItemSubType);
+            equipments.Remove(sameType);
+            equipments.Add(resultItem);
+
+            var avatarState = game.States.CurrentAvatarState;
+            game.Lobby.FriendCharacter.Set(avatarState, costumes, equipments);
         }
     }
 }
