60c1f1654d5ffc40c52e31f90b40059a475b83e6
sonohoshi <dev.sonohoshi@gmail.com>
2024-07-19 17:20:20 +0900

    remove setting cancelCallback

--- nekoyume/Assets/_Scripts/Game/Game.cs
+++ nekoyume/Assets/_Scripts/Game/Game.cs
@@ -1937,7 +1937,7 @@ namespace Nekoyume.Game
                     IconAndButtonSystem.SystemType.Information);
             }
 
-            popup.ConfirmCallback = popup.CancelCallback = OpenUpdateURL;
+            popup.ConfirmCallback = OpenUpdateURL;
             return true;
         }
 
