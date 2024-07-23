8d055e841180b4fb8df26c8a66c8bdba606073aa
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2024-07-23 19:13:16 +0900

    apply coding rule

--- nekoyume/Assets/Editor/IntroSceneSwitcher.cs
+++ nekoyume/Assets/Editor/IntroSceneSwitcher.cs
@@ -4,52 +4,51 @@ using UnityToolbarExtender;
 
 namespace Nekoyume.Editor
 {
-	[InitializeOnLoad]
-	public class SceneSwitchLeftButton
-	{
-		static class ToolbarStyles
-		{
-			public static readonly GUIStyle commandButtonStyle;
+    [InitializeOnLoad]
+    public class SceneSwitchLeftButton
+    {
+        private static class ToolbarStyles
+        {
+            public static readonly GUIStyle commandButtonStyle;
 
-			static ToolbarStyles()
-			{
-				commandButtonStyle = new GUIStyle("Command")
-				{
-					fontSize = 16,
-					alignment = TextAnchor.MiddleCenter,
-					imagePosition = ImagePosition.ImageAbove,
-					fontStyle = FontStyle.Bold
-				};
-			}
-		}
-		
-		static SceneSwitchLeftButton()
-		{
-			ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
-		}
+            static ToolbarStyles()
+            {
+                commandButtonStyle = new GUIStyle("Command")
+                {
+                    fontSize = 16,
+                    alignment = TextAnchor.MiddleCenter,
+                    imagePosition = ImagePosition.ImageAbove,
+                    fontStyle = FontStyle.Bold
+                };
+            }
+        }
 
-		static void OnToolbarGUI()
-		{
-			GUILayout.FlexibleSpace();
-			const string toolTip = "Start IntroScene";
-			const string sceneName = "IntroScene";
-			
-			var sprite = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Resources/UI/Textures/icon_executable.png");
-			if (sprite == null)
-			{
-				if(GUILayout.Button(new GUIContent("Intro", toolTip), ToolbarStyles.commandButtonStyle))
-				{
-					SceneHelper.StartScene(sceneName);
-				}
-			}
-			else
-			{
-				if(GUILayout.Button(new GUIContent(sprite, toolTip), ToolbarStyles.commandButtonStyle))
-				{
-					SceneHelper.StartScene(sceneName);
-				}
-			}
-		}
-	}
+        static SceneSwitchLeftButton()
+        {
+            ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+        }
+
+        private static void OnToolbarGUI()
+        {
+            GUILayout.FlexibleSpace();
+            const string toolTip = "Start IntroScene";
+            const string sceneName = "IntroScene";
+
+            var sprite = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Resources/UI/Textures/icon_executable.png");
+            if (sprite == null)
+            {
+                if (GUILayout.Button(new GUIContent("Intro", toolTip), ToolbarStyles.commandButtonStyle))
+                {
+                    SceneHelper.StartScene(sceneName);
+                }
+            }
+            else
+            {
+                if (GUILayout.Button(new GUIContent(sprite, toolTip), ToolbarStyles.commandButtonStyle))
+                {
+                    SceneHelper.StartScene(sceneName);
+                }
+            }
+        }
+    }
 }
-
--- nekoyume/Assets/Editor/SceneHelper.cs
+++ nekoyume/Assets/Editor/SceneHelper.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 namespace Nekoyume.Editor
 {
-    static class SceneHelper
+    internal static class SceneHelper
     {
-        static string sceneToOpen;
+        private static string _sceneToOpen;
 
         public static void StartScene(string sceneName)
         {
-            if(EditorApplication.isPlaying)
+            if (EditorApplication.isPlaying)
             {
                 EditorApplication.isPlaying = false;
             }
 
-            sceneToOpen = sceneName;
+            _sceneToOpen = sceneName;
             EditorApplication.update += OnUpdate;
         }
 
-        static void OnUpdate()
+        private static void OnUpdate()
         {
-            if (sceneToOpen == null ||
+            if (_sceneToOpen == null ||
                 EditorApplication.isPlaying || EditorApplication.isPaused ||
                 EditorApplication.isCompiling || EditorApplication.isPlayingOrWillChangePlaymode)
             {
@@ -30,23 +30,24 @@ namespace Nekoyume.Editor
 
             EditorApplication.update -= OnUpdate;
 
-            if(EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
             {
                 // need to get scene via search because the path to the scene
                 // file contains the package version so it'll change over time
-                string[] guids = AssetDatabase.FindAssets("t:scene " + sceneToOpen, null);
+                var guids = AssetDatabase.FindAssets("t:scene " + _sceneToOpen, null);
                 if (guids.Length == 0)
                 {
                     Debug.LogWarning("Couldn't find scene file");
                 }
                 else
                 {
-                    string scenePath = AssetDatabase.GUIDToAssetPath(guids[0]);
+                    var scenePath = AssetDatabase.GUIDToAssetPath(guids[0]);
                     EditorSceneManager.OpenScene(scenePath);
                     EditorApplication.isPlaying = true;
                 }
             }
-            sceneToOpen = null;
+
+            _sceneToOpen = null;
         }
     }
 }
