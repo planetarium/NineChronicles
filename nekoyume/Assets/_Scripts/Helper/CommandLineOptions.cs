9b22117d88bfb7f1ad01b3f2c88da440eb66efc1
Hyun Seungmin <boscohyun@gmail.com>
2024-08-27 20:07:55 +0900

    add MimirUrl to CommandLineOptions

--- nekoyume/Assets/_Scripts/Helper/CommandLineOptions.cs
+++ nekoyume/Assets/_Scripts/Helper/CommandLineOptions.cs
@@ -122,6 +122,8 @@ namespace Nekoyume.Helper
 
         private bool _mixpanelDebugWarning;
 
+        private string _mimirUrl;
+
         public bool Empty { get; private set; } = true;
 
         public string genesisBlockPath;
@@ -621,6 +623,17 @@ namespace Nekoyume.Helper
             set
             {
                 _enableGuestLogin = value;
+                Empty = false;
+            }
+        }
+        
+        [Option("mimir-url", Required = false, HelpText = "mimir url")]
+        public string MimirUrl
+        {
+            get => _mimirUrl;
+            set
+            {
+                _mimirUrl = value;
                 Empty = false;
             }
         }
