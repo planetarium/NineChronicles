4dfade6c10d573f90da1c3937b0a2408ebcbdb5b
sonohoshi <dev.sonohoshi@gmail.com>
2024-08-27 17:36:14 +0900

    add CustomCraftStatScroll

---
+++ nekoyume/Assets/_Scripts/UI/Scroller/CustomCraftStatScroll.cs
@@ -0,0 +1,10 @@
+﻿namespace Nekoyume.UI.Scroller
+{
+    public class CustomCraftStatScroll : RectScroll<CustomCraftStatCell.Model, CustomCraftStatScroll.ContextModel>
+    {
+        public class ContextModel : RectScrollDefaultContext
+        {
+            public CustomCraftStatCell.Model CurrentModel;
+        }
+    }
+}
