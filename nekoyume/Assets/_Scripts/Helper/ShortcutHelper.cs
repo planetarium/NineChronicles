9c0450019719ec5818cde833d22f56d05a7279c9
sonohoshi <dev.sonohoshi@gmail.com>
2024-08-29 14:29:04 +0900

    rename

--- nekoyume/Assets/_Scripts/Helper/ShortcutHelper.cs
+++ nekoyume/Assets/_Scripts/Helper/ShortcutHelper.cs
@@ -106,11 +106,11 @@ namespace Nekoyume.Helper
                         }
 
                         break;
-                    case ItemSubType.Drawing:
+                    case ItemSubType.Scroll:
                         acquisitionPlaceList.Add(GetAcquisitionPlace(caller, PlaceType.AdventureBoss));
                         acquisitionPlaceList.Add(GetAcquisitionPlace(caller, PlaceType.PCShop));
                         break;
-                    case ItemSubType.DrawingTool:
+                    case ItemSubType.Circle:
                         acquisitionPlaceList.Add(GetAcquisitionPlace(caller, PlaceType.Staking));
                         acquisitionPlaceList.Add(GetAcquisitionPlace(caller, PlaceType.PCShop));
                         break;
