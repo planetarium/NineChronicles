2564585283ff385f6ff4a46d66cabefdac628562
tyrosine1153 <tyrosine1153@gmail.com>
2024-09-12 19:21:45 +0900

    update WorldbossRewards and reward UI

--- nekoyume/Assets/_Scripts/UI/Module/WorldBoss/WorldBossGradeRewardItem.cs
+++ nekoyume/Assets/_Scripts/UI/Module/WorldBoss/WorldBossGradeRewardItem.cs
@@ -23,16 +23,20 @@ namespace Nekoyume.UI.Module.WorldBoss
         private TextMeshProUGUI crystalCountText;
 
         [SerializeField]
+        private TextMeshProUGUI circleCountText;
+
+        [SerializeField]
         private GameObject received;
 
         [SerializeField]
         private GameObject active;
 
-        public void Set(int score, int runeCount, int crystalCount)
+        public void Set(int score, int runeCount, int crystalCount, int circleCount)
         {
             scoreText.text = $"{score:#,0}";
             runeCountText.text = $"{runeCount:#,0}";
             crystalCountText.text = $"{crystalCount:#,0}";
+            circleCountText.text = $"{circleCount:#,0}";
         }
 
         public void SetStatus(Status status)
