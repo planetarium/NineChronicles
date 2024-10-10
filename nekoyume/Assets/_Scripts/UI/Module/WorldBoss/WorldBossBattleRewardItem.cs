024be062ddc799a1e5a97ccba54c3be6bcdaa9b8
tyrosine1153 <tyrosine1153@gmail.com>
2024-10-10 15:46:30 +0900

    refactor WorldBossBattleRewardItem

--- nekoyume/Assets/_Scripts/UI/Module/WorldBoss/WorldBossBattleRewardItem.cs
+++ nekoyume/Assets/_Scripts/UI/Module/WorldBoss/WorldBossBattleRewardItem.cs
@@ -1,24 +1,34 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Nekoyume.Helper;
 using Nekoyume.TableData;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Nekoyume.UI.Module.WorldBoss
 {
     public class WorldBossBattleRewardItem : MonoBehaviour
     {
-        [SerializeField]
-        private TextMeshProUGUI rune;
+        [Serializable]
+        private class RewardItem
+        {
+            public GameObject container;
+            public Image icon;
+            public TextMeshProUGUI text;
+        }
 
         [SerializeField]
-        private TextMeshProUGUI crystal;
+        private RewardItem runeItem;
+
+        [SerializeField]
+        private RewardItem crystalItem;
+
+        [SerializeField]
+        private RewardItem[] materialItems;
 
         [SerializeField]
         private TextMeshProUGUI rankText;
-
-        [SerializeField]
-        private GameObject rankTextContainer;
 
         [SerializeField]
         private GameObject rankingImageContainer;
@@ -35,14 +45,20 @@ namespace Nekoyume.UI.Module.WorldBoss
 
         public void Set(WorldBossBattleRewardSheet.Row row)
         {
-            rune.text = $"{row.RuneMin:#,0}~{row.RuneMax:#,0}";
-            crystal.text = $"{row.Crystal:#,0}";
+            rune.container.gameObject.SetActive(row.RuneMax > 0);
+            rune.text.text = $"{row.RuneMin:#,0}~{row.RuneMax:#,0}";
+
+            crystal.container.gameObject.SetActive(row.Crystal > 0);
+            crystal.text.text = $"{row.Crystal:#,0}";
         }
 
         public void Set(WorldBossKillRewardSheet.Row row)
         {
-            rune.text = $"{row.RuneMin:#,0}~{row.RuneMax:#,0}";
-            crystal.text = $"{row.Crystal:#,0}";
+            rune.container.gameObject.SetActive(row.RuneMax > 0);
+            rune.text.text = $"{row.RuneMin:#,0}~{row.RuneMax:#,0}";
+
+            crystal.container.gameObject.SetActive(row.Crystal > 0);
+            crystal.text.text = $"{row.Crystal:#,0}";
         }
 
         public void Set(WorldBossRankingRewardSheet.Row row, int myRank, int userCount)
@@ -52,43 +68,44 @@ namespace Nekoyume.UI.Module.WorldBoss
                 Destroy(_rankObject);
             }
 
-            rankTextContainer.gameObject.SetActive(false);
+            rankText.gameObject.SetActive(false);
             rankingImageContainer.gameObject.SetActive(false);
-            switch (row.RankingMin)
+            if (row.RankingMin != 0 && row.RankingMax != 0)
             {
-                case 1:
-                case 2:
-                case 3:
+                if (row.RankingMin == row.RankingMax)
+                {
                     rankingImageContainer.gameObject.SetActive(true);
-                    var rankPrefab = WorldBossFrontHelper.GetRankPrefab(row.RankingMin);
-                    _rankObject = Instantiate(rankPrefab, rankingImageContainer.transform);
+                    _rankObject = Instantiate(
+                        WorldBossFrontHelper.GetRankPrefab(row.RankingMin),
+                        rankingImageContainer.transform);
                     selected.SetActive(row.RankingMin == myRank);
-                    break;
-                default:
-                    rankTextContainer.gameObject.SetActive(true);
-                    if (row.RankingMin == 0)
-                    {
-                        rankText.text = row.RateMin > 1
-                            ? $"{row.RateMin}%~{row.RateMax}%"
-                            : $"{row.RateMax}%";
+                }
+                else
+                {
+                    rankText.gameObject.SetActive(true);
+                    rankText.text = $"{row.RankingMin}~{row.RankingMax}";
+                    var value = row.RankingMin <= myRank && myRank <= row.RankingMax;
+                    selected.SetActive(value);
+                }
+            }
+            else
+            {
+                rankText.gameObject.SetActive(true);
+                rankText.text = row.RateMin > 1
+                    ? $"{row.RateMin}%~{row.RateMax}%"
+                    : $"{row.RateMax}%";
 
-                        var rate = userCount > 0 ? (int)((float)myRank / userCount * 100) : 0;
-                        var value = myRank > 100 && row.RateMin <= rate && rate <= row.RateMax;
-                        selected.SetActive(value);
-                    }
-                    else
-                    {
-                        rankText.text = $"{row.RankingMin}~{row.RankingMax}";
-                        var value = row.RankingMin <= myRank && myRank <= row.RankingMax;
-                        selected.SetActive(value);
-                    }
-
-                    break;
+                var rate = userCount > 0 ? (int)((float)myRank / userCount * 100) : 0;
+                var value = myRank > 100 && row.RateMin <= rate && rate <= row.RateMax;
+                selected.SetActive(value);
             }
 
             var runeSum = row.Runes.Sum(x => x.RuneQty);
-            rune.text = $"{runeSum:#,0}";
-            crystal.text = $"{row.Crystal:#,0}";
+            rune.container.gameObject.SetActive(runeSum > 0);
+            rune.text.text = $"{runeSum:#,0}";
+
+            crystal.container.gameObject.SetActive(row.Crystal > 0);
+            crystal.text.text = $"{row.Crystal:#,0}";
         }
     }
 }
