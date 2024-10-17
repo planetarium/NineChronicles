db0bed07e648b3362f411b43130988f88d7835b0
jonny <jonny@planetariumhq.com>
2024-10-17 18:37:50 +0900

    add iap mileage

---
+++ nekoyume/Assets/_Scripts/UI/Module/Currency/IAPMileage.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+namespace Nekoyume.UI.Module
+{
+    public class IAPMileage : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI amountText;
+
+        [SerializeField]
+        private GameObject loadingObject;
+
+        private void OnEnable()
+        {
+        }
+
+        private void OnDisable()
+        {
+        }
+    }
+}
