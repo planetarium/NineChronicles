8fc5b95ec88ba34f70100cfe6b96166086285464
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2024-10-28 17:03:02 +0900

    add Thor PlanetId

--- nekoyume/Assets/_Scripts/Multiplanetary/PlanetId.Define.cs
+++ nekoyume/Assets/_Scripts/Multiplanetary/PlanetId.Define.cs
@@ -5,9 +5,11 @@ namespace Nekoyume.Multiplanetary
         // Main
         public static readonly PlanetId Odin = new("0x000000000000");
         public static readonly PlanetId Heimdall = new("0x000000000001");
+        public static readonly PlanetId Thor = new("0x000000000003");
 
         // Internal
         public static readonly PlanetId OdinInternal = new("0x100000000000");
         public static readonly PlanetId HeimdallInternal = new("0x100000000001");
+        public static readonly PlanetId ThorInternal = new("0x100000000003");
     }
 }
