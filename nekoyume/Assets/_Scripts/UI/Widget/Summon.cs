f73ab4a96419dc5999798a74fc0931ab9a7e0a7c
sonohoshi <dev.sonohoshi@gmail.com>
2024-11-08 18:30:17 +0900

    rollback Summon tutorial

--- nekoyume/Assets/_Scripts/UI/Widget/Summon.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/Summon.cs
@@ -359,6 +359,43 @@ namespace Nekoyume.UI
                 L10nManager.Localize("UI_COST_BLOCK", 1),
                 false);
         }
+
+        // Do not use with Aura summon tutorial. this logic is fake.
+        public void SetCostUIForTutorial()
+        {
+            var summonObject = summonObjects.FirstOrDefault(info => info.summonResult == SummonResult.Aura);
+            if (summonObject is null)
+            {
+                NcDebug.LogError("SummonObject for tutorial not found.");
+                return;
+            }
+
+            summonObject.tabToggle.isOn = true;
+            OnClickSummonTabToggle(summonObject);
+
+            costButtons.FirstOrDefault()?.SetFakeUI(CostType.SilverDust, 0);
+        }
+
+        // Invoke from TutorialController.PlayAction() by TutorialTargetType
+        public void TutorialActionClickNormal1SummonButton()
+        {
+            var summonRow = Game.Game.instance.TableSheets.EquipmentSummonSheet.First;
+            var resultEquipment =
+                States.Instance.CurrentAvatarState.inventory.Equipments.FirstOrDefault(e =>
+                    e is Aura);
+            var button = costButtons.FirstOrDefault();
+            if (resultEquipment is null)
+            {
+                button.OnClickSubject.OnNext(button.CurrentState.Value);
+                return;
+            }
+
+            Find<SummonResultPopup>().Show(summonRow, 1, new List<Equipment> { resultEquipment },
+                () => { Game.Game.instance.Stage.TutorialController.Play(50005); });
+
+            // UI Reset for SetCostUIForTutorial() invoking
+            button.UpdateObjects();
+        }
 #endregion
     }
 }
