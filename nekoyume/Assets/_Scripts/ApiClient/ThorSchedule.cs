1fd78ed1119411d89c4f29812c5c90ee77acf97d
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2024-11-12 16:49:08 +0900

    set default to others

--- nekoyume/Assets/_Scripts/ApiClient/ThorSchedule.cs
+++ nekoyume/Assets/_Scripts/ApiClient/ThorSchedule.cs
@@ -7,24 +7,24 @@ namespace Nekoyume.ApiClient
     public class ThorSchedule
     {
         public string StartDateUtcString { get; set; }
-        
+
         public string EndDateUtcString { get; set; }
-        
+
         public string InformationUrl { get; set; }
-        
+
         public DateTimeOffset StartDateUtc => DateTimeOffset.Parse(StartDateUtcString);
-        
+
         public DateTimeOffset EndDateUtc => DateTimeOffset.Parse(EndDateUtcString);
-        
+
         public bool IsOpened => StartDateUtc <= DateTimeOffset.UtcNow && DateTimeOffset.UtcNow <= EndDateUtc;
-        
+
         public TimeSpan DiffFromEndTimeSpan => EndDateUtc - DateTimeOffset.UtcNow;
-        
+
         public double DiffFromEndSeconds => DiffFromEndTimeSpan.TotalSeconds;
 
         public long DiffFromEndBlockIndex => (long)(DiffFromEndSeconds / Util.BlockInterval);
     }
-    
+
     [Serializable]
     public class ThorSchedules
     {
