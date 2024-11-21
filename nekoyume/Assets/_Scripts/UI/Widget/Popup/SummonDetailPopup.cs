f106d9c19b27c795f6cc5229cada11a303862839
tyrosine1153 <tyrosine1153@gmail.com>
2024-11-21 16:38:20 +0900

    add cp text in SummonDetailPopup

--- nekoyume/Assets/_Scripts/UI/Widget/Popup/SummonDetailPopup.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/Popup/SummonDetailPopup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using Nekoyume.Battle;
 using Nekoyume.Helper;
 using Nekoyume.Model.EnumType;
 using Nekoyume.Model.Item;
@@ -11,6 +12,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Nekoyume.Game;
+using Nekoyume.Model.Stat;
 
 namespace Nekoyume.UI
 {
@@ -18,6 +20,7 @@ namespace Nekoyume.UI
     {
         [SerializeField] private Button closeButton;
 
+        [SerializeField] private TextMeshProUGUI combatPointText;
         [SerializeField] private TextMeshProUGUI[] mainStatTexts;
         [SerializeField] private RecipeOptionView recipeOptionView;
         [SerializeField] private TextMeshProUGUI titleText;
@@ -67,20 +70,15 @@ namespace Nekoyume.UI
 
                 // OptionView
                 recipeOptionView.SetOptions(model.EquipmentOptions, false, false);
-                titleText.SetText(model.EquipmentRow.GetLocalizedName(useElementalIcon: false));
-                foreach (var iconImage in iconImages)
-                {
-                    iconImage.sprite = SpriteHelper.GetItemIcon(model.EquipmentRow.Id);
-                }
 
+                var optionSheet = TableSheets.Instance.EquipmentItemOptionSheet;
+                var skillSheet = TableSheets.Instance.SkillSheet;
+                var optionRows = model.EquipmentOptions.Select(info => optionSheet[info.Id]).ToList();
+
+                // SkillTooltip
                 SkillSheet.Row skillOption = null;
-                var skillOptionRow = model.EquipmentOptions
-                    .Select(info =>
-                        TableSheets.Instance.EquipmentItemOptionSheet[info.Id])
-                    .FirstOrDefault(optionRow =>
-                        TableSheets.Instance.SkillSheet.TryGetValue(optionRow.SkillId,
-                            out skillOption));
-
+                var skillOptionRow = optionRows.FirstOrDefault(optionRow =>
+                    skillSheet.TryGetValue(optionRow.SkillId, out skillOption));
                 if (skillOption != null)
                 {
                     skillTooltip.Show(skillOption, skillOptionRow);
@@ -88,6 +86,23 @@ namespace Nekoyume.UI
                 else
                 {
                     skillTooltip.gameObject.SetActive(false);
+                }
+
+                var cp = CPHelper.GetStatCP(stat.StatType, stat.BaseValue);
+                foreach (var optionRow in optionRows.Where(option => option.StatType != StatType.NONE))
+                {
+                    cp += CPHelper.GetStatCP(optionRow.StatType, optionRow.StatMax);
+                }
+
+                var skillCount = optionRows.Count(option => option.StatType == StatType.NONE);
+                cp += CPHelper.GetSkillsMultiplier(skillCount);
+
+                combatPointText.text = $"CP {(int)cp}";
+
+                titleText.SetText(model.EquipmentRow.GetLocalizedName(useElementalIcon: false));
+                foreach (var iconImage in iconImages)
+                {
+                    iconImage.sprite = SpriteHelper.GetItemIcon(model.EquipmentRow.Id);
                 }
             }
 
