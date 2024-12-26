42a052aa6dfad3dccf7b88383b5357a33d09cd5e
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2024-12-26 10:50:22 +0900

    add graphTypes

---
+++ nekoyume/Assets/_Scripts/ApiClient/GraphQL/GraphTypes/NodeStatusType.cs
@@ -0,0 +1,39 @@
+#nullable enable
+
+using System.Text.Json.Serialization;
+
+namespace Nekoyume.GraphQL.GraphTypes
+{
+    // TODO: 필요한 파라메터 추가 구현 필요
+    public class NodeStatusType
+    {
+        public class InnerType
+        {
+            [JsonPropertyName("nodeStatus")]
+            public TipType? Tip;
+
+            public override string ToString()
+            {
+                return $"Tip: {Tip}";
+            }
+        }
+
+        [JsonPropertyName("nodeStatus")]
+        public InnerType? NodeStatus;
+
+        [JsonPropertyName("isMining")]
+        public bool? IsMining;
+
+        [JsonPropertyName("informationalVersion")]
+        public string? InformationalVersion;
+
+        [JsonPropertyName("stagedTxIdsCount")]
+        public int? StagedTxIdsCount;
+
+        public override string ToString()
+        {
+            var nodeStatus = NodeStatus is null ? "null" : NodeStatus.ToString();
+            return $"NodeStatus: {{ {nodeStatus} }}, IsMining: {IsMining}, InformationalVersion: {InformationalVersion}, StagedTxIdsCount: {StagedTxIdsCount}";
+        }
+    }
+}
