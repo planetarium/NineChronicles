42f871ca328e4c1d876278a55e19e6aa8ebc43cc
Yang Chun Ung <qooraven@gmail.com>
2025-01-08 21:19:04 +0900

    Enable error popup when stage transaction failed

--- nekoyume/Assets/_Scripts/Blockchain/RPCAgent.cs
+++ nekoyume/Assets/_Scripts/Blockchain/RPCAgent.cs
@@ -98,6 +98,8 @@ namespace Nekoyume.Blockchain
 
         public readonly Subject<(RPCAgent, int retryCount)> OnRetryAttempt = new();
 
+        public readonly Subject<bool> OnTxStageEnded = new ();
+
         public BlockHash BlockTipHash { get; private set; }
 
         public HashDigest<SHA256> BlockTipStateRootHash { get; private set; }
@@ -719,6 +721,19 @@ namespace Nekoyume.Blockchain
                 .ObserveOnMainThread()
                 .Subscribe()
                 .AddTo(_disposables);
+            OnTxStageEnded
+                .ObserveOnMainThread()
+                .Subscribe(result =>
+                {
+                    if (!result)
+                    {
+                        var popup = Widget.Find<IconAndButtonSystem>();
+                        popup.Show(L10nManager.Localize("UI_ERROR"),
+                            "banned", L10nManager.Localize("UI_OK"), false);
+                        popup.SetConfirmCallbackToExit();
+                    }
+                })
+                .AddTo(_disposables);
             Game.Event.OnUpdateAddresses.AddListener(UpdateSubscribeAddresses);
 
             cancellationTokenSource = new CancellationTokenSource();
@@ -886,7 +901,8 @@ namespace Nekoyume.Blockchain
                 $" Actions=[{actionsText}]");
 
             _onMakeTransactionSubject.OnNext((tx, actions));
-            await _service.PutTransaction(tx.Serialize());
+            var result = await _service.PutTransaction(tx.Serialize());
+            OnTxStageEnded.OnNext(result);
             foreach (var action in actions)
             {
                 if (action is GameAction gameAction)
