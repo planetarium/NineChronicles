b0c1b88b2b08e480825a8fcf313ef5aca949fbf4
jonny <jonny@planetariumhq.com>
2025-01-31 18:32:41 +0900

    fixing clanranking scrollers

--- nekoyume/Assets/_Scripts/UI/Widget/Popup/ClanRankPopup.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/Popup/ClanRankPopup.cs
@@ -2,21 +2,16 @@ using Cysharp.Threading.Tasks;
 using Nekoyume.L10n;
 using Nekoyume.State;
 using Nekoyume.UI.Model;
-using Nekoyume.UI.Module;
 using Nekoyume.UI.Scroller;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Nekoyume.Game.Controller;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using Toggle = Nekoyume.UI.Module.Toggle;
 
 namespace Nekoyume.UI
 {
-    using Nekoyume.Model.Item;
     using UniRx;
 
     public class ClanRankPopup : PopupWidget
@@ -25,10 +20,7 @@ namespace Nekoyume.UI
         private Button closeButton = null;
 
         [SerializeField]
-        private RankScroll rankScroll = null;
-
-        [SerializeField]
-        private RankCellPanel myInfoCell = null;
+        private ClanRankScroll clanRankScroll = null;
 
         [SerializeField]
         private GameObject preloadingObject = null;
@@ -62,30 +54,7 @@ namespace Nekoyume.UI
         public override void Show(bool ignoreShowAnimation = false)
         {
             base.Show(ignoreShowAnimation);
-        }
-
-        private void SetScroll<T>(
-            IReadOnlyDictionary<int, T> myRecordMap,
-            IEnumerable<T> rankingInfos)
-            where T : RankingModel
-        {
-            if (rankingInfos is null)
-            {
-                Find<Alert>().Show("UI_ERROR", "UI_RANKING_CATEGORY_ERROR");
-                rankingInfos = new List<T>();
-            }
-
-            var states = States.Instance;
-            if (myRecordMap.TryGetValue(states.CurrentAvatarKey, out var rankingInfo))
-            {
-                myInfoCell.SetData(rankingInfo);
-            }
-            else
-            {
-                myInfoCell.SetEmpty(states.CurrentAvatarState);
-            }
-
-            rankScroll.Show(rankingInfos, true);
+            //clanRankScroll.Show(rankingInfos, true);
         }
     }
 }
