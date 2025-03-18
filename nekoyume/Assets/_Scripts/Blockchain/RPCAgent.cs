a7afc10aefc03e9717cc5a07f18e5fca790b208b
Yang Chun Ung <qooraven@gmail.com>
2025-03-18 15:50:43 +0900

    Update GetSheets

--- nekoyume/Assets/_Scripts/Blockchain/RPCAgent.cs
+++ nekoyume/Assets/_Scripts/Blockchain/RPCAgent.cs
@@ -714,7 +714,7 @@ namespace Nekoyume.Blockchain
             var result = new Dictionary<Address, IValue>();
             foreach (var kv in cd)
             {
-                result[new Address(kv.Key)] = _codec.Decode(MessagePackSerializer.Deserialize<byte[]>(kv.Value, _lz4Options));
+                result[new Address(kv.Key)] = (Text)Encoding.UTF8.GetString(kv.Value);
             }
 
             sw.Stop();
