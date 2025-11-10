fe045b27b863f2644389e592d7927a183b019dc4
Yang Chun Ung <qooraven@gmail.com>
2025-11-10 21:42:53 +0900

    Introduce MarketExchangePopup

--- nekoyume/Assets/_Scripts/UI/MainCanvas.cs
+++ nekoyume/Assets/_Scripts/UI/MainCanvas.cs
@@ -483,6 +483,7 @@ namespace Nekoyume.UI
             yield return null;
             _secondWidgets.Add(Widget.Create<SummonDetailPopup>());
             yield return null;
+            _secondWidgets.Add(Widget.Create<MarketExchangePopup>());
 
             // tooltip
             _secondWidgets.Add(Widget.Create<EquipmentTooltip>());
---
+++ nekoyume/Assets/_Scripts/UI/Widget/Popup/MarketExchangePopup.cs
@@ -0,0 +1,36 @@
+using Nekoyume.Game.Controller;
+using Nekoyume.UI.Module;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Nekoyume.UI
+{
+    public class MarketExchangePopup : PopupWidget
+    {
+        [SerializeField] private Button marketButton;
+        [SerializeField] private Button exchangeButton;
+        [SerializeField] private Button closeButton;
+
+        protected override void Awake()
+        {
+            marketButton.onClick.AddListener(() =>
+            {
+                AudioController.PlayClick();
+                Close();
+                Helper.Util.OpenURL(Game.Game.instance.CommandLineOptions.WebMarketUrl);
+            });
+
+            exchangeButton.onClick.AddListener(() =>
+            {
+                AudioController.PlayClick();
+                Close();
+                Find<ShopBuy>().Show();
+                Find<HeaderMenuStatic>().UpdateAssets(HeaderMenuStatic.AssetVisibleState.Shop);
+            });
+
+            CloseWidget = () => Close(true);
+            base.Awake();
+        }
+
+    }
+}
